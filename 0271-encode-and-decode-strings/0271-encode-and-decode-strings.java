3344667c92983b06f89d85fff7c792f5991b7baf
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-07 00:58:19 -0800

    Time: 4 ms (95.58%), Space: 44.3 MB (85.21%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -1,11 +1,11 @@
 /*
-.substring is exclusive
+.substring is exclusive // Remember to reset index
 1. Encode, use stringbuilder, construct str in strs in the form 
     (5/hello) -> (str.length(), '/' and str)
 2. Decode, find index of slash from current index, take length 
+    (index)int length, (index)int slash, String str
     user index to find int length, String str// Hint: use Integer.valueOf and substring
 */
-    
 public class Codec {
 
     // Encodes a list of strings to a single string.
@@ -20,8 +20,8 @@ public class Codec {
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List<String> result = new ArrayList<>();
-        
         int index = 0;
+        
         while (index < s.length()) {
             int slash = s.indexOf('/', index);
             int length = Integer.valueOf(s.substring(index, slash));
