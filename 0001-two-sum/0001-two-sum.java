f01811180411af5ecda8b43b3610c6b2c60cebca
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-06 19:12:41 -0800

    Time: 1 ms (99.32%), Space: 43.6 MB (47.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,40 +1,15 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int remain = target - nums[i];
-            if (map.containsKey(remain)) {
-                return new int[]{i, map.get(remain)};
-            }
-            map.put(nums[i], i);
-        }
-        return new int[0];
-    }
-}
-/*
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        int[] copy = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(copy);
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            int sum = copy[left] + copy[right];
-            if (sum == target) {
-                break;
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == copy[left]) result[0] = i;
-        }
-        for (int i = nums.length - 1; i >= 0; i--) {
-            if (nums[i] == copy[right]) result[1] = i;
-        }
-        return result;
-    }
-}
+/**
 */
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); //{number, index};
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target - nums[i]), i};
+            } else {
+                map.putIfAbsent(nums[i], i);        
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
