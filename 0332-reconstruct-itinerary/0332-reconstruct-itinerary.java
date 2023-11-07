832ef8b354bbd0899aa64a441f0de7709d0aae45
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-07 12:51:16 -0800

    Time: 10 ms (10.84%), Space: 43.7 MB (98.57%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.java
@@ -1,31 +1,41 @@
 /**
-Graph, with DFS
-1. Create adjacency list, which needs to be SORTED! -> Map<String, PriorityQueue<String>>
-Return the sorted lexical ordering
+Graph, run DFS
+1. Create adjacency list, which needs to be SORTED! 
+    -> Map<String, PriorityQueue<String>> //minHeap by default, natural ordering
+    The priorityQueue here stores the LEXICOGRAPHICALLY smallest character
+2. We want to print out the itinerary as a string List, use a Stack (LIFO) to help us construct travel path
+3. We are running DFS on the graph, so the order should be reversed, We are given List<List<String>> tickets
+    use LinkedList<String> to store travel path
+4. If we reach a DEAD END, we need to BACKTRACK, 
+    the condition to backtrack is when graph has no element -> graph.isEmpty() OR if (!graph.isEmpty) else
+    When backtracking, add stack.pop to the Head of itinerary
+5. When backtracking decision, declear next City as PEEK()!, when dead-end, add to head of itinerary use POP()!
+Return itinerary
 */
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
-        LinkedList<String> itinerary = new LinkedList<>();
+        List<String> itinerary = new ArrayList<>();
         Map<String, PriorityQueue<String>> graph = new HashMap<>();
         Stack<String> stack = new Stack<>();
-
-        for (List<String> ticket : tickets) {
-            String depart = ticket.get(0);
-            String arrive = ticket.get(1);
-            graph.putIfAbsent(depart, new PriorityQueue<>());
-            graph.get(depart).add(arrive);
-        }
-
         stack.push("JFK");
+        
+        for (List<String> ticket : tickets) {
+            String departCity = ticket.get(0);
+            String arriveCity = ticket.get(1);
+            graph.putIfAbsent(departCity, new PriorityQueue<String>());
+            graph.get(departCity).add(arriveCity);
+        }
+        
         while (!stack.isEmpty()) {
-            String nextArrival = stack.peek();
-
-            if (!graph.getOrDefault(nextArrival, new PriorityQueue<>()).isEmpty()) {
-                stack.push(graph.get(nextArrival).poll());
+            String nextCity = stack.peek();
+            
+            if (!graph.getOrDefault(nextCity, new PriorityQueue<>()).isEmpty()) {
+                stack.push(graph.get(nextCity).poll());
             } else {
-                itinerary.addFirst(stack.pop());
+                itinerary.add(stack.pop());
             }
         }
+        Collections.reverse(itinerary);
         return itinerary;
     }
 }
