4c85eee019b00129551ce19ed382eeaa3b62062d
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-06 17:42:08 -0800

    Time: 16 ms (85.66%), Space: 56.2 MB (60.88%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,18 @@
+/*
+Two distinct indices i and j
+Fixed Sliding window, maintain k size
+*/
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
