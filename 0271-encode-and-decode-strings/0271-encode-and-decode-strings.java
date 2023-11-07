9658b42274a4ff7cd28fe54848e8867665dedab3
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-07 00:38:30 -0800

    Time: 4 ms (95.58%), Space: 44.8 MB (15.21%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -0,0 +1,38 @@
+/*
+.substring is exclusive
+1. Encode, use stringbuilder, construct str in strs in the form 
+    (5/hello) -> (str.length(), '/' and str)
+2. Decode, find index of slash from current index, take length 
+    user index to find int length, String str// Hint: use Integer.valueOf and substring
+*/
+    
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length()).append('/').append(str);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> result = new ArrayList<>();
+        
+        int index = 0;
+        while (index < s.length()) {
+            int slash = s.indexOf('/', index);
+            int length = Integer.valueOf(s.substring(index, slash));
+            String str = s.substring(slash + 1, slash + 1 + length);
+            index = slash + 1 + length;
+            result.add(str);
+        }
+        return result;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
