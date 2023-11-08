e90eff13e27560392e4d7b0bc15562255a429016
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-07 18:21:26 -0800

    Time: 32 ms (38.68%), Space: 46.4 MB (91.06%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,14 +1,15 @@
+/*
+
+*/
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         int[][] result = new int[k][2];
-        
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare (int[] p1, int[] p2) {
-                return distance(p2) - distance(p1);
+            public int compare (int[] a, int[] b) {
+                int distanceA = a[0] * a[0] + a[1] * a[1];
+                int distanceB = b[0] * b[0] + b[1] * b[1];
+                return distanceB - distanceA;
             }
-            public int distance (int[] p) {
-               return p[0] * p[0] + p[1] * p[1];
-           }
         });
         
         for (int[] point : points) {
