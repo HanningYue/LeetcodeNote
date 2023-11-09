9a79e6bb043dece55e8cc0f9418bdee0dfdfccd7
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-09 14:32:13 -0800

    Time: 3 ms (97.76%), Space: 40.3 MB (94.06%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.java
+++ 0269-alien-dictionary/0269-alien-dictionary.java
@@ -1,10 +1,13 @@
-/* 关键点在于构建map, the final map should be {single character -> Set<Character> set}
+/* 关键点在于构建map, the final map should be {single character -> Set<Character> set}. !!!Break after 1
 1. Map<Character, Set<Character>, In this process : Check currentWord length with nextWord length, and if currentWord startsWith(nextWord). If equal and startswith nextword, clear map and return empty map
-2. After, in the boundary of min(currentword and nextword), iterate character and check equivalence. Only want first one letter corresponding to the next one. (比如acb，aed) c一定在e之前,但b不一定在d之前 so break after one iteration of comparsion in while loop
+2. After, in the boundary of Math.min(currentword and nextword), iterate character and check equivalence. Only want first one letter corresponding to the next one. (比如acb，aed) c一定在e之前,但b不一定在d之前 so break after one iteration of comparsion in while loop
 
 Step one: build map
+    For loop one to fill map with <Character, Set<Character>>
+    For loop two to compare the length of current word and next word
+        For loop three to add rear letter to desired front hash set. .min();
 Step two: DFSII, check cycle, use a stringbuilder to record order
-Step three: Return the .reverse of stringbuilder because nature of DFS
+Step three: Return the sb.reverse() of stringbuilder because nature of DFS
 */
 class Solution {
     public String alienOrder(String[] words) {
@@ -13,23 +16,22 @@ class Solution {
         Set<Character> visiting = new HashSet<>();
         Set<Character> visited = new HashSet<>();
         
-        for (char c : map.keySet()) {
-            if (!dfs(visiting, visited, c, map, sb)) {
+        for (char c : map.keySet())
+            if (!dfs(visiting, visited, map, sb, c))
                 return "";
-            }
-        }
+        
         return sb.reverse().toString();
     }
-    
-    private boolean dfs(Set<Character> visiting, Set<Character> visited, char c, 
-                        Map<Character, Set<Character>> map, StringBuilder sb) 
+    private boolean dfs(Set<Character> visiting, Set<Character> visited, 
+                       Map<Character, Set<Character>> map, StringBuilder sb, char c) 
     {
-        if (visiting.contains(c)) return false;
-        if (visited.contains(c)) return true;
-        
+        if (visiting.contains(c))
+            return false;    
+        if (visited.contains(c))
+            return true;
         visiting.add(c);
         for (char neighbor : map.get(c)) {
-            if (!dfs(visiting, visited, neighbor, map, sb)) {
+            if (!dfs(visiting, visited, map, sb, neighbor)) {
                 return false;
             }
         }
@@ -38,22 +40,23 @@ class Solution {
         sb.append(c);
         return true;
     }
-    
-    private Map<Character, Set<Character>> buildMap(String[] words) {
+    private Map<Character, Set<Character>> buildMap (String[] words) {
         Map<Character, Set<Character>> map = new HashMap<>();
         for (String word : words) {
             for (char c : word.toCharArray()) {
-                map.putIfAbsent(c, new HashSet<Character>());
+                map.putIfAbsent(c, new HashSet<>());
             }
         }
         
         for (int i = 0; i < words.length - 1; i++) {
             String currentWord = words[i];
             String nextWord = words[i + 1];
+            
             if (currentWord.length() > nextWord.length() && currentWord.startsWith(nextWord)) {
                 map.clear();
                 return map;
             }
+            
             for (int j = 0; j < Math.min(currentWord.length(), nextWord.length()); j++) {
                 char currentChar = currentWord.charAt(j);
                 char nextChar = nextWord.charAt(j);
