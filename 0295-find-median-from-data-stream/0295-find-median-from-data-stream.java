91801d8cf5b010a27b2ddfb937e2ddfcb5cfd9d3
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-09 14:43:28 -0800

    Time: 84 ms (82.74%), Space: 66.3 MB (78.41%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -10,31 +10,31 @@ Scenario one: even total number, combine maxHeap max with minHeap min / 2
 Scenario two: odd total number, one heap is bigger than the other
     if maxHeap is bigger, return the maxHeap.peek()
     if minHeap is bigger, return the minHeap.peek();
+When implementing findMedian, always use peek(), because the nature of stream, we need to keep element complete
 */
 class MedianFinder {
-    private PriorityQueue<Integer> minHeap;
-    private PriorityQueue<Integer> maxHeap;
-    
+    public PriorityQueue<Integer> maxHeap;
+    public PriorityQueue<Integer> minHeap;
     public MedianFinder() {
-        minHeap = new PriorityQueue<>((a, b) -> (a - b));
         maxHeap = new PriorityQueue<>((a, b) -> (b - a));
+        minHeap = new PriorityQueue<>();
     }
     
     public void addNum(int num) {
-        maxHeap.add(num);
+        maxHeap.offer(num);
         if (maxHeap.size() > minHeap.size() + 1 || !minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) {
-            minHeap.add(maxHeap.poll());
-        }
+            minHeap.offer(maxHeap.poll());
+        } 
         if (minHeap.size() > maxHeap.size() + 1) {
             maxHeap.offer(minHeap.poll());
         }
     }
     
     public double findMedian() {
-        if (maxHeap.size() == minHeap.size()) {
-            return (double) (maxHeap.peek() + minHeap.peek()) / 2;
-        } else if (maxHeap.size() < minHeap.size()) {
-            return (double) minHeap.peek();
+        if (minHeap.size() == maxHeap.size()) {
+            return (double) (minHeap.peek() + maxHeap.peek()) / 2;
+        } else if (minHeap.size() > maxHeap.size()) {
+            return (double) minHeap.peek(); 
         } else {
             return (double) maxHeap.peek();
         }
