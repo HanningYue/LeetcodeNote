ff19508bb2e89aca0e7254cbd73cf35eced7c91e
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-09 16:34:50 -0800

    Time: 4 ms (85.54%), Space: 55.5 MB (44.38%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,18 +1,21 @@
-/**
-1. Want to find the container with most water == The largest satisfied area (Rectangle area)
-2. Two pointer(index), given int[], right pointer index - left pointer index -> find length
-3. Math.min(int[left], int[right]) -> find height
-4. Update area each iteration, length * height
-5. Move left pointer to right if left height is smaller and vice versa
+/*
+本质上，这道题是在求最大长方形面积
+Use 2-pointer technique, update the area each iteration
+    QUESTION IS, HOW TO DETERMINE WHEN TO MOVE THE POINTER?
+        看题目，我们要尽量optimize高度差，that is两个柱状体的高度差尽量最小
+        For example, 当a方高度高于当前b方时，a方左边所有柱体都小于a方，这时move b方向a方的方向. And vice versa
 */
 class Solution {
     public int maxArea(int[] height) {
-        int left = 0, right = height.length - 1;
+        int left = 0;
+        int right = height.length - 1;
         int maxArea = 0;
+        
         while (left < right) {
-            int area = 0;
-            area = (right - left) * Math.min(height[right], height[left]);
-            maxArea = Math.max(maxArea, area);
+            int rectangular_length = right - left;
+            int rectangular_height = Math.min(height[right], height[left]);
+            
+            maxArea = Math.max(maxArea, rectangular_height * rectangular_length);    
             if (height[left] < height[right]) {
                 left++;
             } else {
