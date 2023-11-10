26ec2f206686056ed87617628aec3aee715a2a33
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-09 21:42:28 -0800

    Time: 31 ms (67.08%), Space: 50.1 MB (92.99%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,37 +1,35 @@
 /*
 Three sum, [a, b, c, d, e, f, g, h, i, j, k, l, m, n]
             i  j  k
-首先需要Arrays.sort(nums)
-k每次向右走动，每一次iteration都check nums[i] + nums[j] + nums[k]// i = 0 // j = k - 1
-ATTENTION!!查重时，如果k遇到duplicate， k++并且skip当前的iteration， 所以用while loop
-                  如果i遇到duplicate, i++, ATTENTION!,最后i还要再次increase 到新的数字上
-常规sum < > == 0 的操作
-check如果等于0，加到result list
+首先需要Arrays.sort(nums), i = 0, j = k - 1;
+CHECK DUPLICATE
+k每次向右走动，每一次iteration都check duplicate, while (k + 1 < nums.length),如果相同，跳过当前k, (continue)
+ATTENTION!!当遇到结果时，因为是sorted， 查重复 i++ and i + 1 < j ATTENTION!,最后i还要再次increase 到新的数字上
+常规sum < or > or == 0 的操作
+return时 return Arrays.asList
 */
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        
+        List<List<Integer>> result = new ArrayList<>();
         for (int k = 2; k < nums.length; k++) {
-            if (k + 1 < nums.length && nums[k] == nums[k + 1]) {
+            if (k + 1 < nums.length && nums[k + 1] == nums[k]) {
                 continue;
             }
-            
             int i = 0;
             int j = k - 1;
             while (i < j) {
                 int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
-                    i++;
-                } else if (sum > 0) {
-                    j--;
-                } else {
+                if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while (i + 1 < j && nums[i] == nums[i + 1]) {
-                        i++;
+                    while (i + 1 < j && nums[i + 1] == nums[i]) {
+                        i++;   
                     }
                     i++;
+                } else if (sum < 0) {
+                    i++;
+                } else {
+                    j--;
                 }
             }
         }
