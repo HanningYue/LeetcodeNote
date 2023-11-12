c1179face62eeae62f716a0bd8d5fdf0ffed0110
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-11 20:49:16 -0800

    Time: 0 ms (100.00%), Space: 41.2 MB (11.69%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -9,10 +9,10 @@ class Solution {
         
         while (low < high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] < nums[high]) {
-                high = mid;
-            } else {
+            if (nums[mid] > nums[high]) {
                 low = mid + 1;
+            } else {
+                high = mid;
             }
         }
         return nums[low];
