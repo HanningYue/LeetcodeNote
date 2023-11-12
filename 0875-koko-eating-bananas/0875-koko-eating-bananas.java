0bdc044828af2cc0dcf0c50cfd64530b51afbf05
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-11 20:46:51 -0800

    Time: 21 ms (62.40%), Space: 44.1 MB (45.94%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,33 +1,30 @@
 /*
-Rate : k/hour, want to show the minimum rate K so bananas == 0 within h hours
 We need to find the pile with most banana
-The low and high in this context will be 0 and the pile with most banana 
+The low will be 0 and the high will be the pile with most banana 
     (Koko can eats 0 or at-Most pile banana in each hour)
-因为要找minimum integer k,所以求左边界,退出条件就是while (low <= high)
-Mid will be the RATE of eating MEADIAN number of bananas, calculate by low + (high - low) / 2
-我们找到总时间,对于每一个pile, 用Math.ceil() round up (因为Koko 每小时只吃一pile香蕉 不管RATE多少) the time finish this pile with current RATE
-如果时间等于或小于h, 把RATE减1 因为要找到最小的RATE
-    Total / rate = time , Total 不变 Time越大RATE越小
+求左边界,退出条件就是while (low < high), 判定条件 low = rate + 1
+Mid will be the 'rate' of eating x-# of bananas, calculate by low + (high - low) / 2
+我们找到总时间,对于每一个pile, 用Math.ceil() round up (因为Koko 每小时只吃一pile香蕉 不管RATE多少) 
+如果时间大于h, 移动左边界+1,因为超时了 其他情况移动右边界
 */
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 0;
-        int high = 0;
+        int low = 0, high = 0;
         for (int pile : piles) {
-            high = Math.max(pile, high);
+            high = Math.max(high, pile);
         }
-
+        
         while (low < high) {
-            int speed = low + (high - low) / 2;
+            int rate = low + (high - low) / 2;
             double time = 0;
             for (int pile : piles) {
-                time += Math.ceil((double) pile / speed);
+                time += Math.ceil((double) pile / rate);
             }
             
             if (time > h) {
-                low = speed + 1;
+                low = rate + 1;
             } else {
-                high = speed;
+                high = rate;
             }
         }
         return low;
