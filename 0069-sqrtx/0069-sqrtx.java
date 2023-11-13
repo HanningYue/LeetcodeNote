ab9693a6acdfb6d125a24593421e77150531b565
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-13 14:04:51 -0800

    Time: 1 ms (97.30%), Space: 39.3 MB (85.12%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,22 +1,21 @@
 class Solution {
-  public int mySqrt(int x) {
-    if (x < 2)
-      return x; // return x if it is 0 or 1
-
-    int left = 2, right = x / 2; // initialize left and right pointers
-    int pivot;
-    long num; // use long to store result of pivot * pivot to prevent overflow
-    while (left <= right) { // binary search for the square root
-      pivot = left + (right - left) / 2; // find the middle element
-      num = (long) pivot * pivot;
-      if (num > x)
-        right = pivot - 1; // if pivot * pivot is greater than x, set right to pivot - 1
-      else if (num < x)
-        left = pivot + 1; // if pivot * pivot is less than x, set left to pivot + 1
-      else
-        return pivot; // if pivot * pivot is equal to x, return pivot
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+        int low = 2, high = x / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long target = (long) mid * mid;
+            
+            if (target > x) {
+                high = mid - 1;
+            } else if (target < x) {
+                low = mid + 1;
+            } else if (target == x) {
+                return mid;
+            }
+        }
+        return high;
     }
-
-    return right; // return right after the loop
-  }
 }
