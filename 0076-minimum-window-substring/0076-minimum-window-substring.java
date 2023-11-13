459959f522aea62c051e9ce050a9e8ffdba16804
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-13 14:23:48 -0800

    Time: 64 ms (25.44%), Space: 44.5 MB (24.99%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,18 +1,17 @@
 /*
-
+For each character in t, use map save their character and frequency
+Iterate String s, for each appeared character, minus map frequency by 1, if the frequency is greater than 0. Increase match
+When match is equals to t length, update length to the current sliding window size, update substring
+If character exists in map. Increase map frequency by 1, if it is greater than 0 , minus match by 1. Return result
 */
 class Solution {
     public String minWindow(String s, String t) {
-        if (s.length() < t.length()) return "";
-        
         Map<Character, Integer> map = new HashMap<>();
         for (char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
-        int match = 0;
-        int slow = 0;
-        int length = Integer.MAX_VALUE;
+        int match = 0, slow = 0, length = Integer.MAX_VALUE;
         String result = "";
         
         for (int fast = 0; fast < s.length(); fast++) {
@@ -25,11 +24,10 @@ class Solution {
             }
             
             while (match == t.length()) {
-                if (fast - slow + 1 < length) {
+                if (length > fast - slow + 1) {
                     length = fast - slow + 1;
                     result = s.substring(slow, fast + 1);
                 }
-                
                 current = s.charAt(slow);
                 if (map.containsKey(current)) {
                     map.put(current, map.get(current) + 1);
