6f9226ad3337105ecfaec7a12e0699f897bc555b
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-12 22:48:20 -0800

    Time: 7 ms (39.60%), Space: 43.3 MB (76.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,24 +1,24 @@
-/**
-1. want to find the longest substring without repeating characters
-2. need to apply sliding window technique, non-fixed size, using a hashset to handle duplicates
-3. the removing condition is when we cannot add fast char into set
-4. ATTENTION, increase slow aftert removing the slow char, to the next non-fast-duplicate char
+/*
+Non-fixed size sliding window, need to check duplicate, use a Set<>
+If the character at FAST index is already contained in Set, remove the character at SLOW index
+    Because we want the longest substring without duplicate character, we keep removing 
+    slow index character until remove the one that is currently at FAST index
+Update max length at each iteration of fast, compare Math.max(fast - slow + 1, maxLength)
 */
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         Set<Character> set = new HashSet<>();
-        int slow = 0, length = 0;
-
+        int slow = 0;
+        int maxLength = 0;
+        
         for (int fast = 0; fast < s.length(); fast++) {
-            if (!set.add(s.charAt(fast))) {
-                while (s.charAt(slow) != s.charAt(fast)) {
-                    set.remove(s.charAt(slow));
-                    slow++;
-                }
+            while (set.contains(s.charAt(fast))) {
+                set.remove(s.charAt(slow));
                 slow++;
             }
-            length = Math.max(length, fast - slow + 1);
+            set.add(s.charAt(fast));
+            maxLength = Math.max(maxLength, fast - slow + 1);
         }
-        return length;
+        return maxLength;
     }
 }
