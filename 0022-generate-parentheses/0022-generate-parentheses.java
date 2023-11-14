24af4b19dbb8b05d1cade689f17400f2405029bc
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-14 01:19:53 -0800

    Time: 1 ms (89.12%), Space: 43.8 MB (15.34%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,27 @@
 /*
-Using a DFS to solve the problem, 
+Using a DFS to solve the problem
+We are given n-pairs of '(' and ')'. To find all the combination, we can use DFS n times
+1. WE NEED TO MAKE SURE LEFTBRACKET ) IS ALWAYS GREATER THAN THE LEFTBRACKET (
+2. So, when rightBracket is not zero and greater than leftBracket, minus the number of rightBracket by 1, add ')' to the path
+3. Same for leftBracket but only need leftBracket to be greater than 0
+4. When both n pairs of leftBracket and rightBracket reach 0, add new String to result, return result.
 */
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        dfs(n, n, "", result);
+        dfs(n, n, result, "");
         return result;
     }
-    private void dfs(int leftBracket, int rightBracket, String path, List<String> result) {
+    private void dfs(int leftBracket, int rightBracket, List<String> result, String path) {
         if (leftBracket == 0 && rightBracket == 0) {
             result.add(new String(path));
             return;
         }
-        if (rightBracket > 0 && leftBracket < rightBracket) {
-            dfs(leftBracket, rightBracket - 1, path + ')', result);
+        if (rightBracket > 0 && rightBracket > leftBracket) {
+            dfs(leftBracket, rightBracket - 1, result, path + ')');
         }
         if (leftBracket > 0) {
-            dfs(leftBracket - 1, rightBracket, path + '(', result);
+            dfs(leftBracket - 1, rightBracket, result, path + '(');
         }
     }
 }
