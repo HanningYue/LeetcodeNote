b2bc1a0c5aff7f5bb811f34e7a2428909dde60ab
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-14 01:17:21 -0800

    Time: 63 ms (26.54%), Space: 45.2 MB (5.20%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,19 +1,22 @@
 /*
 For each character in t, use map save their character and frequency
 Iterate String s, for each appeared character, minus map frequency by 1, if the frequency is greater than 0. Increase match
-When match is equals to t length, update length to the current sliding window size, update substring
-If character exists in map. Increase map frequency by 1, if it is greater than 0 , minus match by 1. Return result
+While match is equals to t length, update length to the current sliding window size, update substring.
+If slow character exists in map. Increase map frequency by 1, if it is greater than 0 , minus match by 1.  Increase slow pointer while match == t.length();
+Return result
 */
 class Solution {
     public String minWindow(String s, String t) {
+        String result = "";
+        
         Map<Character, Integer> map = new HashMap<>();
         for (char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
-        int match = 0, slow = 0, length = Integer.MAX_VALUE;
-        String result = "";
-        
+        int slow = 0;
+        int match = 0;
+        int length = Integer.MAX_VALUE;
         for (int fast = 0; fast < s.length(); fast++) {
             char current = s.charAt(fast);
             if (map.containsKey(current)) {
