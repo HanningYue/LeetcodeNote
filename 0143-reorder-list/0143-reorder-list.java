38c6b54dea2647618b0980a2a544eb46349b39a9
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-14 16:57:24 -0800

    Time: 2 ms (89.10%), Space: 45.9 MB (54.04%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -1,11 +1,11 @@
 /**
-
+1. 先使用slow和fast 快慢指针将慢指针走到中间的位置
+2. Reverse后半段，慢指针这时是后半段的第一个node. Reverse完成后，previous成为original的最后一个node
+3. 用first和second来构建最后的listnode，退出条件是当后半段走到最后的node时(.next != null)
 */
 class Solution {
     public void reorderList(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        
+        ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
