377c5da3676f2b507ca814d83eb419183b8a67ca
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-14 17:48:49 -0800

    Time: 0 ms (100.00%), Space: 43.1 MB (46.24%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,26 +1,17 @@
 /*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
+用DFS的方法解决这道问题
+对于每一个node的next和random node
+用recursion call original function
+return copy of each node
 */
-
 class Solution {
     Map<Node, Node> map = new HashMap<>();
+    
     public Node copyRandomList(Node head) {
         if (head == null) return null;
-        
         if (map.containsKey(head)) {
             return map.get(head);
-        }
+        }    
         
         Node copy = new Node(head.val);
         map.put(head, copy);
