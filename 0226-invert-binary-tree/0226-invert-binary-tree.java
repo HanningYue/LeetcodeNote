c32867999912fdcb2c055f67417e295537380ea6
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-16 01:07:53 -0800

    Time: 0 ms (100.00%), Space: 39.5 MB (89.27%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -1,10 +1,15 @@
+/*
+1. 向右边要右边invert过的tree，向左边要左边invert过的tree
+2. 当前这层 declare 一个新head 把左边指向原来node右边 把右边指向原来node左边
+3. 返回invert过的 tree
+*/
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
         
-        TreeNode newRoot = new TreeNode(root.val);
-        newRoot.left = invertTree(root.right);
-        newRoot.right = invertTree(root.left);
-        return newRoot;
+        TreeNode newHead = new TreeNode(root.val);
+        newHead.right = invertTree(root.left);
+        newHead.left = invertTree(root.right);
+        return newHead;
     }
 }
