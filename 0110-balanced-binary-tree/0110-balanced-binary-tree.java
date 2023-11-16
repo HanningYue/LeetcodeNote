42ca26901a402a49162abbb50f370854ff4a5544
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-16 14:36:29 -0800

    Time: 1 ms (35.93%), Space: 43.2 MB (38.66%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,28 +1,32 @@
 /**
-1. 向左检查左边的height， 向右检查右边的height
-2. 本层对比左右的height是否绝对值差1
-3. 向上传递本层是否balanced
+多个value传输 新建一个class TYPE
+Type 包含属性：isBalanced和height
+1. 检查左边的height，检查右边的height
+2. 本层对比左右的math.max height是否绝对值差1
+3. isBalanced一定是左.isBalanced 和右.isBalanced 并且高度差绝对值小于1
+3. 向上传递一个新的Type是否balanced 以及本层的height
 */
 class Solution {
     class Type {
-        boolean isBalanced;
+        boolean balanced;
         int height;
-        public Type (boolean isBalanced, int height) {
-            this.isBalanced = isBalanced;
+        public Type (boolean balanced, int height) {
+            this.balanced = balanced;
             this.height = height;
         }
     }
     public boolean isBalanced(TreeNode root) {
-        return dfs(root).isBalanced;
+        return dfs(root).balanced;    
     }
     private Type dfs(TreeNode root) {
         if (root == null) return new Type(true, 0);
+        
         Type left = dfs(root.left);
         Type right = dfs(root.right);
-        boolean isBalanced = left.isBalanced && right.isBalanced 
-            && (Math.abs(left.height - right.height) <= 1);
+        
+        boolean balanced = left.balanced && right.balanced && Math.abs(left.height - right.height) <= 1;
         int height = Math.max(left.height, right.height) + 1;
-
-        return new Type(isBalanced, height);
+        
+        return new Type(balanced, height);
     }
 }
