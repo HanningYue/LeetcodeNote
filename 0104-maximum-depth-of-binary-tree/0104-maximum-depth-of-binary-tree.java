5c43808306ad0f7f32582c641b5d64822a8ae5f6
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-16 01:04:45 -0800

    Time: 0 ms (100.00%), Space: 42 MB (12.41%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -1,9 +1,13 @@
+/*
+1. 向右要右边最深的path 向左要左边最深的path
+2. 每层+1， 加上当前的level， 返回最大的path
+*/
 class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        int height = Math.max(left, right) + 1;
-        return height;
+        int current = Math.max(left, right) + 1;
+        return current;
     }
 }
