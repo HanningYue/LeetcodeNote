c53949628089168bfb5e82d75515b51527820ffc
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-16 00:52:05 -0800

    Time: 0 ms (100.00%), Space: 40.2 MB (18.51%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -1,15 +1,11 @@
-/**
-1. Use a copy node
-2. Recursion
-*/
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
-
-        TreeNode node = new TreeNode(root.val);
-        node.val = root.val;
-        node.right = invertTree(root.left);
-        node.left = invertTree(root.right);
-        return node;
+        
+        TreeNode newRoot = new TreeNode(root.val);
+        newRoot.val = root.val;
+        newRoot.left = invertTree(root.right);
+        newRoot.right = invertTree(root.left);
+        return newRoot;
     }
 }
