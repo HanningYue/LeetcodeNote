73ef391e9180b24cebd2bcfcfee279b38497336e
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-16 01:10:40 -0800

    Time: 0 ms (100.00%), Space: 41.9 MB (28.39%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,15 +1,20 @@
+/*
+1. 向右边要最大的diameter 向左边要最大的diameter
+2. 本层比较左右两边的diameter 加上自己的level +1
+3. 返回本层加1后的level到上一层
+*/
 class Solution {
     private int diameter;
     public int diameterOfBinaryTree(TreeNode root) {
-        diameter = 0;
         dfs(root);
         return diameter;
     }
     private int dfs(TreeNode root) {
         if (root == null) return 0;
+        
         int left = dfs(root.left);
         int right = dfs(root.right);
-        diameter = Math.max(left + right, diameter);
+        diameter = Math.max(diameter, left + right);
         return Math.max(left, right) + 1;
     }
 }
