b8f059ee097a8c4af349ed76466015cf6b4a2c8e
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-18 15:09:41 -0800

    Time: 1 ms (36.12%), Space: 43.4 MB (23.02%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -1,32 +1,42 @@
 /**
-多个value传输 新建一个class TYPE
-Type 包含属性：isBalanced和height
-1. 检查左边的height，检查右边的height
-2. 本层对比左右的math.max height是否绝对值差1
-3. isBalanced一定是左.isBalanced 和右.isBalanced 并且高度差绝对值小于1
-3. 向上传递一个新的Type是否balanced 以及本层的height
-*/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     class Type {
-        boolean balanced;
+        boolean isBalanced;
         int height;
-        public Type (boolean balanced, int height) {
-            this.balanced = balanced;
+        public Type (int height, boolean isBalanced) {
             this.height = height;
+            this.isBalanced = isBalanced;
         }
     }
+    
     public boolean isBalanced(TreeNode root) {
-        return dfs(root).balanced;    
+        return dfs(root).isBalanced;
     }
+    
     private Type dfs(TreeNode root) {
-        if (root == null) return new Type(true, 0);
+        if (root == null) return new Type(0, true);
         
         Type left = dfs(root.left);
         Type right = dfs(root.right);
         
-        boolean balanced = left.balanced && right.balanced && Math.abs(left.height - right.height) <= 1;
+        boolean isBalanced = left.isBalanced && right.isBalanced 
+            && Math.abs(left.height - right.height) <= 1;
         int height = Math.max(left.height, right.height) + 1;
         
-        return new Type(balanced, height);
+        return new Type(height, isBalanced);
     }
 }
