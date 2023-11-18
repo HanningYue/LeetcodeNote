3642575c88a4980b778304d3a6ff1ee8854756fd
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-18 15:01:44 -0800

    Time: 0 ms (100.00%), Space: 43.6 MB (78.06%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,23 +1,26 @@
 /*
+In a BST, for any given node:
+All nodes in its left subtree must have values less than the node's value.
+All nodes in its right subtree must have values greater than the node's value.
     Integer _, Integer _
 1. Return boolean, we need to check
-    The current node is greater than the MAXIMUM of left subtree, 
-                     is less than the MINIMUM of right subtree
+    The current node is GREATER than the MAXIMUM of left subtree, 
+                     is LESS than the MINIMUM of right subtree
 2. When checking left and right subtree is also BST, left root.val be 
-    the rightMinimum when checking left-subtree
-    the leftMaximum when checking right-subtree
+    the rightMin when checking left-subtree, we update rightMin from null to root.val
+    the leftMax when checking right-subtree, we update leftMax from null to root.val
 */
 class Solution {
     public boolean isValidBST(TreeNode root) {
         return isBST(root, null, null);
     }
-    private boolean isBST (TreeNode root, Integer leftMax, Integer rightMin) {
+    private boolean isBST(TreeNode root, Integer leftMax, Integer rightMin) {
         if (root == null) return true;
         
-        if (leftMax != null && root.val <= leftMax
-          ||rightMin != null && root.val >= rightMin)
+        if (leftMax != null && root.val <= leftMax 
+            || rightMin != null && rightMin <= root.val) 
         {
-            return false;
+            return false;    
         }
         
         boolean leftBST = isBST(root.left, leftMax, root.val);
