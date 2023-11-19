d0d4155c0e9bd24d4438a7fe19da88394c688590
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-19 01:00:51 -0800

    Time: 6 ms (92.71%), Space: 41.4 MB (66.93%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,16 @@
-/*
-We can use array or hashmap to store the frequency
-*/
 class Solution {
     public int characterReplacement(String s, int k) {
-        Map<Character, Integer> map = new HashMap<>();
-        int slow = 0;
+        int[] frequency = new int[26];
         int length = 0;
         int maxFreq = 0;
         
+        int slow = 0;
         for (int fast = 0; fast < s.length(); fast++) {
-            char c = s.charAt(fast);
-            map.put(c, map.getOrDefault(c, 0) + 1);
-            maxFreq = Math.max(maxFreq, map.get(c));
+            frequency[s.charAt(fast) - 'A']++;
+            maxFreq = Math.max(maxFreq, frequency[s.charAt(fast) - 'A']);
             
             while (fast - slow + 1 - maxFreq > k) {
-                map.put(s.charAt(slow), map.get(s.charAt(slow)) - 1);
-                if (map.get(s.charAt(slow)) == 0) {
-                    map.remove(s.charAt(slow));
-                }
+                frequency[s.charAt(slow) - 'A']--;
                 slow++;
             }
             length = Math.max(length, fast - slow + 1);
