64fa833ab03b8612ad54d7ecbb9855f0b87fcf86
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-19 00:58:02 -0800

    Time: 23 ms (30.44%), Space: 44.1 MB (5.40%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,23 +1,23 @@
 /*
-If we can flip k characters to the same character,consider this question alternative, what is the longest repeating character with at most k different character
-Since we want to know the frequency of each character in the window, we can use a frequency[] to store all the character and update it in the window
-1. Declare a frequency array, during each fast iteration, increase 1 of the character at fast
-2. When fast - slow + 1 - maxFrequent is greater than k, it means we have more than k distinct characters exclude the most frequent character.
-3. We need to remove the character at the slow. And minus the frequency of slow character by 1 for each iteration
-4. Update the size of window each iteration
+We can use array or hashmap to store the frequency
 */
 class Solution {
     public int characterReplacement(String s, int k) {
-        int length = 0;
-        int[] frequency = new int[26];
-        int maxFrequency = 0;
-        
+        Map<Character, Integer> map = new HashMap<>();
         int slow = 0;
+        int length = 0;
+        int maxFreq = 0;
+        
         for (int fast = 0; fast < s.length(); fast++) {
-            frequency[s.charAt(fast) - 'A']++;
-            maxFrequency = Math.max(maxFrequency, frequency[s.charAt(fast) - 'A']);
-            while (fast - slow - maxFrequency + 1 > k) {
-                frequency[s.charAt(slow) - 'A']--;
+            char c = s.charAt(fast);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+            maxFreq = Math.max(maxFreq, map.get(c));
+            
+            while (fast - slow + 1 - maxFreq > k) {
+                map.put(s.charAt(slow), map.get(s.charAt(slow)) - 1);
+                if (map.get(s.charAt(slow)) == 0) {
+                    map.remove(s.charAt(slow));
+                }
                 slow++;
             }
             length = Math.max(length, fast - slow + 1);
