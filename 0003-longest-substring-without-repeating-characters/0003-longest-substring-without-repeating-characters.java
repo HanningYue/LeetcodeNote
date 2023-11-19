1ae8e7aa798dbe0a75383dc9cb79ed166a2d8f26
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-19 00:48:54 -0800

    Time: 6 ms (65.95%), Space: 44 MB (24.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,24 +1,18 @@
-/*
-Non-fixed size sliding window, need to check duplicate, use a Set<>
-If the character at FAST index is already contained in Set, remove the character at SLOW index
-    Because we want the longest substring without duplicate character, we keep removing 
-    slow index character until remove the one that is currently at FAST index
-Update max length at each iteration of fast, compare Math.max(fast - slow + 1, maxLength)
-*/
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         Set<Character> set = new HashSet<>();
         
-        int length = 0;
         int slow = 0;
+        int length = Integer.MIN_VALUE;
+        
         for (int fast = 0; fast < s.length(); fast++) {
             while (set.contains(s.charAt(fast))) {
                 set.remove(s.charAt(slow));
                 slow++;
             }
-            length = Math.max(length, fast - slow + 1);
             set.add(s.charAt(fast));
+            length = Math.max(length, fast - slow + 1);
         }
-        return length;
+        return length == Integer.MIN_VALUE ? 0 : length;
     }
 }
