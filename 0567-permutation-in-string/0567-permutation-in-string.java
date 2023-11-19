fefa66eb6516fe377633956ecbbcc26d8ac24a6b
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-19 01:22:45 -0800

    Time: 5 ms (96.23%), Space: 42 MB (42.95%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
-        
         int[] frequency = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            frequency[s1.charAt(i) - 'a']++;
+        for (char c : s1.toCharArray()) {
+            frequency[c - 'a']++;
         }
         
         int slow = 0;
         for (int fast = 0; fast < s2.length(); fast++) {
             frequency[s2.charAt(fast) - 'a']--;
-            if (arrayZero(frequency)) {
+            if (allZero(frequency)) {
                 return true;
-            }        
+            }
             
-            if (fast - slow + 1 == s1.length()) {
+            while (fast - slow + 1 >= s1.length()) {
                 frequency[s2.charAt(slow) - 'a']++;
                 slow++;
             }
@@ -22,7 +20,7 @@ class Solution {
         return false;
     }
     
-    private boolean arrayZero(int[] arr) {
+    private boolean allZero(int[] arr) {
         for (int num : arr) {
             if (num != 0) {
                 return false;
