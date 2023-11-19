ea98600804796ecb00069e04cebf8d22653688f4
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-18 23:16:07 -0800

    Time: 0 ms (100.00%), Space: 43.5 MB (15.34%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,6 +1,20 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
 class Solution {
     Map<Node, Node> map = new HashMap<>();
-    
     public Node copyRandomList(Node head) {
         if (head == null) {
             return null;
@@ -8,11 +22,11 @@ class Solution {
         if (map.containsKey(head)) {
             return map.get(head);
         }
+        Node copy = new Node(head.val);
+        map.put(head, copy);
         
-        Node newHead = new Node(head.val);
-        map.put(head, newHead);
-        newHead.random = copyRandomList(head.random);
-        newHead.next = copyRandomList(head.next);
-        return newHead;
+        copy.next = copyRandomList(head.next);
+        copy.random = copyRandomList(head.random);
+        return copy;
     }
 }
