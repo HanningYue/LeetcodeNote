7350ba935f9cc0d1058df1a81953b2b823d1ca4d
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-19 00:44:52 -0800

    Time: 7 ms (71.14%), Space: 42 MB (39.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,22 +1,18 @@
-/*
-Return boolean, FIXED-Size(size of s1.length()) sliding window
-1. Want to check the permutation, IN OTHER WORDS, want to make sure character and their frequency int s1 appears exactly in s2
-2. For each character in s1, store their frequency in an Array
-3. Iterate s2, for each character, minus the character frequency at fast by 1 in Array, when the size of window reaches length of s1 Check if frequency array s1 is empty (write helper function),  if so, return true, otherwise, add the character frequency at slow by 1 (offset)
-*/
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+        
         int[] frequency = new int[26];
-        for (char c : s1.toCharArray()) {
-            frequency[c - 'a']++;
+        for (int i = 0; i < s1.length(); i++) {
+            frequency[s1.charAt(i) - 'a']++;
         }
         
         int slow = 0;
         for (int fast = 0; fast < s2.length(); fast++) {
             frequency[s2.charAt(fast) - 'a']--;
-            if (allZero(frequency)) {
+            if (arrayZero(frequency)) {
                 return true;
-            }
+            }        
             
             if (fast - slow + 1 == s1.length()) {
                 frequency[s2.charAt(slow) - 'a']++;
@@ -26,9 +22,9 @@ class Solution {
         return false;
     }
     
-    private boolean allZero(int[] arr) {
-        for (int i : arr) {
-            if (i != 0) {
+    private boolean arrayZero(int[] arr) {
+        for (int num : arr) {
+            if (num != 0) {
                 return false;
             }
         }
