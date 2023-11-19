4a8d8da5099803b43898cbacfce357ae093534ac
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-18 16:03:34 -0800

    Time: 1 ms (95.08%), Space: 44 MB (12.94%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,6 +1,18 @@
-/*
-
-*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -9,8 +21,8 @@ class Solution {
         }
         return dfs(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
     }
-    private TreeNode dfs(int[] preorder, int preleft, int preright, 
-                         int[] inorder, int inleft, int inright, Map<Integer, Integer> map) 
+    private TreeNode dfs(int[] preorder, int preleft, int preright,
+                        int[] inorder, int inleft, int inright, Map<Integer, Integer> map)
     {
         if (preleft > preright) {
             return null;
@@ -21,9 +33,8 @@ class Solution {
         
         newHead.left = dfs(preorder, preleft + 1, preleft + mid - inleft, 
                            inorder, inleft, mid - 1, map);
-        
-        newHead.right = dfs(preorder, preleft + 1 + mid - inleft, preright,
-                           inorder, mid + 1, inright, map);
+        newHead.right = dfs(preorder, preleft + 1 + mid - inleft, preright, 
+                            inorder, mid + 1, inright, map);
         return newHead;
     }
 }
