69525b4a9c587a45a7d0b646aba4f2e917ba3611
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-18 22:46:15 -0800

    Time: 0 ms (100.00%), Space: 44.2 MB (6.56%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,22 +1,18 @@
-/*
-用DFS的方法解决这道问题
-对于每一个node的next和random node
-用recursion call original function
-return copy of each node
-*/
 class Solution {
     Map<Node, Node> map = new HashMap<>();
     
     public Node copyRandomList(Node head) {
-        if (head == null) return null;
+        if (head == null) {
+            return null;
+        }
         if (map.containsKey(head)) {
             return map.get(head);
-        }    
+        }
         
-        Node copy = new Node(head.val);
-        map.put(head, copy);
-        copy.next = copyRandomList(head.next);
-        copy.random = copyRandomList(head.random);
-        return copy;
+        Node newHead = new Node(head.val);
+        map.put(head, newHead);
+        newHead.random = copyRandomList(head.random);
+        newHead.next = copyRandomList(head.next);
+        return newHead;
     }
 }
