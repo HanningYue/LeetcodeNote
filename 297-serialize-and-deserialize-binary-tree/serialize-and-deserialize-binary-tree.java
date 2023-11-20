6610b89f8fcbb38f8660982e58e68a0ee9966edb
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-19 18:56:10 -0800

    Time: 6 ms (95.69%) | Memory: 44.6 MB (59.39%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,39 @@
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        se(root, sb);
+        return sb.toString();
+    }
+    private void se(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("#").append(",");
+            return;
+        }
+        sb.append(root.val).append(",");
+        se(root.left, sb);
+        se(root.right, sb);
+    }
+
+    int index = 0;
+    public TreeNode deserialize(String data) {
+        String[] tree = data.split(",");
+        return de(tree);
+    }
+    private TreeNode de(String[] tree) {
+        if (tree[index].equals("#")) {
+            index++;
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.valueOf(tree[index]));
+        index++;
+        root.left = de(tree);
+        root.right = de(tree);
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
