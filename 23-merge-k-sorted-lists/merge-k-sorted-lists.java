00cab1f96350025fae968a2e47bdbce20b045063
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-19 16:21:51 -0800

    Time: 1 ms (100.00%) | Memory: 44.2 MB (13.49%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,40 @@
+/**
+Divide and conquer
+Use recursive call and binary search
+Merge left part and right part
+*/
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return divide(lists, 0, lists.length - 1);
+    }
+    
+    private ListNode divide(ListNode[] lists, int low, int high) {
+        if (low > high) {
+            return null;
+        }
+        if (low == high) {
+            return lists[low];
+        }
+        int mid = low + (high - low) / 2;
+        ListNode left = divide(lists, low, mid);
+        ListNode right = divide(lists, mid + 1, high);
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode n1, ListNode n2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        while (n1 != null && n2 != null) {
+            if (n1.val > n2.val) {
+                current.next = n2;
+                n2 = n2.next;
+            } else {
+                current.next = n1;
+                n1 = n1.next;
+            }
+            current = current.next;
+        }
+        current.next = n1 == null ? n2 : n1;
+        return dummy.next;
+    }
+}
