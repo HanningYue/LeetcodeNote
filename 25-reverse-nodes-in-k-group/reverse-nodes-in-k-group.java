f5004aa66827ac27d1d5f968d9746bb2f00c942d
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-19 21:19:11 -0800

    Time: 0 ms (100.00%) | Memory: 43.8 MB (8.02%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -0,0 +1,43 @@
+/* Need a reverse helper function, divide the lists in to k-lists, reverse and connect them one-by-one
+1. First we let node current reach to k-th place. If current becomes null, it means list length 
+   is less than k, return head
+2. Record the next node of current k-th place node. Let current next point to null, declare newhead be the 
+   reverse function head. 
+3. 
+   
+*/
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode current = head;
+        int count = 1;
+        while (count < k && current != null) {
+            current = current.next;
+            count++;
+        }
+
+        if (current == null) {
+            return head;
+        }
+
+        ListNode next = current.next;
+        current.next = null;
+        
+        ListNode newHead = reverse(head);
+        head.next = reverseKGroup(next, k);
+        return newHead;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
