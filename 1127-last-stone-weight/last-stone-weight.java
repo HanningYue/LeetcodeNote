80e9dd2c96d29365d07db3ad9a27e604b92feb5d
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-21 00:17:44 -0800

    Time: 1 ms (99.38%) | Memory: 40.2 MB (19.22%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -0,0 +1,31 @@
+/*
+1. Create PriorityQueue maxHeap, to save all the stones weight
+    Exit Condition : while (!maxHeap.isEmpty())
+2. Declear two stones, stone1 and stone2 (stone1 pop first, is heavier)
+    Compare two stone, if equal, pop both // if x != y, declear stone 3 equal stone1 - stone2
+3. Add condition loop (if maxHeap.size() > 1)
+4. Return the remaining stone or 0
+*/
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> (b - a));
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        while (!maxHeap.isEmpty()) {
+            if (maxHeap.size() > 1) {
+                int y = maxHeap.poll();
+                int x = maxHeap.poll();
+                if (y != x) {
+                    int newStone = y - x;
+                    maxHeap.offer(newStone);
+                }
+            }
+            if (maxHeap.size() == 1) {
+                return maxHeap.poll();
+            }
+        }
+        return 0;
+    }
+}
