962582eaebef3ebeec68da3b9a98df7c7f7b5f7f
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-21 15:19:14 -0800

    Time: 29 ms (59.47%) | Memory: 47.5 MB (75.68%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,28 @@
+/*
+Want to return the closest point to origin, use maxHeap<int[]>
+1. In maxHeap, write customizable comparator where the a[0]*a[0] + a[1]*a[1] and b[0]*b[0] + b[1]*b[1]
+2. After comparison, fill maxHeap with int[]points, the maxHeap will sort them from greatest distance to smallest distance. After the maxHeap size exceeds k, pop. So the remaining are the K-th smallest.
+3. Declear result[][], iterate k times, fill result[i] with maxHeap poll()
+*/
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return (b[0] * b[0] + b[1] * b[1] - (a[0] * a[0] + a[1] * a[1]));
+            }
+        });
+        for (int[] point : points) {
+            maxHeap.offer(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        int[][] result = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            int[] kth = maxHeap.poll();
+            result[i][0] = kth[0];
+            result[i][1] = kth[1];
+        }
+        return result;
+    }
+}
