0fa6a27ed21e5984de48d390f244e34283a6c123
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-21 15:20:51 -0800

    Time: 31 ms (73.97%) | Memory: 56.8 MB (31.85%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -3,40 +3,28 @@
 2. maxHeap or minHeap, poll maxHeap and minus k while k is greater than 1
 3. return the kth largest element
 */
-public class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        int left = 0, right = nums.length - 1;
-        Random rand = new Random();
-        while (true) {
-            int pivot_index = left + rand.nextInt(right - left + 1);
-            int new_pivot_index = partition(nums, left, right, pivot_index);
-            if (new_pivot_index == nums.length - k) {
-                return nums[new_pivot_index];
-            } else if (new_pivot_index > nums.length - k) {
-                right = new_pivot_index - 1;
-            } else {
-                left = new_pivot_index + 1;
-            }
-        }
-    }
-
-    private int partition(int[] nums, int left, int right, int pivot_index) {
-        int pivot = nums[pivot_index];
-        swap(nums, pivot_index, right);
-        int stored_index = left;
-        for (int i = left; i < right; i++) {
-            if (nums[i] < pivot) {
-                swap(nums, i, stored_index);
-                stored_index++;
-            }
-        }
-        swap(nums, right, stored_index);
-        return stored_index;
-    }
-
+class Solution {
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+    private int quick_select(int[] nums, int lo, int hi, int k) {
+        int lt = lo, gt = hi;
+        int v = nums[hi];
+        int i = hi - 1;
+        while (i >= lt) {
+            int cmp = nums[i] - v;
+            if      (cmp < 0) swap(nums, lt++, i);
+            else if (cmp > 0) swap(nums, i--, gt--);
+            else              i--;
+        }
+        if (k < lt) return quick_select(nums, lo, lt - 1, k);
+        else if (k > gt) return quick_select(nums, gt + 1, hi, k);
+        else return v;
+    }
+    public int findKthLargest(int[] nums, int k) {
+        k = nums.length - k;
+        return quick_select(nums, 0, nums.length - 1, k);
     }
 }
