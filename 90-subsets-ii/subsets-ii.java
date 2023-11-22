27d9d3daa40c9859344917692db4a1a68370cb88
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-22 13:37:15 -0800

    Time: 1 ms (99.92%) | Memory: 44.3 MB (5.63%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,10 +1,11 @@
 /**
-When we dealing with duplicate subsets, we need to sort the arrays first, because without sorting, the same number could be scattered across different parts of the array. The HashSet would then only be effective within a single recursion level, but not across multiple levels, leading to potential duplicate subsets in the result.
+When we dealing with duplicate subsets, we need to sort the arrays first, because without sorting, the same number could be scattered across different parts of the array. Then manipulate array index, comparing nums[i]
+with nums[i - 1], if they are the same, skip the current iteration
           a   a  b   c
 
-              " "                    --   size 0
+              " "                    --   size 0, level 0
         /         \           \
-      a          a           c      -- size 1
+      a          a           c      -- size 1, level 1
     /    \       /   \          /   \
   ac         ac           ca   cb 
   /             /
@@ -15,18 +16,21 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTracking(nums, result, list, 0);
+        backTracking(nums, 0, result, list);
         return result;
     }
-    private void backTracking(int[] nums, List<List<Integer>> result, List<Integer> list, int level) {
-        Set<Integer> set = new HashSet<>();
+
+    private void backTracking(int[] nums, int level, List<List<Integer>> result, List<Integer> list) {
         result.add(new ArrayList<>(list));
-        for (int i = level; i < nums.length; i++) {
-            if (set.add(nums[i])) {
-                list.add(nums[i]);
-                backTracking(nums, result, list, i + 1);
-                list.remove(list.size() - 1);
+
+        for(int i = level; i < nums.length; i++){
+            if(i > level && nums[i] == nums[i - 1]) {
+                continue;
             }
+
+            list.add(nums[i]);
+            backTracking(nums, i + 1, result, list);
+            list.remove(list.size()-1);
         }
     }
 }
