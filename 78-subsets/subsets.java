217a054c6366bd2d88f9bad21d8f0cee3737cb33
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-21 18:28:47 -0800

    Time: 1 ms (71.33%) | Memory: 42.1 MB (13.68%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -6,7 +6,7 @@ class Solution {
         return result;
     }
     private void backtracking(int[] nums, List<List<Integer>> result, List<Integer> list, int level) {
-        result.add(new ArrayList<Integer>(list));
+        result.add(new ArrayList<>(list));
         for (int i = level; i < nums.length; i++) {
             list.add(nums[i]);
             backtracking(nums, result, list, i + 1);
