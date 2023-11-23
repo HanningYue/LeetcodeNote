6bc44ee71f936772b165d74919ec45c67004e08f
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-22 16:59:07 -0800

    Time: 1 ms (71.34%) | Memory: 42.4 MB (8.02%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,5 +1,5 @@
 /*
-每次要加入新的object，对于当前level，backtracking时加入level + 1，因为想要所有的组合
+每次要加入新的object，对于当前level，backtracking时加入 startIndex + 1，因为想要所有的组合
     [0 element] [1 element] [2 elements] [3 elements]
 */
 class Solution {
@@ -9,9 +9,10 @@ class Solution {
         backTracking(nums, result, list, 0);
         return result;
     }
-    private void backTracking(int[] nums, List<List<Integer>> result, List<Integer> list, int level) {
+    private void backTracking(int[] nums, List<List<Integer>> result, List<Integer> list, int startIndex) {
         result.add(new ArrayList<>(list));
-        for (int i = level; i < nums.length; i++) {
+
+        for (int i = startIndex; i < nums.length; i++) {
             list.add(nums[i]);
             backTracking(nums, result, list, i + 1);
             list.remove(list.size() - 1);
