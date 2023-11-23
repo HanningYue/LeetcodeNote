5412941b894af094d9a3598c1148785feb60ad30
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-22 23:08:06 -0800

    Time: 23 ms (6.29%) | Memory: 68.8 MB (5.21%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -22,7 +22,9 @@ public class Solution {
 
     public boolean isPalindrome(String s, int n) {
         for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) != s.charAt(n - i - 1)) return false;
+            if (s.charAt(i) != s.charAt(n - i - 1)) {
+                return false;
+            }
         }
         return true;
     }
