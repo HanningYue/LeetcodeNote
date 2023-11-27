72bb270acac9ac15bfebd47eaa61fc0eecc12671
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-27 12:30:53 -0800

    Time: 8 ms (33.96%) | Memory: 54.7 MB (94.47%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,18 +1,15 @@
 /**
-substring() is exclusive
+BackTracking, substring() is exclusive
 helper function to check palindrome condition
 */
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
         List<String> list = new ArrayList<>();
-
-        backTracking(s, result, list, 0);
+        backTracking(result, list, 0, s);
         return result;
     }
-    
-    private void backTracking(String s, List<List<String>> result, List<String> list, int startIndex)
-    {
+    private void backTracking(List<List<String>> result, List<String> list, int startIndex, String s) {
         if (startIndex == s.length()) {
             result.add(new ArrayList(list));
             return;
@@ -20,12 +17,11 @@ class Solution {
         for (int i = startIndex + 1; i <= s.length(); i++) {
             if (isPalindrome(s.substring(startIndex, i))) {
                 list.add(s.substring(startIndex, i));
-                backTracking(s, result, list, i);
+                backTracking(result, list, i, s);
                 list.remove(list.size() - 1);
             }
         }
     }
-
     private boolean isPalindrome(String s) {
         int left = 0, right = s.length() - 1;
         while (left < right) {
