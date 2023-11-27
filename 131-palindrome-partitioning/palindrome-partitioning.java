01a35e3294cbb45f44a7e70fd78b47c9c9b6fd3f
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-27 12:35:55 -0800

    Time: 6 ms (96.75%) | Memory: 54.5 MB (98.12%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -6,24 +6,26 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
         List<String> list = new ArrayList<>();
-        backTracking(result, list, 0, s);
+        backTracking(0, s, list, result);
         return result;
     }
-    private void backTracking(List<List<String>> result, List<String> list, int startIndex, String s) {
+
+    private void backTracking(int startIndex, String s, List<String> list, List<List<String>> result) {
         if (startIndex == s.length()) {
             result.add(new ArrayList(list));
             return;
         }
-        for (int i = startIndex + 1; i <= s.length(); i++) {
-            if (isPalindrome(s.substring(startIndex, i))) {
-                list.add(s.substring(startIndex, i));
-                backTracking(result, list, i, s);
+
+        for (int i = startIndex; i < s.length(); i++) {
+            if (isPalindrome(s, startIndex, i)) {
+                list.add(s.substring(startIndex, i + 1));
+                backTracking(i + 1, s, list, result);
                 list.remove(list.size() - 1);
             }
         }
     }
-    private boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
+
+    private boolean isPalindrome(String s, int left, int right) {
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
