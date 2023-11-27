d5d6ed8001d740f44d9562ce8c80e14e853555ad
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-27 12:47:30 -0800

    Time: 1 ms (80.82%) | Memory: 41.3 MB (40.48%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,3 +1,7 @@
+/**
+All permutations, start each level at zero, increase the digits index by 1 for each iteration
+digits[i] is a digit in the range ['2', '9']. So we can assume no '1' nor '0'
+*/
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
@@ -15,21 +19,21 @@ class Solution {
         map.put('9', "wxyz");
 
         StringBuilder sb = new StringBuilder();
-        backTracking(digits, sb, map, result, 0);
+        backTracking(digits, map, 0, result, sb);
         return result;
     }
-    private void backTracking(String digits, StringBuilder sb, Map<Character, String> map, 
-        List<String> result, int startIndex)
+    private void backTracking(String digits, Map<Character, String> map, int startIndex, 
+    List<String> result, StringBuilder sb)
     {
         if (startIndex == digits.length()) {
             result.add(sb.toString());
             return;
         }
 
-        String digitCharacter = map.get(digits.charAt(startIndex));
-        for (int i = 0; i < digitCharacter.length(); i++) {
-            sb.append(digitCharacter.charAt(i));
-            backTracking(digits, sb, map, result, startIndex + 1);
+        String numberRepresent = map.get(digits.charAt(startIndex));
+        for (int i = 0; i < numberRepresent.length(); i++) {
+            sb.append(numberRepresent.charAt(i));
+            backTracking(digits, map, startIndex + 1, result, sb);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
