04e16b233a6391cc5fa635a39f9642859861df1b
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-27 14:23:28 -0800

    Time: 8 ms (51.44%) | Memory: 42 MB (7.96%) - LeetSync

--- 768-partition-labels/partition-labels.java
+++ 768-partition-labels/partition-labels.java
@@ -2,28 +2,29 @@
 Form a window, we want to return the window size to a List<Integer>
     For each character in the window, we want to have the last appearance of the character
     Use a HashMap for this purpose, {character, last index}
-    Iterate the string, form a window, UPDATE the right pointer to ->
-        the Math.max(right pointer, the right-most index of character in the window)
+    Iterate the string, form a window, UPDATE the rightMost pointer to ->
+        the Math.max(rightMost pointer, the rightMost-most index of character in the window)
     Increase size of window and left pointer, while loop inside a while loop
 */
 class Solution {
     public List<Integer> partitionLabels(String s) {
         List<Integer> result = new ArrayList<>();
+
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             map.put(s.charAt(i), i);
         }
-
-        int left = 0, right = 0;
+        int left = 0, rightMost = 0;
+        
         while (left < s.length()) {
-            int step = 0;
-            right = Math.max(right, map.get(s.charAt(left)));
-            while (left <= right) {
-                right = Math.max(right, map.get(s.charAt(left)));
+            int size = 0;
+            rightMost = Math.max(rightMost, map.get(s.charAt(left)));
+            while (left <= rightMost) {
+                rightMost = Math.max(rightMost, map.get(s.charAt(left)));
                 left++;
-                step++;
+                size++;
             }
-            result.add(step);
+            result.add(size);
         }
         return result;
     }
