e25a5665b6c289c782e29c8d782e09f428e0db16
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-26 22:46:27 -0800

    Time: 1 ms (21.85%) | Memory: 44.7 MB (5.08%) - LeetSync

--- 333-largest-bst-subtree/largest-bst-subtree.java
+++ 333-largest-bst-subtree/largest-bst-subtree.java
@@ -1,37 +1,38 @@
 class Solution {
-	class Type {
+    class Type {
+        int min;
+        int max;
         boolean isBST;
-        int min, max, size;
-        public Type (int min, int max, int size, boolean isBST) {
+        int size;
+        public Type(int min, int max, boolean isBST, int size) {
             this.min = min;
             this.max = max;
-            this.size = size;
             this.isBST = isBST;
+            this.size = size;
         }
     }
-    int maxSize;
-	public int largestBSTSubtree(TreeNode root) {
-        int[] count = {0};
+    private int maxSize;
+    public int largestBSTSubtree(TreeNode root) {
         dfs(root);
         return maxSize;
     }
-
     private Type dfs(TreeNode root) {
-        if (root == null) return new Type(Integer.MAX_VALUE, Integer.MIN_VALUE, 0, true);
+        if (root == null) {
+            return new Type(Integer.MAX_VALUE, Integer.MIN_VALUE, true, 0);
+        }
         
         Type left = dfs(root.left);
         Type right = dfs(root.right);
 
+        int min = Math.min(root.val, Math.min(left.min, right.min));
+        int max = Math.max(root.val, Math.max(left.max, right.max));
         boolean isBST = left.isBST && right.isBST && ((root.val > left.max) && (root.val < right.min));
-
         int size = left.size + right.size + 1;
 
-        int min = Math.min(Math.min(left.min, right.min), root.val);
-        int max = Math.max(Math.max(left.max, right.max), root.val);
         if (isBST) {
             maxSize = Math.max(maxSize, size);
         }
 
-        return new Type(min, max, size, isBST);
+        return new Type(min, max, isBST, size);
     }
 }
