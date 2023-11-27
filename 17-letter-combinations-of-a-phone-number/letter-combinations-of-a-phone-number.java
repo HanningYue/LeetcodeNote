e01dc6df0a33943fd30170c61af0447b6e69f016
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-27 14:42:57 -0800

    Time: 1 ms (80.82%) | Memory: 41.4 MB (21.12%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -4,10 +4,6 @@ digits[i] is a digit in the range ['2', '9']. So we can assume no '1' nor '0'
 */
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-        if (digits == null || digits.length() == 0) {
-            return result;
-        }
         Map<Character, String> map = new HashMap<>();
         map.put('2', "abc");
         map.put('3', "def");
@@ -18,22 +14,24 @@ class Solution {
         map.put('8', "tuv");
         map.put('9', "wxyz");
 
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
         StringBuilder sb = new StringBuilder();
-        backTracking(digits, map, 0, result, sb);
+        backTracking(result, sb, 0, digits, map);
         return result;
     }
-    private void backTracking(String digits, Map<Character, String> map, int startIndex, 
-    List<String> result, StringBuilder sb)
-    {
+    private void backTracking(List<String> result, StringBuilder sb, int startIndex, String digits,Map<Character, String> map) {
         if (startIndex == digits.length()) {
             result.add(sb.toString());
             return;
         }
 
-        String numberRepresent = map.get(digits.charAt(startIndex));
-        for (int i = 0; i < numberRepresent.length(); i++) {
-            sb.append(numberRepresent.charAt(i));
-            backTracking(digits, map, startIndex + 1, result, sb);
+        String digitCharacters = map.get(digits.charAt(startIndex));
+        for (int i = 0; i < digitCharacters.length(); i++) {
+            sb.append(digitCharacters.charAt(i));
+            backTracking(result, sb, startIndex + 1, digits, map);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
