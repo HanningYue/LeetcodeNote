ed0e02953e9ffcc266ca3584282d0e66ec12860c
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-28 14:52:25 -0800

    Time: 3 ms (99.89%) | Memory: 44.9 MB (9.33%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,30 @@
+/**
+Create two int[], one for SORTED meeting start, one for SORTED meeting end
+If the second meeting starts before the first meeting ends, increase the room count
+    In code, this is start[current] < end[previousStart]
+    Increase the previous by 1, since it is sorted, we need to check the MAX ending time
+*/
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int n = intervals.length;
+
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
+        }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        
+        int rooms = 0, previousStart = 0;
+        for (int current = 0; current < n; current++) {
+            if (starts[current] < ends[previousStart]) {
+                rooms++;
+            } else {
+                previousStart++;
+            }
+        }
+        return rooms;
+    }
+}
