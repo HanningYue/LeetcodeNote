4c3f48c1c661eaf2e9ad3f341b446af323dc7bc3
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-28 19:09:13 -0800

    Time: 1 ms (99.42%) | Memory: 45 MB (10.97%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -15,6 +15,7 @@ class Solution {
     public int jump(int[] nums) {
         int left = 0, right = 0;
         int steps = 0;
+
         while (right < nums.length - 1) {
             int furthest = 0;
             for (int i = left; i <= right; i++) {
