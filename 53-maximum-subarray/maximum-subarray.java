08ca8e5ff2c4cb0c9bd6dddafe413c75e4b4bd9d
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-28 19:23:29 -0800

    Time: 1 ms (100.00%) | Memory: 60.1 MB (10.02%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,19 +1,18 @@
 /**
 Greedy
 Add current num to runningSum each iteration
-Update each max during each iteration, reset runningSum to 0, when less than 0.
+Update each max during each iteration, before reset runningSum to 0, when less than 0.
 */
 class Solution {
     public int maxSubArray(int[] nums) {
-        int result = Integer.MIN_VALUE;
-        int runningSum = 0;
-        for (int num : nums) {    
+        int runningSum = 0, maxSum = Integer.MIN_VALUE;
+        for (int num : nums) {
             runningSum += num;
-            result = Math.max(result, runningSum);
+            maxSum = Math.max(maxSum, runningSum);
             if (runningSum < 0) {
                 runningSum = 0;
             }
         }
-        return result;
+        return maxSum;
     }
 }
