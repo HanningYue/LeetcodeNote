6554fc8c09a3b10aefb399b973ab2ed016326a1b
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-11-30 16:31:45 -0800

    Time: 28 ms (83.79%) | Memory: 63 MB (7.78%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,26 @@
+/*
+fixed size sliding window, size为k
+nums的长度为n, window的size为k， 每个window取一个当前最大值, 最后的array size是 n - k + 1
+需要用一个保存maximum的单调栈(存INDEX) 每一次对比进来的数字和末尾的数字
+用单调栈保持一个window的size (fast - slow), 如果当前超出了window size (fast - slow > 头部index)去掉头部的index
+如果当前刚好等于window size (fast - slow + 1 >= 0) 加入头部的value到result
+*/
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] result = new int[nums.length - k + 1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offer(i);
+            while (i - k >= deque.peekFirst()) {
+                deque.pollFirst();
+            }
+            if (i - k + 1 >= 0) {
+                result[i - k + 1] = nums[deque.peekFirst()];
+            }
+        }
+        return result;
+    }
+}
