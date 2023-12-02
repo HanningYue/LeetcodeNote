bd0a4ceaceeaa48eacf1ee2f1ed0d13474787556
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-01 18:21:11 -0800

    Time: 7 ms (51.01%) | Memory: 43.9 MB (17.52%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -8,6 +8,7 @@ class Solution {
             for (int j = 0; j < i; j++) { 
                 if (dp[j] && set.contains(s.substring(j, i))) {
                     dp[i] = true;
+                    break;
                 }
             }
         }
