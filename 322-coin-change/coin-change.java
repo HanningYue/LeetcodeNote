58e0f998ca2638a6d94aa90a3616bcf49263a874
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-01 18:32:19 -0800

    Time: 14 ms (85.98%) | Memory: 43.8 MB (25.07%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,27 +1,16 @@
 class Solution {
-    private int[] memo;
     public int coinChange(int[] coins, int amount) {
-        memo = new int[amount + 1];
-        Arrays.fill(memo, -100);
-        return dp(coins, amount);
-    }
-    private int dp(int[] coins, int amount) {
-        if (amount == 0) {
-            return 0;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
         }
-        if (amount < 0) {
-            return -1;
-        }
-        if (memo[amount] != -100) {
-            return memo[amount];
-        }
-        int result = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            int subProblem = dp(coins, amount - coin);
-            if (subProblem == -1) continue;
-            result = Math.min(result, subProblem + 1);
-        }
-        memo[amount] = (result == Integer.MAX_VALUE ? -1 : result);
-        return memo[amount];
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 }
