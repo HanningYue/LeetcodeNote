328adeb9e0534fb3505cb3906ebbb6551e744e38
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-02 22:57:49 -0800

    Time: 3 ms (95.18%) | Memory: 44.4 MB (33.68%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -6,39 +6,42 @@ VISITING VS. VISITED
 3. When dfs traversing, return a boolean value to keep track of the current state of cycle detection, if there is a cycle, return false
 */
 class Solution {
+    boolean[] onPath;
+    boolean[] visited;
+    boolean cycle = false;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        boolean[] visited = new boolean[numCourses];
-        boolean[] visiting = new boolean[numCourses];
-        
-        Map<Integer, Set<Integer>> map = new HashMap<>(); //Map Init
-        for (int[] pre : prerequisites) {
-            map.putIfAbsent(pre[0], new HashSet<>());
-            map.get(pre[0]).add(pre[1]);
+        List<Integer>[] graph = buildGraph(numCourses, prerequisites);
+        visited = new boolean[numCourses];
+        onPath = new boolean[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            traverse(graph, i);
         }
-
-        for (int course = 0; course < numCourses; course++) {
-            if (!dfs(visiting, visited, course, map)) {
-                return false;
-            }
-        }
-        return true;
+        return !cycle;
     }
-
-    private boolean dfs(boolean[] visiting, boolean[] visited, int course, Map<Integer, Set<Integer>> map) {
-        if (!map.containsKey(course) || visited[course]) {
-            return true;
+    private void traverse(List<Integer>[] graph, int vertex) {
+        if (onPath[vertex]) {
+            cycle = true;
         }
-        if (visiting[course]) {
-            return false;
+        if (visited[vertex] || cycle) {
+            return;
         }
-        visiting[course] = true;
-        for (int neighbor : map.get(course)) {
-            if (!dfs(visiting, visited, neighbor, map)) {
-                return false;
-            }
+        visited[vertex] = true;
+        onPath[vertex] = true;
+        for (int neighbor : graph[vertex]) {
+            traverse(graph, neighbor);
         }
-        visiting[course] = false;
-        visited[course] = true;
-        return true;
+        onPath[vertex] = false;
+    }
+    private List<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graph = new LinkedList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new LinkedList<>();
+        }
+        for (int[] edge : prerequisites) {
+            int from = edge[1];
+            int to = edge[0];
+            graph[from].add(to);
+        }
+        return graph;
     }
 }
