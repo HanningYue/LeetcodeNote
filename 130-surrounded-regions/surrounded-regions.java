d994feb24b8c5c112801f1869223b15520307487
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-03 17:24:09 -0800

    Time: 1 ms (99.82%) | Memory: 44.1 MB (40.58%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,6 +1,7 @@
 class Solution {
     public void solve(char[][] board) {
-        int rows = board.length, columns = board[0].length;
+        int rows = board.length;
+        int columns = board[0].length;
         for (int i = 0; i < rows; i++) {
             if (board[i][0] == 'O') {
                 dfs(board, i, 0);
@@ -9,7 +10,6 @@ class Solution {
                 dfs(board, i, columns - 1);
             }
         }
-
         for (int j = 0; j < columns; j++) {
             if (board[0][j] == 'O') {
                 dfs(board, 0, j);
@@ -18,7 +18,6 @@ class Solution {
                 dfs(board, rows - 1, j);
             }
         }
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
                 if (board[i][j] == 'O') {
@@ -26,10 +25,11 @@ class Solution {
                 }
                 if (board[i][j] == '#') {
                     board[i][j] = 'O';
-                } 
+                }
             }
         }
     }
+
     private void dfs(char[][] board, int row, int column) {
         if (row < 0 || row >= board.length || column < 0 || column >= board[0].length || board[row][column] != 'O') {
             return;
@@ -37,7 +37,7 @@ class Solution {
         board[row][column] = '#';
         dfs(board, row + 1, column);
         dfs(board, row - 1, column);
-        dfs(board, row, column - 1);
         dfs(board, row, column + 1);
+        dfs(board, row, column - 1);
     }
 }
