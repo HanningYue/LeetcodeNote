fc73aac0741f5fe6581e43b1e08eb5f68bfac930
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-05 00:14:13 -0800

    Time: 33 ms (35.15%) | Memory: 49.2 MB (49.59%) - LeetSync

--- 1100-connecting-cities-with-minimum-cost/connecting-cities-with-minimum-cost.java
+++ 1100-connecting-cities-with-minimum-cost/connecting-cities-with-minimum-cost.java
@@ -1,54 +1,72 @@
 class Solution {
     public int minimumCost(int n, int[][] connections) {
-        UF uf = new UF(n + 1);
-        Arrays.sort(connections, (a, b) -> (a[2] - b[2]));
-        int mst = 0;
-
-        for (int[] connection : connections) {
-            int vertexOne = connection[0];
-            int vertexTwo = connection[1];
-            int weight = connection[2];
-
-            if (uf.connected(vertexOne, vertexTwo)) {
-                continue;
-            }
-            uf.union(vertexOne, vertexTwo);
-            mst += weight;
+        List<int[]>[] graph = buildGraph(n, connections);
+        Prim prim = new Prim(graph);
+        if (!prim.connected()) {
+            return -1;
         }
-        return uf.count() == 2 ? mst : -1;
+        return prim.weightSum();
+    }
+    public List<int[]>[] buildGraph(int n, int[][] connections) {
+        List<int[]>[] graph = new LinkedList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new LinkedList<>();
+        }
+        for (int[] connection : connections) {
+            int vertexOne = connection[0] - 1;
+            int vertexTwo = connection[1] - 1;
+            int weight= connection[2];
+            graph[vertexOne].add(new int[]{vertexOne, vertexTwo, weight});
+            graph[vertexTwo].add(new int[]{vertexTwo, vertexOne, weight});
+        }
+        return graph;
     }
 }
-class UF {
-    int count;
-    int[] parent;
-    public UF(int n) {
-        parent = new int[n];
-        this.count = n;
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+class Prim {
+    PriorityQueue<int[]> pq;
+    boolean[] inMST;
+    int weightSum = 0;
+    List<int[]>[] graph;
+    public Prim(List<int[]>[] graph) {
+        this.graph = graph;
+        this.pq = new PriorityQueue<>((a, b) -> {
+            return a[2] - b[2];
+        });
+        int n = graph.length;
+        this.inMST = new boolean[n];
+        inMST[0] = true;
+        cut(0);
+
+        while (!pq.isEmpty()) {
+            int[] edge = pq.poll();
+            int to = edge[1];
+            int weight = edge[2];
+            if (inMST[to]) {
+                continue;
+            }
+            weightSum += weight;
+            inMST[to] = true;
+            cut(to);
         }
     }
-    public int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);
+    public void cut(int vertex) {
+        for (int[] edge : graph[vertex]) {
+            int to = edge[1];
+            if (inMST[to]) {
+                continue;
+            }
+            pq.offer(edge);
         }
-        return parent[x];
     }
-    public void union(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        if (rootP == rootQ) {
-            return;
+    public int weightSum() {
+        return weightSum;
+    }
+    public boolean connected() {
+        for (int i = 0; i < inMST.length; i++) {
+            if (!inMST[i]) {
+                return false;
+            }
         }
-        parent[rootP] = rootQ;
-        count--;
-    }
-    public boolean connected(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        return rootP == rootQ;
-    }
-    public int count() {
-        return count;
+        return true;
     }
 }
