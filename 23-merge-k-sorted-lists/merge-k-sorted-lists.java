d804b8ba1e628afeb65c55f6409999563d2da169
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-08 15:01:10 -0800

    Time: 4 ms (72.94%) | Memory: 43.4 MB (53.94%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,39 +1,34 @@
 /**
-Divide and conquer
-1. Divide the lists array into sub-array until each array contain only one list
-    Base case is when low == high, return the smallest element in each sub-array
-2. Compare the ListNode of each array and merge them together
-*/
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        return divide(lists, 0, lists.length - 1);
-    }
-    private ListNode divide(ListNode[] lists, int low, int high) {
-        if (low > high) {
-            return null;
-        }
-        if (low == high) {
-            return lists[low];
-        }
-        int mid = low + (high - low) / 2;
-        ListNode left = divide(lists, low, mid);
-        ListNode right = divide(lists, mid + 1, high);
-        return merge(left, right);
-    }
-    private ListNode merge(ListNode n1, ListNode n2) {
+        if (lists.length == 0) return null;
+
         ListNode dummy = new ListNode(-1);
         ListNode current = dummy;
-        while (n1 != null && n2 != null) {
-            if (n1.val > n2.val) {
-                current.next = n2;
-                n2 = n2.next;
-            } else {
-                current.next = n1;
-                n1 = n1.next;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, ((a, b) -> (a.val - b.val)));
+
+        for (ListNode head : lists) {
+            if (head != null) {
+                pq.offer(head);
+            }
+        }
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            current.next = node;
+            if (node.next != null) {
+                pq.offer(node.next);
             }
             current = current.next;
         }
-        current.next = (n1 == null) ? n2 : n1;
         return dummy.next;
     }
 }
