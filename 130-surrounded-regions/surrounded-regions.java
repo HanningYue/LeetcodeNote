c59fe61b67f16049160149c71dea3a932c5d6439
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-07 17:11:35 -0800

    Time: 1 ms (99.78%) | Memory: 44 MB (50.90%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,43 +1,44 @@
 class Solution {
     public void solve(char[][] board) {
-      int rows = board.length;
-      int columns = board[0].length;
-      for (int i = 0; i < rows; i++) {
-        if (board[i][0] == 'O') {
-          dfs(board, i, 0);
+        int rows = board.length, cols = board[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            if (board[i][0] == 'O') {
+                dfs(board, i, 0, visited);
+            }
+            if (board[i][cols - 1] == 'O') {
+                dfs(board, i, cols - 1, visited);
+            }
         }
-        if (board[i][columns - 1] == 'O') {
-          dfs(board, i, columns - 1);
+        for (int j = 0; j < cols; j++) {
+            if (board[0][j] == 'O') {
+                dfs(board, 0, j, visited);
+            }
+            if (board[rows - 1][j] == 'O') {
+                dfs(board, rows - 1, j, visited);
+            }
         }
-      }
-      for (int j = 0; j < columns; j++) {
-        if (board[0][j] == 'O') {
-          dfs(board, 0, j);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
         }
-        if (board[rows - 1][j] == 'O') {
-          dfs(board, rows - 1, j);
-        }
-      }
-      for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-          if (board[i][j] == 'O') {
-            board[i][j] = 'X';
-          }
-          if (board[i][j] == '#') {
-            board[i][j] = 'O';
-          }
-        }
-      }
     }
-    private void dfs(char[][] board, int row, int col) {
-      if (row < 0 || row >= board.length || col < 0 || col >= board[0].length 
-      || board[row][col] != 'O') {
-        return;
-      }
-      board[row][col] = '#';
-      dfs(board, row + 1, col);
-      dfs(board, row - 1, col);
-      dfs(board, row, col + 1);
-      dfs(board, row, col - 1);
+    private void dfs(char[][] board, int i, int j, boolean[][] visited) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length 
+        || visited[i][j] || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = '#';
+        visited[i][j] = true;
+        dfs(board, i + 1, j, visited);
+        dfs(board, i - 1, j, visited);
+        dfs(board, i, j + 1, visited);
+        dfs(board, i, j - 1, visited);
     }
 }
