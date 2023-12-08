49830efdc0fcc6817642fd9e52d0094351640695
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-07 17:32:03 -0800

    Time: 8 ms (78.06%) | Memory: 47.4 MB (22.14%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.java
+++ 286-walls-and-gates/walls-and-gates.java
@@ -14,16 +14,18 @@ class Solution {
 
         int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!queue.isEmpty()) {
-            int[] gate = queue.poll();
-            for (int[] dir : directions) {
-                int row = gate[0], col = gate[1];
-                int x = row + dir[0], y = col + dir[1];
-
-                if (x >= 0 && x < rooms.length && y >= 0 && y < rooms[0].length && rooms[x][y] == Integer.MAX_VALUE) {
-                    rooms[x][y] = rooms[row][col] + 1;
-                    queue.offer(new int[]{x, y});
-                }
-            }            
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] gate = queue.poll();
+                for (int[] dir : directions) {
+                    int row = gate[0], col = gate[1];
+                    int x = row + dir[0], y = col + dir[1];
+                    if (x >= 0 && x < rooms.length && y >= 0 && y < rooms[0].length && rooms[x][y] == Integer.MAX_VALUE) {
+                        rooms[x][y] = rooms[row][col] + 1;
+                        queue.offer(new int[]{x, y});
+                    }
+                }   
+            }         
         }
     }
 }
