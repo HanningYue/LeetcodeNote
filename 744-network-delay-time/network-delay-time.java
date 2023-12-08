20e656ff0ee1d1fe2203cd06c746a19ff6d36897
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-07 23:44:47 -0800

    Time: 13 ms (72.28%) | Memory: 46.6 MB (67.44%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -5,7 +5,7 @@
 class State {
     int id;
     int distFromStart;
-    State(int id, int distFromStart) {
+    public State(int id, int distFromStart) {
         this.id = id;
         this.distFromStart = distFromStart;
     }
@@ -13,7 +13,7 @@ class State {
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         List<int[]>[] graph = new LinkedList[n + 1];
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             graph[i] = new LinkedList<>();
         }
         for (int[] time : times) {
@@ -32,13 +32,10 @@ class Solution {
             result = Math.max(result, distTo[i]);
         }
         return result;
-        
     }
-
-    public int[] dijkstra(int start, List<int[]>[] graph) {
+    private int[] dijkstra(int start, List<int[]>[] graph) {
         int[] distTo = new int[graph.length];
         Arrays.fill(distTo, Integer.MAX_VALUE);
-
         distTo[start] = 0;
         PriorityQueue<State> pq = new PriorityQueue<>((a, b) -> {
             return a.distFromStart - b.distFromStart;
@@ -48,17 +45,16 @@ class Solution {
         while (!pq.isEmpty()) {
             State currentState = pq.poll();
             int currentNode = currentState.id;
-            int currentDistFromStart = currentState.distFromStart;
-            
-            if (currentDistFromStart > distTo[currentNode]) {
+            int currentDist = currentState.distFromStart;
+            if (currentDist > distTo[currentNode]) {
                 continue;
             }
             for (int[] neighbor : graph[currentNode]) {
                 int nextNode = neighbor[0];
-                int distToNextNode = distTo[currentNode] + neighbor[1];
-                if (distTo[nextNode] > distToNextNode) {
-                    distTo[nextNode] = distToNextNode;
-                    pq.offer(new State(nextNode, distToNextNode));
+                int nextDist = distTo[currentNode] + neighbor[1];
+                if (distTo[nextNode] > nextDist) {
+                    distTo[nextNode] = nextDist;
+                    pq.offer(new State(nextNode, nextDist));
                 }
             }
         }
