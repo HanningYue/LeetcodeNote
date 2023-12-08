80bba0cd5920d6ef0777afd70ae6fc1e3bb5e8aa
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-07 17:28:24 -0800

    Time: 9 ms (58.12%) | Memory: 47.6 MB (20.09%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.java
+++ 286-walls-and-gates/walls-and-gates.java
@@ -1,17 +1,9 @@
 /**
-BFS, same as rotting oranges
-Want to fill each empty room with the nearest distance to any arbitrary gate
-    Start with GATE, iterate the grid, add all the GATE int[]   
-    Poll any gate int[]
-    Declear four directions, with rows = gate[0] + direction[0]
-    Check Boundaries and Whether the cell is an Empty Room
-    If satisfied, skip the iteration
-    If not satisfied, offer the new cell to Queue, mark the gate cell to be the previous cell + 1
-    (rooms[x][y] = rooms[rows][columns] + 1)
+BFS, same as rotten orange, but we add gate to queue
 */
 class Solution {
     public void wallsAndGates(int[][] rooms) {
-        Queue<int[]> queue = new ArrayDeque<>();
+        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < rooms.length; i++) {
             for (int j = 0; j < rooms[0].length; j++) {
                 if (rooms[i][j] == 0) {
@@ -23,16 +15,15 @@ class Solution {
         int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!queue.isEmpty()) {
             int[] gate = queue.poll();
-            int rows = gate[0], columns = gate[1];
-            for (int[] direction : directions) {
-                int x = rows + direction[0], y = columns + direction[1];
-                if (x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length 
-                || rooms[x][y] != Integer.MAX_VALUE) {
-                    continue;
+            for (int[] dir : directions) {
+                int row = gate[0], col = gate[1];
+                int x = row + dir[0], y = col + dir[1];
+
+                if (x >= 0 && x < rooms.length && y >= 0 && y < rooms[0].length && rooms[x][y] == Integer.MAX_VALUE) {
+                    rooms[x][y] = rooms[row][col] + 1;
+                    queue.offer(new int[]{x, y});
                 }
-                queue.offer(new int[]{x, y});
-                rooms[x][y] = rooms[rows][columns] + 1;
-            }
+            }            
         }
     }
 }
