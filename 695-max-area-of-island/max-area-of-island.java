980b796c788837f15810be07fff57e7f3c8d1c81
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-07 17:00:25 -0800

    Time: 2 ms (72.86%) | Memory: 43.7 MB (26.42%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,27 +1,28 @@
 class Solution {
-  public int maxAreaOfIsland(int[][] grid) {
-    int maxArea = 0;
-    boolean[][] visited = new boolean[grid.length][grid[0].length];
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[0].length; j++) {
-        if (!visited[i][j] && grid[i][j] == 1) {
-          maxArea = Math.max(maxArea, dfs(grid, i, j, visited));
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (!visited[i][j] && grid[i][j] != 0) {
+                    maxArea = Math.max(maxArea, dfs(grid, i, j, visited));
+                }        
+            }
         }
-      }
+        return maxArea;
     }
-    return maxArea;
-  }
-  private int dfs(int[][] grid, int i, int j, boolean[][] visited) {
-    if (i < 0 || j < 0 || i >= grid.length || j >= grid[i].length 
-    || visited[i][j] || grid[i][j] == 0) {
-      return 0;
+
+    private int dfs(int[][] grid, int i, int j, boolean[][] visited) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || visited[i][j] || grid[i][j] != 1) {
+            return 0;
+        }
+        int area = 1;
+        visited[i][j] = true;
+        area += dfs(grid, i + 1, j, visited);
+        area += dfs(grid, i - 1, j, visited);
+        area += dfs(grid, i, j + 1, visited);
+        area += dfs(grid, i, j - 1, visited);
+        return area;
     }
-    int area = 1;
-    visited[i][j] = true;
-    area += dfs(grid, i + 1, j, visited);
-    area += dfs(grid, i - 1, j, visited);
-    area += dfs(grid, i, j + 1, visited);
-    area += dfs(grid, i, j - 1, visited);
-    return area;
-  }
 }
