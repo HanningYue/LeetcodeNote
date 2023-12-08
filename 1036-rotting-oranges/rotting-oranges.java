ed1a07241054ee4b1d736545bda4f445e7c1dbd6
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-07 16:28:22 -0800

    Time: 2 ms (84.46%) | Memory: 41.9 MB (17.58%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -8,9 +8,9 @@ class Solution {
         int fresh = 0;
         Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 2) {
-                    queue.offer(new int[] {i, j});
+                    queue.offer(new int[]{i, j});
                 } else if (grid[i][j] == 1) {
                     fresh++;
                 }
@@ -29,8 +29,8 @@ class Solution {
                     int y = rotten[1] + dir[1];
                     if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1) {
                         grid[x][y] = 2;
-                        fresh--;
                         queue.offer(new int[]{x, y});
+                        fresh--;
                     }
                 }
             }
