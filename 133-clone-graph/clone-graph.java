12a2216a31bc25ac50f0b622b2f179babcb5c9b7
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-07 16:39:48 -0800

    Time: 27 ms (46.80%) | Memory: 42.4 MB (8.40%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -1,23 +1,39 @@
 /*
-1. Each node has a list of neighbors, we need to perform dfs1 on the neighbors of each node
-2. Add copys of neighbor to the list of current copy (current level)
-3. Using a hashmap checking if the current node is already visited
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
 */
+
 class Solution {
-    Map<Node, Node> map = new HashMap<>();
+    Map<Node, Node> graph = new HashMap<>();
 
     public Node cloneGraph(Node node) {
         if (node == null) {
             return null;
-        }
-        if (map.containsKey(node)) {
-            return map.get(node);
+        }    
+        if (graph.containsKey(node)) {
+            return graph.get(node);
         }
 
         Node copy = new Node(node.val);
-        map.put(node, copy);
+        graph.put(node, copy);
         for (Node neighbor : node.neighbors) {
-            copy.neighbors.add(cloneGraph(neighbor));
+            Node copyNeighbor = cloneGraph(neighbor);
+            copy.neighbors.add(copyNeighbor);
         }
         return copy;
     }
