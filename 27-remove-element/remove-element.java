7dbf304f1ebbb7e97943f4a458b6c4f08e2d84ba
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-12 14:37:48 -0800

    Time: 0 ms (100.00%) | Memory: 41.6 MB (8.59%) - LeetSync

---
+++ 27-remove-element/remove-element.java
@@ -0,0 +1,16 @@
+/**
+快慢指针，遇到val就跳过
+*/
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;           
+    }
+}
