4e1c6538ab3c90b1e9aee06cc97b4020a7be1057
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-12 13:55:34 -0800

    Time: 10 ms (44.69%) | Memory: 46.9 MB (46.22%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.java
+++ 286-walls-and-gates/walls-and-gates.java
@@ -5,27 +5,33 @@ class Solution {
     public void wallsAndGates(int[][] rooms) {
         Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < rooms.length; i++) {
-            for (int j = 0; j < rooms[0].length; j++) {
+            for (int j = 0; j < rooms[i].length; j++) {
                 if (rooms[i][j] == 0) {
                     queue.offer(new int[]{i, j});
                 }
             }
         }
 
-        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        boolean[][] visited = new boolean[rooms.length][rooms[0].length];
+        int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 int[] gate = queue.poll();
-                for (int[] dir : directions) {
+                for (int[] dir : dirs) {
                     int row = gate[0], col = gate[1];
-                    int x = row + dir[0], y = col + dir[1];
-                    if (x >= 0 && x < rooms.length && y >= 0 && y < rooms[0].length && rooms[x][y] == Integer.MAX_VALUE) {
-                        rooms[x][y] = rooms[row][col] + 1;
-                        queue.offer(new int[]{x, y});
+                    int x = row + dir[0];
+                    int y = col + dir[1];
+
+                    if (x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length 
+                    || rooms[x][y] != Integer.MAX_VALUE || visited[x][y]) {
+                        continue;
                     }
-                }   
-            }         
+                    visited[x][y] = true;
+                    rooms[x][y] = rooms[row][col] + 1;
+                    queue.offer(new int[]{x, y});
+                }
+            }
         }
     }
 }
