5a1c9852a41e3f770e0ad3a3cf2e58ccacca4fee
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-13 23:56:01 -0800

    Time: 0 ms (100.00%) | Memory: 51 MB (36.12%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,15 @@
+/**
+双向指针 一前一后
+*/
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while (left <= right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
