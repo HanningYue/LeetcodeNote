0dc9cbfa4c40fc37efffaa4d1e58d26f15027187
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-16 00:03:52 -0800

    Time: 20 ms (56.95%) | Memory: 44.3 MB (30.38%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,19 +1,19 @@
 /**
-从中间 向两边，substring是exclusive 所以right不用加1 
+主方程 比长度
+helper方程 从中间 向两边，substring是exclusive 所以right不用加1 
 */
 class Solution {
     public String longestPalindrome(String s) {
         String result = "";
-
         for (int i = 0; i < s.length(); i++) {
-            String even = helper(s, i, i + 1);
-            String odd = helper(s, i, i);
+            String even = palindrome(s, i , i + 1);
+            String odd = palindrome(s, i, i);
             result = even.length() > result.length() ? even : result;
             result = odd.length() > result.length() ? odd : result;
         }
         return result;
     }
-    private String helper(String s, int left, int right) {
+    private String palindrome(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
