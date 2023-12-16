a74a117a00ff53001e37c943ec3bdc361739a825
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-15 23:14:51 -0800

    Time: 0 ms (100.00%) | Memory: 41.4 MB (44.16%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -1,8 +1,13 @@
 /**
-1. We need to use two ListNodes, prev and next to record the head ListNode pointer
-2. we need to change the head pointer next reference to null, the prev reference to next
-3. Repeat this 
-*/
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
