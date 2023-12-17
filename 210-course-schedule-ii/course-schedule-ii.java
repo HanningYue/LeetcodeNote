4fb43036bc13ee8b3696afcdbed9916703f1328b
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-17 04:26:39 -0800

    Time: 4 ms (91.83%) | Memory: 45.3 MB (10.58%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,45 +1,10 @@
-/**
-在第一题的基础上加入一个list来存拓扑序
-要reverse因为我们构图的方式是从前到后
-*/
 class Solution {
-    boolean[] onPath, visited;
-    boolean cycle = false;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<Integer> order = new ArrayList<>();
-        List<Integer>[] graph = buildGraph(numCourses, prerequisites);
-        onPath = new boolean[numCourses];
-        visited = new boolean[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            traverse(graph, i, order);
-        }
-        if (cycle) {
-            return new int[]{};
-        }
-        Collections.reverse(order);
-        int[] result = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            result[i] = order.get(i);
-        }
-        return result;
-    }
-    private void traverse(List<Integer>[] graph, int vertex, List<Integer> order) {
-        if (onPath[vertex]) {
-            cycle = true;
-        }
-        if (cycle || visited[vertex]) {
-            return;
-        }
-        visited[vertex] = true;
-        onPath[vertex] = true;
-        for (int neighbor : graph[vertex]) {
-            traverse(graph, neighbor, order);
-        }
-        order.add(vertex);
-        onPath[vertex] = false;
-    }
-    private List<Integer>[] buildGraph(int numCourses, int[][] prerequisites) {
+        boolean[] visiting = new boolean[numCourses];
+        boolean[] visited = new boolean[numCourses];
         List<Integer>[] graph = new LinkedList[numCourses];
+        List<Integer> order = new ArrayList<>();
+
         for (int i = 0; i < numCourses; i++) {
             graph[i] = new LinkedList<>();
         }
@@ -48,6 +13,36 @@ class Solution {
             int to = pre[0];
             graph[from].add(to);
         }
-        return graph;
+        for (int i = 0; i < numCourses; i++) {
+            if (!traverse(i, visiting, visited, order, graph)) {
+                return new int[]{};
+            }
+        }
+
+        int[] result = new int[numCourses];
+        Collections.reverse(order);
+        for (int i = 0; i < numCourses; i++) {
+            result[i] = order.get(i);
+        }
+        return result;
+    }
+    private boolean traverse(int vertex, boolean[] visiting, boolean[] visited,
+    List<Integer> order, List<Integer>[] graph) {
+        if (visited[vertex]) {
+            return true;
+        }
+        if (visiting[vertex]) {
+            return false;
+        }
+        visiting[vertex] = true;
+        for (int neighbor : graph[vertex]) {
+            if (!traverse(neighbor, visiting, visited, order, graph)) {
+                return false;
+            }
+        }
+        visiting[vertex] = false;
+        visited[vertex] = true;
+        order.add(vertex);
+        return true;
     }
 }
