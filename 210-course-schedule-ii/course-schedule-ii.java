fad1b8c73d373dd2331d3824bc7645ae7ca615a4
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2023-12-31 06:46:17 -0800

    Time: 4 ms (91.59%) | Memory: 45.6 MB (13.84%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -3,36 +3,35 @@ class Solution {
         boolean[] visiting = new boolean[numCourses];
         boolean[] visited = new boolean[numCourses];
         List<Integer>[] graph = new LinkedList[numCourses];
-        List<Integer> order = new ArrayList<>();
-
         for (int i = 0; i < numCourses; i++) {
             graph[i] = new LinkedList<>();
-        }
+        }       
         for (int[] pre : prerequisites) {
             int from = pre[1];
             int to = pre[0];
             graph[from].add(to);
         }
+
+        List<Integer> order = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
             if (!traverse(i, visiting, visited, order, graph)) {
                 return new int[]{};
             }
         }
-
         int[] result = new int[numCourses];
         Collections.reverse(order);
-        for (int i = 0; i < numCourses; i++) {
+        for (int i = 0; i < order.size(); i++) {
             result[i] = order.get(i);
         }
         return result;
     }
-    private boolean traverse(int vertex, boolean[] visiting, boolean[] visited,
+    private boolean traverse(int vertex, boolean[] visiting, boolean[] visited, 
     List<Integer> order, List<Integer>[] graph) {
-        if (visited[vertex]) {
-            return true;
-        }
         if (visiting[vertex]) {
             return false;
+        }
+        if (visited[vertex]) {
+            return true;
         }
         visiting[vertex] = true;
         for (int neighbor : graph[vertex]) {
