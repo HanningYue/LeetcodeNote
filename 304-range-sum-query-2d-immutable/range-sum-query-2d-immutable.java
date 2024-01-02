acfa37a451b654da7b05aeba603b5872e326b890
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-01 19:48:03 -0800

    Time: 103 ms (41.15%) | Memory: 71 MB (6.31%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,16 +1,16 @@
 class NumMatrix {
-    int[][] preSum;
+    private int[][] preSum;
     public NumMatrix(int[][] matrix) {
         preSum = new int[matrix.length + 1][matrix[0].length + 1];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                preSum[i + 1][j + 1] = preSum[i][j + 1] + preSum[i + 1][j] + matrix[i][j] - preSum[i][j];
+                preSum[i + 1][j + 1] = preSum[i][j + 1] + preSum[i + 1][j] - preSum[i][j] + matrix[i][j];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return preSum[row2 + 1][col2 + 1] - preSum[row1][col2 + 1] - preSum[row2 + 1][col1] + preSum[row1][col1];
+        return preSum[row2 + 1][col2 + 1] - preSum[row2 + 1][col1] - preSum[row1][col2 + 1] + preSum[row1][col1];
     }
 }
 
