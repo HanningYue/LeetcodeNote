bd786622b74e55977806c13704a749db269cce4c
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-02 21:30:59 -0800

    Time: 5 ms (50.44%) | Memory: 42.7 MB (62.45%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,14 +1,20 @@
 public class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-
-        Deque<Character> deque = new LinkedList<>();
-        for (char c : s.toCharArray()) {
-            deque.addFirst(c);
-        }
-        while (deque.size() > 1) {
-            if (!deque.pollFirst().equals(deque.pollLast())) {
-                return false;
+        int left = 0, right = s.length() - 1;
+        s = s.toUpperCase();
+        while (left < right) {
+            char leftC = s.charAt(left);
+            char rightC = s.charAt(right);
+            if (!Character.isLetterOrDigit(leftC)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightC)) {
+                right--;
+            } else {
+                if (leftC != rightC) {
+                    return false;
+                }
+                left++;
+                right--;
             }
         }
         return true;
