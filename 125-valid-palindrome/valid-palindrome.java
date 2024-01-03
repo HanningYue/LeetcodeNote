2ad8aa2f7e40ffef8df7024638661756c31cb6b3
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-02 21:22:04 -0800

    Time: 29 ms (15.52%) | Memory: 45.2 MB (9.29%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,12 @@
 public class Solution {
     public boolean isPalindrome(String s) {
-        // Remove all non-alphanumeric characters and convert to lowercase
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        // Create a deque (double-ended queue) from the string
+
         Deque<Character> deque = new LinkedList<>();
         for (char c : s.toCharArray()) {
-            deque.addLast(c);
+            deque.addFirst(c);
         }
-
-        // Continue until there is 0 or 1 character left
         while (deque.size() > 1) {
-            // Remove and compare characters from both ends
             if (!deque.pollFirst().equals(deque.pollLast())) {
                 return false;
             }
