949ccb34c8055089c27889311a57b898576581a8
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-03 22:17:35 -0800

    Time: 1 ms (44.24%) | Memory: 44.8 MB (11.61%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -1,20 +1,29 @@
 /**
-1. We want the kth smallest TreeNode in a Binary Search Tree. Since it is a BST, the left most TreeNode is the smallest
-2. Follow the above property, use recursion inorder, save the tree into a list of Integer
-3. Return list.get(k - 1) since the kth is 1-indexed
-*/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    List<Integer> list = new ArrayList<>();
     public int kthSmallest(TreeNode root, int k) {
-        List<Integer> result = new ArrayList<>();
-        inOrder(root, result);
-        return result.get(k - 1);
+        inorder(root);
+        return list.get(k - 1);
     }
-    private void inOrder(TreeNode root, List<Integer> result) {
-        if (root == null) {
-            return;
+    private void inorder(TreeNode root) {
+        if (root != null) {
+            inorder(root.left);
+            list.add(root.val);
+            inorder(root.right);
         }
-        inOrder(root.left, result);
-        result.add(root.val);
-        inOrder(root.right, result);
     }
 }
