9f18b0a4b3ac054cc6aeea762d57acb5d11d89f3
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-06 13:55:29 -0800

    Time: 0 ms (100.00%) | Memory: 41.8 MB (31.59%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,23 +1,14 @@
-/*
-In Binary Search, the low and high are usually index-boundary, not value boundary
-In this proble, we starts at TOP-RIGHT, let row = 0 and column = matrix[0].length - 1
-    Condition is while row in matrix.length boundary and column >= 0
-    For each value, START at top-right
-        if smaller than target -> increase row number
-        if bigger than target -> decrease column number
-*/
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = 0;
-        int column = matrix[0].length - 1;
-        while (row < matrix.length && column >= 0) {
-            if (matrix[row][column] == target) {
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
                 return true;
-            } else if (matrix[row][column] > target){
-                column--;
-            } else {
+            } else if (matrix[row][col] < target) {
                 row++;
-            }   
+            } else if (matrix[row][col] > target) {
+                col--;
+            }
         }
         return false;
     }
