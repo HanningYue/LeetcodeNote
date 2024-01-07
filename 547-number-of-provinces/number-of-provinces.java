4dda9fed5b38b8451fc515517e78e403aa9adffb
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-06 22:23:49 -0800

    Time: 1 ms (95.39%) | Memory: 45.4 MB (89.84%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -6,8 +6,8 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                provinces++;
                 dfs(isConnected, visited, i);
+                provinces++;
             }
         }
         return provinces;
