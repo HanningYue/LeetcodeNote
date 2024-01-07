901da91f86f3dda13afabbad4917e030b947f978
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-07 11:24:14 -0800

    Time: 11 ms (49.32%) | Memory: 82.4 MB (48.50%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,14 +1,14 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
         List<Integer> result = new ArrayList<>();
-        int[] sourceNode = new int[n];
+        int[] source = new int[n];
         for (List<Integer> edge : edges) {
             int from = edge.get(0);
             int to = edge.get(1);
-            sourceNode[to]++;
+            source[to]++;
         }
-        for (int i = 0; i < sourceNode.length; i++) {
-            if (sourceNode[i] == 0) {
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == 0) {
                 result.add(i);
             }
         }
