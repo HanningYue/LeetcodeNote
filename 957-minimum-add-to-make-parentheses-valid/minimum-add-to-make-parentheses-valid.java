45d12d0468a0494972f07e937cd10a2954428359
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-07 21:24:41 -0800

    Time: 0 ms (100.00%) | Memory: 40.8 MB (44.86%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        if (s.isEmpty()) {
+            return 0;
+        }
+        int balance = 0;
+        int ifAllClose = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                balance++;
+            } else if (c == ')') {
+                balance--;
+            }
+            if (balance < 0) {
+                ifAllClose++;
+                balance = 0;
+            }
+        }
+        int sum = ifAllClose + balance;
+        return sum;
+    }
+}
