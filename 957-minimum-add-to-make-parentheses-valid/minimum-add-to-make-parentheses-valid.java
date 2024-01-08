de2eda98c93d96a05ac0a1e0cda0d70379fdf340
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-07 23:18:14 -0800

    Time: 0 ms (100.00%) | Memory: 41.3 MB (24.64%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,3 +1,6 @@
+/**
+Why ifAllClose? Think of this scenario: ))))))), we can add just one ( at the very front to make it valid. After that, we need to set the balance between opening and closing bracket to 0;
+*/
 class Solution {
     public int minAddToMakeValid(String s) {
         if (s.isEmpty()) {
