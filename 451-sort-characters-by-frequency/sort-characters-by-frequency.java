11d27a514cd71ab53716b1468f781be4528942c3
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-09 18:07:13 -0800

    Time: 18 ms (45.01%) | Memory: 45.5 MB (25.53%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -5,16 +5,16 @@ class Solution {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         PriorityQueue<Map.Entry<Character, Integer>> maxHeap 
-        = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
-
+            = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        
         for (Map.Entry<Character, Integer> entry : map.entrySet()) {
             maxHeap.offer(entry);
         }
         StringBuilder sb = new StringBuilder();
         while (!maxHeap.isEmpty()) {
-            Map.Entry<Character, Integer> maxFreq = maxHeap.poll();
-            for (int i = 0; i < maxFreq.getValue(); i++) {
-                sb.append(maxFreq.getKey());
+            Map.Entry<Character, Integer> mostFreq = maxHeap.poll();
+            for (int i = 0; i < mostFreq.getValue(); i++) {
+                sb.append(mostFreq.getKey());
             }
         }
         return sb.toString();
