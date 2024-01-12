aad25fd453a76d5a4fbf9945edb9ee83e92f0b39
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-11 19:58:05 -0800

    Time: 9 ms (90.60%) | Memory: 56.9 MB (27.45%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -16,28 +16,27 @@ class Solution {
         if (head.next == null) {
             return head;
         }
-        ListNode slow = head, fast = head, previous = null;
+        ListNode previous = null, slow = head, fast = head;
         while (fast != null && fast.next != null) {
             previous = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
         previous.next = null;
-        
-        ListNode firstHalf = sortList(head);
-        ListNode secondHalf = sortList(slow);
-        return merge(firstHalf, secondHalf);
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+        return merge(left, right);
     }
     private ListNode merge(ListNode p1, ListNode p2) {
         ListNode dummy = new ListNode(-1);
         ListNode current = dummy;
         while (p1 != null && p2 != null) {
-            if (p1.val > p2.val) {
-                current.next = p2;
-                p2 = p2.next;
-            } else {
+            if (p1.val < p2.val) {
                 current.next = p1;
                 p1 = p1.next;
+            } else {
+                current.next = p2;
+                p2 = p2.next;
             }
             current = current.next;
         }
