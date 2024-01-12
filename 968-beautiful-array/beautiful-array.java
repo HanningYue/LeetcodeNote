98c19f1716e95390cad50473fb93bbc56d7e3bda
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-11 20:51:02 -0800

    Time: 0 ms (100.00%) | Memory: 42.4 MB (27.20%) - LeetSync

--- 968-beautiful-array/beautiful-array.java
+++ 968-beautiful-array/beautiful-array.java
@@ -3,15 +3,14 @@ class Solution {
         if (n == 1) {
             return new int[]{1};
         }
+        int[] result = new int[n];
         int[] odd = beautifulArray((n + 1) / 2);
         int[] even = beautifulArray(n / 2);
-        int[] result = new int[n];
-
         for (int i = 0; i < odd.length; i++) {
-            result[i] = 2 * odd[i] - 1;
+            result[i] = odd[i] * 2 - 1;
         }
         for (int i = 0; i < even.length; i++) {
-            result[i + odd.length] = 2 * even[i];
+            result[i + odd.length] = even[i] * 2;
         }
         return result;
     }
