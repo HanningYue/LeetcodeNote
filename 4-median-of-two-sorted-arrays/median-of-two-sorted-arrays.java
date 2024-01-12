aa5f2461dd583c45d4c404e5d62ca59025fc2b94
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-11 19:43:38 -0800

    Time: 1 ms (100.00%) | Memory: 46.8 MB (5.03%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,22 +3,24 @@ class Solution {
         if (nums2.length > nums1.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
-
-        int pointer1 = 0, pointer2 = 0;
-        int current = 0, last = 0;
         int totalLength = nums1.length + nums2.length;
+        int pointer1 = 0, pointer2 = 0;
+        int lastElement = 0, currentElement = 0;
         boolean isEven = totalLength % 2 == 0;
- 
+
         for (int i = 0; i <= totalLength / 2; i++) {
-            last = current;
-            if (pointer2 >= nums2.length || (pointer1 < nums1.length && nums1[pointer1] < nums2[pointer2])) {
-                current = nums1[pointer1];
-                pointer1++;
+            lastElement = currentElement;
+            if (pointer1 == nums1.length) {
+                currentElement = nums2[pointer2];
+                pointer2++;
+            } else if (pointer2 == nums2.length || nums1[pointer1] < nums2[pointer2]) {
+                currentElement = nums1[pointer1];
+                pointer1++; 
             } else {
-                current = nums2[pointer2];
+                currentElement = nums2[pointer2];
                 pointer2++;
             }
         }
-        return isEven ? (last + current) / 2.0 : current;
+        return isEven ? (lastElement + currentElement) / 2.0 : currentElement;
     }
 }
