7309614c7c27458867de0136727f199154629007
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-13 13:48:45 -0800

    Time: 4 ms (67.44%) | Memory: 45.1 MB (5.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,9 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        int[] sChar = new int[26];
-        int[] tChar = new int[26];
-        for (char c : s.toCharArray()) {
-            sChar[c - 'a']++;
-        }
-        for (char c : t.toCharArray()) {
-            tChar[c - 'a']++;
-        }
-        return Arrays.equals(sChar, tChar);
-    }
+  public boolean isAnagram(String s, String t) {
+    char[] sArr = s.toCharArray();
+    char[] tArr = t.toCharArray();
+    Arrays.sort(sArr);
+    Arrays.sort(tArr);
+    return Arrays.equals(sArr, tArr);
+  }
 }
