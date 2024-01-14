858785dfc404927864acfdbdd664f5a4cfcd0413
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-13 21:04:56 -0800

    Time: 13 ms (28.67%) | Memory: 45.3 MB (5.72%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,9 +1,17 @@
 class Solution {
-  public boolean isAnagram(String s, String t) {
-    char[] sArr = s.toCharArray();
-    char[] tArr = t.toCharArray();
-    Arrays.sort(sArr);
-    Arrays.sort(tArr);
-    return Arrays.equals(sArr, tArr);
-  }
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+        for (char key : map.keySet()) {
+            if (map.get(key) != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
