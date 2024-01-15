d9f406cfd3e414d250e280eedc39739e47c0030b
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-14 16:12:22 -0800

    Time: 3 ms (62.99%) | Memory: 50.4 MB (24.83%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,14 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }   
-        for (int key : map.keySet()) {
-            if (map.get(key) > nums.length / 2) {
-                return key;
+        return divide(nums, 0, nums.length - 1);
+    }
+    private int divide(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
+        }
+        int mid = left + (right - left) / 2;
+        int leftHalf = divide(nums, left, mid);
+        int rightHalf = divide(nums, mid + 1, right);
+        int leftMax = conquer(nums, leftHalf, left, right);
+        int rightMax = conquer(nums, rightHalf, left, right);
+        return leftMax > rightMax ? leftHalf : rightHalf;
+    }
+    private int conquer(int[] nums, int target, int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] == target) {
+                count++;
             }
         }
-        return 0;
+        return count;
     }
 }
