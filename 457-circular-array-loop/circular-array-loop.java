82e263ae59a03054d2c85d4765706d5d294de4f6
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-15 15:31:30 -0800

    Time: 16 ms (46.07%) | Memory: 41.6 MB (10.99%) - LeetSync

---
+++ 457-circular-array-loop/circular-array-loop.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean circularArrayLoop(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            boolean forward = nums[i] >= 0;
+            int slow = i, fast = i;
+
+            do {
+                slow = findNext(nums, forward, slow);
+                fast = findNext(nums, forward, fast);
+                if (fast != -1)
+                    fast = findNext(nums, forward, fast);
+            } while (slow != -1 && fast != -1 && slow != fast);
+
+            if (slow != -1 && slow == fast) 
+                return true;
+        }        
+        return false;
+    }
+    private int findNext(int[] nums, boolean forward, int currentIdx) {
+        boolean direction = nums[currentIdx] >= 0;
+        if (direction != forward)
+            return -1;
+
+        int nextIdx = (currentIdx + nums[currentIdx]) % nums.length;
+        if (nextIdx < 0) 
+            nextIdx += nums.length;
+        if (nextIdx == currentIdx)
+            nextIdx = -1;
+
+        return nextIdx;
+    }
+}
