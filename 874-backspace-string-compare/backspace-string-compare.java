496d80bdef63bd7c1e0f23e42d46d21fe2a2ab36
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-14 22:07:50 -0800

    Time: 0 ms (100.00%) | Memory: 41.3 MB (49.40%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,21 +1,35 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        StringBuilder sbOne = new StringBuilder();
-        StringBuilder sbTwo = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            if (c == '#' && sbOne.length() != 0) {
-                sbOne.deleteCharAt(sbOne.length() - 1);
-            } else if (c != '#') {
-                sbOne.append(c);
+        int pointer1 = s.length() - 1, pointer2 = t.length() - 1;
+        while (pointer1 >= 0 || pointer2 >= 0) {
+            int validOne = nextIdx(s, pointer1);
+            int validTwo = nextIdx(t, pointer2);
+            if (validOne < 0 && validTwo < 0) {
+                return true;
             }
-        }
-        for (char c : t.toCharArray()) {
-            if (c == '#' && sbTwo.length() != 0) {
-                sbTwo.deleteCharAt(sbTwo.length() - 1);
-            } else if (c != '#') {
-                sbTwo.append(c);
+            if (validOne < 0 || validTwo < 0) {
+                return false;
             }
+            if (s.charAt(validOne) != t.charAt(validTwo)) {
+                return false;
+            }
+            pointer1 = validOne - 1;
+            pointer2 = validTwo - 1;
         }
-        return sbOne.toString().equals(sbTwo.toString());
+        return true;
+    }
+    private int nextIdx(String s, int idx) {
+        int backSpace = 0;
+        while (idx >= 0) {
+            if (s.charAt(idx) == '#') {
+                backSpace++;
+            } else if (backSpace > 0) {
+                backSpace--;
+            } else if (backSpace == 0 && s.charAt(idx) != '#') {
+                break;
+            }
+            idx--;
+        }
+        return idx;
     }
 }
