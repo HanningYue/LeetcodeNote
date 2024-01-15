61b47499d57b1c2c47fd0eeb2bee501add70d929
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-14 16:05:21 -0800

    Time: 1 ms (94.29%) | Memory: 41 MB (13.97%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -2,17 +2,17 @@ class Solution {
     public int mySqrt(int x) {
         if (x == 1) {
             return x;
-        }
+        }   
         int left = 0, right = x / 2;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            long square = (long) mid * mid;
-            if (square > x) {
-                right = mid - 1;
-            } else if (square < x){
+            long product = (long)mid * mid;
+            if (product == x) {
+                return mid;
+            } else if (product < x) {
                 left = mid + 1;
             } else {
-                return mid;
+                right = mid - 1;
             }
         }
         return right;
