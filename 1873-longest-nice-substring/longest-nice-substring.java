4acb3441f0515f0ab0595237eab5540524c2d4d1
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-14 17:06:23 -0800

    Time: 2 ms (95.71%) | Memory: 42.7 MB (50.09%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.java
+++ 1873-longest-nice-substring/longest-nice-substring.java
@@ -3,19 +3,19 @@ class Solution {
         if (s.length() < 2) {
             return "";
         }
-
         Set<Character> set = new HashSet<>();
         for (char c : s.toCharArray()) {
             set.add(c);
         }
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if (set.contains(Character.toLowerCase(c)) && set.contains(Character.toUpperCase(c))) {
+            if (set.contains(Character.toLowerCase(c)) 
+            && set.contains(Character.toUpperCase(c))) {
                 continue;
             }
-            String left = longestNiceSubstring(s.substring(0, i));
-            String right = longestNiceSubstring(s.substring(i + 1));
-            return left.length() >= right.length() ? left : right;
+            String leftHalf = longestNiceSubstring(s.substring(0, i));
+            String rightHalf = longestNiceSubstring(s.substring(i + 1));
+            return leftHalf.length() >= rightHalf.length() ? leftHalf : rightHalf;
         }
         return s;
     }
