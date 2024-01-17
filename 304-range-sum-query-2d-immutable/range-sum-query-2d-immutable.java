18946c5044ff9be11e2043976293e9adbfd7958a
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-16 17:38:59 -0800

    Time: 99 ms (62.48%) | Memory: 68.4 MB (40.57%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,16 +1,21 @@
 class NumMatrix {
-    private int[][] preSum;
+    private int[][] presum;
     public NumMatrix(int[][] matrix) {
-        preSum = new int[matrix.length + 1][matrix[0].length + 1];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                preSum[i + 1][j + 1] = preSum[i][j + 1] + preSum[i + 1][j] - preSum[i][j] + matrix[i][j];
+        int m = matrix.length, n = matrix[0].length;
+        presum = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                presum[i + 1][j + 1] 
+                = presum[i][j + 1] + presum[i + 1][j] - presum[i][j] + matrix[i][j];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return preSum[row2 + 1][col2 + 1] - preSum[row2 + 1][col1] - preSum[row1][col2 + 1] + preSum[row1][col1];
+        return presum[row2 + 1][col2 + 1] 
+        - presum[row2 + 1][col1] 
+        - presum[row1][col2 + 1]
+        + presum[row1][col1];
     }
 }
 
