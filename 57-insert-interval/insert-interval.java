a6421f35f79ee6805649110da4221ff3d39babc5
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-17 23:33:32 -0800

    Time: 1 ms (99.33%) | Memory: 45.1 MB (12.07%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,29 +1,22 @@
-/**
-Refer to 56, same merge process
-EXTRA step, add the new interval at the end of starts and ends array
-*/
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        int n = intervals.length + 1;
-        int[] start = new int[n];
-        int[] end = new int[n];
-
-        for (int i = 0; i < intervals.length; i++) {
-            start[i] = intervals[i][0];
-            end[i] = intervals[i][1];
-        }
-        start[intervals.length] = newInterval[0];
-        end[intervals.length] = newInterval[1];
-        Arrays.sort(start);
-        Arrays.sort(end);
-
-        int previousStart = 0;
         List<int[]> result = new ArrayList<>();
-        for (int current = 0; current < n; current++) {
-            if (current == n - 1 || start[current + 1] > end[current]) {
-                result.add(new int[]{start[previousStart], end[current]});
-                previousStart = current + 1;
-            }
+        int start = newInterval[0];
+        int end = newInterval[1];
+        int i = 0;
+        while (i < intervals.length && intervals[i][1] < start) {
+            result.add(intervals[i]);
+            i++;
+        }
+        while (i < intervals.length && intervals[i][0] <= end) {
+            start = Math.min(start, intervals[i][0]);
+            end = Math.max(end, intervals[i][1]);
+            i++;
+        }
+        result.add(new int[]{start, end});
+        while (i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
         }
         return result.toArray(new int[result.size()][]);
     }
