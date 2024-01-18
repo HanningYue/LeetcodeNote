7afa98d812c804437b8d2fde20f3a7552a58e272
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-17 23:36:57 -0800

    Time: 1 ms (99.98%) | Memory: 46 MB (10.75%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,15 +1,9 @@
-/**
-To reach last index, we need to start from back, check each index forward can be reached
-(READ THE ABOVE LINE AGAIN)
-Use greedy, if each index is reachable(>=) by previous index + nums[index], update index
-*/
 class Solution {
     public boolean canJump(int[] nums) {
         int furthest = nums.length - 1;
         for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] + i >= furthest) {
+            if (nums[i] + i >= furthest)
                 furthest = i;
-            }
         }
         return furthest == 0;
     }
