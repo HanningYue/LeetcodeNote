5c3a483a29f40d1d02b781f05058c00b112f1e26
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-17 17:54:11 -0800

    Time: 34 ms (24.52%) | Memory: 45.5 MB (44.01%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,41 +1,24 @@
-/**
-两边互相add的解法
-MAXHEAP add， Queue add. REMEMBER TO DECLEAR maxHeap!
-Queue -> save task and coolingtime <Pair>
-PriorityQueue -> save maximum frequency
-We do not acctually care about the specific character, just their frequency
-1. Since tasks[i] only contains upper-case English Letter, use array to save all letters frequency
-2. Use maxHeap, save all the frequencies
-3. We only add frequency to maxHeap if it is greater than 0, (If a character does not appear, ignore)
-4. Increase time for each iteration
-5. Decrease frequency by one, when adding to queue, check if decreased freuqnecy is > 0
-6. If queue is not empty, and time is equal to the curren time, add back frequency to maxHeap
-*/
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> (b - a));
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
         Queue<Pair<Integer, Integer>> queue = new ArrayDeque<>();
-
-        int[] frequency = new int[26];
-        for (char c : tasks) {
-            frequency[c - 'A']++;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char task : tasks) {
+            map.put(task, map.getOrDefault(task, 0) + 1);
         }
-
-        for (int freq : frequency) {
-            if (freq > 0) {
-                maxHeap.offer(freq);
+        for (int frequency : map.values()) {
+            if (frequency > 0) {
+                maxHeap.offer(frequency);
             }
         }
-
         int time = 0;
         while (!maxHeap.isEmpty() || !queue.isEmpty()) {
             time++;
             if (!maxHeap.isEmpty()) {
-                int maxFreq = maxHeap.poll();
-                maxFreq--;
-                if (maxFreq > 0) {
-                    queue.offer(new Pair(maxFreq, time + n));
-                }
+                int mostFreq = maxHeap.poll();
+                mostFreq--;
+                if (mostFreq > 0)
+                    queue.offer(new Pair(mostFreq, time + n));
             }
             if (!queue.isEmpty() && queue.peek().getValue() == time) {
                 maxHeap.offer(queue.poll().getKey());
