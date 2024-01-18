ef74fbc833eda8f2cce9d8bc2c97b3d0078c4ffe
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-18 15:22:31 -0800

    Time: 49 ms (55.02%) | Memory: 73.3 MB (87.25%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,23 +1,17 @@
-/**
-1. Sort the intervals by their ending, ascending order
-2. Instead of counting how many need to be deducted, count how many intervals are independent
-3. let the interval with smallest ending be the previous, 
-4. Start traversing at 1, any later interval that has a greater(or equal) start than the previous end, 
-    increase the independent count
-5. update previous be the current
-6. Return the DIFFERENCE of length - independent count
-*/
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> (a[1] - b[1]));
-        
-        int previousEnd = 0, independent = 1;
-        for (int current = 0; current < intervals.length; current++) {
-            if (intervals[current][0] >= intervals[previousEnd][1]) {
-                independent++;
-                previousEnd = current;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int result = 0;
+        int[] previous = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] current = intervals[i];
+            if (previous[1] > current[0]) {
+                result++;
+                previous[1] = Math.min(previous[1], current[1]);
+            } else {
+                previous = current;
             }
         }
-        return intervals.length - independent;
+        return result;
     }
 }
