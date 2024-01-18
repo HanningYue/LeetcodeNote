a5c6279ae6257919f69f7abb8450d42f8d77561e
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-18 14:52:12 -0800

    Time: 15 ms (53.59%) | Memory: 41.2 MB (18.97%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.java
+++ 457-circular-array-loop/circular-array-loop.java
@@ -1,28 +1,30 @@
 class Solution {
     public boolean circularArrayLoop(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            boolean forward = nums[i] >= 0;
+            boolean direction = nums[i] >= 0;
             int slow = i, fast = i;
             do {
-                slow = findNext(nums, forward, slow);
-                fast = findNext(nums, forward, fast);
+                slow = nextIdx(nums, direction, slow);
+                fast = nextIdx(nums, direction, fast);
                 if (fast != -1)
-                    fast = findNext(nums, forward, fast);
-            } while (slow != -1 && fast != -1 && slow != fast);
-            if (slow != -1 && slow == fast) 
+                    fast = nextIdx(nums, direction, fast);
+            } while (slow != fast && slow != -1 && fast != -1);
+
+            if (slow != -1 && slow == fast) {
                 return true;
+            }
         }
         return false;
     }
-    private int findNext(int[] nums, boolean forward, int currentIdx) {
-        boolean direction = nums[currentIdx] >= 0;
-        if (direction != forward) 
+    private int nextIdx(int[] nums, boolean direction, int currentIdx) {
+        boolean nextDirection = nums[currentIdx] >= 0;
+        if (nextDirection != direction)
             return -1;
-        int nextIdx = (nums[currentIdx] + currentIdx) % nums.length;
-        if (nextIdx < 0) 
-            nextIdx += nums.length;
-        if (nextIdx == currentIdx)
+        int nextStart = (nums[currentIdx] + currentIdx) % nums.length;
+        if (nextStart < 0)
+            nextStart += nums.length;
+        if (nextStart == currentIdx)
             return -1;
-        return nextIdx;
+        return nextStart;
     }
 }
