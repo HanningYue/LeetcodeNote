d4bdc5818446b826bfb3139028f1ad3ae2076aaa
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-17 22:51:01 -0800

    Time: 9 ms (60.87%) | Memory: 47.1 MB (6.15%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,29 +1,22 @@
-/**
-1. int[][], sort starts and ends in seperate arrays int[] starts and int[] ends
-2. Traverse int[][], fill starts and ends
-3. Sort both arrays,
-*/
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
-
-        int[] starts = new int[n];
-        int[] ends = new int[n];
-        for (int i = 0; i < n; i++) {
-            starts[i] = intervals[i][0];
-            ends[i] = intervals[i][1];
-        }
-        Arrays.sort(starts);
-        Arrays.sort(ends);
-
-        int previousStart = 0;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         List<int[]> result = new ArrayList<>();
-        for (int current = 0; current < intervals.length; current++) {
-            if (current == intervals.length - 1 || starts[current + 1] > ends[current]) {
-                result.add(new int[]{starts[previousStart], ends[current]});
-                previousStart = current + 1;
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] currentInterval = intervals[i];
+            int currentStart = currentInterval[0];
+            int currentEnd = currentInterval[1];
+            if (currentStart <= end) {
+                end = Math.max(end, currentEnd);
+            } else {
+                result.add(new int[]{start, end});
+                start = currentStart;
+                end = currentEnd;
             }
         }
+        result.add(new int[]{start, end});
         return result.toArray(new int[result.size()][]);
     }
 }
