e1b34e1dcdc98e823d64665ab87c5bea068680f1
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-18 14:24:16 -0800

    Time: 2322 ms (20.33%) | Memory: 45.6 MB (42.79%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -2,17 +2,16 @@ class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> result = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
-        for (String word : words) 
+        for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
-        
-        int wordCount = words.length;
+        }
         int wordLength = words[0].length();
-
-        for (int i = 0; i <= s.length() - wordCount * wordLength; i++) {
+        int wordCount = words.length;
+        for (int i = 0; i <= s.length() - wordLength * wordCount; i++) {
             Map<String, Integer> localMap = new HashMap<>();
             for (int j = 0; j < wordCount; j++) {
-                int nextStartIdx = i + j * wordLength;
-                String nextWord = s.substring(nextStartIdx, nextStartIdx + wordLength);
+                Integer nextWordStart = i + j * wordLength;
+                String nextWord = s.substring(nextWordStart, nextWordStart + wordLength);
                 if (!map.containsKey(nextWord))
                     break;
                 localMap.put(nextWord, localMap.getOrDefault(nextWord, 0) + 1);
