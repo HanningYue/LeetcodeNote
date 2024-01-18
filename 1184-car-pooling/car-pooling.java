07104594643fe8e1bcb2d91d550978ce69981197
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-17 18:22:50 -0800

    Time: 2 ms (73.89%) | Memory: 44.3 MB (28.97%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int[] diff = new int[1001];
+        int[] result = new int[1001];
         for (int[] trip : trips) {
             int val = trip[0];
             int left = trip[1];
-            int right = trip[2]; // Note the change here
-            increase(diff, left, right, val);
+            int right = trip[2] - 1;
+            increase(result, left, right, val);
         }
         int currentCapacity = 0;
-        for (int i = 0; i < diff.length; i++) {
-            currentCapacity += diff[i];
+        for (int i = 0; i < result.length; i++) {
+            currentCapacity += result[i];
             if (currentCapacity > capacity)
                 return false;
         }
         return true;
     }
 
-    private void increase(int[] diff, int left, int right, int val) {
-        diff[left] += val;
-        if (right < diff.length) { // Adjust the condition here
-            diff[right] -= val;
+    private void increase(int[] result, int left, int right, int val) {
+        result[left] += val;
+        if (right + 1 < result.length) {
+            result[right + 1] -= val;
         }
     }
 }
