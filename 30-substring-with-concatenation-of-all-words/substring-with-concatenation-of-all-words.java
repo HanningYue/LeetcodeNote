94b35d284c9267256514c1433b0329916f045b3a
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-17 21:44:44 -0800

    Time: 2386 ms (16.35%) | Memory: 46.1 MB (17.85%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
         Map<String, Integer> map = new HashMap<>();
-        for (String word : words)
+        for (String word : words) 
             map.put(word, map.getOrDefault(word, 0) + 1);
-
-        List<Integer> result = new ArrayList<Integer>();
-        int wordsCount = words.length;
+        
+        int wordCount = words.length;
         int wordLength = words[0].length();
 
-        for (int i = 0; i <= s.length() - wordsCount * wordLength; i++) {
+        for (int i = 0; i <= s.length() - wordCount * wordLength; i++) {
             Map<String, Integer> localMap = new HashMap<>();
-            for (int j = 0; j < wordsCount; j++) {
-                int nextWordStart = i + j * wordLength;
-                String word = s.substring(nextWordStart, nextWordStart + wordLength);
-                if (!map.containsKey(word))
+            for (int j = 0; j < wordCount; j++) {
+                int nextStartIdx = i + j * wordLength;
+                String nextWord = s.substring(nextStartIdx, nextStartIdx + wordLength);
+                if (!map.containsKey(nextWord))
                     break;
-                localMap.put(word, localMap.getOrDefault(word, 0) + 1);
-                if (localMap.get(word) > map.get(word))
+                localMap.put(nextWord, localMap.getOrDefault(nextWord, 0) + 1);
+                if (localMap.get(nextWord) > map.get(nextWord))
                     break;
-                if (j + 1 == wordsCount)
+                if (j + 1 == wordCount)
                     result.add(i);
             }
         }
