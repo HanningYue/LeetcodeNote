a42d91feec91fae8b3f2284a3756ffa02c415af8
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-17 21:17:28 -0800

    Time: 2361 ms (17.45%) | Memory: 45.9 MB (30.72%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -4,24 +4,24 @@ class Solution {
         for (String word : words)
             map.put(word, map.getOrDefault(word, 0) + 1);
 
-        List<Integer> resultIndices = new ArrayList<Integer>();
+        List<Integer> result = new ArrayList<Integer>();
         int wordsCount = words.length;
         int wordLength = words[0].length();
 
         for (int i = 0; i <= s.length() - wordsCount * wordLength; i++) {
             Map<String, Integer> localMap = new HashMap<>();
             for (int j = 0; j < wordsCount; j++) {
-                int nextWordIndex = i + j * wordLength;
-                String word = s.substring(nextWordIndex, nextWordIndex + wordLength);
+                int nextWordStart = i + j * wordLength;
+                String word = s.substring(nextWordStart, nextWordStart + wordLength);
                 if (!map.containsKey(word))
                     break;
                 localMap.put(word, localMap.getOrDefault(word, 0) + 1);
-                if (localMap.get(word) > map.getOrDefault(word, 0))
+                if (localMap.get(word) > map.get(word))
                     break;
                 if (j + 1 == wordsCount)
-                    resultIndices.add(i);
+                    result.add(i);
             }
         }
-        return resultIndices;
+        return result;
     }
 }
