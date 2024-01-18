7e886df62935c4bf3c409ad94ff4c55fff30aa11
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-18 15:04:13 -0800

    Time: 15 ms (53.59%) | Memory: 41.5 MB (13.59%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.java
+++ 457-circular-array-loop/circular-array-loop.java
@@ -4,27 +4,26 @@ class Solution {
             boolean direction = nums[i] >= 0;
             int slow = i, fast = i;
             do {
-                slow = nextIdx(nums, direction, slow);
-                fast = nextIdx(nums, direction, fast);
+                slow = nextStart(nums, slow, direction);
+                fast = nextStart(nums, fast, direction);
                 if (fast != -1)
-                    fast = nextIdx(nums, direction, fast);
-            } while (slow != fast && slow != -1 && fast != -1);
-
-            if (slow != -1 && slow == fast) {
+                    fast = nextStart(nums, fast, direction);
+            } while (fast != slow && fast != -1 && slow != -1);
+            if (slow == fast && slow != -1) {
                 return true;
             }
         }
         return false;
     }
-    private int nextIdx(int[] nums, boolean direction, int currentIdx) {
-        boolean nextDirection = nums[currentIdx] >= 0;
-        if (nextDirection != direction)
+    private int nextStart(int[] nums, int currentIdx, boolean direction) {
+        boolean currentDir = nums[currentIdx] >= 0;
+        if (direction != currentDir)
             return -1;
-        int nextStart = (nums[currentIdx] + currentIdx) % nums.length;
-        if (nextStart < 0)
-            nextStart += nums.length;
-        if (nextStart == currentIdx)
+        int nextIdx = (nums[currentIdx] + currentIdx) % nums.length;
+        if (nextIdx < 0) 
+            nextIdx += nums.length;
+        if (nextIdx == currentIdx)
             return -1;
-        return nextStart;
+        return nextIdx;
     }
 }
