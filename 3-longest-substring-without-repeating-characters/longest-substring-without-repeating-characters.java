b02d604b02ca234b9f343feb4d6a8b84dedadf49
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-19 12:03:29 -0800

    Time: 7 ms (45.41%) | Memory: 43.7 MB (73.30%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -6,7 +6,6 @@ class Solution {
         int res = 0; // 记录结果
         while (right < s.length()) {
             char c = s.charAt(right);
-            right++;
             // 进行窗口内数据的一系列更新
             window.put(c, window.getOrDefault(c, 0) + 1);
             // 判断左侧窗口是否要收缩
@@ -17,7 +16,8 @@ class Solution {
                 window.put(d, window.get(d) - 1);
             }
             // 在这里更新答案
-            res = Math.max(res, right - left);
+            res = Math.max(res, right - left + 1);
+            right++;
         }
         return res;
     }
