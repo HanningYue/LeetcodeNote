63091ba16725bf31a311807a3c01f8e5c6558543
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-19 12:11:57 -0800

    Time: 139 ms (10.55%) | Memory: 45.3 MB (18.99%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -4,33 +4,35 @@ class Solution {
         for (char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        int slow = 0, length = Integer.MAX_VALUE;
+        int length = Integer.MAX_VALUE;
+        int slow = 0, fast = 0;
         String result = "";
-        int matchChar = 0;
-        for (int fast = 0; fast < s.length(); fast++) {
+        while (fast < s.length()) {
             char c = s.charAt(fast);
             if (map.containsKey(c)) {
                 map.put(c, map.get(c) - 1);
-                if (map.get(c) >= 0) {
-                    matchChar ++;
-                }
             }
-
-            while (matchChar == t.length()) {
-                if (length > fast - slow + 1) {
+            while (allZero(map)) {
+                if (fast - slow + 1 < length) {
                     length = fast - slow + 1;
                     result = s.substring(slow, fast + 1);
                 }
                 c = s.charAt(slow);
                 if (map.containsKey(c)) {
                     map.put(c, map.get(c) + 1);
-                    if (map.get(c) > 0) {
-                        matchChar--;
-                    }
                 }
                 slow++;
             }
+            fast++;
         }
         return result;
+    }
+    private boolean allZero(Map<Character, Integer> map) {
+        for (int value : map.values()) {
+            if (value > 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
