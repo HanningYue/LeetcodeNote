dc3518759b2624db9b429ca36b49379827fef5b0
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-20 15:08:21 -0800

    Time: 4 ms (61.73%) | Memory: 43.4 MB (28.89%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,30 +1,25 @@
-/**
-Based on the Combination Sum I, we only need to change i to i + 1, because we only allowed to use the current startIndex number once
-*/
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTracking(candidates, target, result, list, 0);
+        backTracking(result, 0, target, list, candidates);
         return result;
     }
-    private void backTracking(int[] candidates, int target, List<List<Integer>> result,
-    List<Integer> list, int startIndex)
-    {
+    private void backTracking(List<List<Integer>> result, int current, int target, 
+    List<Integer> list, int[] candidates) {
         if (target < 0) {
             return;
         }
         if (target == 0) {
             result.add(new ArrayList(list));
         }
-
-        for (int i = startIndex; i < candidates.length; i++) {
-            if (i > startIndex && candidates[i] == candidates[i - 1]) {
+        for (int i = current; i < candidates.length; i++) {
+            if (i > current && candidates[i] == candidates[i - 1]) {
                 continue;
             }
             list.add(candidates[i]);
-            backTracking(candidates, target - candidates[i], result, list, i + 1);
+            backTracking(result, i + 1, target - candidates[i], list, candidates);
             list.remove(list.size() - 1);
         }
     }
