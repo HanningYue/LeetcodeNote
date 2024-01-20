78839305e9bac371e1a64822fae34da500ababfa
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-19 19:30:19 -0800

    Time: 65 ms (21.95%) | Memory: 45.3 MB (18.99%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -3,36 +3,34 @@ class Solution {
         Map<Character, Integer> map = new HashMap<>();
         for (char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        int length = Integer.MAX_VALUE;
-        int slow = 0, fast = 0;
+        }        
+        int match = 0, length = Integer.MAX_VALUE;
         String result = "";
+        int slow = 0, fast = 0;
         while (fast < s.length()) {
             char c = s.charAt(fast);
             if (map.containsKey(c)) {
                 map.put(c, map.get(c) - 1);
+                if (map.get(c) >= 0) {
+                    match++;
+                }                
             }
-            while (allZero(map)) {
-                if (fast - slow + 1 < length) {
+            while (match == t.length()) {
+                if (length > fast - slow + 1) {
                     length = fast - slow + 1;
                     result = s.substring(slow, fast + 1);
                 }
-                c = s.charAt(slow);
-                if (map.containsKey(c)) {
-                    map.put(c, map.get(c) + 1);
+                char slowChar = s.charAt(slow);
+                if (map.containsKey(slowChar)) {
+                    map.put(slowChar, map.get(slowChar) + 1);
+                    if (map.get(slowChar) > 0) {
+                        match--;
+                    }
                 }
                 slow++;
             }
             fast++;
         }
         return result;
-    }
-    private boolean allZero(Map<Character, Integer> map) {
-        for (int value : map.values()) {
-            if (value > 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
