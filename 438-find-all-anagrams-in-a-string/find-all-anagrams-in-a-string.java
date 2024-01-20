3df5dcc53fa841b1735c976fe8def77c9bdd678e
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-19 19:36:34 -0800

    Time: 26 ms (53.47%) | Memory: 45 MB (43.72%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -2,30 +2,33 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
         Map<Character, Integer> map = new HashMap<>();
-        for (char c : p.toCharArray()) 
+        for (char c : p.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
-        int slow = 0, fast = 0;
+        }
+        int match = 0, slow = 0, fast = 0;
         while (fast < s.length()) {
-            char c = s.charAt(fast);
-            if (map.containsKey(c))
-                map.put(c, map.get(c) - 1);
-            if (allZero(map))
-                result.add(slow);
-            while (fast - slow + 1 == p.length()) {
-                c = s.charAt(slow);
-                if (map.containsKey(c))
-                    map.put(c, map.get(c) + 1);
+            char fastChar = s.charAt(fast);
+            if (map.containsKey(fastChar)) {
+                map.put(fastChar, map.get(fastChar) - 1);
+                if (map.get(fastChar) >= 0) {
+                    match++;
+                }
+            }
+            while (fast - slow + 1 >= p.length()) {
+                if (match == p.length()) {
+                    result.add(slow);
+                }
+                char slowChar = s.charAt(slow);
+                if (map.containsKey(slowChar)) {
+                    map.put(slowChar, map.get(slowChar) + 1);
+                    if (map.get(slowChar) > 0) {
+                        match--;
+                    }
+                }
                 slow++;
             }
             fast++;
         }
         return result;
-    }
-    private boolean allZero(Map<Character, Integer> map) {
-        for (int value : map.values()) {
-            if (value != 0) 
-                return false;
-        }
-        return true;
     }
 }
