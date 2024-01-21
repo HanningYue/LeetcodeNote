1cd1f21ac2021c76c454afb9612a24aeb62dd446
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-20 23:54:02 -0800

    Time: 2 ms (98.87%) | Memory: 44.2 MB (18.23%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,14 +1,17 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] result = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            int greaterNum = -1;
-            for (int j = nums2.length - 1; j >= 0 && nums2[j] != nums1[i]; j--) {
-                if (nums2[j] > nums1[i]) {
-                    greaterNum = nums2[j];
-                }
+        Stack<Integer> stack = new Stack<>();
+        int[] greater = new int[10001];
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+                stack.pop();
             }
-            result[i] = greaterNum;
+            greater[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums2[i]);
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = greater[nums1[i]];
         }
         return result;
     }
