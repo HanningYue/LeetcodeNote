f72bc68ebb6af92031316004ed5631ae588945ff
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-20 21:20:24 -0800

    Time: 2 ms (87.92%) | Memory: 48.3 MB (28.34%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -12,28 +12,25 @@ class Solution {
     public void reorderList(ListNode head) {
         ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
-            fast = fast.next.next;
             slow = slow.next;
+            fast = fast.next.next;
         }
+        ListNode secondHalfStart = reverse(slow);
+        while (head != null && secondHalfStart != null) {
+            ListNode temp = head.next;
+            head.next = secondHalfStart;
+            head = temp;
 
-        ListNode firstHalf = head;
-        ListNode secondHalf = reverse(slow);
-        while (firstHalf != null && secondHalf != null) {
-            ListNode temp = firstHalf.next;
-            firstHalf.next = secondHalf;
-            firstHalf = temp;
-            
-            temp = secondHalf.next;
-            secondHalf.next = firstHalf;
-            secondHalf = temp;
+            temp = secondHalfStart.next;
+            secondHalfStart.next = head;
+            secondHalfStart = temp;
         }
-        if (firstHalf != null) {
-            firstHalf.next = null;
+        if (head != null) {
+            head.next = null;
         }
     }
     private ListNode reverse(ListNode head) {
-        ListNode current = head;
-        ListNode previous = null;
+        ListNode previous = null, current = head;
         while (current != null) {
             ListNode next = current.next;
             current.next = previous;
