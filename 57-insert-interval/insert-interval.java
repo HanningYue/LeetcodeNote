ef94fb5514f0cba55e23d4c2bf41c70862c833c4
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-20 16:08:12 -0800

    Time: 2 ms (51.88%) | Memory: 44.8 MB (28.84%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,27 +1,18 @@
-import java.util.*;
-
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> result = new ArrayList<>();
-
         for (int i = 0; i < intervals.length; i++) {
             if (intervals[i][1] < newInterval[0]) {
-                // Current interval ends before newInterval starts
                 result.add(intervals[i]);
             } else if (intervals[i][0] > newInterval[1]) {
-                // Current interval starts after newInterval ends
                 result.add(newInterval);
-                newInterval = intervals[i]; // Update newInterval to current interval
-            } else {
-                // Overlapping intervals, merge with newInterval
+                newInterval = intervals[i];
+            } else if (intervals[i][0] <= newInterval[1] && intervals[i][1] >= newInterval[0]){
                 newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
                 newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             }
         }
-
-        // Add the last interval
         result.add(newInterval);
-
         return result.toArray(new int[result.size()][]);
     }
 }
