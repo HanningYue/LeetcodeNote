bf53a20e101f780bd02f26f300eb034cd0be10b6
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-20 21:07:53 -0800

    Time: 1 ms (36.78%) | Memory: 44 MB (45.02%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -1,40 +1,39 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || k <= 1) {
-            return head;
-        }
-
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode current = head, previousNodeOfSublist = dummy;
+        ListNode previous = null, current = head;
         while (current != null) {
-            ListNode lastNodeOfSublist = current;
+            // Check if there are at least k nodes left to reverse
+            ListNode check = current;
             int count = 0;
-            while (current != null && count < k) {
-                current = current.next;
+            while (check != null && count < k) {
+                check = check.next;
                 count++;
             }
-
-            if (count == k) {
-                ListNode reverseHead = reverse(lastNodeOfSublist, k);
-                previousNodeOfSublist.next = reverseHead;
-                previousNodeOfSublist = lastNodeOfSublist;
-            } else {
-                previousNodeOfSublist.next = lastNodeOfSublist;
+            if (count < k) {
+                // Fewer than k nodes remaining, do not reverse
                 break;
             }
+
+            ListNode previousOfSublist = previous;
+            ListNode endOfSublist = current;
+            for (int i = 0; i < k; i++) {
+                ListNode next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+            }
+            
+            if (previousOfSublist != null) {
+                previousOfSublist.next = previous;
+            } else {
+                head = previous;
+            }
+            endOfSublist.next = current;
+            if (current == null) {
+                break;
+            }
+            previous = endOfSublist;
         }
-        return dummy.next;
-    }
-    private ListNode reverse(ListNode head, int k) {
-        ListNode current = head, previous = null;
-        while (k > 0 && current != null) {
-            ListNode next = current.next;
-            current.next = previous;
-            previous = current;
-            current = next;
-            k--;
-        }
-        return previous;
+        return head;
     }
 }
