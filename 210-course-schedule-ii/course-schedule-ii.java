0da72d49f1206ddbba0331ff685827eab58bd1b6
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-21 18:25:23 -0800

    Time: 5 ms (80.03%) | Memory: 46.1 MB (18.67%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,32 +1,31 @@
 class Solution {
-    private boolean[] visiting;
-    private boolean[] visited;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        visiting = new boolean[numCourses];
-        visited = new boolean[numCourses];
-        List<List<Integer>> graph = new ArrayList<>();
+        Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
+            graph.put(i, new ArrayList<>());
         }
         for (int[] pre : prerequisites) {
             int from = pre[1];
             int to = pre[0];
             graph.get(from).add(to);
         }
+        boolean[] visiting = new boolean[numCourses];
+        boolean[] visited = new boolean[numCourses];
         List<Integer> order = new ArrayList<>();
-        int[] result = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(graph, i, order)) {
+            if (!dfs(i, graph, visiting, visited, order)) {
                 return new int[]{};
             }
         }
         Collections.reverse(order);
+        int[] result = new int[numCourses];
         for (int i = 0; i < order.size(); i++) {
             result[i] = order.get(i);
         }
         return result;
     }
-    private boolean dfs(List<List<Integer>> graph, int vertex, List<Integer> order) {
+    private boolean dfs(int vertex, Map<Integer, List<Integer>> graph, 
+    boolean[] visiting, boolean[] visited, List<Integer> order) {
         if (visiting[vertex]) {
             return false;
         }
@@ -35,7 +34,7 @@ class Solution {
         }
         visiting[vertex] = true;
         for (int neighbor : graph.get(vertex)) {
-            if (!dfs(graph, neighbor, order)) {
+            if (!dfs(neighbor, graph, visiting, visited, order)) {
                 return false;
             }
         }
