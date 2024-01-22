497eca6173ea20d97b125c4b2aebe6f8bdb9ffde
Hanning Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-21 18:06:45 -0800

    Time: 62 ms (61.38%) | Memory: 57.3 MB (67.03%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,21 +1,14 @@
-/**
-1. Stack, saving the Index of each daily temperatures, from left to right
-2. Iterate the array, compare current temperatures with the previous(stack.peek())
-3. If previous temperatures are lower, fill result int[] with Index Difference
-*/
-//For better Time-Complexity, use a Deque and ArrayDeque instead of Stack
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] answer = new int[temperatures.length];
+        int[] result = new int[temperatures.length];
         Stack<Integer> stack = new Stack<>();
-        
         for (int i = 0; i < temperatures.length; i++) {
-            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
                 int lastIndex = stack.pop();
-                answer[lastIndex] = i - lastIndex;
+                result[lastIndex] = i - lastIndex;
             }
             stack.push(i);
-        }     
-        return answer;
+        }
+        return result;
     }
 }
