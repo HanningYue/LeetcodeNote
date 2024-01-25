1479fd4ed53160c5f9e28ba7d878e9d88d75649f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-24 19:47:26 -0800

    Time: 17 ms (75.86%) | Memory: 53.5 MB (27.02%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,15 +1,19 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
         Set<String> set = new HashSet<>();
         Set<String> seen = new HashSet<>();
         for (int i = 0; i + 10 <= s.length(); i++) {
-            String current = s.substring(i, i + 10);
-            if (seen.contains(current)) {
-                set.add(current);
+            String sub = s.substring(i, i + 10);
+            if (seen.contains(sub)) {
+                set.add(sub);
             } else {
-                seen.add(current);
+                seen.add(sub);
             }
         }
-        return new ArrayList<>(set);
+        for (String dna : set) {
+            result.add(dna);
+        }
+        return result;
     }
 }
