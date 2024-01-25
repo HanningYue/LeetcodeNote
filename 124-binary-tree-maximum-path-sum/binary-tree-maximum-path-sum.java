df0e7ed1021d408bca919b25bf10846d082ee3db
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-24 23:10:01 -0800

    Time: 0 ms (100.00%) | Memory: 44.7 MB (24.64%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -1,8 +1,18 @@
 /**
-1. 需要左边的max和0对比 需要右边的max和0对比
-2. 本层返回本层node加上 左右两边更大的呢一边
-3. globalMax需要每一层都更新 和本层加左加右对比
-*/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     int globalMax = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
@@ -13,11 +23,9 @@ class Solution {
         if (root == null) {
             return 0;
         }
-        int leftMax = Math.max(0, dfs(root.left));
-        int rightMax = Math.max(0, dfs(root.right));
-
-        globalMax = Math.max(globalMax, root.val + leftMax + rightMax);
-
-        return root.val + Math.max(leftMax, rightMax);
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+        globalMax = Math.max(globalMax, left + right + root.val);
+        return root.val + Math.max(left, right);
     }
 }
