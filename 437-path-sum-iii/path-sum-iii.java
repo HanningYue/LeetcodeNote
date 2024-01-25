b1a3a0a6746e93d6fb69efefcf7536e0a0da6073
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-25 12:07:30 -0800

    Time: 2 ms (100.00%) | Memory: 44.2 MB (25.44%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.java
+++ 437-path-sum-iii/path-sum-iii.java
@@ -15,25 +15,27 @@
  */
 class Solution {
     public int pathSum(TreeNode root, int targetSum) {
-        Map<Long, Integer> map = new HashMap<>(); //currentPathSum, previousPathSum
-        return dfs(root, targetSum, map, 0l);
+        Map<Long, Integer> map = new HashMap<>();
+        return dfs(root, targetSum, 0l, map);
     }
-    private int dfs(TreeNode root, int targetSum, Map<Long, Integer> map, Long prefixSum) {
+    private int dfs(TreeNode root, int targetSum, Long pathSum, Map<Long, Integer> map) {
         if (root == null) {
             return 0;
         }
         int count = 0;
-        prefixSum += root.val;
-        if (prefixSum == targetSum) {
+        
+        pathSum += root.val;
+        if (pathSum == targetSum) {
             count++;
         }
-        count += map.getOrDefault(prefixSum - targetSum, 0);
-        map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        count += map.getOrDefault(pathSum - targetSum, 0);
+        map.put(pathSum, map.getOrDefault(pathSum, 0) + 1);
 
-        count += dfs(root.left, targetSum, map, prefixSum);
-        count += dfs(root.right, targetSum, map,prefixSum);
-        
-        map.put(prefixSum, map.getOrDefault(prefixSum, 1) - 1);
+        count += dfs(root.left, targetSum, pathSum, map);
+        count += dfs(root.right, targetSum, pathSum, map);
+
+        map.put(pathSum, map.getOrDefault(pathSum, 1) - 1);
+
         return count;
     }
 }
