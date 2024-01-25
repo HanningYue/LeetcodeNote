20db48abc7863cbde00befef576e9f566086010c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-25 13:36:19 -0800

    Time: 108 ms (54.94%) | Memory: 144.8 MB (55.17%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,20 +1,18 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        boolean[] visited = new boolean[n];
         List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
         for (int[] edge : edges) {
-            int from = edge[0];
-            int to = edge[1];
+            int from = edge[0], to = edge[1];
             graph.get(from).add(to);
             graph.get(to).add(from);
-        }
-        return dfs(graph, source, destination, visited);
+        }        
+        boolean[] visited = new boolean[n];
+        return dfs(source, graph, destination, visited);
     }
-    private boolean dfs(List<List<Integer>> graph, int vertex, int target, 
-    boolean[] visited) {
+    private boolean dfs(int vertex, List<List<Integer>> graph, int target, boolean[] visited) {
         if (vertex == target) {
             return true;
         }
@@ -23,7 +21,7 @@ class Solution {
         }
         visited[vertex] = true;
         for (int neighbor : graph.get(vertex)) {
-            if (dfs(graph, neighbor, target, visited)) {
+            if (dfs(neighbor, graph, target, visited)) {
                 return true;
             }
         }
