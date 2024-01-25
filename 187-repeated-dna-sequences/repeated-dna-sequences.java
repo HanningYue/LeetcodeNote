ed0af281f5c1d4db5b5d5df508d863139238734e
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-24 19:59:40 -0800

    Time: 13 ms (98.30%) | Memory: 50.6 MB (91.04%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,19 +1,41 @@
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> result = new ArrayList<>();
-        Set<String> set = new HashSet<>();
-        Set<String> seen = new HashSet<>();
-        for (int i = 0; i + 10 <= s.length(); i++) {
-            String sub = s.substring(i, i + 10);
-            if (seen.contains(sub)) {
-                set.add(sub);
-            } else {
-                seen.add(sub);
+        int[] nums = new int[s.length()];
+        for (int i = 0; i < nums.length; i++) {
+            switch(s.charAt(i)) {
+                case 'A':
+                    nums[i] = 0;
+                    break;
+                case 'C':
+                    nums[i] = 1;
+                    break;
+                case 'G':
+                    nums[i] = 2;
+                    break;
+                case 'T':
+                    nums[i] = 3;
+                    break;
             }
         }
-        for (String dna : set) {
-            result.add(dna);
+
+        Set<Integer> seen = new HashSet<>();
+        Set<String> set = new HashSet<>();
+        int length = 10, base = 4, highestDigit = (int)Math.pow(base, length - 1);
+        int hashing = 0;
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            hashing = base * hashing + nums[fast];
+            if (fast - slow + 1 == length) {
+                if (seen.contains(hashing)) {
+                    set.add(s.substring(slow, fast + 1));
+                } else {
+                    seen.add(hashing);
+                }
+                hashing = hashing - nums[slow] * highestDigit;
+                slow++;
+            }
+            fast++;
         }
-        return result;
+        return new ArrayList<>(set);
     }
 }
