9ea0b19589160be7a3c57160b6af7430a0b08359
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-25 16:07:11 -0800

    Time: 4 ms (22.62%) | Memory: 45.4 MB (5.22%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,28 +1,37 @@
 class Solution {
-    public int maxAreaOfIsland(int[][] grid) {
-        int maxArea = 0;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
+    public int maxAreaOfIsland(int[][] matrix) {
+        int area = 0;
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (!visited[i][j] && matrix[i][j] == 1) {
+                    area = Math.max(area, bfs(matrix, visited, i, j));
+                }
+            }
+        }        
+        return area;
+    }
+    private int bfs(int[][] matrix, boolean[][] visited, int row, int col) {
+        int area = 1;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{row, col});
+        visited[row][col] = true;
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (!visited[i][j] && grid[i][j] != 0) {
-                    maxArea = Math.max(maxArea, dfs(grid, i, j, visited));
-                }        
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            for (int[] dir : directions) {
+                int newRow = current[0] + dir[0];
+                int newCol = current[1] + dir[1];
+                if (newRow < 0 || newRow >= matrix.length || newCol < 0 || newCol >= matrix[0].length 
+                || matrix[newRow][newCol] != 1 || visited[newRow][newCol]) {
+                    continue;
+                }
+                area++;
+                visited[newRow][newCol] = true;
+                queue.offer(new int[]{newRow, newCol});
             }
         }
-        return maxArea;
-    }
-
-    private int dfs(int[][] grid, int i, int j, boolean[][] visited) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || visited[i][j] || grid[i][j] != 1) {
-            return 0;
-        }
-        int area = 1;
-        visited[i][j] = true;
-        area += dfs(grid, i + 1, j, visited);
-        area += dfs(grid, i - 1, j, visited);
-        area += dfs(grid, i, j + 1, visited);
-        area += dfs(grid, i, j - 1, visited);
         return area;
     }
 }
