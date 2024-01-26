880e78f04edf6173f261df37f392daf5ed59c42a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-25 17:58:13 -0800

    Time: 4 ms (43.21%) | Memory: 42.6 MB (19.01%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -1,15 +1,17 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
+        Set<Character> set = new HashSet<>();
+        Stack<Character> stack = new Stack<>();
         Map<Character, Integer> map = new HashMap<>();
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        Stack<Character> stack = new Stack<>();
-        Set<Character> set = new HashSet<>();
+        
         for (char c : s.toCharArray()) {
             if (!set.contains(c)) {
                 while (!stack.isEmpty() && stack.peek() > c && map.get(stack.peek()) > 0) {
-                    set.remove(stack.pop());
+                    char lastChar = stack.pop();
+                    set.remove(lastChar);
                 }
                 set.add(c);
                 stack.push(c);
