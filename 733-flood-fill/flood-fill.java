093cf68b3ba8249289232bf793ccd93dbd30e790
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-25 16:26:52 -0800

    Time: 1 ms (48.05%) | Memory: 45 MB (15.00%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,26 +1,20 @@
 class Solution {
   public int[][] floodFill(int[][] matrix, int x, int y, int newColor) {
-    Queue<int[]> queue = new LinkedList<>();
-    queue.offer(new int[]{x, y});
-    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     boolean[][] visited = new boolean[matrix.length][matrix[0].length];
     int originalColor = matrix[x][y];
-    matrix[x][y] = newColor;
-
-    while (!queue.isEmpty()) {
-      int[] current = queue.poll();
-      for (int[] dir : directions) {
-        int row = current[0] + dir[0];
-        int col = current[1] + dir[1];
-        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length 
-        || matrix[row][col] != originalColor || visited[row][col]) {
-          continue;
-        }
-        visited[row][col] = true;
-        queue.offer(new int[]{row, col});
-        matrix[row][col] = newColor;
-      }
-    }
+    dfs(matrix, x, y, newColor, visited, originalColor);
     return matrix;
+  }
+  private void dfs(int[][] matrix, int x, int y, int newColor, boolean[][] visited, int originalColor) {
+    if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[0].length
+    || matrix[x][y] != originalColor || visited[x][y]) {
+      return;
+    }
+    matrix[x][y] = newColor;
+    visited[x][y] = true;
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    for (int[] dir : directions) {
+      dfs(matrix, x + dir[0], y + dir[1], newColor, visited, originalColor);
+    }
   }
 }
