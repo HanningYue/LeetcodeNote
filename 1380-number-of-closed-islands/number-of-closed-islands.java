cf2a2a529209ceed64a02da48e97e610450c78fe
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-26 20:58:27 -0800

    Time: 4 ms (25.29%) | Memory: 44.2 MB (52.67%) - LeetSync

---
+++ 1380-number-of-closed-islands/number-of-closed-islands.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int closedIsland(int[][] matrix) {
+        int count = 0;
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0 && !visited[i][j]) {
+                    if (dfs(matrix, i, j, visited)) {
+                        count++;
+                    }
+                }
+            }
+        }    
+        return count;
+    }
+
+    private boolean dfs(int[][] matrix, int row, int col, boolean[][] visited) {
+        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length) {
+            return true;
+        }
+        if (matrix[row][col] == 1 || visited[row][col]) {
+            return true;
+        }
+        if (onEdge(matrix, row, col)) {
+            return false;
+        }
+
+        visited[row][col] = true;
+        boolean isClosed = true;
+        int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for (int[] dir : directions) {
+            isClosed &= dfs(matrix, row + dir[0], col + dir[1], visited);
+        }
+        return isClosed;
+    }
+
+    private boolean onEdge(int[][] matrix, int row, int col) {
+        return row == 0 || row == matrix.length - 1 || col == 0 || col == matrix[0].length - 1;
+    }
+}
