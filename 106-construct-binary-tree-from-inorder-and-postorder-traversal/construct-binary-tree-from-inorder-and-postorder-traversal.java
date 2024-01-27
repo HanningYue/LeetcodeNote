0f1ec5ef000931bba449eff40e68da877d0ffde6
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-26 19:18:19 -0800

    Time: 1 ms (98.01%) | Memory: 44.9 MB (9.43%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -18,7 +18,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
-        }
+        }   
         return build(postorder, 0, postorder.length - 1, inorder, 0, inorder.length - 1, map);
     }
     private TreeNode build(int[] postorder, int postleft, int postright, 
@@ -28,14 +28,15 @@ class Solution {
             return null;
         }
         TreeNode head = new TreeNode(postorder[postright]);
-        int headIdx = map.get(head.val);
-        int leftSize = headIdx - inleft;
-        
-        head.left = build(postorder, postleft, postleft + leftSize - 1,
-        inorder, inleft, headIdx - 1, map);
+        int headIdxInorder = map.get(head.val);
+        int leftSize = headIdxInorder - inleft;
+
+        head.left = build(postorder, postleft, postleft + leftSize - 1, 
+        inorder, inleft, headIdxInorder - 1, map);
 
         head.right = build(postorder, postleft + leftSize, postright - 1,
-        inorder, headIdx + 1, inright, map);
+        inorder, headIdxInorder + 1, inright, map);
+
         return head;
     }
 }
