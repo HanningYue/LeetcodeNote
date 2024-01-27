a3a4529429fb27fcc22f2c20b352c3e6d9b7ef94
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-26 18:43:21 -0800

    Time: 1 ms (95.55%) | Memory: 44.3 MB (37.25%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -19,17 +19,23 @@ class Solution {
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
         }
-        return dfs(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
+        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
     }
-    private TreeNode dfs(int[] preorder, int preleft, int preright, 
-    int[] inorder, int inleft, int inright, Map<Integer, Integer> map) {
+    private TreeNode build(int[] preorder, int preleft, int preright, 
+    int[] inorder, int inleft, int inright, Map<Integer, Integer> map)
+    {
         if (preleft > preright) {
             return null;
         }
-        TreeNode newHead = new TreeNode(preorder[preleft]);
-        int mid = map.get(newHead.val);
-        newHead.left = dfs(preorder, preleft + 1, preleft + mid - inleft, inorder, inleft, mid - 1, map);
-        newHead.right = dfs(preorder, preleft + mid - inleft + 1, preright, inorder, mid + 1, inright, map);
-        return newHead;
+        TreeNode head = new TreeNode(preorder[preleft]);
+        int headIdx = map.get(head.val);
+        int leftSize = headIdx - inleft;
+        
+        head.left = build(preorder, preleft + 1, preleft + leftSize, 
+        inorder, inleft, headIdx - 1, map);
+        
+        head.right = build(preorder, preleft + leftSize + 1, preright,
+        inorder, headIdx + 1, inright, map);
+        return head;
     }
 }
