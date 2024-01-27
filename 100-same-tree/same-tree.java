ad8ffd0a57c0881e584c3005318ee381b635040c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-26 20:06:34 -0800

    Time: 0 ms (100.00%) | Memory: 41.2 MB (21.55%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,3 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) {
@@ -6,29 +21,10 @@ class Solution {
         if (p == null || q == null) {
             return false;
         }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right) && p.val == q.val;
+        boolean leftSame = isSameTree(p.left, q.left);
+        boolean rightSame = isSameTree(p.right, q.right);
+        boolean currentSame = p.val == q.val;
+
+        return currentSame && rightSame && leftSame;
     }
 }
-/*
-class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(p);
-        queue.offer(q);
-
-        while (!queue.isEmpty()) {
-            TreeNode root1 = queue.poll();
-            TreeNode root2 = queue.poll();
-
-            if (root1 == null && root2 == null) continue;
-            if (root1 == null || root2 == null) return false;
-            if (root1.val != root2.val) return false;
-            queue.offer(root1.left);
-            queue.offer(root2.left);
-            queue.offer(root1.right);
-            queue.offer(root2.right);
-        }
-        return true;
-    }
-}
-*/
