64299355c40f03bd9d8f8f11544c9f20e842f77d
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-27 00:03:29 -0800

    Time: 0 ms (100.00%) | Memory: 44.9 MB (37.04%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,20 @@
 class Solution {
-  public int[][] floodFill(int[][] matrix, int x, int y, int newColor) {
-    boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-    int originalColor = matrix[x][y];
-    dfs(matrix, x, y, newColor, visited, originalColor);
-    return matrix;
-  }
-  private void dfs(int[][] matrix, int x, int y, int newColor, boolean[][] visited, int originalColor) {
-    if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[0].length
-    || matrix[x][y] != originalColor || visited[x][y]) {
-      return;
+    public int[][] floodFill(int[][] matrix, int sr, int sc, int color) {
+        int startColor = matrix[sr][sc];
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        dfs(matrix, sr, sc, startColor, visited, color);
+        return matrix;
     }
-    matrix[x][y] = newColor;
-    visited[x][y] = true;
-    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    for (int[] dir : directions) {
-      dfs(matrix, x + dir[0], y + dir[1], newColor, visited, originalColor);
+    private void dfs(int[][] matrix, int row, int col, int startColor, boolean[][] visited, int color) {
+        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length 
+        || matrix[row][col] != startColor || visited[row][col]) {
+            return;
+        }
+        visited[row][col] = true;
+        matrix[row][col] = color;
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (int[] dir : directions) {
+            dfs(matrix, row + dir[0], col + dir[1], startColor, visited, color);
+        }
     }
-  }
 }
