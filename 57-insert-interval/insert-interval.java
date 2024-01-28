fa0b669580e6047ec2ee92720e6fbae6cc8855a7
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-27 20:03:19 -0800

    Time: 2 ms (51.95%) | Memory: 44.5 MB (64.26%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -7,7 +7,7 @@ class Solution {
             } else if (intervals[i][0] > newInterval[1]) {
                 result.add(newInterval);
                 newInterval = intervals[i];
-            } else if (intervals[i][0] <= newInterval[1] && intervals[i][1] >= newInterval[0]){
+            } else if (intervals[i][0] <= newInterval[1] && intervals[i][1] >= newInterval[0]) {
                 newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
                 newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             }
