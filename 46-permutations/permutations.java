8b17835417d3db585ccfd0f1cce8a75fe8b0923f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-28 01:03:10 -0800

    Time: 1 ms (95.85%) | Memory: 44.5 MB (27.53%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,27 +1,23 @@
-/**
-Iterate whole nums array despite the level number, we dont need startIndex since the result always have 
-nums.length as size
-Use a boolean[] to mark the visited number
-*/
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-        backTracking(nums, result, list, used);
+        backTrack(nums, visited, result, list);
         return result;
     }
-    private void backTracking(int[] nums, List<List<Integer>> result, List<Integer> list, boolean[] used) {
+    private void backTrack(int[] nums, boolean[] visited,
+    List<List<Integer>> result, List<Integer> list) {
         if (list.size() == nums.length) {
-            result.add(new ArrayList(list));
+            result.add(new ArrayList<>(list));
         }
         for (int i = 0; i < nums.length; i++) {
-            if (!used[i]) {
-                used[i] = true;
+            if (!visited[i]) {
+                visited[i] = true;
                 list.add(nums[i]);
-                backTracking(nums, result, list, used);
+                backTrack(nums, visited, result, list);
                 list.remove(list.size() - 1);
-                used[i] = false;
+                visited[i] = false;
             }
         }
     }
