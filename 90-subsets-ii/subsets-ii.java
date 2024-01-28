542f2bc157b0fda35eb94388efcbd6dcecee3b72
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-27 23:26:26 -0800

    Time: 1 ms (99.88%) | Memory: 43.2 MB (86.33%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,35 +1,20 @@
-/**
-When we dealing with duplicate subsets, we need to sort the arrays first, because without sorting, the same number could be scattered across different parts of the array. Then manipulate array index, comparing nums[i]
-with nums[i - 1], if they are the same, skip the current iteration
-          a   a  b   c
-
-              " "                    --   size 0, startIndex 0
-        /         \           \
-      a          a           c      -- size 1, startIndex 1
-    /    \       /   \          /   \
-  ac         ac           ca   cb 
-  /             /
-abc       abc
-    i represents the current index in the array being processed.
-    startIndex represents the starting index for the current recursive call.
-*/
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTracking(nums, 0, result, list);
+        backTrack(nums, list, result, 0);
         return result;
     }
-    private void backTracking(int[] nums, int startIndex, List<List<Integer>> result, List<Integer> list) {
+    private void backTrack(int[] nums, List<Integer> list, List<List<Integer>> result, int index) {
         result.add(new ArrayList<>(list));
-        
-        for (int i = startIndex; i < nums.length; i++) {
-            if (i > startIndex && nums[i] == nums[i - 1]) {
+
+        for (int i = index; i < nums.length; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
                 continue;
             }
             list.add(nums[i]);
-            backTracking(nums, i + 1, result, list);
+            backTrack(nums, list, result, i + 1);
             list.remove(list.size() - 1);
         }
     }
