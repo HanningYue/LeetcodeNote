59573639cacf03e1a9933dbc08f985884f14733a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-28 14:13:35 -0800

    Time: 56 ms (7.63%) | Memory: 45.3 MB (56.67%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -3,27 +3,28 @@ class Solution {
         List<String> result = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
         backTrack(s, sb, result, 0);
-        return result;
+        return result;        
     }
     private void backTrack(String s, StringBuilder sb, List<String> result, int index) {
         if (sb.length() == s.length()) {
             result.add(sb.toString());
-            return;
         }
-        char c = s.charAt(index);
-        if (Character.isLetter(c)) {
-            sb.append(Character.toLowerCase(c));
-            backTrack(s, sb, result, index + 1);
-            sb.deleteCharAt(sb.length() - 1);
+        for (int i = index; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isLetter(c)) {
+                sb.append(Character.toUpperCase(c));
+                backTrack(s, sb, result, i + 1);
+                sb.deleteCharAt(sb.length() - 1);
 
-            sb.append(Character.toUpperCase(c));
-            backTrack(s, sb, result, index + 1);
-            sb.deleteCharAt(sb.length() - 1);
-        }
-        else {
-            sb.append(c);
-            backTrack(s, sb, result, index + 1);
-            sb.deleteCharAt(sb.length() - 1);
+                sb.append(Character.toLowerCase(c));
+                backTrack(s, sb, result, i + 1);
+                sb.deleteCharAt(sb.length() - 1);
+            } 
+            else if (!Character.isLetter(c)) {
+                sb.append(c);
+                backTrack(s, sb, result, i + 1);
+                sb.deleteCharAt(sb.length() - 1);
+            }
         }
     }
 }
