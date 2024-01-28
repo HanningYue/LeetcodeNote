0139c89c4f91d689fedf25e882d78cf35f12860d
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-28 15:43:22 -0800

    Time: 1 ms (71.37%) | Memory: 42.3 MB (68.01%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,20 +1,16 @@
-/*
-每次要加入新的object，对于当前level，backtracking时加入 startIndex + 1，因为想要所有的组合
-    [0 element] [1 element] [2 elements] [3 elements]
-*/
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTracking(nums, result, list, 0);
+        backTrack(nums, result, list, 0);
         return result;
     }
-    private void backTracking(int[] nums, List<List<Integer>> result, List<Integer> list, int startIndex) {
+    private void backTrack(int[] nums, List<List<Integer>> result, List<Integer> list, int index) {
         result.add(new ArrayList<>(list));
 
-        for (int i = startIndex; i < nums.length; i++) {
+        for (int i = index; i < nums.length; i++) {
             list.add(nums[i]);
-            backTracking(nums, result, list, i + 1);
+            backTrack(nums, result, list, i + 1);
             list.remove(list.size() - 1);
         }
     }
