50f39b74e710771f4130ea0c7b58d1da49ba5b1b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-28 13:34:48 -0800

    Time: 9 ms (93.17%) | Memory: 57.3 MB (13.74%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -5,42 +5,46 @@
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.ne-xt = next; }
  * }
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if (head == null) {
-            return null;
-        }
-        if (head.next == null) {
+        if (head == null || head.next == null) {
             return head;
         }
-        ListNode previous = null, slow = head, fast = head;
+        ListNode previous = null;
+        ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
             previous = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
         previous.next = null;
-        ListNode left = sortList(head);
-        ListNode right = sortList(slow);
-        return merge(left, right);
+
+        ListNode leftNode = sortList(head);
+        ListNode rightNode = sortList(slow);
+        return merge(leftNode, rightNode);
     }
-    private ListNode merge(ListNode p1, ListNode p2) {
-        ListNode dummy = new ListNode(-1);
+    private ListNode merge(ListNode p, ListNode q) {
+        ListNode dummy = new ListNode(0);
         ListNode current = dummy;
-        while (p1 != null && p2 != null) {
-            if (p1.val < p2.val) {
-                current.next = p1;
-                p1 = p1.next;
+        while (p != null && q != null) {
+            if (p.val < q.val) {
+                current.next = p;
+                p = p.next;
             } else {
-                current.next = p2;
-                p2 = p2.next;
+                current.next = q;
+                q = q.next;
             }
             current = current.next;
         }
-        current.next = p1 == null ? p2 : p1;
+        if (p != null) {
+            current.next = p;
+        }
+        if (q != null) {
+            current.next = q;
+        }
         return dummy.next;
-    }
+    } 
 }
