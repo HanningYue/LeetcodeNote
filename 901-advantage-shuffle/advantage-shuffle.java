630ec21beed0f358ce4206c9afd9144fb29eaff7
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-27 17:39:08 -0800

    Time: 68 ms (55.91%) | Memory: 57.3 MB (75.00%) - LeetSync

--- 901-advantage-shuffle/advantage-shuffle.java
+++ 901-advantage-shuffle/advantage-shuffle.java
@@ -1,19 +1,23 @@
 class Solution {
     public int[] advantageCount(int[] nums1, int[] nums2) {
         int[] result = new int[nums1.length];
-        //maxHeap to save int[], int[0] = index, int[1] = value
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        for (int i = 0; i < nums2.length; i++) {
-            maxHeap.offer(new int[]{i, nums2[i]});
-        }
+        //pq save int[] {value, index}
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return b[0] - a[0];
+            }
+        });
 
+        for (int i = 0; i < nums2.length; i++) {
+            maxHeap.offer(new int[]{nums2[i], i});
+        }
+        
         Arrays.sort(nums1);
         int left = 0, right = nums1.length - 1;
         while (!maxHeap.isEmpty()) {
-            int[] maxOfTwo = maxHeap.poll();
-            int index = maxOfTwo[0], maxVal = maxOfTwo[1];
-
-            if (nums1[right] > maxVal) {
+            int[] nums2Max = maxHeap.poll();
+            int value = nums2Max[0], index = nums2Max[1];
+            if (nums1[right] > value) {
                 result[index] = nums1[right];
                 right--;
             } else {
