3ee708be67cce9237a9ea6d92608c3bd5a2f9098
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-27 22:51:44 -0800

    Time: 95 ms (29.91%) | Memory: 58.2 MB (77.49%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,28 +1,29 @@
 class Solution {
-    public int findMaximizedCapital(int number, int initial, int[] profits, int[] capital) {
-        //int[] {Ascending capital, Descending profit}
-        PriorityQueue<int[]> minCapital = new PriorityQueue<>(new Comparator<int[]>() {
+    public int findMaximizedCapital(int number, int initialCap, int[] profits, int[] capital) {
+        //{capital, profit}
+        PriorityQueue<int[]> minCap = new PriorityQueue<>(new Comparator<int[]>(){
             public int compare(int[] a, int[] b) {
                 return a[0] - b[0];
             }
         });
-        PriorityQueue<int[]> maxProfit = new PriorityQueue<>(new Comparator<int[]>(){
+        PriorityQueue<int[]> maxPro = new PriorityQueue<>(new Comparator<int[]>() {
             public int compare(int[] a, int[] b) {
                 return b[1] - a[1];
-            }
+            } 
         });
+
         for (int i = 0; i < capital.length; i++) {
-            minCapital.offer(new int[]{capital[i], profits[i]});
-        }
+            minCap.offer(new int[]{capital[i], profits[i]});
+        }    
         for (int i = 0; i < number; i++) {
-            while (!minCapital.isEmpty() && minCapital.peek()[0] <= initial) {
-                maxProfit.offer(minCapital.poll());
+            while (!minCap.isEmpty() && minCap.peek()[0] <= initialCap) {
+                maxPro.offer(minCap.poll());
             }
-            if (maxProfit.isEmpty()) {
+            if (maxPro.isEmpty()) {
                 break;
             }
-            initial += maxProfit.poll()[1];
+            initialCap += maxPro.poll()[1];
         }
-        return initial;
+        return initialCap;
     }
 }
