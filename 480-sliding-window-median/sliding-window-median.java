46a1352c5150fdbe8eb7cddf44ef9a3fed42cc9f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-28 16:59:34 -0800

    Time: 103 ms (51.35%) | Memory: 56 MB (67.53%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.java
+++ 480-sliding-window-median/sliding-window-median.java
@@ -2,10 +2,12 @@ class Solution {
     public double[] medianSlidingWindow(int[] nums, int k) {
         Comparator<Integer> comparator = (a, b) -> nums[a] != nums[b] 
         ? Integer.compare(nums[a], nums[b]) : a - b;
+
+        TreeSet<Integer> maxSet = new TreeSet<>(comparator.reversed());
         TreeSet<Integer> minSet = new TreeSet<>(comparator);
-        TreeSet<Integer> maxSet = new TreeSet<>(comparator.reversed());       
 
         double[] result = new double[nums.length - k + 1];
+
         int slow = 0, fast = 0;
         while (fast < nums.length) {
             if (fast - slow + 1 > k) {
@@ -15,21 +17,20 @@ class Solution {
                 slow++;
             }
 
-            if (maxSet.isEmpty() || nums[maxSet.first()] > nums[fast]) {
+            if (maxSet.isEmpty() || nums[fast] < nums[maxSet.first()]) {
                 maxSet.add(fast);
             } else {
                 minSet.add(fast);
             }
-
             if (maxSet.size() > minSet.size() + 1) {
                 minSet.add(maxSet.pollFirst());
             } else if (minSet.size() > maxSet.size()) {
                 maxSet.add(minSet.pollFirst());
             }
-            
+
             if (fast - slow + 1 == k) {
-                result[slow] = k % 2 == 0 ? 
-                ((double) nums[maxSet.first()] + nums[minSet.first()]) / 2.0 
+                result[slow] = k % 2 == 0 
+                ? ((double)nums[maxSet.first()] + nums[minSet.first()]) / 2.0
                 : nums[maxSet.first()];
             }
             fast++;
