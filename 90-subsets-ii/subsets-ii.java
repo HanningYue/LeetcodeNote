5ae29ad78131d4dedf847a0e12d88b5c0d3a51fe
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-28 16:04:54 -0800

    Time: 1 ms (99.88%) | Memory: 43.5 MB (51.68%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -3,18 +3,18 @@ class Solution {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTrack(nums, list, result, 0);
+        backTrack(nums, result, list, 0);
         return result;
     }
-    private void backTrack(int[] nums, List<Integer> list, List<List<Integer>> result, int index) {
+    private void backTrack(int[] nums, List<List<Integer>> result, List<Integer> list, int level) {
         result.add(new ArrayList<>(list));
 
-        for (int i = index; i < nums.length; i++) {
-            if (i > index && nums[i] == nums[i - 1]) {
+        for (int i = level; i < nums.length; i++) {
+            if (i > level && nums[i] == nums[i - 1]) {
                 continue;
             }
             list.add(nums[i]);
-            backTrack(nums, list, result, i + 1);
+            backTrack(nums, result, list, i + 1);
             list.remove(list.size() - 1);
         }
     }
