529c4e121b664b06ba13ae8df4d34b45b44b8774
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-29 11:25:00 -0800

    Time: 0 ms (100.00%) | Memory: 44.9 MB (65.34%) - LeetSync

---
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ /**
+We are searching the val, if val is greater than current root value, search(root.right)
+if val is smaller than current root value, search(root.left)
+
+*/
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        } else if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+        return root;
+    }
+}
