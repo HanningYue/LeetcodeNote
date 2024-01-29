a8c53c07d54d9b40a3a8f6be29ae2a63f190e12c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-28 17:11:25 -0800

    Time: 27 ms (36.67%) | Memory: 46.8 MB (85.06%) - LeetSync

--- 436-find-right-interval/find-right-interval.java
+++ 436-find-right-interval/find-right-interval.java
@@ -1,36 +1,37 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
-        //{start, index} {end, index}
-        PriorityQueue<int[]> minStart = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
         PriorityQueue<int[]> minEnd = new PriorityQueue<>(new Comparator<int[]>(){
             public int compare(int[] a, int[] b) {
                 return a[0] - b[0];
             }
         });
+        PriorityQueue<int[]> minStart = new PriorityQueue<>(new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
         for (int i = 0; i < intervals.length; i++) {
-            minStart.offer(new int[]{intervals[i][0], i});
             minEnd.offer(new int[]{intervals[i][1], i});
+            minStart.offer(new int[]{intervals[i][0], i});
         }
 
         int[] result = new int[intervals.length];
         for (int i = 0; i < intervals.length; i++) {
             result[i] = -1;
         }
+
         while (!minEnd.isEmpty()) {
-            int[] currentMinEnd = minEnd.poll();
-            int currentVal = currentMinEnd[0];
-            int currentIdx = currentMinEnd[1];
-            while (!minStart.isEmpty() && minStart.peek()[0] < currentVal) {
+            int[] currentMinEndInterval = minEnd.poll();
+            int currentMinEnd = currentMinEndInterval[0];
+            int currentMinEndIdx = currentMinEndInterval[1];
+
+            while (!minStart.isEmpty() && minStart.peek()[0] < currentMinEnd) {
                 minStart.poll();
             }
             if (minStart.isEmpty()) {
                 return result;
             }
-            result[currentIdx] = minStart.peek()[1];
+            result[currentMinEndIdx] = minStart.peek()[1];
         }
         return result;
     }
