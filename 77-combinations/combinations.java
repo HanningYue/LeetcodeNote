a40d6098ce5bcd4f73ea7732103d8fba76de37be
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-29 12:42:49 -0800

    Time: 19 ms (55.08%) | Memory: 94.7 MB (29.87%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -2,10 +2,10 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTrack(n, k, 1, result, list);
+        backTrack(n, k, result, list, 1);
         return result;
     }
-    private void backTrack(int n, int k, int index, List<List<Integer>> result, List<Integer> list) {
+    private void backTrack(int n, int k, List<List<Integer>> result, List<Integer> list, int index) {
         if (list.size() == k) {
             result.add(new ArrayList<>(list));
             return;
@@ -13,7 +13,7 @@ class Solution {
 
         for (int i = index; i <= n; i++) {
             list.add(i);
-            backTrack(n, k, i + 1, result, list);
+            backTrack(n, k, result, list, i + 1);
             list.remove(list.size() - 1);
         }
     }
