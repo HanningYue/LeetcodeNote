9d1d645eb7e4e0783254a3c4c42f5aec65bf8ba1
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-28 17:47:59 -0800

    Time: 1 ms (87.78%) | Memory: 42.7 MB (80.90%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        backTrack(n, n, result, sb);
+        return result;
+    }
+    private void backTrack(int leftBracket, int rightBracket, List<String> result, StringBuilder sb) {
+        if (leftBracket == 0 && rightBracket == 0) {
+            result.add(sb.toString());
+            return;
+        }
+        if (rightBracket > 0 && rightBracket > leftBracket) {
+            sb.append(')');
+            backTrack(leftBracket, rightBracket - 1, result, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if (leftBracket > 0) {
+            sb.append('(');
+            backTrack(leftBracket - 1, rightBracket, result, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
