68e93cbd2f2c73184a842b54a78ea239e4a6a7e2
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-29 17:11:27 -0800

    Time: 0 ms (100.00%) | Memory: 46 MB (29.74%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -13,56 +13,27 @@
  *     }
  * }
  */
-/**
-If node going to delete is leaf, delete it
-    if (root.left == null && root.right == null) {
-        return null;
-    }
-If node going to delete has only one child, switch that child to the position
-    if (root.left == null) {
-        return root.right
-    } else if (root.right == null) {
-        return root.left;
-    }
-
-If node going to delete has two children
-    Either, we find the smallest node in right subtree
-    if (root.left != null && root.right != null) {
-        TreeNode minRightSub = getMin(root.right);
-        //Delete the minRightSub
-        root.right = deleteNode(root.right, minNode.val)
-        
-        minNode.left = root.left;
-        minNode.right = root.right;
-        root = minNode;
-    }
-
-getMin(TreeNode root)
-*/
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) {
             return null;
         }
-
         if (root.val == key) {
             if (root.left == null) {
                 return root.right;
-            }            
-            if (root.right == null) {
+            } else if (root.right == null) {
                 return root.left;
             }
             TreeNode minNode = getMin(root.right);
-            root.right = deleteNode(root.right, minNode.val);
             root.val = minNode.val;
-        } 
+            root.right = deleteNode(root.right, minNode.val);
+        }
         else if (root.val < key) {
             root.right = deleteNode(root.right, key);
-        } 
+        }
         else if (root.val > key) {
             root.left = deleteNode(root.left, key);
         }
-
         return root;
     }
     private TreeNode getMin(TreeNode root) {
