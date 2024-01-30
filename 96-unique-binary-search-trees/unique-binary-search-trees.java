9504035c251da4e5428007de4894d26247ce0eff
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-29 16:30:45 -0800

    Time: 0 ms (100.00%) | Memory: 40.3 MB (33.09%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,24 +1,25 @@
 class Solution {
-    int[][] memo;
+    int[][] table;
     public int numTrees(int n) {
-        memo = new int[n + 1][n + 1];
+        table = new int[n + 1][n + 1];
         return count(1, n);
     }
-    private int count(int left, int right) {
-        if (left > right) {
+    private int count(int start, int end) {
+        if (start > end) {
             return 1;
         }
-        if (memo[left][right] != 0) {
-            return memo[left][right];
+        if (table[start][end] != 0) {
+            return table[start][end];
         }
 
         int result = 0;
-        for (int i = left; i <= right; i++) {
-            int leftCount = count(left, i - 1);
-            int rightCount = count(i + 1, right);
-            result += leftCount * rightCount;
+        for (int mid = start; mid <= end; mid++) {
+            int left = count(start, mid - 1);
+            int right = count(mid + 1, end);
+            result += left * right;
         }
-        memo[left][right] = result;
+
+        table[start][end] = result;
         return result;
     }
 }
