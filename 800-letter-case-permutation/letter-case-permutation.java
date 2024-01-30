e9364454b341f8a541b18db3ffd671a8e1545287
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-29 19:33:47 -0800

    Time: 54 ms (7.81%) | Memory: 45.9 MB (9.65%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.java
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -2,27 +2,28 @@ class Solution {
     public List<String> letterCasePermutation(String s) {
         List<String> result = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        backTrack(s, sb, result, 0);
-        return result;        
+        backTrack(s, 0, sb, result);
+        return result;
     }
-    private void backTrack(String s, StringBuilder sb, List<String> result, int index) {
+    private void backTrack(String s, int index, StringBuilder sb, List<String> result) {
         if (sb.length() == s.length()) {
             result.add(sb.toString());
+            return;
         }
         for (int i = index; i < s.length(); i++) {
             char c = s.charAt(i);
             if (Character.isLetter(c)) {
                 sb.append(Character.toUpperCase(c));
-                backTrack(s, sb, result, i + 1);
+                backTrack(s, i + 1, sb, result);
                 sb.deleteCharAt(sb.length() - 1);
 
                 sb.append(Character.toLowerCase(c));
-                backTrack(s, sb, result, i + 1);
+                backTrack(s, i + 1, sb, result);
                 sb.deleteCharAt(sb.length() - 1);
             } 
-            else if (!Character.isLetter(c)) {
+            else {
                 sb.append(c);
-                backTrack(s, sb, result, i + 1);
+                backTrack(s, i + 1, sb, result);
                 sb.deleteCharAt(sb.length() - 1);
             }
         }
