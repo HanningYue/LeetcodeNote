174726ee4315fcd5eeaa1ee868f38120d48382ae
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-31 00:36:13 -0800

    Time: 0 ms (100.00%) | Memory: 42.5 MB (7.51%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,20 +1,11 @@
-/*
-Since the original array nums is guranteed to be in ascending order
-    In the range of rotating 0 to n, rotate either 0, 1, 2, 3,.. times, the rotated array will always have a number at the start of array that is smaller than the number at the end of array UNLESS rotated by n times Which original array stays the same
-    Thus, run a binary search to find 左边界
-*/
 class Solution {
     public int findMin(int[] nums) {
-        int low = 0, high = nums.length - 1;
-        
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] > nums[high]) {
-                low = mid + 1;
-            } else {
-                high = mid;
+        int minIdx = -1, minVal = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < minVal) {
+                minVal = nums[i];
             }
         }
-        return nums[low];
+        return minVal;
     }
 }
