d15dc21ac74052a021a97e2fb3f96fd753e0939b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-01-30 23:02:06 -0800

    Time: 1 ms (99.30%) | Memory: 59.4 MB (21.79%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        if (nums[0] <= nums[nums.length - 1]) { //Increasing
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i - 1] > nums[i]) {
+                    return false;
+                }
+            }
+        } else if (nums[0] >= nums[nums.length - 1]) {
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[i - 1] < nums[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
