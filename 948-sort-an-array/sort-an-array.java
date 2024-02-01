8b8a4cd6f0771727b5059c1b15da63fc9915e7cc
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-01 00:46:39 -0800

    Time: 1786 ms (5.03%) | Memory: 61.1 MB (5.26%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,41 +1,45 @@
 class Solution {
-    int[] temp;
     public int[] sortArray(int[] nums) {
-        temp = new int[nums.length];
+        shuffle(nums);
         sort(nums, 0, nums.length - 1);
         return nums;
     }
     private void sort(int[] nums, int left, int right) {
-        if (left == right) {
+        if (left >= right) {
             return;
         }
-        int mid = left + (right - left) / 2;
-        sort(nums, left, mid);
-        sort(nums, mid + 1, right);
-        merge(nums, left, mid, right);
+        int pivot = partition(nums, left, right);
+        sort(nums, left, pivot - 1);
+        sort(nums, pivot + 1, right);
     }
-    private void merge(int[] nums, int left, int mid, int right) {
-        int leftP = left, rightP = mid + 1;
-        int arrayP = left;
-        while (leftP <= mid && rightP <= right) {
-            if (nums[leftP] < nums[rightP]) {
-                temp[arrayP] = nums[leftP];
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[left];
+        int leftP = left + 1, rightP = right;
+        while (leftP <= rightP) {
+            while (leftP < right && nums[leftP] <= pivot) {
                 leftP++;
-                arrayP++;
-            } else {
-                temp[arrayP] = nums[rightP];
-                rightP++;
-                arrayP++;
             }
+            while (rightP > left && nums[rightP] > pivot) {
+                rightP--;
+            }
+            if (leftP >= rightP) {
+                break;
+            }
+            swap(nums, leftP, rightP);
         }
-        while (leftP <= mid) {
-            temp[arrayP++] = nums[leftP++];
-        } 
-        while (rightP <= right) {
-            temp[arrayP++] = nums[rightP++];
+        swap(nums, left, rightP);
+        return rightP;
+    }
+    private void shuffle(int[] nums) {
+        Random rand = new Random();
+        for (int i = 0; i < nums.length; i++) {
+            int right = i + rand.nextInt(nums.length - i);
+            swap(nums, i, right);
         }
-        for (int i = left; i <= right; i++) {
-            nums[i] = temp[i];
-        }
+    }
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
