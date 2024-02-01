81a59b1412eca583c7eaa2cba18008bdc6a1e2c3
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-01 02:32:58 -0800

    Time: 15 ms (37.53%) | Memory: 46.7 MB (86.97%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,21 +1,11 @@
-/*
-Want to design a algorithm where the algo return the k-th largest element in a 
-    stream(constanly adding new numbers). Since we are not removing any number, 
-    we can use a minHeap with size k, to track k-th largest value
-1. Declear a minHeap
-2. In add function, add val to minHeap, when minHeap size is greater than k, compare the val 
-    with minHeap.peek()//The smallest in minHeap.
-    If smaller than minHeap.peek(), poll out and offer new val
-*/
 class KthLargest {
-    int k;
     PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
+    int k;
     public KthLargest(int k, int[] nums) {
         this.k = k;
         for (int num : nums) {
             add(num);
-        }    
+        }
     }
     
     public int add(int val) {
