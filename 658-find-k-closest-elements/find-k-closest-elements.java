9bfccd0d2ba7a74b5e732616f95a4318ec584ddd
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-01 03:30:07 -0800

    Time: 35 ms (16.66%) | Memory: 45.6 MB (62.67%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,33 +1,21 @@
-class Pair {
-    int key, value;
-    Pair(int key, int value) {
-        this.key = key;
-        this.value = value;
-    }
-}
-
 class Solution {
     public List<Integer> findClosestElements(int[] nums, int k, int x) {
         List<Integer> result = new ArrayList<>();
-        PriorityQueue<Pair> heap = new PriorityQueue<>(new Comparator<Pair>() {
-            public int compare(Pair a, Pair b) {
-                if ((b.key - a.key) == 0) {
-                    return b.value - a.value;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                if (a[0] == b[0]) {
+                    return Integer.compare(a[1], b[1]);
                 }
-                return b.key - a.key;
+                return Integer.compare(a[0], b[0]);
             }
         });
-
-        for (int num : nums) {
-            heap.offer(new Pair(Math.abs(num - x), num));
-            if (heap.size() > k) {
-                heap.poll();
-            }
+        for (int i = 0; i < nums.length; i++) {
+            int absDiff = Math.abs(nums[i] - x);
+            minHeap.offer(new int[]{absDiff, nums[i]});
         }
-
-        while (heap.size() != 0) {
-            Pair p = heap.poll();
-            result.add(p.value);
+        while (!minHeap.isEmpty() && k > 0) {
+            result.add(minHeap.poll()[1]);
+            k--;
         }
         Collections.sort(result);
         return result;
