0434bf2b8794d3236b2d17ed1f670d0533eaf489
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-01 03:48:49 -0800

    Time: 49 ms (8.58%) | Memory: 46.2 MB (20.62%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<Integer> findClosestElements(int[] nums, int k, int x) {
         List<Integer> result = new ArrayList<>();
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>() {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(new Comparator<int[]>() {
             public int compare(int[] a, int[] b) {
                 if (a[0] == b[0]) {
-                    return Integer.compare(a[1], b[1]);
+                    return Integer.compare(b[1], a[1]);
                 }
-                return Integer.compare(a[0], b[0]);
+                return Integer.compare(b[0], a[0]);
             }
         });
         for (int i = 0; i < nums.length; i++) {
             int absDiff = Math.abs(nums[i] - x);
-            minHeap.offer(new int[]{absDiff, nums[i]});
+            maxHeap.offer(new int[]{absDiff, nums[i]});
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
         }
-        while (!minHeap.isEmpty() && k > 0) {
-            result.add(minHeap.poll()[1]);
-            k--;
+        while (!maxHeap.isEmpty()) {
+            result.add(maxHeap.poll()[1]);
         }
         Collections.sort(result);
         return result;
