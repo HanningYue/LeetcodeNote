16a0067a30e13dd422fc335fe9683425043faffa
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-03 13:46:43 -0800

    Time: 0 ms (100.00%) | Memory: 50 MB (13.34%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,24 +1,22 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int left = matrix[0][0];
-        int right = matrix[matrix.length - 1][matrix.length - 1];
+        int left = matrix[0][0], right = matrix[matrix.length - 1][matrix.length - 1];
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            int count = countSearch(matrix, mid);
-            if (count < k) {
+            if (countLessThanK(matrix, mid) < k) {
                 left = mid + 1;
-            } else if (count > k) {
+            } else if (countLessThanK(matrix, mid) > k) {
                 right = mid - 1;
-            } else if (count == k) {
+            } else if (countLessThanK(matrix, mid) == k) {
                 right = mid - 1;
             }
         }
         return left;
     }
-
-    private int countSearch(int[][] matrix, int target) {
+    private int countLessThanK(int[][] matrix, int target) {
+        int row = matrix.length - 1;;
+        int col = 0;
         int count = 0;
-        int row = matrix.length - 1, col = 0;
         while (col < matrix.length && row >= 0) {
             if (matrix[row][col] <= target) {
                 count += row + 1;
