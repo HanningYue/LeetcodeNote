87e966466e54de65a23085e61fcbc50e3b2811a1
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-02 16:44:45 -0800

    Time: 0 ms (100.00%) | Memory: 50.2 MB (7.49%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -2,18 +2,18 @@ class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int left = matrix[0][0];
         int right = matrix[matrix.length - 1][matrix.length - 1];
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
             int count = countSearch(matrix, mid);
             if (count < k) {
                 left = mid + 1;
             } else if (count > k) {
-                right = mid;
+                right = mid - 1;
             } else if (count == k) {
-                right = mid;
+                right = mid - 1;
             }
         }
-        return right;
+        return left;
     }
 
     private int countSearch(int[][] matrix, int target) {
