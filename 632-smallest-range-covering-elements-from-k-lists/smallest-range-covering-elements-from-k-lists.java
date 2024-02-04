532cb9c9ba426c5b38eabf8dff89c48720eb07b5
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-03 18:12:25 -0800

    Time: 32 ms (71.43%) | Memory: 48.1 MB (94.70%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -10,32 +10,33 @@ class Pair {
 }
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        int intervalLeft = Integer.MAX_VALUE, intervalRight = Integer.MIN_VALUE;
+        int resultRight = Integer.MAX_VALUE, resultLeft = 0;
+
         PriorityQueue<Pair> minHeap = new PriorityQueue<>((a, b) -> a.value - b.value);
+        int currentRight = Integer.MIN_VALUE;
         for (int i = 0; i < nums.size(); i++) {
-            intervalRight = Math.max(intervalRight, nums.get(i).get(0));
             minHeap.offer(new Pair(nums.get(i).get(0), i, 0));
+            currentRight = Math.max(currentRight, nums.get(i).get(0));
         }
 
-        int resultLeft = 0, resultRight = Integer.MAX_VALUE;
-
+        int currentLeft;
         while (minHeap.size() == nums.size()) {
             Pair current = minHeap.poll();
-            int value = current.value, arrayNum = current.arrayNum, index = current.index;
-            intervalLeft = value;
-
-            if (intervalRight - intervalLeft < resultRight - resultLeft) {
-                resultRight = intervalRight;
-                resultLeft = intervalLeft;
-            }
+            int value = current.value;
+            int arrayNum = current.arrayNum;
+            int index = current.index;
             
+            currentLeft = value;    
+            if (currentRight - currentLeft < resultRight - resultLeft) {
+                resultRight = currentRight;
+                resultLeft = currentLeft;
+            }
+
             if (index + 1 < nums.get(arrayNum).size()) {
-                Pair nextPair = new Pair(nums.get(arrayNum).get(index + 1), arrayNum, index + 1);
-                minHeap.offer(nextPair);
-                intervalRight = Math.max(intervalRight, nextPair.value);
+                minHeap.offer(new Pair(nums.get(arrayNum).get(index + 1), arrayNum, index + 1));
+                currentRight = Math.max(currentRight, nums.get(arrayNum).get(index + 1));
             }
         }
-
         return new int[]{resultLeft, resultRight};
     }
 }
