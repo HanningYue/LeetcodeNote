f40f22eabff5df6ace4e9dbcc956570262f6e571
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-03 19:15:59 -0800

    Time: 31 ms (31.57%) | Memory: 45.4 MB (69.17%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,25 +1,25 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
         Map<Character, Integer> map = new HashMap<>();
-        for (char task : tasks) {
-            map.put(task, map.getOrDefault(task, 0) + 1);
+        for (char c : tasks) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         maxHeap.addAll(map.values());
 
         int time = 0;
+        Queue<int[]> queue = new LinkedList<>();
         while (!maxHeap.isEmpty() || !queue.isEmpty()) {
             time++;
             if (!maxHeap.isEmpty()) {
                 int mostFreq = maxHeap.poll();
                 mostFreq--;
                 if (mostFreq > 0) {
-                    queue.offer(new Pair(mostFreq, time + n));
+                    queue.offer(new int[]{mostFreq, time + n});
                 }
             }
-            if (!queue.isEmpty() && queue.peek().getValue() == time) {
-                maxHeap.offer(queue.poll().getKey());
+            if (!queue.isEmpty() && queue.peek()[1] == time) {
+                maxHeap.offer(queue.poll()[0]);
             }
         }
         return time;
