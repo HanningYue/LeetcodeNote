48bdc59a5c6830a3077074b157e5c9c2a53b197a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-04 14:49:19 -0800

    Time: 105 ms (50.09%) | Memory: 57 MB (13.11%) - LeetSync

--- 480-sliding-window-median/sliding-window-median.java
+++ 480-sliding-window-median/sliding-window-median.java
@@ -1,35 +1,36 @@
 class Solution {
     public double[] medianSlidingWindow(int[] nums, int k) {
-        Comparator<Integer> comparator = (a, b) -> nums[a] != nums[b] 
-        ? Integer.compare(nums[a], nums[b]) : Integer.compare(a, b);
+        Comparator<Integer> comparator = (a, b) -> nums[a] == nums[b] 
+        ? Integer.compare(a, b) : Integer.compare(nums[a], nums[b]);
 
+        //Index
         TreeSet<Integer> minSet = new TreeSet<>(comparator);
         TreeSet<Integer> maxSet = new TreeSet<>(comparator.reversed());
 
         double[] result = new double[nums.length - k + 1];
-
         int slow = 0, fast = 0;
         while (fast < nums.length) {
             if (fast - slow + 1 > k) {
-                if (!maxSet.remove(slow)) {
-                    minSet.remove(slow);
+                if (!minSet.remove(slow)) {
+                    maxSet.remove(slow);
                 }
                 slow++;
             }
 
-            if (maxSet.isEmpty() || nums[fast] < nums[maxSet.first()]) {
+            if (maxSet.isEmpty() || nums[maxSet.first()] > nums[fast]) {
                 maxSet.add(fast);
             } else {
                 minSet.add(fast);
             }
+
             if (maxSet.size() > minSet.size() + 1) {
                 minSet.add(maxSet.pollFirst());
-            } else if (minSet.size() > maxSet.size()) {
+            } else if (maxSet.size() < minSet.size()) {
                 maxSet.add(minSet.pollFirst());
             }
 
             if (fast - slow + 1 == k) {
-                result[slow] = k % 2 == 0 
+                result[slow] = (k % 2 == 0)
                 ? ((double)nums[maxSet.first()] + nums[minSet.first()]) / 2.0
                 : nums[maxSet.first()];
             }
