01a86dd2b9e49082a531491ec638da1b6dcedde2
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-03 19:02:56 -0800

    Time: 26 ms (96.37%) | Memory: 58 MB (77.06%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -2,37 +2,32 @@ class Pair {
     int sum;
     int num1;
     int num2;
-    public Pair(int num1, int num2) {
+    int num2Idx;
+    public Pair(int num1, int num2, int num2Idx) {
         this.sum = num1 + num2;
         this.num1 = num1;
         this.num2 = num2;
+        this.num2Idx = num2Idx;
     }
 }
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<Pair> maxHeap = new PriorityQueue<Pair>((a, b) -> b.sum - a.sum);
         List<List<Integer>> result = new ArrayList<>();
-
+        PriorityQueue<Pair> minHeap = new PriorityQueue<Pair>((a, b) -> a.sum - b.sum);
         for (int i = 0; i < nums1.length && i < k; i++) {
-            for (int j = 0; j < nums2.length && j < k; j++) {
-                if (maxHeap.size() < k) {
-                    maxHeap.offer(new Pair(nums1[i], nums2[j]));
-                } 
-                else if (maxHeap.size() >= k){
-                    if (nums1[i] + nums2[j] >= maxHeap.peek().sum) {
-                        break;
-                    } 
-                    else if (nums1[i] + nums2[j] < maxHeap.peek().sum){
-                        maxHeap.poll();
-                        maxHeap.offer(new Pair(nums1[i], nums2[j]));
-                    }
-                }
-            }
+            Pair newPair = new Pair(nums1[i], nums2[0], 0);
+            minHeap.offer(newPair);
         }
 
-        while (!maxHeap.isEmpty() && k > 0) {
-            Pair pair = maxHeap.poll();
-            result.add(Arrays.asList(pair.num1, pair.num2));
+        while (!minHeap.isEmpty() && k > 0) {
+            Pair current = minHeap.poll();
+            int num1 = current.num1, num2 = current.num2, num2Idx = current.num2Idx;
+
+            result.add(Arrays.asList(num1, num2));
+
+            if (num2Idx + 1 < nums2.length) {
+                minHeap.add(new Pair(num1, nums2[num2Idx + 1], num2Idx + 1));
+            }
             k--;
         }
         return result;
