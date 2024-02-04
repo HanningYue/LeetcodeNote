e910f7d5ff1cc8acb19801171a699efa5fda20e5
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-04 13:15:04 -0800

    Time: 4 ms (53.57%) | Memory: 44 MB (38.91%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] result = new int[nums1.length];
-        int[] greaterInNums2 = new int[10001];
-
+        Map<Integer, Integer> map = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
         for (int i = nums2.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+            while (!stack.isEmpty() && nums2[i] >= stack.peek()) {
                 stack.pop();
             }
-            greaterInNums2[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
+            map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
             stack.push(nums2[i]);
         }
+
+        int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
-            result[i] = greaterInNums2[nums1[i]];
+            result[i] = map.get(nums1[i]);
         }
         return result;
     }
