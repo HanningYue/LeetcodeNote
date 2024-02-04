555cbe360981b0af1f2f94644b3ddfa9d348b4c8
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-03 19:43:06 -0800

    Time: 74 ms (23.78%) | Memory: 57.9 MB (56.05%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -1,19 +1,18 @@
 class Solution {
-    public int findLeastNumOfUniqueInts(int[] nums, int k) {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
+        for (int num : arr) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        PriorityQueue<Map.Entry<Integer, Integer>> minHeap 
+        PriorityQueue<Map.Entry<Integer, Integer>> minHeap
         = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
         minHeap.addAll(map.entrySet());
 
         while (!minHeap.isEmpty() && k > 0) {
-            Map.Entry<Integer, Integer> current = minHeap.peek();
-            current.setValue(current.getValue() - 1);
-            if (current.getValue() == 0) {
-                minHeap.poll();
+            Map.Entry<Integer, Integer> current = minHeap.poll();
+            current.setValue(current.getValue() - 1);    
+            if (current.getValue() > 0){
+                minHeap.offer(current);
             }
             k--;
         }
