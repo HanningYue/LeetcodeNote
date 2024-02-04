c5567e6af48b2d49b3120383bb93950cfecb0c82
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-03 19:52:55 -0800

    Time: 34 ms (17.37%) | Memory: 45.2 MB (89.64%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,35 +1,25 @@
 class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+    public List<Integer> findClosestElements(int[] nums, int distance, int target) {
         List<Integer> result = new ArrayList<>();
-        int xIdx = binarySearch(arr, 0, arr.length - 1, x);
-
-        int leftBound = xIdx - k, rightBound = xIdx + k;
-        leftBound = Math.max(leftBound, 0);
-        rightBound = Math.min(arr.length - 1, rightBound);
-
-        while (rightBound - leftBound + 1 > k) {
-            if (x - arr[leftBound] > arr[rightBound] - x) {
-                leftBound++;
-            } else if (x - arr[leftBound] <= arr[rightBound] - x) {
-                rightBound--;
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>() { // {value, diff}
+            public int compare(int[] a, int[] b) {
+                if (a[1] == b[1]) {
+                    return a[0] - b[0];
+                }
+                return a[1] - b[1];
             }
+        });
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = Math.abs(nums[i] - target);
+            minHeap.offer(new int[]{nums[i], diff});
         }
-        for (int i = leftBound; i <= rightBound; i++) {
-            result.add(arr[i]);
+
+        while (!minHeap.isEmpty() && distance > 0) {
+            result.add(minHeap.poll()[0]);
+            distance--;
         }
+        Collections.sort(result);
         return result;
-    }
-    private int binarySearch(int[] nums, int left, int right, int target) {
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-        }
-        return left; //Maintain insertion order
     }
 }
