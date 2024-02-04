017148660697b5f682add4acb68c7f377ab1e031
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-03 18:41:58 -0800

    Time: 78 ms (31.44%) | Memory: 59.4 MB (58.26%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -10,26 +10,26 @@ class Pair {
 }
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<Pair>((a, b) -> b.sum - a.sum);
         List<List<Integer>> result = new ArrayList<>();
-        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a, b) -> b.sum - a.sum);
+
         for (int i = 0; i < nums1.length && i < k; i++) {
             for (int j = 0; j < nums2.length && j < k; j++) {
-                Pair newPair = new Pair(nums1[i], nums2[j]);
-                
                 if (maxHeap.size() < k) {
-                    maxHeap.offer(newPair);
+                    maxHeap.offer(new Pair(nums1[i], nums2[j]));
                 } 
-                else if (maxHeap.size() >= k) {
-                    if (newPair.sum >= maxHeap.peek().sum) {
+                else if (maxHeap.size() >= k){
+                    if (nums1[i] + nums2[j] >= maxHeap.peek().sum) {
                         break;
-                    } else if (newPair.sum < maxHeap.peek().sum) {
+                    } 
+                    else if (nums1[i] + nums2[j] < maxHeap.peek().sum){
                         maxHeap.poll();
-                        maxHeap.offer(newPair);
+                        maxHeap.offer(new Pair(nums1[i], nums2[j]));
                     }
                 }
-
             }
         }
+
         while (!maxHeap.isEmpty() && k > 0) {
             Pair pair = maxHeap.poll();
             result.add(Arrays.asList(pair.num1, pair.num2));
