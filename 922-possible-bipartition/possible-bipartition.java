d4be1a8b60c3033f6be88ca992692ed2512cc828
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-05 00:34:50 -0800

    Time: 16 ms (73.13%) | Memory: 50.6 MB (84.50%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.java
+++ 922-possible-bipartition/possible-bipartition.java
@@ -1,46 +1,44 @@
 class Solution {
-    boolean[] visited, color;
-    boolean isBipartite = true;
-
+    private boolean bipartite = true;
     public boolean possibleBipartition(int n, int[][] dislikes) {
-        List<Integer>[] graph = buildGraph(n, dislikes);
-        visited = new boolean[n + 1];
-        color = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                traverse(graph, i);
-            }
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
         }
-        return isBipartite;
+        for (int[] dislike : dislikes) {
+            int personA = dislike[0] - 1;
+            int personB = dislike[1] - 1;
+            graph.get(personA).add(personB);
+            graph.get(personB).add(personA);
+        }
+        boolean[] visited = new boolean[n];
+        boolean[] color = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            checkBipartite(i, graph, visited, color);
+        }
+        if (bipartite) {
+            return true;
+        }
+        return false;
     }
-    private void traverse(List<Integer>[] graph, int vertex) {
-        if (!isBipartite) {
+    private void checkBipartite(int vertex, List<List<Integer>> graph, 
+    boolean[] visited, boolean[] color) 
+    {
+        if (!bipartite) {
             return;
         }
         visited[vertex] = true;
-        for (int neighbor : graph[vertex]) {
+        for (int neighbor : graph.get(vertex)) {
             if (!visited[neighbor]) {
                 color[neighbor] = !color[vertex];
-                traverse(graph, neighbor);
-            } else {
-                if (color[neighbor] == color[vertex]) {
-                    isBipartite = false;
+                checkBipartite(neighbor, graph, visited, color);
+            } 
+            else {
+                if (color[vertex] == color[neighbor]) {
+                    bipartite = false;
+                    return;
                 }
             }
         }
-    }
-    private List<Integer>[] buildGraph(int n, int[][] dislikes) {
-        List<Integer>[] graph = new LinkedList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new LinkedList<>();
-        }
-
-        for (int[] edge : dislikes) {
-            int one = edge[0];
-            int two = edge[1];
-            graph[one].add(two);
-            graph[two].add(one);
-        }
-        return graph;
     }
 }
