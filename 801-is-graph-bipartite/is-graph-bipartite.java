a55180d486ef706cfe9fab311b8572ca0a060a2a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-05 16:40:46 -0800

    Time: 3 ms (26.20%) | Memory: 44.8 MB (61.39%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,43 +1,47 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        UF uf = new UF(graph.length * 2); // Extend the number of vertices to 2 times
-        for (int u = 0; u < graph.length; u++) {
-            int parentU = uf.find(u); // Find the set of u
-            for (int v : graph[u]) {
-                if (parentU == uf.find(v)) return false; // u and v are in the same set
-                uf.union(u + graph.length, v); // Union u's opposite set with v
+        UF uf = new UF(graph.length);
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].length; j++) {
+                if (uf.connected(i, graph[i][j])) {
+                    return false;
+                }
+                if (graph[i].length > 0) {
+                    uf.union(graph[i][0], graph[i][j]);
+                }
             }
         }
         return true;
     }
 }
-
 class UF {
-    private int[] parent;
-
-    public UF(int n) {
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
+    int count;
+    int[] parent;
+    public UF(int numberOfVertex) {
+        this.count = numberOfVertex;
+        parent = new int[numberOfVertex];
+        for (int i = 0; i < numberOfVertex; i++) {
             parent[i] = i;
         }
     }
-
-    public int find(int x) {
-        if (x != parent[x]) {
-            parent[x] = find(parent[x]); // Path compression
+    public int find(int vertex) {
+        if (parent[vertex] != vertex) {
+            parent[vertex] = find(parent[vertex]);
         }
-        return parent[x];
+        return parent[vertex];
     }
-
-    public void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) {
-            parent[rootX] = rootY; // Union the roots
+    public void union(int vertexOne, int vertexTwo) {
+        int parentOne = find(vertexOne);
+        int parentTwo = find(vertexTwo);
+        if (parentOne != parentTwo) {
+            parent[parentOne] = parentTwo;
+            count--;
         }
     }
-
-    public boolean connected(int x, int y) {
-        return find(x) == find(y);
+    public boolean connected(int vertexOne, int vertexTwo) {
+        return find(vertexOne) == find(vertexTwo);
+    }
+    public int count() {
+        return count;
     }
 }
