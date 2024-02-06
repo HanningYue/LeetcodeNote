45e695e79b0901d9037c62ff95c9c52c90ebe60e
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-05 19:35:31 -0800

    Time: 3 ms (51.99%) | Memory: 44.8 MB (24.76%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.java
+++ 1380-number-of-closed-islands/number-of-closed-islands.java
@@ -1,29 +1,29 @@
 class Solution {
-    public int closedIsland(int[][] matrix) {
+    public int closedIsland(int[][] grid) {
         int count = 0;
-        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (!visited[i][j] && matrix[i][j] == 0 && dfs(matrix, i, j, visited)) {
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (!visited[i][j] && grid[i][j] == 0 && dfs(grid, i, j, visited)) {
                     count++;
                 }
             }
         }
         return count;
     }
-    private boolean dfs(int[][] matrix, int row, int col, boolean[][] visited) {
-        if (matrix[row][col] == 1 || visited[row][col]) {
+    private boolean dfs(int[][] grid, int row, int col, boolean[][] visited) {
+        if (grid[row][col] == 1 || visited[row][col]) {
             return true;
         }
-        if (row == 0 || row == matrix.length - 1 || col == 0 || col == matrix[0].length - 1) {
+        if (row == 0 || row == grid.length - 1 || col == 0 || col == grid[0].length - 1) {
             return false;
         }
-        visited[row][col] = true;
-        boolean isClosed = true;
         int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        boolean closed = true;
+        visited[row][col] = true;
         for (int[] dir : directions) {
-            isClosed &= dfs(matrix, row + dir[0], col + dir[1], visited);
+            closed &= dfs(grid, row + dir[0], col + dir[1], visited);
         }
-        return isClosed;
+        return closed;
     }
 }
