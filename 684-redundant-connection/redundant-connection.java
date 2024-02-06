2d6b8f0d1d74e1b9c0aadb6eebb7808ff5348aec
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-05 19:17:41 -0800

    Time: 1 ms (82.52%) | Memory: 43.2 MB (48.34%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,30 +1,29 @@
 class UF {
     private int count;
     private int[] parent;
-    public UF(int n) {
-        this.count = n;
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
+    public UF(int numberOfVertex) {
+        this.count = numberOfVertex;
+        parent = new int[numberOfVertex];
+        for (int i = 0; i < numberOfVertex; i++) {
             parent[i] = i;
         }
     }
-    public int find(int x) {
-        if (parent[x] == x) {
-            return x;
+    public int find(int vertex) {
+        if (parent[vertex] != vertex) {
+            parent[vertex] = find(parent[vertex]);
         }
-        return find(parent[x]);
+        return parent[vertex];
     }
-    public void union(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        if (rootQ == rootP) {
-            return;
+    public void union(int vertexOne, int vertexTwo) {
+        int parentOne = find(vertexOne);
+        int parentTwo = find(vertexTwo);
+        if (parentOne != parentTwo) {
+            parent[parentOne] = parentTwo;
+            count--;
         }
-        parent[rootQ] = rootP;
-        count--;
     }
-    public boolean connected(int p, int q) {
-        return find(p) == find(q);
+    public boolean connected(int vertexOne, int vertexTwo) {
+        return find(vertexOne) == find(vertexTwo);
     }
     public int count() {
         return count;
@@ -34,12 +33,12 @@ class Solution {
     public int[] findRedundantConnection(int[][] edges) {
         UF uf = new UF(edges.length);
         for (int[] edge : edges) {
-            int vertexOne = edge[0] - 1;
-            int vertexTwo = edge[1] - 1;
-            if (uf.connected(vertexOne, vertexTwo)) {
+            int start = edge[0] - 1;
+            int end = edge[1] - 1;
+            if (uf.connected(start, end)) {
                 return edge;
             }
-            uf.union(vertexOne, vertexTwo);
+            uf.union(start, end);
         }
         return new int[]{};
     }
