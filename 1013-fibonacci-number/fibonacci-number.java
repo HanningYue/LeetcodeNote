ed12291cfc16048b47dcf9f208b7018f88b96051
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-06 19:53:17 -0800

    Time: 0 ms (100.00%) | Memory: 40.1 MB (60.12%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,14 +1,23 @@
+/**
+top-bottom recursion
+f(20) -> f(19) -> ... -> f(1)
+bottom-top dp
+    Create a dp table to save from dp[0] -> dp[1] -> dp[i] = dp[i - 2] + dp[i - 1]
+    When creating, always int[n + 1] because dp[0] = 0
+*/
 class Solution {
     public int fib(int n) {
-        if (n == 0) {
-            return 0;
+        if (n == 0 || n == 1) {
+            return n;
         }
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
+        
+        int dpi1 = 0, dpi2 = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            int dpi3 = dpi1 + dpi2;
+            //Update DP table
+            dpi1 = dpi2;
+            dpi2 = dpi3;
         }
-        return dp[n];
+        return dpi2;
     }
 }
