2072af9c9dd03a7a5effa4d0ef76593b7ac935b2
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-06 17:07:10 -0800

    Time: 5 ms (100.00%) | Memory: 44.9 MB (61.08%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -1,19 +1,29 @@
 /**
-1. Because this is a binary search tree, the desired node must be greater than its left child node and also less than its right child node
-2. So three situations, 
-    if greater than both p and q, recursion check root.left.because root.left < root 
-    if less than both p and q, recursion check root.right. because root.right > root
-    return desired root if it is the TreeNode itself or it is the right father TreeNode
-*/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root.val > p.val && root.val > q.val) {
-            return lowestCommonAncestor(root.left, p, q);
-        } else if (root.val < p.val && root.val < q.val) {
-            return lowestCommonAncestor(root.right, p, q);
-        } else {
-            return root;
+        int min = Math.min(p.val, q.val);
+        int max = Math.max(p.val, q.val);
+        return find(root, min, max);
+    }
+    private TreeNode find(TreeNode root, int min, int max) {
+        if (root == null) {
+            return null;
         }
+        if (root.val > max) {
+            return find(root.left, min, max);
+        }
+        if (root.val < min) {
+            return find(root.right, min, max);
+        }
+        return root;
     }
 }
