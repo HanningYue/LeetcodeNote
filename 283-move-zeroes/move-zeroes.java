f4f26701dabb6ca65e92b527731b958a923b0b0f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-06 17:42:52 -0800

    Time: 1 ms (100.00%) | Memory: 46.2 MB (37.97%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,19 +1,15 @@
-/**
-
-*/
 class Solution {
     public void moveZeroes(int[] nums) {
-        int slow = 0, fast = 0;
-        while (fast < nums.length) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
             if (nums[fast] != 0) {
                 nums[slow] = nums[fast];
                 slow++;
             }
-            fast++;
         }
-        for (int i = slow; i < nums.length; i++) {
-            nums[i] = 0;
+        while (slow < nums.length) {
+            nums[slow] = 0;
+            slow++;
         }
-        return;
     }
 }
