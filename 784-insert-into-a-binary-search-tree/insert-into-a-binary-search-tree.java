7cdc355e9809153adbf070e179647a3f621f9e98
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-06 16:08:00 -0800

    Time: 0 ms (100.00%) | Memory: 45.4 MB (25.75%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.java
@@ -13,23 +13,16 @@
  *     }
  * }
  */
- /**
-We need to insert, what we need? 
-The correct position of this inserting root, so if val > root.val, root.right = insert(root.right)
-if val < root.val, root.left = insert(root.left)
-Since the node is guaranteed that the new value does not exist in the original BST. 
-We can insert at the null space. When inserting, when null, create new TreeNode
-*/
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
         if (root == null) {
-            TreeNode insert = new TreeNode(val);
-            return insert;
+            root = new TreeNode(val);
+            return root;
         }
-        if (root.val < val) {
-            root.right = insertIntoBST(root.right, val);
-        } else if (root.val > val) {
+        if (root.val > val) {
             root.left = insertIntoBST(root.left, val);
+        } else if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
         }
         return root;
     }
