51435075683c0aadbb1ec057b4a4bb48ae972adb
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-07 19:30:47 -0800

    Time: 1 ms (99.79%) | Memory: 57.3 MB (31.12%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -3,11 +3,11 @@ class Solution {
         int result = Integer.MIN_VALUE;
         int runningSum = 0;
         for (int num : nums) {
-            runningSum += num;
-            result = Math.max(result, runningSum);
             if (runningSum < 0) {
                 runningSum = 0;
             }
+            runningSum += num;
+            result = Math.max(result, runningSum);
         }
         return result;
     }
