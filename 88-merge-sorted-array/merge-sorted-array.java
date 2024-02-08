2b844815ef58a5ad99f6cbe9daedc87e36ac2664
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-07 23:21:46 -0800

    Time: 0 ms (100.00%) | Memory: 42.5 MB (5.75%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -4,22 +4,16 @@ class Solution {
         int onePointer = m - 1, twoPointer = n - 1;
         while (onePointer >= 0 && twoPointer >= 0) {
             if (nums2[twoPointer] > nums1[onePointer]) {
-                nums1[resultPointer] = nums2[twoPointer];
-                resultPointer--;
-                twoPointer--;
+                nums1[resultPointer--] = nums2[twoPointer--];
             } else {
-                nums1[resultPointer] = nums1[onePointer];
-                resultPointer--;
-                onePointer--;
+                nums1[resultPointer--] = nums1[onePointer--];
             }
         }
         if (onePointer >= 0) {
             return;
         } else if (twoPointer >= 0) {
             while (resultPointer >= 0 && twoPointer >= 0) {
-                nums1[resultPointer] = nums2[twoPointer];
-                resultPointer--;
-                twoPointer--;
+                nums1[resultPointer--] = nums2[twoPointer--];
             }
         }
     }
