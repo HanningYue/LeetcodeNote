13434ea4f94e0ab707e99dd10daada64498799d7
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-07 19:40:54 -0800

    Time: 2 ms (13.82%) | Memory: 57.6 MB (21.29%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -13,7 +13,7 @@ class Solution {
         return result;
     }
 }
-*/
+
 class Solution {
     public int maxSubArray(int[] nums) {
         int[] presum = new int[nums.length + 1];
@@ -26,6 +26,26 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             minVal = Math.min(minVal, presum[i]);
             result = Math.max(result, presum[i + 1] - minVal);
+        }
+        return result;
+    }
+}
+*/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
+        }
+
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            result = Math.max(result, dp[i]);
         }
         return result;
     }
