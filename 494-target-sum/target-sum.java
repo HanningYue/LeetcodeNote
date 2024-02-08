790b7a66c67a6af3aa2d4f7c8ed4f8972bcac750
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-07 22:56:00 -0800

    Time: 96 ms (50.87%) | Memory: 45.5 MB (10.54%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,25 +1,27 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        if (nums.length == 0) {
-            return 0;
-        }        
-        backTrack(nums, 0, target, 0);
-        return result;
+    int findTargetSumWays(int[] nums, int target) {
+        if (nums.length == 0) return 0;
+        return dp(nums, 0, target);
     }
-    private int result = 0;
-    private void backTrack(int[] nums, int index, int target, int sum) {
-        if (index == nums.length) {
-            if (target == sum) {
-                result++;
-            }
-            return;
-        }
-        sum += nums[index];
-        backTrack(nums, index + 1, target, sum);
-        sum -= nums[index];
 
-        sum -= nums[index];
-        backTrack(nums, index + 1, target, sum);
-        sum += nums[index];
+    // 备忘录
+    HashMap<String, Integer> memo = new HashMap<>();
+    int dp(int[] nums, int i, int remain) {
+        // base case
+        if (i == nums.length) {
+            if (remain == 0) return 1;
+            return 0;
+        }
+        // 把它俩转成字符串才能作为哈希表的键
+        String key = i + "," + remain;
+        // 避免重复计算
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        // 还是穷举
+        int result = dp(nums, i + 1, remain - nums[i]) + dp(nums, i + 1, remain + nums[i]);
+        // 记入备忘录
+        memo.put(key, result);
+        return result;
     }
 }
