48b0ff93cf0bd48f412eb47b1364a1a944a60f61
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-07 19:22:53 -0800

    Time: 3 ms (97.86%) | Memory: 44.9 MB (47.86%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -14,23 +14,22 @@ class Solution {
         if (j == -1) {
             return i + 1;
         }
-
         if (dpTable[i][j] != -1) {
             return dpTable[i][j];
         }
+        
         if (s1.charAt(i) == s2.charAt(j)) {
             dpTable[i][j] = dp(s1, i - 1, s2, j - 1);
         } else {
-            dpTable[i][j] 
-            = min(
+            dpTable[i][j] = findMin (
                 dp(s1, i - 1, s2, j) + 1,
                 dp(s1, i - 1, s2, j - 1) + 1,
                 dp(s1, i, s2, j - 1) + 1
-                );
+            );
         }
         return dpTable[i][j];
     }
-    private int min(int a, int b, int c) {
-        return Math.min(Math.min(a, b), c);
+    private int findMin(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
     }
 }
