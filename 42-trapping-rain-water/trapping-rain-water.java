1876e47009dadb06fc81cfbcf6925b43fcc61e47
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-08 00:58:09 -0800

    Time: 0 ms (100.00%) | Memory: 46 MB (51.56%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,33 +1,22 @@
-/**
-    For current
-    min(Max(left), Max(right)) - h[current]
-
-input               0 1 0 2 1 0 1 3 2 1 2 1
-maxLeft             0 0 1 1 2 2 2 2 3 3 3 3
-maxRight            3 3 3 3 3 3 3 2 2 2 1 0
-min(Left, Right)    0 0 1 1 2 2 2 2 2 2 1 0
-Result =            0 0 1 0 1 2 1 0 0 1 0 0 = 1 + 1 + 2 + 1 + 1 = 6
-O(n) O(n)
-The amount of water that can be trapped at any given position is determined by the height of the shorter wall on either side of that position. Water will overflow from the shorter side, so the height of the shorter side acts as the limiting factor.
-*/
 class Solution {
     public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
         int result = 0;
-        int maxLeft = 0, maxRight = 0;
-        int leftPointer = 0, rightPointer = height.length - 1;
-        while (leftPointer < rightPointer) {
-            if (height[leftPointer] < height[rightPointer]) {
-                maxLeft = Math.max(maxLeft, height[leftPointer]);
-                if (maxLeft > height[leftPointer]) {
-                    result += maxLeft - height[leftPointer];
-                }              
-                leftPointer++;  
-            } else {
-                maxRight = Math.max(maxRight, height[rightPointer]);
-                if (maxRight > height[rightPointer]) {
-                    result += maxRight - height[rightPointer];
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                leftMax = Math.max(leftMax, height[left]);
+                if (leftMax > height[left]) {
+                    result += leftMax - height[left];
                 }
-                rightPointer--;
+                left++;
+            } else {
+                rightMax = Math.max(rightMax, height[right]);
+                if (rightMax > height[right]) {
+                    result += rightMax - height[right];
+                }
+                right--;
             }
         }
         return result;
