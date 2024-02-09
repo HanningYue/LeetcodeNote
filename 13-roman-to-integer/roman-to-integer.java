e4c0b3a7b3699acd3a7ab7439d2cc73d615949ee
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-08 17:44:02 -0800

    Time: 2 ms (100.00%) | Memory: 44.4 MB (74.11%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,42 +1,40 @@
 /**
-IV IX  XL  XC  CD   CM
-4   9  40  90  400  900
+Since roman letter writtern from largest to smallest, traverse backwards. 
+Anything current that are smaller than the previous are categorized as IV, IX, XL, XC, CD, CM
+In the above 6 instances, add previous, minus current to get correct result. All other scenario 
+Just add up current to get final result
 */
 class Solution {
     public int romanToInt(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-        
-        Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            stack.push(c);
-        }
-        int result = 0;
-        while (!stack.isEmpty()) {
-            char current = stack.pop();
-            if (!stack.isEmpty()) {
-                if ((current == 'V' || current == 'X') && stack.peek() == 'I') {
-                    result += current == 'X' ? 9 : 4;
-                    stack.pop();
-                } else if ((current == 'L' || current == 'C') && stack.peek() == 'X') {
-                    result += current == 'L' ? 40 : 90;
-                    stack.pop();
-                } else if ((current == 'D' || current == 'M') && stack.peek() == 'C') {
-                    result += current == 'D' ? 400 : 900;
-                    stack.pop();
-                } else {
-                    result += map.get(current);
-                }
+        int prev = 0, result = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int current = romanToInt(s.charAt(i));
+            if (current < prev) {
+                result -= current;
             } else {
-                result += map.get(current);
+                result += current;
             }
+            prev = current;
         }
         return result;
+    }
+    private int romanToInt(char c) {
+        switch(c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return 0;
     }
 }
