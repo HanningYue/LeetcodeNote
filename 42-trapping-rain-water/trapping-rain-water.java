a1db73f37be3219122b4644836af2466bf83a357
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-08 19:08:31 -0800

    Time: 0 ms (100.00%) | Memory: 46.6 MB (18.48%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -2,8 +2,8 @@ class Solution {
     public int trap(int[] height) {
         int left = 0, right = height.length - 1;
         int leftMax = 0, rightMax = 0;
-        int result = 0;
 
+        int result = 0;
         while (left < right) {
             if (height[left] < height[right]) {
                 leftMax = Math.max(leftMax, height[left]);
@@ -11,7 +11,8 @@ class Solution {
                     result += leftMax - height[left];
                 }
                 left++;
-            } else {
+            }
+            else if (height[left] >= height[right]) {
                 rightMax = Math.max(rightMax, height[right]);
                 if (rightMax > height[right]) {
                     result += rightMax - height[right];
