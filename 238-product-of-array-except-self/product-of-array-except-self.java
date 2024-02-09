b58759c8c177c0a95cab00a3611adc265cfa3b3c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-08 18:50:28 -0800

    Time: 2 ms (73.64%) | Memory: 53.3 MB (58.48%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,18 +1,23 @@
+/**
+[1, 2, 3, 4]. Use prefix product array
+left to right : [1, 1, 2, 6]
+right to left : [1 * 24, 1 * 12, 2 * 4, 6 because this is the final product from above]
+*/
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
-        Arrays.fill(result, 1);
 
-        int runningProduct = 1;
-        for (int i = 0; i < nums.length; i++) {
-            result[i] = result[i] * runningProduct;
-            runningProduct = runningProduct * nums[i];
+        result[0] = 1;
+        int currentProduct = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = currentProduct;
+            currentProduct *= nums[i];
         }
 
-        runningProduct = 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            result[i] = result[i] * runningProduct;
-            runningProduct = runningProduct * nums[i];
+        currentProduct = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            result[i] *= currentProduct;
+            currentProduct *= nums[i];
         }
         return result;
     }
