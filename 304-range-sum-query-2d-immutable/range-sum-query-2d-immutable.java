acc9519fc4dd9b084d3d0182e9ed5adc7247a3c4
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-10 01:32:18 -0800

    Time: 102 ms (57.81%) | Memory: 70.9 MB (36.10%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,21 +1,17 @@
 class NumMatrix {
-    private int[][] presum;
+    private int[][] prefixsum;
     public NumMatrix(int[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
-        presum = new int[m + 1][n + 1];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                presum[i + 1][j + 1] 
-                = presum[i][j + 1] + presum[i + 1][j] - presum[i][j] + matrix[i][j];
+        prefixsum = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int i = 1; i < prefixsum.length; i++) {
+            for (int j = 1; j < prefixsum[0].length; j++) {
+                prefixsum[i][j] = prefixsum[i - 1][j] + prefixsum[i][j - 1] + matrix[i - 1][j - 1] - prefixsum[i - 1][j - 1];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return presum[row2 + 1][col2 + 1] 
-        - presum[row2 + 1][col1] 
-        - presum[row1][col2 + 1]
-        + presum[row1][col1];
+        return prefixsum[row2 + 1][col2 + 1] - prefixsum[row2 + 1][col1] 
+        - prefixsum[row1][col2 + 1] + prefixsum[row1][col1];
     }
 }
 
