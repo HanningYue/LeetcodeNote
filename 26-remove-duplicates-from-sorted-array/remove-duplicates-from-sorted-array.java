aaf77458c703cb10ace3d9cb7c431f4353a40843
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-09 22:30:05 -0800

    Time: 1 ms (81.36%) | Memory: 44.3 MB (88.30%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,15 +1,11 @@
-/**
-快慢指针，当快指针遇到慢指针相同的val，跳过
-*/
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int slow = 0, fast = 0;
-        while (fast < nums.length) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
             if (nums[fast] != nums[slow]) {
                 slow++;
                 nums[slow] = nums[fast];
             }
-            fast++;
         }
         return slow + 1;
     }
