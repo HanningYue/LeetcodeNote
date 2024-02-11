2031ae0a75631a9e017d098a5c9235cd5ac3bf24
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-10 22:50:04 -0800

    Time: 2 ms (73.94%) | Memory: 53.2 MB (68.55%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -6,8 +6,8 @@ right to left : [1 * 24, 1 * 12, 2 * 4, 6 because this is the final product from
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
-
         result[0] = 1;
+
         int currentProduct = nums[0];
         for (int i = 1; i < nums.length; i++) {
             result[i] = currentProduct;
@@ -16,7 +16,7 @@ class Solution {
 
         currentProduct = nums[nums.length - 1];
         for (int i = nums.length - 2; i >= 0; i--) {
-            result[i] *= currentProduct;
+            result[i] = currentProduct * result[i];
             currentProduct *= nums[i];
         }
         return result;
