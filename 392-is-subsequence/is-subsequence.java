48e0e2b46a3fbe8d11ca1cc90b1c8214c0fad93f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-11 02:10:12 -0800

    Time: 1 ms (91.27%) | Memory: 41.5 MB (39.34%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0 && t.length() == 0) {
+            return true;
+        }
+        if (s.length() == 0 && t.length() != 0) {
+            return true;
+        } else if (s.length() != 0 && t.length() == 0) {
+            return false;
+        }
+        
+        int sPointer = 0, tPointer = 0;
+        while (tPointer < t.length()) {
+            char tChar = t.charAt(tPointer);
+            char sChar = s.charAt(sPointer);
+            if (tChar == sChar) {
+                sPointer++;
+                tPointer++;
+                if (sPointer == s.length()) {
+                    return true;
+                }
+            } else {
+                tPointer++;
+            }
+        }
+        return false;
+    }
+}
