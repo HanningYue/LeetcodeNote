42085a9a9a10f9fd1d0869045eba18a8892447f8
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-10 17:11:24 -0800

    Time: 8 ms (39.77%) | Memory: 58.6 MB (75.24%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -1,21 +1,36 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        int[] result = new int[n];
+        DiffArr diff = new DiffArr(new int[n]);
         for (int[] booking : bookings) {
-            int from = booking[0] - 1;
-            int to = booking[1] - 1;
+            int left = booking[0] - 1;
+            int right = booking[1] - 1;
             int value = booking[2];
-            prefix(result, from, to, value);
-        }
-        for (int i = 1; i < result.length; i++) {
-            result[i] = result[i] + result[i - 1];
-        }
-        return result;
+            diff.increase(left, right, value);
+        }        
+        return diff.result();
     }
-    private void prefix(int[] nums, int left, int right, int value) {
-        nums[left] += value;
-        if (right + 1 < nums.length) {
-            nums[right + 1] -= value;
+}
+class DiffArr {
+    private int[] diff;
+    public DiffArr(int[] nums) {
+        diff = new int[nums.length];
+        diff[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            diff[i] = nums[i] - nums[i - 1];
         }
+    }
+    public void increase(int left, int right, int value) {
+        diff[left] += value;
+        if (right + 1 < diff.length) {
+            diff[right + 1] -= value;
+        }
+    }
+    public int[] result() {
+        int[] res = new int[diff.length];
+        res[0] = diff[0];
+        for (int i = 1; i < diff.length; i++) {
+            res[i] = res[i - 1] + diff[i];
+        }
+        return res;
     }
 }
