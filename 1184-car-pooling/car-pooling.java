ef916ff1cfee0332a41a00b5c62ca283c5aa2175
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-10 22:48:15 -0800

    Time: 5 ms (56.45%) | Memory: 43.7 MB (91.59%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        DiffArr diff = new DiffArr(new int[1001]);
+        Diff diff = new Diff(new int[1001]);
         for (int[] trip : trips) {
             int from = trip[1];
             int to = trip[2] - 1;
             int value = trip[0];
             diff.increase(from, to, value);
-        }
+        }        
         int[] result = diff.result();
         for (int i = 0; i < result.length; i++) {
             if (result[i] > capacity) {
@@ -16,9 +16,9 @@ class Solution {
         return true;
     }
 }
-class DiffArr {
+class Diff {
     private int[] diff;
-    public DiffArr(int[] nums) {
+    public Diff(int[] nums) {
         diff = new int[nums.length];
         diff[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
@@ -35,7 +35,7 @@ class DiffArr {
         int[] res = new int[diff.length];
         res[0] = diff[0];
         for (int i = 1; i < diff.length; i++) {
-            res[i] = res[i - 1] + diff[i];
+            res[i] = diff[i] + res[i - 1];
         }
         return res;
     }
