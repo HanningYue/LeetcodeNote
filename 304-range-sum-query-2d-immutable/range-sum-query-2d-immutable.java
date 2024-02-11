1f8438b85add36d962ff8355f004d7d20e2faf15
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-10 17:01:37 -0800

    Time: 101 ms (63.54%) | Memory: 71.1 MB (27.43%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,22 +1,15 @@
 class NumMatrix {
-    private int[][] prefixsum;
+    private int[][] presum;
     public NumMatrix(int[][] matrix) {
-        prefixsum = new int[matrix.length + 1][matrix[0].length + 1];
-        for (int i = 1; i < prefixsum.length; i++) {
-            for (int j = 1; j < prefixsum[0].length; j++) {
-                prefixsum[i][j] = prefixsum[i - 1][j] + prefixsum[i][j - 1] + matrix[i - 1][j - 1] - prefixsum[i - 1][j - 1];
+        presum = new int[matrix.length + 1][matrix[0].length + 1];
+        for (int i = 1; i < presum.length; i++) {
+            for (int j = 1; j < presum[0].length; j++) {
+                presum[i][j] = matrix[i - 1][j - 1] + presum[i][j - 1] + presum[i - 1][j] - presum[i - 1][j - 1];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return prefixsum[row2 + 1][col2 + 1] - prefixsum[row2 + 1][col1] 
-        - prefixsum[row1][col2 + 1] + prefixsum[row1][col1];
+        return presum[row2 + 1][col2 + 1] - presum[row2 + 1][col1] - presum[row1][col2 + 1] + presum[row1][col1];
     }
 }
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
