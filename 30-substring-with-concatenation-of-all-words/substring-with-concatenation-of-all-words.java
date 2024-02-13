4a2166f30b16be5f92624d10090fd01629109ed4
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-12 20:51:21 -0800

    Time: 2343 ms (22.83%) | Memory: 46.1 MB (30.77%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -5,20 +5,24 @@ class Solution {
         for (String word : words) {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
-        int wordLength = words[0].length();
-        int wordCount = words.length;
-        for (int i = 0; i <= s.length() - wordLength * wordCount; i++) {
+        int eachWordLength = words[0].length();
+        int totalLength = words.length * eachWordLength;
+
+        for (int i = 0; i + totalLength <= s.length(); i++) {
             Map<String, Integer> localMap = new HashMap<>();
-            for (int j = 0; j < wordCount; j++) {
-                Integer nextWordStart = i + j * wordLength;
-                String nextWord = s.substring(nextWordStart, nextWordStart + wordLength);
-                if (!map.containsKey(nextWord))
+            for (int j = 0; j < words.length; j++) {
+                int nextWordStart = i + j * eachWordLength;
+                String nextWord = s.substring(nextWordStart, nextWordStart + eachWordLength);
+                if (!map.containsKey(nextWord)){
                     break;
+                }
                 localMap.put(nextWord, localMap.getOrDefault(nextWord, 0) + 1);
-                if (localMap.get(nextWord) > map.get(nextWord))
+                if (map.get(nextWord) < localMap.get(nextWord)) {
                     break;
-                if (j + 1 == wordCount)
+                }
+                if (j + 1 == words.length) {
                     result.add(i);
+                }
             }
         }
         return result;
