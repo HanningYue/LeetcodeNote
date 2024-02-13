4993e2767e333d5d1b7accb0e1b0ee28fa83684d
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-12 21:03:44 -0800

    Time: 2 ms (81.37%) | Memory: 44 MB (85.23%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         for (int i = 0; i < board.length; i++) {
-            Set<Character> row = new HashSet<>();
-            Set<Character> column = new HashSet<>();
-
+            Set<Character> rowSet = new HashSet<>();
+            Set<Character> colSet = new HashSet<>();
             for (int j = 0; j < board.length; j++) {
                 if (board[i][j] != '.') {
-                    if (row.contains(board[i][j])) {
+                    if (!rowSet.add(board[i][j])) {
                         return false;
                     }
-                    row.add(board[i][j]);
                 }
                 if (board[j][i] != '.') {
-                    if (column.contains(board[j][i])) {
+                    if (!colSet.add(board[j][i])) {
                         return false;
                     }
-                    column.add(board[j][i]);
                 }
             }
         }
@@ -33,10 +30,9 @@ class Solution {
         for (int i = row; i < row + 3; i++) {
             for (int j = col; j < col + 3; j++) {
                 if (board[i][j] != '.') {
-                    if (set.contains(board[i][j])) {
+                    if (!set.add(board[i][j])) {
                         return false;
                     }
-                    set.add(board[i][j]);
                 }
             }
         }
