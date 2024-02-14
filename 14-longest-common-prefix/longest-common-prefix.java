615d4da8127f8b91dbf29e6e6f06830d520ed8d4
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-13 17:00:52 -0800

    Time: 1 ms (80.08%) | Memory: 41.3 MB (65.55%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -4,13 +4,12 @@
 // 2 flight
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0 || strs == null) return "";
-        
         String prefix = strs[0];
         for (int i = 0; i < prefix.length(); i++) {
             char c = prefix.charAt(i);
             for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                String currentString = strs[j];
+                if (i >= currentString.length() || currentString.charAt(i) != c) {
                     return prefix.substring(0, i);
                 }
             }
