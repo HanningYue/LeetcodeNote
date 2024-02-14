04e2e803496924ed9fa5aace19501da4df768f7e
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-13 16:42:22 -0800

    Time: 0 ms (100.00%) | Memory: 41.4 MB (68.28%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,36 +1,33 @@
 class Solution {
-    private int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, 
-    {0, 1}, {1, 1}, {1, -1}, {1, 0}};
-
     public void gameOfLife(int[][] board) {
-        int m = board.length, n = board[0].length;
-        int[][] result = new int[m][n];
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        int[][] result = new int[board.length][board[0].length];
+        int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 int count = 0;
                 for (int[] dir : directions) {
                     int newRow = i + dir[0];
                     int newCol = j + dir[1];
-                    if (newRow >= m || newRow < 0 || newCol >= n 
-                    || newCol < 0 || board[newRow][newCol] != 1) {
-                        continue;
+                    if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length
+                    && board[newRow][newCol] == 1)
+                    {
+                        count++;
                     }
-                    count++;
                 }
                 if (board[i][j] == 1) {
                     if (count == 2 || count == 3) {
                         result[i][j] = 1;
                     }
-                } else {
+                }
+                else if (board[i][j] == 0) {
                     if (count == 3) {
                         result[i][j] = 1;
                     }
                 }
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
                 board[i][j] = result[i][j];
             }
         }
