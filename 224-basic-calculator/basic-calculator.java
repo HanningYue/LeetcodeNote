e0bcda5ea8a399429cacf22e24c3eed0a1026c3c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-14 11:22:25 -0800

    Time: 18 ms (26.29%) | Memory: 49.3 MB (7.69%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int calculate(String s) {
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            if (c != ' ')
+                queue.offer(c);
+        }
+        queue.offer('+');
+        return helper(queue);
+    }
+    private int helper(Queue<Character> queue) {
+        Stack<Integer> stack = new Stack<>();
+        int num = 0;
+        char sign = '+';
+        while (!queue.isEmpty()) {
+            char current = queue.poll();
+
+            if (Character.isDigit(current)) {
+                num = num * 10 + (current - '0');
+            } 
+
+            if (current == '(') {
+                num = helper(queue);
+            }
+
+            if (!Character.isDigit(current) || queue.isEmpty()) {
+                if (sign == '+') {
+                    stack.push(num);
+                } else if (sign == '-') {
+                    stack.push(-num);
+                }
+                sign = current;
+                num = 0;
+
+                if (current == ')') {
+                    break;
+                }
+            }
+        }
+
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+}
