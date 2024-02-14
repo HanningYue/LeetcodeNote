721fa1e93fd92d60bf06491b15f96dca509a698f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-13 16:46:19 -0800

    Time: 0 ms (100.00%) | Memory: 41.7 MB (39.02%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,6 +1,5 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int[][] result = new int[board.length][board[0].length];
         int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
@@ -9,26 +8,26 @@ class Solution {
                     int newRow = i + dir[0];
                     int newCol = j + dir[1];
                     if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length
-                    && board[newRow][newCol] == 1)
+                    && (board[newRow][newCol] == 1 || board[newRow][newCol] == 2))
                     {
                         count++;
                     }
                 }
                 if (board[i][j] == 1) {
-                    if (count == 2 || count == 3) {
-                        result[i][j] = 1;
+                    if (count < 2 || count > 3) {
+                        board[i][j] = 2;
                     }
                 }
                 else if (board[i][j] == 0) {
                     if (count == 3) {
-                        result[i][j] = 1;
+                        board[i][j] = 3;
                     }
                 }
             }
         }
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[0].length; j++) {
-                board[i][j] = result[i][j];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = board[i][j] % 2;
             }
         }
     }
