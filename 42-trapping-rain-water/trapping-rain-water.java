071162e9a901973cdcfd4d24f8d800bc69bd8938
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-14 21:07:30 -0800

    Time: 661 ms (5.00%) | Memory: 46.2 MB (44.53%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,22 +1,15 @@
 class Solution {
     public int trap(int[] height) {
-        int[] leftMaxArr = new int[height.length];
-        int[] rightMaxArr = new int[height.length];
-
-        leftMaxArr[0] = height[0];
-        for (int i = 1; i < height.length; i++) {
-            leftMaxArr[i] = Math.max(height[i], leftMaxArr[i - 1]);
-        }
-
-        rightMaxArr[height.length - 1] = height[height.length - 1];
-        for (int j = height.length - 2; j >= 0; j--) {
-            rightMaxArr[j] = Math.max(height[j], rightMaxArr[j + 1]);
-        }
-
         int result = 0;
         for (int i = 1; i < height.length - 1; i++) {
-            int minOfTwoMax = Math.min(leftMaxArr[i], rightMaxArr[i]);
-            result += minOfTwoMax - height[i];
+            int leftMax = 0, rightMax = 0;
+            for (int j = i; j < height.length; j++) {
+                rightMax = Math.max(rightMax, height[j]);
+            }
+            for (int j = i; j >= 0; j--) {
+                leftMax = Math.max(leftMax, height[j]);
+            }
+            result += Math.min(rightMax, leftMax) - height[i];
         }
         return result;
     }
