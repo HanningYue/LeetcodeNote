0e22f81de5f7414a741faa75850ae17533f204ed
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-14 21:00:31 -0800

    Time: 2 ms (34.74%) | Memory: 56.5 MB (90.45%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,10 +1,21 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-        reverse(nums, 0, n - 1);
+        int[] newArr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            newArr[(i + k) % nums.length] = nums[i];
+        }
+        for (int i = 0; i < newArr.length; i++) {
+            nums[i] = newArr[i];
+        }
+    }
+}
+/*
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
+        reverse(nums, k, nums.length - 1);
     }
     private void reverse(int[] nums, int left, int right) {
         while (left < right) {
@@ -16,3 +27,4 @@ class Solution {
         }
     }
 }
+*/
