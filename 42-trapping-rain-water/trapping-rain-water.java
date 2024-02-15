ed48ff82ad7c8806c2d6af502d399139e88cb468
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-14 21:06:10 -0800

    Time: 1 ms (73.97%) | Memory: 45.7 MB (77.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,24 +1,22 @@
 class Solution {
     public int trap(int[] height) {
-        int left = 0, right = height.length - 1;
-        int leftMax = 0, rightMax = 0;
+        int[] leftMaxArr = new int[height.length];
+        int[] rightMaxArr = new int[height.length];
+
+        leftMaxArr[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            leftMaxArr[i] = Math.max(height[i], leftMaxArr[i - 1]);
+        }
+
+        rightMaxArr[height.length - 1] = height[height.length - 1];
+        for (int j = height.length - 2; j >= 0; j--) {
+            rightMaxArr[j] = Math.max(height[j], rightMaxArr[j + 1]);
+        }
 
         int result = 0;
-        while (left < right) {
-            if (height[left] < height[right]) {
-                leftMax = Math.max(leftMax, height[left]);
-                if (leftMax > height[left]) {
-                    result += leftMax - height[left];
-                }
-                left++;
-            }
-            else if (height[left] >= height[right]) {
-                rightMax = Math.max(rightMax, height[right]);
-                if (rightMax > height[right]) {
-                    result += rightMax - height[right];
-                }
-                right--;
-            }
+        for (int i = 1; i < height.length - 1; i++) {
+            int minOfTwoMax = Math.min(leftMaxArr[i], rightMaxArr[i]);
+            result += minOfTwoMax - height[i];
         }
         return result;
     }
