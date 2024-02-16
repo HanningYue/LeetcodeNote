b0550369fcbcfdde26bdf08e987a92de1058f41f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-15 20:02:42 -0800

    Time: 54 ms (67.81%) | Memory: 68.9 MB (60.39%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[1], b[1]);
+            }
+        });
+        int[] current = points[0];
+        int result = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] > current[1]) {
+                result++;
+                current = points[i];
+            }
+        }
+        return result;
+    }
+}
