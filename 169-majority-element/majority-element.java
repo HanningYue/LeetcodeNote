8468f350551dd3cef441807ce63d31b200107af6
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-15 20:18:27 -0800

    Time: 4 ms (55.82%) | Memory: 52.8 MB (5.34%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,20 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        return compare(nums, 0, nums.length - 1);
+        return divide(nums, 0, nums.length - 1);
     }
-    private int compare(int[] nums, int left, int right) {
+    private int divide (int[] nums, int left, int right) {
         if (left == right) {
             return nums[left];
         }
         int mid = left + (right - left) / 2;
-        int leftNum = compare(nums, left, mid);
-        int rightNum = compare(nums, mid + 1, right);
+        int leftPart = divide(nums, left, mid);
+        int rightPart = divide(nums, mid + 1, right);
 
-        int leftFrequent = find(nums, left, right, leftNum);
-        int rightFrequent = find(nums, left, right, rightNum);
-        return leftFrequent > rightFrequent ? leftNum : rightNum;
+        int leftMaxCount = conquer(nums, left, right, leftPart);
+        int rightMaxCount = conquer(nums, left, right, rightPart);
+        return leftMaxCount > rightMaxCount ? leftPart : rightPart;
     }
-    private int find(int[] nums, int left, int right, int target) {
+    private int conquer(int[] nums, int left, int right, int target) {
         int count = 0;
         for (int i = left; i <= right; i++) {
             if (nums[i] == target) {
