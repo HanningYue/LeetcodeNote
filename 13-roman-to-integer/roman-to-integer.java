501d108eaaae41f2b5af19d9b9fc08d43f2fc17b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-15 20:43:43 -0800

    Time: 5 ms (61.48%) | Memory: 45 MB (18.22%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,41 +1,25 @@
-/**
-Since roman letter writtern from largest to smallest, traverse backwards. 
-Anything current that are smaller than the previous are categorized as IV, IX, XL, XC, CD, CM
-In the above 6 instances, add previous, minus current to get correct result. All other scenario 
-Just add up current to get final result
-*/
 class Solution {
     public int romanToInt(String s) {
-        int prev = 0, result = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int current = romanToInt(s.charAt(i));
-            if (current < prev) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = map.get(s.charAt(s.length() - 1));
+        for (int i = s.length() - 2; i >= 0; i--) {
+            int previous = map.get(s.charAt(i + 1));
+            int current = map.get(s.charAt(i));
+            if (current < previous) {
                 result -= current;
-            } else {
+            } 
+            else if (current >= previous) {
                 result += current;
             }
-            prev = current;
         }
         return result;
-    }
-    private int romanToInt(char c) {
-        switch(c) {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-            default:
-                return 0;
-        }
     }
 }
