4baf0c21d665e743bf656e461e1c33b8943a183b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-18 14:17:30 -0800

    Time: 3 ms (89.10%) | Memory: 45.8 MB (20.54%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,24 +1,24 @@
 class Solution {
     public int candy(int[] ratings) {
-        int[] arr = new int[ratings.length];
-        Arrays.fill(arr, 1);
+        int[] resultArr = new int[ratings.length];
+        Arrays.fill(resultArr, 1);
 
         for (int i = 1; i < ratings.length; i++) {
             if (ratings[i] > ratings[i - 1]) {
-                arr[i] = arr[i - 1] + 1;
+                resultArr[i] = resultArr[i - 1] + 1;
             }
         }
 
         for (int i = ratings.length - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
-                arr[i] = Math.max(arr[i], arr[i + 1] + 1);
+                resultArr[i] = Math.max(resultArr[i + 1] + 1, resultArr[i]);
             }
         }
 
-        int total = 0;
-        for (int candy : arr) {
-            total += candy;
+        int result = 0;
+        for (int num : resultArr) {
+            result += num;
         }
-        return total;
+        return result;
     }
 }
