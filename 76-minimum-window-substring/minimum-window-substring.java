78741a88293661bf81a28113df01157fff6d16ba
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-19 15:50:14 -0800

    Time: 61 ms (20.15%) | Memory: 45.8 MB (9.22%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -5,31 +5,33 @@ class Solution {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        String result = "";
-        int minLength = Integer.MAX_VALUE;
         int slow = 0, fast = 0, match = 0;
+        String result = "";
+        int length = Integer.MAX_VALUE;
         while (fast < s.length()) {
-            char sChar = s.charAt(fast);
-            if (map.containsKey(sChar)) {
-                map.put(sChar, map.get(sChar) - 1);
-                if (map.get(sChar) >= 0) {
+            char c = s.charAt(fast);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+                if (map.get(c) >= 0) {
                     match++;
                 }
             }
+
             while (match == t.length()) {
-                if (fast - slow + 1 < minLength) {
-                    minLength = Math.min(minLength, fast - slow + 1);
+                if (length > fast - slow + 1) {
+                    length = fast - slow + 1;
                     result = s.substring(slow, fast + 1);
                 }
-                char slowChar = s.charAt(slow);
-                if (map.containsKey(slowChar)) {
-                    map.put(slowChar, map.get(slowChar) + 1);
-                    if (map.get(slowChar) > 0) {
+                c = s.charAt(slow);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) + 1);
+                    if (map.get(c) > 0) {
                         match--;
                     }
                 }
                 slow++;
             }
+            
             fast++;
         }
         return result;
