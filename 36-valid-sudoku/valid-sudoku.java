bcc74d713f0f7cbcb78273189c2425a6a7dbd09d
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-19 16:00:51 -0800

    Time: 2 ms (80.88%) | Memory: 44.5 MB (57.74%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -3,36 +3,32 @@ class Solution {
         for (int i = 0; i < board.length; i++) {
             Set<Character> rowSet = new HashSet<>();
             Set<Character> colSet = new HashSet<>();
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j] != '.') {
-                    if (!rowSet.add(board[i][j])) {
-                        return false;
-                    }
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != '.' && !rowSet.add(board[i][j])) {
+                    return false;
                 }
-                if (board[j][i] != '.') {
-                    if (!colSet.add(board[j][i])) {
-                        return false;
-                    }
+
+                if (board[j][i] != '.' && !colSet.add(board[j][i])) {
+                    return false;
                 }
             }
         }
+
         for (int i = 0; i < board.length; i += 3) {
-            for (int j = 0; j < board.length; j += 3) {
-                if (!smallGrid(i, j, board)) {
+            for (int j = 0; j < board[0].length; j += 3) {
+                if (!smallerGrid(board, i, j)) {
                     return false;
                 }
             }
         }
         return true;
     }
-    private boolean smallGrid(int row, int col, char[][] board) {
+    private boolean smallerGrid(char[][] board, int row, int col) {
         Set<Character> set = new HashSet<>();
         for (int i = row; i < row + 3; i++) {
             for (int j = col; j < col + 3; j++) {
-                if (board[i][j] != '.') {
-                    if (!set.add(board[i][j])) {
-                        return false;
-                    }
+                if (board[i][j] != '.' && !set.add(board[i][j])) {
+                    return false;
                 }
             }
         }
