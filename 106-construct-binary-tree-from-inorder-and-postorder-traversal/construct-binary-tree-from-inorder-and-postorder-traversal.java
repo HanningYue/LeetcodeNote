b83111414052a32e66addbc5809b14c097f70100
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-20 15:44:33 -0800

    Time: 2 ms (69.28%) | Memory: 44.3 MB (45.66%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -19,21 +19,30 @@ class Solution {
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
         }
-        return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, map);
+        return construct(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, map);
     }
-    private TreeNode build(int[] inorder, int inleft, int inright, int[] postorder, int postleft, int postright, 
-    Map<Integer, Integer> map)
+    private TreeNode construct(int[] inorder, int inleft, int inright, 
+    int[] postorder, int postleft, int postright, Map<Integer, Integer> map)
     {
         if (postleft > postright) {
             return null;
         }
         TreeNode head = new TreeNode(postorder[postright]);
         int headIdx = map.get(head.val);
-        int leftSize = headIdx - inleft;
-        
-        head.left = build(inorder, inleft, headIdx - 1, postorder, postleft, postleft + leftSize - 1, map);
-        head.right = build(inorder, headIdx + 1, inright, postorder, postleft + leftSize, postright - 1, map);
+        int leftRange = headIdx - inleft;
 
+        head.left = construct(inorder, inleft, headIdx - 1,
+        postorder, postleft, postleft + leftRange - 1, map);
+
+        head.right = construct(inorder, headIdx + 1, inright,
+        postorder, postleft + leftRange, postright - 1, map);
         return head;
     }
 }
+//     1
+// 2       3
+//     4       5
+// inorder: 2 1 4 3 5
+//          l h 
+// postorder: 2 4 5 3 1
+//            l r r r h
