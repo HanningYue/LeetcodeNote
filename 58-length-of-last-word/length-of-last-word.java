677f99123ad29eaa6bcaedd354255bfa9cce66b7
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-20 16:23:27 -0800

    Time: 0 ms (100.00%) | Memory: 41.3 MB (95.36%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,30 +1,14 @@
-/*
 class Solution {
     public int lengthOfLastWord(String s) {
-        int length = 0;
-        int slow = s.length() - 1, fast = s.length() - 1;
-        while (fast >= 0) {
-            if (s.charAt(fast) != ' ') {
-                slow = fast;
-                while (s.charAt(fast) != ' ' && fast > 0) {
-                    fast--;
-                }
-                if (s.charAt(fast) == ' ') {
-                    length = slow - fast;
-                } else if (fast == 0) {
-                    length = slow - fast + 1;
-                }
-                break;
-            }
-            fast--;
+        int i = s.length() - 1;
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
         }
-        return length;
-    }
-}
-*/
-class Solution {
-    public int lengthOfLastWord(String s) {
-        String[] words = s.split(" ");
-        return words[words.length - 1].length();
+        int result = 0;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            i--;
+            result++;
+        }
+        return result;
     }
 }
