3c67b8b2b0855fc81477ea4402be7546b169725b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-20 16:46:49 -0800

    Time: 0 ms (100.00%) | Memory: 43.3 MB (5.67%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -13,25 +13,23 @@
  *     }
  * }
  */
-/**
-周长，如果是最长，那么一定是leaf
-递归，向左向右要最深的高度，加上1（当前层高度）
-每次递归 更新最大周长
- */
 class Solution {
-    int maxDiameter = Integer.MIN_VALUE;
+    private int maxDiameter = Integer.MIN_VALUE;
     public int diameterOfBinaryTree(TreeNode root) {
         maxDepth(root);
-        return maxDiameter;
+        return maxDiameter == Integer.MIN_VALUE ? 0 : maxDiameter;
     }
+
     private int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
+
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-        maxDiameter = Math.max(maxDiameter, left + right);
         
-        return Math.max(left, right) + 1;
+        maxDiameter = Math.max(maxDiameter, left + right);
+        int currentMaxDepth = Math.max(left, right) + 1;
+        return currentMaxDepth;
     }
 }
