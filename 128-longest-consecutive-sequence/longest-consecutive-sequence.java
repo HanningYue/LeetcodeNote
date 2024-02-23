854a8b71ae48eb6dc36569f73aa1ede7ac5611fd
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-22 17:49:03 -0800

    Time: 51 ms (43.92%) | Memory: 56.5 MB (89.39%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,21 +1,59 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
-        }
-
-        int result = 0;
-        for (int num : set) {
-            if (!set.contains(num - 1)) {
-                int currentLength = 1;
-                while (set.contains(num + 1)) {
-                    num++;
-                    currentLength++;
-                }
-                result = Math.max(result, currentLength);
+        UF uf = new UF(nums.length);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                continue;
+            }
+            map.put(nums[i], i);
+            if (map.containsKey(nums[i] + 1)) {
+                uf.union(i, map.get(nums[i] + 1));
+            }
+            if (map.containsKey(nums[i] - 1)) {
+                uf.union(i, map.get(nums[i] - 1));
             }
         }
-        return result;
+        return uf.maxUnion();
+    }
+}
+class UF {
+    private int[] parents;
+    private int count;
+    public UF(int n) {
+        parents = new int[n];
+        this.count = n;
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+        }
+    }
+    public int find(int vertexOne) {
+        if (parents[vertexOne] != vertexOne) {
+            parents[vertexOne] = find(parents[vertexOne]);
+        }
+        return parents[vertexOne];
+
+    }
+    public void union(int one, int two) {
+        if (find(one) != find(two)) {
+            parents[find(one)] = parents[find(two)];
+            count--;
+        }
+        return;
+    }
+    public boolean connected(int one, int two) {
+        return find(one) == find(two);
+    }
+    public int count() {
+        return count;
+    }
+    public int maxUnion() {
+        int[] result = new int[parents.length];
+        int max = 0;
+        for (int i = 0; i < parents.length; i++) {
+            result[find(i)]++;
+            max = Math.max(max, result[find(i)]);
+        }
+        return max;
     }
 }
