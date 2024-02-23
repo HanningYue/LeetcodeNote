22765fbe7c887041e713cd8dcb8bf346016036ba
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-22 19:10:59 -0800

    Time: 1 ms (70.38%) | Memory: 42 MB (47.43%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -1,17 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        dfs(result, 0, root);
-        return result;
-    }
-    private void dfs(List<Integer> result, int level, TreeNode root) {
         if (root == null) {
-            return;
+            return result;
         }
-        if (level == result.size()) {
-            result.add(root.val);
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                if (i == size - 1) {
+                    result.add(current.val);
+                }
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
         }
-        dfs(result, level + 1, root.right);
-        dfs(result, level + 1, root.left);
+        return result;
     }
 }
