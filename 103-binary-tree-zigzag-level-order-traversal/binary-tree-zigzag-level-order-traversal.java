ea424341e75a38e1961a1e741632d1495ab7fcd3
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-22 19:38:56 -0800

    Time: 1 ms (78.66%) | Memory: 42.4 MB (17.33%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -19,24 +19,25 @@ class Solution {
         if (root == null) {
             return result;
         }
-        Deque<TreeNode> deque = new ArrayDeque<>();
+        Deque<TreeNode> deque = new LinkedList<>();
         deque.offerLast(root);
         boolean even = true;
+
         while (!deque.isEmpty()) {
             int size = deque.size();
             List<Integer> list = new ArrayList<>();
             for (int i = 0; i < size; i++) {
                 if (even) {
                     TreeNode current = deque.pollLast();
-                    list.add(current.val);
                     if (current.left != null) deque.offerFirst(current.left);
                     if (current.right != null) deque.offerFirst(current.right);
-                }
-                if (!even) {
-                    TreeNode current = deque.pollFirst();
                     list.add(current.val);
+                }
+                else {
+                    TreeNode current = deque.pollFirst();
                     if (current.right != null) deque.offerLast(current.right);
                     if (current.left != null) deque.offerLast(current.left);
+                    list.add(current.val);
                 }
             }
             even = !even;
