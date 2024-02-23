d65a3d6273c2bab0c43044e69b1190eb432fdb73
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-22 21:09:17 -0800

    Time: 53 ms (42.98%) | Memory: 61.6 MB (57.82%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -8,35 +8,28 @@ class Solution {
             }
             map.put(nums[i], i);
             if (map.containsKey(nums[i] + 1)) {
-                uf.union(i, map.get(nums[i] + 1));
+                uf.union(map.get(nums[i] + 1), i);
             }
             if (map.containsKey(nums[i] - 1)) {
-                uf.union(i, map.get(nums[i] - 1));
+                uf.union(map.get(nums[i] - 1), i);
             }
         }
-        return uf.maxUnion();
+        return uf.maxUnionGroup();
     }
 }
 class UF {
-    private int[] parents;
     private int count;
-    public UF(int n) {
-        parents = new int[n];
-        this.count = n;
-        for (int i = 0; i < n; i++) {
+    private int[] parents;
+    public UF(int num) {
+        this.count = num;
+        parents = new int[num];
+        for (int i = 0; i < num; i++) {
             parents[i] = i;
         }
     }
-    public int find(int vertexOne) {
-        if (parents[vertexOne] != vertexOne) {
-            parents[vertexOne] = find(parents[vertexOne]);
-        }
-        return parents[vertexOne];
-
-    }
     public void union(int one, int two) {
         if (find(one) != find(two)) {
-            parents[find(one)] = parents[find(two)];
+            parents[find(one)] = find(two);
             count--;
         }
         return;
@@ -44,15 +37,21 @@ class UF {
     public boolean connected(int one, int two) {
         return find(one) == find(two);
     }
+    public int find(int vertex) {
+        if (parents[vertex] != vertex) {
+            parents[vertex] = find(parents[vertex]);
+        }
+        return parents[vertex];
+    }
     public int count() {
         return count;
     }
-    public int maxUnion() {
-        int[] result = new int[parents.length];
+    public int maxUnionGroup() {
+        int[] arrForIdxAsParent = new int[parents.length];
         int max = 0;
         for (int i = 0; i < parents.length; i++) {
-            result[find(i)]++;
-            max = Math.max(max, result[find(i)]);
+            arrForIdxAsParent[find(i)]++;
+            max = Math.max(max, arrForIdxAsParent[find(i)]);
         }
         return max;
     }
