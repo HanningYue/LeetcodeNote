97882345341e59c69b98813fff4bd1a22808f1e4
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-24 00:34:17 -0800

    Time: 2 ms (49.62%) | Memory: 44.7 MB (78.28%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -4,12 +4,10 @@ class Solution {
         for (int i = 0; i < intervals.length; i++) {
             if (intervals[i][1] < newInterval[0]) {
                 result.add(intervals[i]);
-            } 
-            else if (intervals[i][0] > newInterval[1]) {
+            } else if (intervals[i][0] > newInterval[1]) {
                 result.add(newInterval);
                 newInterval = intervals[i];
-            } 
-            else if (intervals[i][1] >= newInterval[0] && intervals[i][0] <= newInterval[1]){
+            } else if (newInterval[1] >= intervals[i][0] && newInterval[0] <= intervals[i][1]) {
                 newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
                 newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             }
