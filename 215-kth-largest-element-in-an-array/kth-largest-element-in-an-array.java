84019ef03918f4828fdeeeeb4d7202290aadd58e
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-23 23:56:31 -0800

    Time: 2208 ms (5.09%) | Memory: 57 MB (88.39%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,23 +1,36 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         k = nums.length - k;
-        shuffle(nums);
-
+        shuffleArr(nums);
+        
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int pivot = partition(nums, left, right);
-            if (pivot == k) {
-                return nums[pivot];
-            } else if (pivot > k) {
-                right = pivot - 1;
-            } else if (pivot < k) {
-                left = pivot + 1;
+            int pivotIdx = partition(nums, left, right);
+            if (pivotIdx == k) {
+                return nums[pivotIdx];
+            } else if (pivotIdx < k) {
+                left = pivotIdx + 1;
+            } else if (pivotIdx > k) {
+                right = pivotIdx - 1;
             }
         }
         return -1;
     }
+    private void shuffleArr(int[] nums) {
+        Random rand = new Random();
+        for (int i = 0; i < nums.length; i++) {
+            int nextIdx = rand.nextInt(nums.length - i);
+            swap(nums, i, nextIdx);
+        }
+    }
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
     private int partition(int[] nums, int left, int right) {
         int pivot = nums[left];
+
         int leftP = left, rightP = right;
         while (leftP < rightP) {
             while (leftP < rightP && nums[rightP] >= pivot) {
@@ -30,17 +43,5 @@ class Solution {
         }
         swap(nums, left, rightP);
         return rightP;
-    }
-    private void shuffle(int[] nums) {
-        Random rand = new Random();
-        for (int i = 0; i < nums.length; i++) {
-            int nextIdx = rand.nextInt(nums.length - i);
-            swap(nums, i, nextIdx);
-        }
-    }
-    private void swap(int[] nums, int left, int right) {
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
     }
 }
