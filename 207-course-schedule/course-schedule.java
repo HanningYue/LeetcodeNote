170a5a6891405e8c72807bb6da2834b27dea56e3
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-24 19:35:34 -0800

    Time: 3 ms (95.62%) | Memory: 44.9 MB (55.97%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,25 +1,24 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < numCourses; i++) {
-            graph.put(i, new ArrayList<>());
+        boolean[] visited = new boolean[numCourses];
+        boolean[] visiting = new boolean[numCourses];
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0 ; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
         }
         for (int[] pre : prerequisites) {
             int from = pre[1];
             int to = pre[0];
             graph.get(from).add(to);
         }
-        boolean[] visiting = new boolean[numCourses];
-        boolean[] visited = new boolean[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(graph, i, visiting, visited)) {
+            if (!dfs(graph, i, visited, visiting)) {
                 return false;
             }
         }
         return true;
     }
-    private boolean dfs(Map<Integer, List<Integer>> graph, int vertex, boolean[] visiting, 
-    boolean[] visited) {
+    private boolean dfs(List<List<Integer>> graph, int vertex, boolean[] visited, boolean[] visiting) {
         if (visiting[vertex]) {
             return false;
         }
@@ -28,7 +27,7 @@ class Solution {
         }
         visiting[vertex] = true;
         for (int neighbor : graph.get(vertex)) {
-            if (!dfs(graph, neighbor, visiting, visited)) {
+            if (!dfs(graph, neighbor, visited, visiting)) {
                 return false;
             }
         }
