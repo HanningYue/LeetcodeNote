1aab3ed8973a963063204c0b61176d935c8e83da
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-24 18:49:43 -0800

    Time: 2 ms (89.73%) | Memory: 45.5 MB (34.97%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,24 +1,24 @@
 class Solution {
     public void solve(char[][] board) {
-        int m = board.length, n = board[0].length;
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < board.length; i++) {
             if (board[i][0] == 'O') {
                 dfs(board, i, 0);
             }
-            if (board[i][n - 1] == 'O') {
-                dfs(board, i, n - 1);
+            if (board[i][board[0].length - 1] == 'O') {
+                dfs(board, i, board[0].length - 1);
             }
         }
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < board[0].length; j++) {
             if (board[0][j] == 'O') {
                 dfs(board, 0, j);
             }
-            if (board[m - 1][j] == 'O') {
-                dfs(board, m - 1, j);
+            if (board[board.length - 1][j] == 'O') {
+                dfs(board, board.length - 1, j);
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == 'O') {
                     board[i][j] = 'X';
                 } else if (board[i][j] == '*') {
@@ -27,12 +27,12 @@ class Solution {
             }
         }
     }
+    private int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     private void dfs(char[][] board, int row, int col) {
         if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] != 'O') {
             return;
         }
         board[row][col] = '*';
-        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         for (int[] dir : directions) {
             dfs(board, row + dir[0], col + dir[1]);
         }
