cd957f892c140f10a78681b0cccf6ac4a66afc54
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-24 18:41:14 -0800

    Time: 9 ms (12.91%) | Memory: 52.5 MB (7.43%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,36 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
-        
         int count = 0;
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         boolean[][] visited = new boolean[grid.length][grid[0].length];
-        Queue<int[]> queue = new LinkedList<>();
         
+        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1' && !visited[i][j]) {
                     queue.offer(new int[]{i, j});
-                    visited[i][j] = true; // Mark as visited when adding to queue
+                    visited[i][j] = true;
                     count++;
+
                     while (!queue.isEmpty()) {
                         int[] current = queue.poll();
-                        int row = current[0], col = current[1];
-                        
-                        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
                         for (int[] dir : directions) {
-                            int newRow = row + dir[0], newCol = col + dir[1];
+                            int newRow = current[0] + dir[0]; 
+                            int newCol = current[1] + dir[1];
                             if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length
-                                && grid[newRow][newCol] == '1' && !visited[newRow][newCol]) {
+                            && grid[newRow][newCol] == '1' && !visited[newRow][newCol]) {
                                 queue.offer(new int[]{newRow, newCol});
-                                visited[newRow][newCol] = true; // Correctly mark the new cell as visited
+                                visited[newRow][newCol] = true;
                             }
                         }
                     }
-
                 }
             }
         }
-        
         return count;
     }
 }
