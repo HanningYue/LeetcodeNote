a326c0499edbbb75d649ea4e4670dace36890280
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-27 14:48:28 -0800

    Time: 1 ms (97.99%) | Memory: 42.4 MB (22.38%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,51 +1,52 @@
 class Node {
-    String num;
-    double val;
+    public String num;
+    public double val;
     public Node(String num, double val) {
         this.num = num;
         this.val = val;
     }
 }
 class Solution {
-    Map<String, List<Node>> map = new HashMap<>();
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        Map<String, List<Node>> graph = new HashMap<>();
         for (int i = 0; i < equations.size(); i++) {
-            List<String> equation = equations.get(i);
-            String dividend = equation.get(0);
-            String divisor = equation.get(1);
-            map.putIfAbsent(dividend, new ArrayList<>());
-            map.get(dividend).add(new Node(divisor, values[i]));
- 
-            map.putIfAbsent(divisor, new ArrayList<>());
-            map.get(divisor).add(new Node(dividend, 1 / values[i]));
+            List<String> currentEquation = equations.get(i);
+            String dividend = currentEquation.get(0);
+            String divisor = currentEquation.get(1);
+            graph.putIfAbsent(dividend, new ArrayList<>());
+            graph.get(dividend).add(new Node(divisor, values[i]));
+
+            graph.putIfAbsent(divisor, new ArrayList<>());
+            graph.get(divisor).add(new Node(dividend, 1 / values[i]));
         }
- 
+
         double[] result = new double[queries.size()];
         for (int i = 0; i < result.length; i++) {
-            String num1 = queries.get(i).get(0);
-            String num2 = queries.get(i).get(1);
-            result[i] = dfs(num1, num2, 1, new HashSet<>());
+            List<String> currentQuery = queries.get(i);
+            String dividend = currentQuery.get(0);
+            String divisor = currentQuery.get(1);
+            result[i] = dfs(graph, dividend, divisor, 1, new HashSet<>());
         }
         return result;
     }
-    private double dfs(String dividend, String divisor, double value, HashSet<String> visited) {
-        if (!map.containsKey(dividend)) {
-            return -1;
+    private double dfs(Map<String, List<Node>> graph, String dividend, String divisor, double value, HashSet<String> visited) {
+        if (!graph.containsKey(dividend)) {
+            return -1.0;
         }
         if (visited.contains(dividend)) {
-            return -1;
+            return -1.0;
         }
         if (dividend.equals(divisor)) {
             return value;
         }
         visited.add(dividend);
-        for (Node next : map.get(dividend)) {
-            double sub = dfs(next.num, divisor, value * next.val, visited);
+        for (Node neighbor : graph.get(dividend)) {
+            double sub = dfs(graph, neighbor.num, divisor, value * neighbor.val, visited);
             if (sub != -1.0) {
                 return sub;
             }
         }
         visited.remove(dividend);
-        return -1;
+        return -1.0;
     }
 }
