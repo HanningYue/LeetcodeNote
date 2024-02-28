f844d09c3e78e496a11a1192eafea2345a604391
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-27 16:05:38 -0800

    Time: 6 ms (77.67%) | Memory: 45 MB (23.59%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.java
+++ 945-snakes-and-ladders/snakes-and-ladders.java
@@ -1,30 +1,31 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
+        boolean[][] visited = new boolean[n][n];
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(1);
-        boolean[][] visited = new boolean[n][n];
         visited[n - 1][0] = true;
-
-        int steps = 0;
+        
+        int step = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 int current = queue.poll();
                 if (current == n * n) {
-                    return steps;
+                    return step;
                 }
 
                 for (int j = 1; j <= 6; j++) {
                     int next = current + j;
-                    int[] nextCoor = squareToCoor(next, n);
                     if (next > n * n) {
                         break;
                     }
 
+                    int[] nextCoor = toCoor(next, board.length);
                     if (visited[nextCoor[0]][nextCoor[1]]) {
                         continue;
                     }
+                    
                     visited[nextCoor[0]][nextCoor[1]] = true;
                     if (board[nextCoor[0]][nextCoor[1]] != -1) {
                         next = board[nextCoor[0]][nextCoor[1]];
@@ -32,16 +33,15 @@ class Solution {
                     queue.offer(next);
                 }
             }
-            steps++;
+            step++;
         }
         return -1;
     }
-
-    private int[] squareToCoor(int square, int n) {
-        int r = (square - 1) / n;
-        int c = (square - 1) % n;
-        int row = n - 1 - r;
-        int col = r % 2 == 0 ? c : n - 1 - c;
+    public int[] toCoor(int num, int colLength) {
+        int r = (num - 1) / colLength;
+        int c = (num - 1) % colLength;
+        int row = colLength - r - 1;
+        int col = r % 2 == 0 ? c : colLength - c - 1;
         return new int[]{row, col};
     }
 }
