7d19d89b9b96e2cba5c3b03bf5741c766596d8db
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-27 17:51:23 -0800

    Time: 0 ms (100.00%) | Memory: 41.5 MB (14.76%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -10,35 +10,26 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummy = new ListNode(0);
-        ListNode previousOfList = dummy;
-        dummy.next = head;
-
-        for (int i = 0; i < left - 1; i++) {
-            previousOfList = previousOfList.next;
-        }
-        
-        ListNode startOfReverse = previousOfList.next;
-        ListNode endOfReverse = startOfReverse;
-        for (int i = 0; i < right - left; i++) {
-            endOfReverse = endOfReverse.next;
-        }
-        ListNode laterHalf = endOfReverse.next;
-        endOfReverse.next = null;
-
-        ListNode reversedHead = reverse(startOfReverse);
-        previousOfList.next = reversedHead;
-        startOfReverse.next = laterHalf;
-        return dummy.next;
-    }
-    private ListNode reverse(ListNode head) {
         ListNode previous = null, current = head;
-        while (current != null) {
+        for (int i = 0; i < left - 1; i++) {
+            previous = current;
+            current = current.next;
+        }
+
+        ListNode previousOfList = previous, endOfList = current;
+        for (int i = 0; i < right - left + 1; i++) {
             ListNode next = current.next;
             current.next = previous;
             previous = current;
             current = next;
         }
-        return previous;
+
+        if (previousOfList == null) {
+            head = previous;
+        } else {
+            previousOfList.next = previous;
+        }
+        endOfList.next = current;
+        return head;
     }
 }
