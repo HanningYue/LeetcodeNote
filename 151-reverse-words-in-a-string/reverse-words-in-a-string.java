91cc0b79c7917a829bbafbe009a7ae8a511ee88d
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-28 17:22:15 -0800

    Time: 9 ms (46.02%) | Memory: 42.7 MB (88.33%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,11 +1,50 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.split(" +");
-        StringBuilder sb = new StringBuilder();
-        for (int i = words.length - 1; i >= 0; i--) {
-            sb.append(words[i]);
-            sb.append(" ");
+        StringBuilder sb = trimSpace(s);
+        reverseSB(sb, 0, sb.length() - 1);
+        reverseEachWord(sb);
+        return sb.toString();
+    }
+    public StringBuilder trimSpace(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left <= right && s.charAt(left) == ' ') {
+            left++;
         }
-        return sb.toString().trim();
+        while (left <= right && s.charAt(right) == ' ') {
+            right--;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+            if (c != ' ') {
+                sb.append(c);
+            }
+            else if (sb.charAt(sb.length() - 1) != ' ') {
+                sb.append(c);
+            }
+            left++;
+        }
+        return sb;
+    }
+    public void reverseSB(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+            left++;
+            right--;
+        }
+    }
+    public void reverseEachWord(StringBuilder sb) {
+        int start = 0, end = 0;
+        while (start < sb.length()) {
+            while (end < sb.length() && sb.charAt(end) != ' ') {
+                end++;
+            }
+            reverseSB(sb, start, end - 1);
+            start = end + 1;
+            end++;
+        }
     }
 }
