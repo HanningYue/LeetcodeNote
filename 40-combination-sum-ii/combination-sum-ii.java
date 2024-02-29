131d51432578d3929d5713df74e92eb79b5bbf7c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-29 15:25:23 -0800

    Time: 5 ms (30.42%) | Memory: 43.1 MB (74.92%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,29 +1,27 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] nums, int target) {
-        Arrays.sort(nums);
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTrack(nums, target, result, list, 0, 0);
+        dfs(candidates, target, result, list, 0);
         return result;
     }
-    private void backTrack(int[] nums, int target, List<List<Integer>> result, List<Integer> list,
-    int runningSum, int index)
+    private void dfs(int[] candidates, int target, List<List<Integer>> result, List<Integer> list,
+    int index)
     {
-        if (runningSum == target) {
+        if (target == 0) {
             result.add(new ArrayList<>(list));
             return;
         }
-        if (runningSum > target) {
+        if (target < 0) {
             return;
         }
-        for (int i = index; i < nums.length; i++) {
-            if (i > index && nums[i] == nums[i - 1]) {
+        for (int i = index; i < candidates.length; i++) {
+            if (i > index && candidates[i - 1] == candidates[i]) {
                 continue;
             }
-            list.add(nums[i]);
-            runningSum += nums[i];
-            backTrack(nums, target, result, list, runningSum, i + 1);
-            runningSum -= nums[i];
+            list.add(candidates[i]);
+            dfs(candidates, target - candidates[i], result, list, i + 1);
             list.remove(list.size() - 1);
         }
     }
