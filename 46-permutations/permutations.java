b720c6a6b7a43c610e7cca43bdb7fbe5d919852b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-29 14:53:30 -0800

    Time: 1 ms (95.62%) | Memory: 44.4 MB (67.50%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,22 +2,23 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        Set<Integer> visited = new HashSet<>();
+        boolean[] visited = new boolean[nums.length];
         dfs(result, list, visited, nums);
         return result;
     }
-    private void dfs(List<List<Integer>> result, List<Integer> list, Set<Integer> visited, int[] nums)
+    private void dfs(List<List<Integer>> result, List<Integer> list, boolean[] visited, int[] nums)
     {
         if (list.size() == nums.length) {
             result.add(new ArrayList<>(list));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (visited.add(nums[i])) {
+            if (!visited[i]) {
                 list.add(nums[i]);
+                visited[i] = true;
                 dfs(result, list, visited, nums);
                 list.remove(list.size() - 1);
-                visited.remove(nums[i]);
+                visited[i] = false;
             }
         }
     }
