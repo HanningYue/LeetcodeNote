0c29c27fad128b566019d83d4d7985bcd2b0b4ff
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-29 14:44:50 -0800

    Time: 2 ms (50.31%) | Memory: 44.4 MB (67.50%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,24 +2,23 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        backTrack(result, list, visited, nums);
+        Set<Integer> visited = new HashSet<>();
+        dfs(result, list, visited, nums);
         return result;
     }
-    private void backTrack(List<List<Integer>> result, List<Integer> list, boolean[] visited, int[] nums) {
+    private void dfs(List<List<Integer>> result, List<Integer> list, Set<Integer> visited, int[] nums)
+    {
         if (list.size() == nums.length) {
             result.add(new ArrayList<>(list));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (visited[i]) {
-                continue;
+            if (visited.add(nums[i])) {
+                list.add(nums[i]);
+                dfs(result, list, visited, nums);
+                list.remove(list.size() - 1);
+                visited.remove(nums[i]);
             }
-            list.add(nums[i]);
-            visited[i] = true;
-            backTrack(result, list, visited, nums);
-            visited[i] = false;
-            list.remove(list.size() - 1);
         }
     }
 }
