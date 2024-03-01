6ceb5aa2700877192d677bfe8ecb5f85821e1d9c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-29 23:14:19 -0800

    Time: 3 ms (32.94%) | Memory: 43.9 MB (97.71%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,37 +1,28 @@
 class Solution {
-    public int maxAreaOfIsland(int[][] matrix) {
-        int area = 0;
-        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (!visited[i][j] && matrix[i][j] == 1) {
-                    area = Math.max(area, bfs(matrix, visited, i, j));
+    public int maxAreaOfIsland(int[][] grid) {
+        int result = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (!visited[i][j] && grid[i][j] == 1) {
+                    int count = dfs(grid, i, j, visited);
+                    result = Math.max(result, count);
                 }
-            }
-        }        
-        return area;
-    }
-    private int bfs(int[][] matrix, boolean[][] visited, int row, int col) {
-        int area = 1;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{row, col});
-        visited[row][col] = true;
-        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            for (int[] dir : directions) {
-                int newRow = current[0] + dir[0];
-                int newCol = current[1] + dir[1];
-                if (newRow < 0 || newRow >= matrix.length || newCol < 0 || newCol >= matrix[0].length 
-                || matrix[newRow][newCol] != 1 || visited[newRow][newCol]) {
-                    continue;
-                }
-                area++;
-                visited[newRow][newCol] = true;
-                queue.offer(new int[]{newRow, newCol});
             }
         }
-        return area;
+        return result;
+    }
+    private int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private int dfs(int[][] grid, int row, int col, boolean[][] visited) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col] 
+        || grid[row][col] == 0) {
+            return 0;
+        }
+        visited[row][col] = true;
+        int count = 1;
+        for (int[] dir : directions) {
+            count += dfs(grid, row + dir[0], col + dir[1], visited);
+        }
+        return count;
     }
 }
