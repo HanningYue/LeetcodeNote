d5b9cd48b796c75c5282588dd551bc190925b1c6
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-02-29 16:24:28 -0800

    Time: 1 ms (97.69%) | Memory: 44.5 MB (25.67%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -18,31 +18,27 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
-        }
-        return construct(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1, map);
+        }        
+        return dfs(postorder, 0, postorder.length - 1, inorder, 0, inorder.length - 1, map);
     }
-    private TreeNode construct(int[] inorder, int inleft, int inright, 
-    int[] postorder, int postleft, int postright, Map<Integer, Integer> map)
+    private TreeNode dfs(int[] postorder, int postleft, int postright, 
+    int[] inorder, int inleft, int inright, Map<Integer, Integer> map)
     {
         if (postleft > postright) {
             return null;
         }
         TreeNode head = new TreeNode(postorder[postright]);
         int headIdx = map.get(head.val);
-        int leftRange = headIdx - inleft;
+        int leftSize = headIdx - inleft;
 
-        head.left = construct(inorder, inleft, headIdx - 1,
-        postorder, postleft, postleft + leftRange - 1, map);
-
-        head.right = construct(inorder, headIdx + 1, inright,
-        postorder, postleft + leftRange, postright - 1, map);
+        head.left = dfs(postorder, postleft, postleft + leftSize - 1, inorder, inleft, headIdx - 1, map);
+        head.right = dfs(postorder, postleft + leftSize, postright - 1, inorder, headIdx + 1, inright, map);
         return head;
     }
 }
+
 //     1
 // 2       3
 //     4       5
-// inorder: 2 1 4 3 5
-//          l h 
-// postorder: 2 4 5 3 1
-//            l r r r h
+// postorder [2 4 5 3 1]
+// inorder [2 1 4 3 5]
