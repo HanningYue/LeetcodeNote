76e5eaea2b2ff152bd7a59d7cdbf8b7d76945dac
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-01 14:22:47 -0800

    Time: 2 ms (46.49%) | Memory: 44.4 MB (20.36%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.java
@@ -29,22 +29,20 @@ class Solution {
 
         Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
-
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 Node current = queue.poll();
-                if (i == size - 1) {
-                    current.next = null;
-                } else {
-                    current.next = queue.peek();
-                }
-
                 if (current.left != null) {
                     queue.offer(current.left);
                 }
                 if (current.right != null) {
                     queue.offer(current.right);
+                }
+                if (i == size - 1) {
+                    current.next = null;
+                } else {
+                    current.next = queue.peek();
                 }
             }
         }
