9fd39dacadfa554a03d1a06e5421bda57082a4d4
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-02 15:17:05 -0800

    Time: 0 ms (100.00%) | Memory: 41.7 MB (98.09%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,20 +1,14 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n = nums.length;
         int left = 0, right = nums.length - 1;
-        while (left + 1 < right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[mid + 1]) {
                 right = mid;
             } else if (nums[mid] < nums[mid + 1]) {
-                left = mid;
+                left = mid + 1;
             }
         }
-
-        if (nums[left] > nums[right]) {
-            return left;
-        } else {
-            return right;
-        }
+        return left;
     }
 }
