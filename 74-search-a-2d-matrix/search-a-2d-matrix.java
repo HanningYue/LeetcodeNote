23e5dd75919780ee73bd9bc182cf21f43455bcde
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-02 14:12:23 -0800

    Time: 0 ms (100.00%) | Memory: 41.8 MB (80.64%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
-        
-        int left = 0, right = m * n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (oneToTwo(matrix, mid) == target) {
+        int endIdx = m * n - 1;
+        int start = 0, end = endIdx;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int midValue = helper(matrix, mid);
+            if (midValue == target) {
                 return true;
-            } else if (oneToTwo(matrix, mid) < target) {
-                left = mid + 1;
-            } else if (oneToTwo(matrix, mid) > target) {
-                right = mid - 1;
+            } else if (midValue < target) {
+                start = mid + 1;;
+            } else if (midValue > target) {
+                end = mid - 1;
             }
         }
         return false;
     }
-    private int oneToTwo(int[][] matrix, int midIdx) {
+    private int helper(int[][] matrix, int midIdx) {
         int colLength = matrix[0].length;
-        int row = midIdx / colLength;
-        int col = midIdx % colLength;
-        return matrix[row][col];
+        int rowIdx = midIdx / colLength;
+        int colIdx = midIdx % colLength;
+        return matrix[rowIdx][colIdx];
     }
 }
