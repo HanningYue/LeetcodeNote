1dbde420a6325c87f70832c74774632b1cd8ef60
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-04 00:58:23 -0800

    Time: 0 ms (100.00%) | Memory: 41.1 MB (38.96%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -18,11 +18,13 @@ class Solution {
         if (root == null) {
             return null;
         }
-
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
-        root.left = right;
-        root.right = left;
+        
+        invertTree(root.left);
+        invertTree(root.right);
+        
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
         return root;
     }
 }
