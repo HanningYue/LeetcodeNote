3df92666d7ceac291c6cea4953067b412348e198
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-03 22:44:34 -0800

    Time: 0 ms (100.00%) | Memory: 45.7 MB (70.13%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -2,9 +2,14 @@ class Solution {
     public int[] searchRange(int[] nums, int target) {
         int leftBoundary = findLeftBound(nums, target);
         if (leftBoundary == -1) {
-            return new int[]{-1, -1}; // Target not found
+            return new int[]{-1, -1};
         }
+
         int rightBoundary = findRightBound(nums, target);
+        if (rightBoundary == -1) {
+            return new int[]{-1, -1};
+        }
+        
         return new int[]{leftBoundary, rightBoundary};
     }
 
@@ -12,14 +17,18 @@ class Solution {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
+            if (nums[mid] == target) {
                 right = mid - 1;
-            } else {
+            }
+            else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+            else if (nums[mid] < target){
                 left = mid + 1;
             }
         }
         if (left == nums.length || nums[left] != target) {
-            return -1; // Target not found
+            return -1;
         }
         return left;
     }
@@ -28,14 +37,18 @@ class Solution {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
+            if (nums[mid] == target) {
                 left = mid + 1;
-            } else {
+            } 
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else if (nums[mid] > target) {
                 right = mid - 1;
             }
         }
         if (right < 0 || nums[right] != target) {
-            return -1; // Target not found
+            return -1;
         }
         return right;
     }
