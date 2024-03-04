ce6195239dea893762e79f6f804408748d4f3433
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-04 00:36:23 -0800

    Time: 1 ms (100.00%) | Memory: 45.9 MB (59.25%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,30 +1,36 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        //Assume nums1 is longer than nums2
-        if (nums2.length > nums1.length) {
+        if (nums1.length > nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
-        
+
         int totalLength = nums1.length + nums2.length;
-        int halfLength = totalLength / 2;
+        int halfLength = (totalLength + 1) / 2;
+        boolean even = totalLength % 2 == 0;
 
-        int pointer1 = 0, pointer2 = 0;
-        int lastElement = 0, currentElement = 0;
-        boolean isEven = totalLength % 2 == 0;
+        int left = 0, right = nums1.length;
+        while (left <= right) {
+            int pointer1 = left + (right - left) / 2;
+            int pointer2 = halfLength - pointer1;
+            
+            int left1 = pointer1 > 0 ? nums1[pointer1 - 1] : Integer.MIN_VALUE;
+            int right1 = pointer1 < nums1.length ? nums1[pointer1] : Integer.MAX_VALUE;
 
-        for (int i = 0; i <= halfLength; i++) {
-            lastElement = currentElement;
-            if (pointer1 == nums1.length) {
-                currentElement = nums2[pointer2];
-                pointer2++;
-            } else if (pointer2 == nums2.length || nums1[pointer1] < nums2[pointer2]) {
-                currentElement = nums1[pointer1];
-                pointer1++;
-            } else {
-                currentElement = nums2[pointer2];
-                pointer2++;
+            int left2 = pointer2 > 0 ? nums2[pointer2 - 1] : Integer.MIN_VALUE;
+            int right2 = pointer2 < nums2.length ? nums2[pointer2] : Integer.MAX_VALUE;
+
+            if (left1 <= right2 && left2 <= right1) {
+                if (even) {
+                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
+                } else {
+                    return Math.max(left1, left2);
+                }
+            } else if (left1 > right2) {
+                right = pointer1 - 1;
+            } else if (left2 > right1) {
+                left = pointer1 + 1;
             }
         }
-        return isEven ? (lastElement + currentElement) / 2.0 : currentElement;
+        return 0.0;
     }
 }
