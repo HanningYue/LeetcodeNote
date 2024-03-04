916ea8bbe8a3891d95b1b8a8b146610d22ac2ef6
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-03 22:41:40 -0800

    Time: 0 ms (100.00%) | Memory: 45.8 MB (70.13%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                right = mid - 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            }
+        int leftBoundary = findLeftBound(nums, target);
+        if (leftBoundary == -1) {
+            return new int[]{-1, -1}; // Target not found
         }
-        if (left < 0 || left >= nums.length) {
-            return new int[]{-1, -1};
-        }
-        int leftBoundary = (target == nums[left] ? left : -1);
-
-        left = 0;
-        right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                left = mid + 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            }
-        }
-        if (right < 0 || right >= nums.length) {
-            return new int[]{-1, -1};
-        }
-        int rightBoundary = (target == nums[right] ? right : -1);
-
+        int rightBoundary = findRightBound(nums, target);
         return new int[]{leftBoundary, rightBoundary};
+    }
+
+    private int findLeftBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        if (left == nums.length || nums[left] != target) {
+            return -1; // Target not found
+        }
+        return left;
+    }
+
+    private int findRightBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (right < 0 || nums[right] != target) {
+            return -1; // Target not found
+        }
+        return right;
     }
 }
