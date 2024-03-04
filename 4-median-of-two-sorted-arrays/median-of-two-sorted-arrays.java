262f4fc9f03abecffe529017394ba5bb3e67f81d
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-04 01:18:42 -0800

    Time: 1 ms (100.00%) | Memory: 45.6 MB (95.24%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,18 +1,18 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
+        if (nums2.length < nums1.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
 
         int totalLength = nums1.length + nums2.length;
         int halfLength = (totalLength + 1) / 2;
         boolean even = totalLength % 2 == 0;
-
         int left = 0, right = nums1.length;
+
         while (left <= right) {
             int pointer1 = left + (right - left) / 2;
             int pointer2 = halfLength - pointer1;
-            
+
             int left1 = pointer1 > 0 ? nums1[pointer1 - 1] : Integer.MIN_VALUE;
             int right1 = pointer1 < nums1.length ? nums1[pointer1] : Integer.MAX_VALUE;
 
@@ -25,9 +25,11 @@ class Solution {
                 } else {
                     return Math.max(left1, left2);
                 }
-            } else if (left1 > right2) {
+            } 
+            else if (left1 > right2) {
                 right = pointer1 - 1;
-            } else if (left2 > right1) {
+            }
+            else if (left2 > right1) {
                 left = pointer1 + 1;
             }
         }
