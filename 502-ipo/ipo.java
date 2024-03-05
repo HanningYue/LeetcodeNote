07220377eea1cc8117a499cecbcd0d15d180f6bd
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-04 22:38:41 -0800

    Time: 92 ms (24.02%) | Memory: 57.3 MB (91.63%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,20 +1,20 @@
 class Solution {
     public int findMaximizedCapital(int number, int initial, int[] profits, int[] capital) {
-        //{cap, profit}
-        PriorityQueue<int[]> minCap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        PriorityQueue<int[]> maxPro = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        for (int i = 0; i < capital.length; i++)  {
-            minCap.offer(new int[]{capital[i], profits[i]});
+        //int[] arr {capital increasing, index} {profit decreasing, index}
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        for (int i = 0; i < capital.length; i++) {
+            minHeap.offer(new int[]{capital[i], profits[i]});
         }
 
         for (int i = 0; i < number; i++) {
-            while (!minCap.isEmpty() && minCap.peek()[0] <= initial) {
-                maxPro.offer(minCap.poll());
+            while (!minHeap.isEmpty() && minHeap.peek()[0] <= initial) {
+                maxHeap.offer(minHeap.poll());
             }
-            if (maxPro.isEmpty()) {
+            if (maxHeap.isEmpty()) {
                 return initial;
             }
-            initial += maxPro.poll()[1];
+            initial += maxHeap.poll()[1];
         }
         return initial;
     }
