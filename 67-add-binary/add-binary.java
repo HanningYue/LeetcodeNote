de2ba10f2c7d0cfec71001b9d089227b562ef784
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-05 00:00:16 -0800

    Time: 2 ms (54.49%) | Memory: 42.4 MB (40.52%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,20 +1,19 @@
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        int p1 = a.length() - 1, p2 = b.length() - 1;
+        int aPointer = a.length() - 1, bPointer = b.length() - 1;
         
         int carry = 0;
-        while (p1 >= 0 || p2 >= 0) {
-            int num1 = p1 >= 0 ? a.charAt(p1) - '0' : 0;
-            int num2 = p2 >= 0 ? b.charAt(p2) - '0' : 0;
-            int sum = num1 + num2 + carry;
+        while (aPointer >= 0 || bPointer >= 0) {
+            int num1 = aPointer >= 0 ? a.charAt(aPointer) - '0' : 0;
+            int num2 = bPointer >= 0 ? b.charAt(bPointer) - '0' : 0;
+            int sum = carry + num1 + num2;
 
             sb.insert(0, sum % 2);
             carry = sum / 2;
-            p1--;
-            p2--;
+            aPointer--;
+            bPointer--;
         }
-
         if (carry != 0) {
             sb.insert(0, carry);
         }
