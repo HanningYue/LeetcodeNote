10fe32a4dee049c0cc1d86ebe35743502ea00e18
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-04 22:15:59 -0800

    Time: 29 ms (88.48%) | Memory: 58 MB (79.63%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,30 +1,27 @@
-class Pair {
-    int sum, num1, num2, num2Idx;
-    public Pair(int num1, int num2, int num2Idx) {
-        this.sum = num1 + num2;
-        this.num1 = num1;
-        this.num2 = num2;
-        this.num2Idx = num2Idx;
-    }
-}
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> result = new ArrayList<>();
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return (a[0] + a[1]) - (b[0] + b[1]);
+            }
+        });        
 
-        PriorityQueue<Pair> minHeap = new PriorityQueue<>((a, b) -> a.sum - b.sum);
         for (int i = 0; i < nums1.length && i < k; i++) {
-            minHeap.offer(new Pair(nums1[i], nums2[0], 0));
+            minHeap.offer(new int[]{nums1[i], nums2[0], 0});
         }
 
         while (!minHeap.isEmpty() && k > 0) {
-            Pair current = minHeap.poll();
-            int num1 = current.num1, num2 = current.num2, num2Idx = current.num2Idx;
-            result.add(Arrays.asList(num1, num2));
-
-            if (num2Idx + 1 < nums2.length) {
-                minHeap.offer(new Pair(num1, nums2[num2Idx + 1], num2Idx + 1));
-            }
             k--;
+            int[] current = minHeap.poll();
+            int num1 = current[0], num2 = current[1];
+            int nums2Idx = current[2];
+
+            result.add(Arrays.asList(num1, num2));
+            if (nums2Idx == nums2.length - 1) {
+                continue;
+            }
+            minHeap.offer(new int[]{num1, nums2[nums2Idx + 1], nums2Idx + 1});
         }
         return result;
     }
