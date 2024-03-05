9bc6c743d74c933030da463bbc146abb5b05bf27
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-04 21:31:55 -0800

    Time: 61 ms (47.57%) | Memory: 61.4 MB (20.99%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,46 +1,12 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        k = nums.length - k;
-        shuffle(nums);
-
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int pivotIdx = partition(nums, left, right);
-            if (pivotIdx == k) {
-                return nums[pivotIdx];
-            } else if (pivotIdx < k) {
-                left = pivotIdx + 1;
-            } else if (pivotIdx > k) {
-                right = pivotIdx - 1;
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
             }
         }
-        return -1;
-    }
-    private void shuffle(int[] nums) {
-        Random rand = new Random();
-        for (int i = 0; i < nums.length; i++) {
-            int next = i + rand.nextInt(nums.length - i);
-            swap(nums, i, next);
-        }
-    }
-    private void swap(int[] nums, int left, int right) {
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
-    }
-    private int partition (int[] nums, int left, int right) {
-        int pivot = nums[left];
-        int leftP = left, rightP = right;
-        while (leftP < rightP) {
-            while (leftP < rightP && nums[rightP] >= pivot) {
-                rightP--;
-            } 
-            while (leftP < rightP && nums[leftP] <= pivot) {
-                leftP++;
-            }
-            swap(nums, leftP, rightP);
-        }
-        swap(nums, left, rightP);
-        return rightP;
+        return minHeap.peek();
     }
 }
