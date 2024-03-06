184c81798b63ee28841ff97db7fb22bad2409f21
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-06 15:54:13 -0800

    Time: 236 ms (60.23%) | Memory: 45 MB (60.49%) - LeetSync

--- 212-word-search-ii/word-search-ii.java
+++ 212-word-search-ii/word-search-ii.java
@@ -1,41 +1,41 @@
 class TrieNode {
     TrieNode[] children;
     boolean isEnd;
-    String word = "";
+    String word;
     public TrieNode() {
-        isEnd = false;
         children = new TrieNode[26];
-        for (int i = 0; i < children.length; i++) {
-            children[i] = null;
-        }
+        isEnd = false;
+        word = "";
     }
 }
 class Solution {
     TrieNode root;
     public List<String> findWords(char[][] board, String[] words) {
         root = new TrieNode();
+        List<String> result = new ArrayList<>();
         for (String word : words) {
             insert(word);
         }
 
         boolean[][] visited = new boolean[board.length][board[0].length];
-        List<String> result = new ArrayList<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 char c = board[i][j];
                 if (root.children[c - 'a'] != null) {
-                    TrieNode current = root;
-                    dfs(board, i, j, current, result, visited);
+                    TrieNode newRoot = root;
+                    dfs(board, i, j, visited, result, newRoot);
                 }
             }
         }
         return result;
     }
+
     private int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private void dfs(char[][] board, int row, int col, TrieNode root, 
-    List<String> result, boolean[][] visited) 
-    {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col]) {
+    private void dfs(char[][] board, int row, int col, boolean[][] visited, 
+    List<String> result, TrieNode root)
+    {   
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length
+        || visited[row][col]) {
             return;
         }
         char c = board[row][col];
@@ -48,10 +48,11 @@ class Solution {
         }
         visited[row][col] = true;
         for (int[] dir : directions) {
-            dfs(board, row + dir[0], col + dir[1], root.children[c - 'a'], result, visited);
+            dfs(board, row + dir[0], col + dir[1], visited, result, root.children[c - 'a']);
         }
         visited[row][col] = false;
     }
+
     private void insert(String word) {
         TrieNode current = root;
         for (char c : word.toCharArray()) {
