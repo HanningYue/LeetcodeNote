53ae66ae2ec4068f8e0b10127b610ead4d8f9895
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-06 15:13:37 -0800

    Time: 23 ms (69.34%) | Memory: 44.4 MB (59.77%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        if (points == null || points.length == 0) {
+        if (points.length == 0 || points == null) {
             return 0;
         }
 
         int result = 0;
         for (int i = 0; i < points.length; i++) {
-            int max = 0, overlap = 0;
-            Map<Double, Integer> map = new HashMap<>(); // {slope, frequency}
+            Map<Double, Integer> map = new HashMap<>(); //{slope, frequency}
+            int overlap = 0;
+            int max = 0;
 
             for (int j = i + 1; j < points.length; j++) {
                 int diffX = points[j][0] - points[i][0];
@@ -17,18 +18,18 @@ class Solution {
                 if (diffX == 0 && diffY == 0) {
                     overlap++;
                     continue;
-                } else if (diffY == 0) {
-                    slope = 0.0;
                 } else if (diffX == 0) {
                     slope = Double.MAX_VALUE;
+                } else if (diffY == 0) {
+                    slope = 0.0;
                 } else {
-                    slope = (double)diffY / diffX;
+                    slope = (double) diffY / diffX;
                 }
 
                 map.put(slope, map.getOrDefault(slope, 0) + 1);
                 max = Math.max(max, map.get(slope));
             }
-            result = Math.max(result, max + overlap + 1);
+            result = Math.max(result, overlap + max + 1);
         }
         return result;
     }
