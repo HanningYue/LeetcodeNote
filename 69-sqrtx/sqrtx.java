94b063b40fe1636469f7210aac859db0a3b07116
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-06 13:26:53 -0800

    Time: 1 ms (86.21%) | Memory: 40.5 MB (83.68%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,17 +1,19 @@
+//(x/2) * (x/2) = x^2 / 4, which is greater than x if x is more than 4
 class Solution {
     public int mySqrt(int x) {
         if (x == 1) {
-            return x;
+            return 1;
         }
+        
         int left = 0, right = x / 2;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            long product = (long)mid * mid;
+            long product = (long) mid * mid;
             if (product == x) {
                 return mid;
             } else if (product < x) {
                 left = mid + 1;
-            } else {
+            } else if (product > x) {
                 right = mid - 1;
             }
         }
