94e972cc0a75221bdc7ae077d6740f6a3ffc5171
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-05 23:43:36 -0800

    Time: 4 ms (58.72%) | Memory: 45.3 MB (67.78%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -3,12 +3,12 @@ class Solution {
         if (nums.length == 1) {
             return nums[0];
         }
-        int result = 0, mask = 1;
+        int result = 0;
         for (int i = 0; i < 32; i++) {
             int sum = 0;
             for (int num : nums) {
                 sum += (num >> i) & 1;
-                sum %= 3;
+                sum = sum % 3;
             }
             result = result | (sum << i);
         }
