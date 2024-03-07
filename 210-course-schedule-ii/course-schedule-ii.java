44e6f3c1fe4c71c652c8cc1ce35b1a38b31fccf1
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-06 21:45:24 -0800

    Time: 6 ms (59.07%) | Memory: 46.2 MB (17.95%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,32 +1,30 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < numCourses; i++) {
-            graph.putIfAbsent(i, new ArrayList<>());
-        }
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+
         for (int[] pre : prerequisites) {
-            int from = pre[1];
-            int to = pre[0];
-            graph.get(from).add(to);
+            int currentCourse = pre[0];
+            int preCourse = pre[1];
+            graph.putIfAbsent(currentCourse, new HashSet<>());
+            graph.get(currentCourse).add(preCourse);
         }
 
         boolean[] visiting = new boolean[numCourses];
         boolean[] visited = new boolean[numCourses];
+
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
             if (!dfs(graph, visiting, visited, i, list)) {
                 return new int[]{};
             }
         }
-        
-        Collections.reverse(list);
         int[] result = new int[numCourses];
         for (int i = 0; i < list.size(); i++) {
             result[i] = list.get(i);
         }
         return result;
     }
-    private boolean dfs(Map<Integer, List<Integer>> graph, boolean[] visiting, 
+    private boolean dfs(Map<Integer, Set<Integer>> graph, boolean[] visiting, 
     boolean[] visited, int vertex, List<Integer> list) {
         if (visiting[vertex]) {
             return false;
@@ -34,15 +32,21 @@ class Solution {
         if (visited[vertex]) {
             return true;
         }
+        if (!graph.containsKey(vertex)) {
+            list.add(vertex);
+            visited[vertex] = true;
+            return true;
+        }
+
         visiting[vertex] = true;
-        visited[vertex] = true;
         for (int neighbor : graph.get(vertex)) {
             if (!dfs(graph, visiting, visited, neighbor, list)) {
                 return false;
             }
         }
-        list.add(vertex);
         visiting[vertex] = false;
+        visited[vertex] = true;
+        list.add(vertex);
         return true;
     }
 }
