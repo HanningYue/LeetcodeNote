fd11b9b24c09334ed48c45789067259e20506e70
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-09 13:40:32 -0800

    Time: 2 ms (99.46%) | Memory: 56.9 MB (68.97%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,30 +1,17 @@
-/**
-Iterate two arrays, calculate the total cost and total gas, compare
-    If cost is greater than gas, return -1
-We want to find the first gas start as start, so if the net gas of (cost - gas) is less than 0,
-the current gas station can not be the starting station. So update result = current + 1
-Iterate the array, since the answer is unique, add total gas to a variable and reset variable to 0 if less than 0
-*/
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalGas = 0, totalCost = 0;
+        int start = 0;
+        int sum = 0, minSum = 0;
         for (int i = 0; i < gas.length; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
-        }
-        if (totalCost > totalGas) {
-            return -1;
-        }
-
-        //Guaranteed to have a result
-        int total = 0, result = 0;
-        for (int i = 0; i < gas.length; i++) {
-            total += gas[i] - cost[i];
-            if (total < 0) {
-                total = 0;
-                result = i + 1;
+            sum += gas[i] - cost[i];
+            if (sum < minSum) {
+                start = i + 1;
+                minSum = sum;
             }
         }
-        return result;
+        if (sum < 0) {
+            return -1;
+        }
+        return start == gas.length ? 0 : start;
     }
 }
