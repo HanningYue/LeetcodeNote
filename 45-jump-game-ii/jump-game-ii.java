fa8ef9ffdc6ffe1f77eebb9e4857d3c1d6e30a05
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-10 00:47:14 -0800

    Time: 1 ms (99.19%) | Memory: 44.3 MB (98.94%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,30 +1,14 @@
-/**
-Find the MINIMUM steps to reach last index, use one-d array BFS
-2 3 1 1 4
-*
-  & &
-    % % %
-* & and % are windows
-Left and Right pointer, iterate the array while right pointer is less than last index, declear a variable furthest to mark the furthest the point can reach at EACH Index 
-inside (left, right) window
-    Find the furthest using Math.max()
-    Update left to be right + 1 and right to be the furthest
-    Update count of steps
-*/
 class Solution {
     public int jump(int[] nums) {
-        int left = 0, right = 0;
-        int steps = 0;
-
-        while (right < nums.length - 1) {
-            int furthest = 0;
-            for (int i = left; i <= right; i++) {
-                furthest = Math.max(furthest, nums[i] + i);
+        int end = 0, farthest = 0;
+        int step = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            farthest = Math.max(i + nums[i], farthest);
+            if (end == i) {
+                step++;
+                end = farthest;
             }
-            left = right + 1;
-            right = furthest;
-            steps++;
         }
-        return steps;
+        return step;
     }
 }
