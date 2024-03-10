24fd90d4678b52066b52a3e1939d2a8c94ed2709
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-10 14:26:40 -0700

    Time: 4 ms (7.16%) | Memory: 45.7 MB (58.29%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (i - 1 == -1) {
                 dp[i][0] = 0;
-                dp[i][1] = - prices[i];
+                dp[i][1] = -prices[i];
                 continue;
             }
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
