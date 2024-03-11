100d5ce5fe5816302572110c3a5917e2540ec78a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-11 00:44:38 -0700

    Time: 2 ms (84.29%) | Memory: 44.3 MB (90.43%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,22 +2,31 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTrack(result, list, candidates, target, 0);
+        backTrack(result, list, candidates, target, 0, 0);
         return result;
     }
     private void backTrack(List<List<Integer>> result, List<Integer> list, int[] candidates, int target, 
-                           int level) {
-        if (target == 0) {
+                           int level, int sum) {
+        if (sum == target) {
             result.add(new ArrayList<>(list));
             return;
         }
-        if (target < 0) {
+        if (sum > target) {
             return;
         }
         for (int i = level; i < candidates.length; i++) {
+            sum += candidates[i];
             list.add(candidates[i]);
-            backTrack(result, list, candidates, target - candidates[i], i);
+            backTrack(result, list, candidates, target, i, sum);
             list.remove(list.size() - 1);
+            sum -= candidates[i];
         }
     }
 }
+/**
+             []
+        /     |      \
+    []        []      []
+/   |  \     / \       |
+[]  []  []  [] []      []
+*/
