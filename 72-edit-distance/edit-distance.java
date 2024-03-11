27274e982c838fcb17397474120c4bcb5f062892
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-10 20:33:23 -0700

    Time: 5 ms (69.71%) | Memory: 44.4 MB (94.74%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,33 +1,24 @@
 class Solution {
-    private int[][] dpTable;
     public int minDistance(String word1, String word2) {
-        dpTable = new int[word1.length()][word2.length()];
-        for (int[] row : dpTable) {
-            Arrays.fill(row, -1);
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
         }
-        return dp(word1, word1.length() - 1, word2, word2.length() - 1);
-    }
-    private int dp(String s1, int i, String s2, int j) {
-        if (i == -1) {
-            return j + 1;
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
         }
-        if (j == -1) {
-            return i + 1;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = findMin(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1);
+                }
+            }
         }
-        if (dpTable[i][j] != -1) {
-            return dpTable[i][j];
-        }
-        
-        if (s1.charAt(i) == s2.charAt(j)) {
-            dpTable[i][j] = dp(s1, i - 1, s2, j - 1);
-        } else {
-            dpTable[i][j] = findMin (
-                dp(s1, i - 1, s2, j) + 1,
-                dp(s1, i - 1, s2, j - 1) + 1,
-                dp(s1, i, s2, j - 1) + 1
-            );
-        }
-        return dpTable[i][j];
+        return dp[m][n];
     }
     private int findMin(int a, int b, int c) {
         return Math.min(a, Math.min(b, c));
