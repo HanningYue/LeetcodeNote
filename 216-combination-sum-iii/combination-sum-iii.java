f9777838314e865df0f63a313bca0f38e0b17c5a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-11 00:36:23 -0700

    Time: 0 ms (100.00%) | Memory: 41 MB (46.68%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -2,23 +2,23 @@ class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTrack(k, n, result, list, 1, 0);
+        backTrack(result, list, k, n, 1, 0);
         return result;
     }
-    private void backTrack(int k, int n, List<List<Integer>> result, List<Integer> list, int index, int runningSum) {
-        if (list.size() == k && runningSum == n) {
+    private void backTrack(List<List<Integer>> result, List<Integer> list, int k, int n, int level, int sum) {
+        if (list.size() == k && sum == n) {
             result.add(new ArrayList<>(list));
             return;
         }
-        if (runningSum > n) {
+        if (sum > n) {
             return;
         }
-        for (int i = index; i <= 9; i++) {
+        for (int i = level; i <= 9; i++) {
+            sum += i;
             list.add(i);
-            runningSum += i;
-            backTrack(k, n, result, list, i + 1, runningSum);
-            runningSum -= i;
+            backTrack(result, list, k, n, i + 1, sum);
             list.remove(list.size() - 1);
+            sum -= i;
         }
     }
 }
