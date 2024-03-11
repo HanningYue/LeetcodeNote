b3eda47482e4a7ddf500d9c6a3c7c0602220dc91
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-11 00:15:10 -0700

    Time: 1 ms (99.96%) | Memory: 44.7 MB (76.24%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -4,21 +4,21 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         boolean[] visited = new boolean[nums.length];
-        dfs(result, list, nums, visited);
+        backTrack(result, list, visited, nums);
         return result;
     }
-    private void dfs(List<List<Integer>> result, List<Integer> list, int[] nums, boolean[] visited) {
+    private void backTrack(List<List<Integer>> result, List<Integer> list, boolean[] visited, int[] nums) {
         if (list.size() == nums.length) {
             result.add(new ArrayList<>(list));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (visited[i] || i > 0 && nums[i - 1] == nums[i] && !visited[i - 1]) {
+            if (visited[i] || i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) {
                 continue;
             }
             visited[i] = true;
             list.add(nums[i]);
-            dfs(result, list, nums, visited);
+            backTrack(result, list, visited, nums);
             list.remove(list.size() - 1);
             visited[i] = false;
         }
