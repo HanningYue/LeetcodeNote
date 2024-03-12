bb4a1804b3edbcb831d1905e8bd17dba986a8f4b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-11 21:17:14 -0700

    Time: 4 ms (52.53%) | Memory: 41.7 MB (47.11%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     List<String>[] dpTable;
     public List<String> wordBreak(String s, List<String> wordDict) {
-        dpTable = new List[s.length()];
+        dpTable = new ArrayList[s.length()];
         Set<String> set = new HashSet<>(wordDict);
         return dp(s, 0, set);
     }
-    private List<String> dp (String s, int index, Set<String> set) {
+    private List<String> dp(String s, int index, Set<String> set) {
         List<String> result = new ArrayList<>();
         if (index == s.length()) {
             result.add("");
@@ -14,7 +14,7 @@ class Solution {
         if (dpTable[index] != null) {
             return dpTable[index];
         }
-        for (int length = 1; length + index <= s.length(); length++) {
+        for (int length = 1; index + length <= s.length(); length++) {
             String prefix = s.substring(index, index + length);
             if (set.contains(prefix)) {
                 List<String> subproblem = dp(s, index + length, set);
