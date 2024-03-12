140307ded865cf520413cd7b2ea920a70c26ae5f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-12 01:26:40 -0700

    Time: 6 ms (13.42%) | Memory: 42.1 MB (9.37%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,7 +1,9 @@
 class Solution {
-    List<String> board = new ArrayList<>();
-    int result = 0;
+    List<String> board;
+    int result;
     public int totalNQueens(int n) {
+        board = new ArrayList<>();
+        result = 0;
         for (int i = 0; i < n; i++) {
             StringBuilder sb = new StringBuilder();
             for (int j = 0; j < n; j++) {
@@ -12,36 +14,30 @@ class Solution {
         backTrack(0);
         return result;
     }
-
     private void backTrack(int row) {
         if (row == board.size()) {
             result++;
             return;
         }
-
         for (int col = 0; col < board.get(row).length(); col++) {
-            if (!valid(row, col)) {
+            if (!isValid(row, col)) {
                 continue;
             }
-
             StringBuilder currentRow = new StringBuilder(board.get(row));
             currentRow.setCharAt(col, 'Q');
             board.set(row, currentRow.toString());
-
             backTrack(row + 1);
-            
             currentRow.setCharAt(col, '.');
             board.set(row, currentRow.toString());
         }
     }
-
-    private boolean valid(int row, int col) {
+    private boolean isValid(int row, int col) {
         for (int i = 0; i < row; i++) {
             if (board.get(i).charAt(col) == 'Q') {
                 return false;
             }
         }
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.size(); i--, j++) {
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.size(); j++, i--) {
             if (board.get(i).charAt(j) == 'Q') {
                 return false;
             }
