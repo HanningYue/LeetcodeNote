ebcb9920d36d196b915e9d61d1274be0fccc86a7
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-11 19:59:05 -0700

    Time: 0 ms (100.00%) | Memory: 40.2 MB (69.97%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    int[] memo;
+    public int climbStairs(int n) {
+        memo = new int[n + 1];//We save n + 1steps
+        return dp(n);
+    }
+
+    private int dp(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        if (memo[n] > 0) {
+            return memo[n];
+        }
+        memo[n] = dp(n - 1) + dp(n - 2);
+        return memo[n];
+    }
+}
