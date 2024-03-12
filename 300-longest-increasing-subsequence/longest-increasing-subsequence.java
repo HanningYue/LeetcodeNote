df1159ca0c5ff3a9ab46b87f43512509db5c4cb9
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-12 00:24:46 -0700

    Time: 37 ms (72.54%) | Memory: 43.4 MB (98.88%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,18 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, 1);
-        
+        int[] dpTable = new int[nums.length];
+        Arrays.fill(dpTable, 1);
+
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], 1 + dp[j]);
+                    dpTable[i] = Math.max(dpTable[i], 1 + dpTable[j]);
                 }
             }
         }
         int result = Integer.MIN_VALUE;
-        for (int i = 0; i < dp.length; i++) {
-            result = Math.max(result, dp[i]);
+        for (int i = 0; i < dpTable.length; i++) {
+            result = Math.max(result, dpTable[i]);
         }
         return result;
     }
