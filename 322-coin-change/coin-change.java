a3e7907d2046d2e1aa66db74e8209535dcc67e12
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-11 21:35:30 -0700

    Time: 25 ms (34.07%) | Memory: 44.5 MB (44.22%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,12 +1,8 @@
-/**
-dp函数就是目前amount需要的最少coin
-dpTable[i] = current most number of coin we needed to reach i amount
-*/
 class Solution {
-    private int[] dpTable;
+    int[] memo;
     public int coinChange(int[] coins, int amount) {
-        dpTable = new int[amount + 1];
-        Arrays.fill(dpTable, -100);
+        memo = new int[amount + 1];
+        Arrays.fill(memo, -100);
         return dp(coins, amount);
     }
     private int dp(int[] coins, int amount) {
@@ -16,20 +12,18 @@ class Solution {
         if (amount < 0) {
             return -1;
         }
-        if (dpTable[amount] != -100) {
-            return dpTable[amount];
+        if (memo[amount] != -100) {
+            return memo[amount];
         }
-
         int result = Integer.MAX_VALUE;
         for (int coin : coins) {
-            int subproblem = dp(coins, amount - coin);
-            if (subproblem == -1) {
+            int sub = dp(coins, amount - coin);
+            if (sub == -1) {
                 continue;
             }
-            result = Math.min(result, subproblem + 1);
+            result = Math.min(result, sub + 1);
         }
-        dpTable[amount] = result == Integer.MAX_VALUE ? -1 : result;
-        return dpTable[amount];
-
+        memo[amount] = (result == Integer.MAX_VALUE) ? -1 : result;
+        return memo[amount];
     }
 }
