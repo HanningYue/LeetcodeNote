10a7dbf2e6b4edaf770c16a8ec20ddd4ecf96246
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-13 11:21:58 -0700

    Time: 0 ms (100.00%) | Memory: 40.3 MB (62.91%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,7 +1,11 @@
 class Solution {
-    private int[][] dpTable;
+    int[][] dpTable;
     public int uniquePaths(int m, int n) {
         dpTable = new int[m][n];
+        for (int[] row : dpTable) {
+            Arrays.fill(row, 0);
+        }
+
         return dp(m - 1, n - 1);
     }
     private int dp(int x, int y) {
@@ -11,22 +15,10 @@ class Solution {
         if (x < 0 || y < 0) {
             return 0;
         }
-        if (dpTable[x][y] > 0) {
+        if (dpTable[x][y] != 0) {
             return dpTable[x][y];
         }
         dpTable[x][y] = dp(x - 1, y) + dp(x, y - 1);
-        return dpTable[x][y];
+        return dpTable[x][y]; 
     }
 }
-// public int uniquePaths(int m, int n) {
-//     return dp(m - 1, n - 1);
-// }
-// private int dp(int x, int y) {
-//     if (x == 0 && y == 0) {
-//         return 1;
-//     }
-//     if (x < 0 || y < 0) {
-//         return 0;
-//     }
-//     return dp(x - 1, y) + dp(x, y - 1);
-// }
