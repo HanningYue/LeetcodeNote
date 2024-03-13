dc37330ac562a32d7239c39e66b8766028b155b3
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-12 18:01:23 -0700

    Time: 0 ms (100.00%) | Memory: 42.3 MB (20.46%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -5,18 +5,21 @@ class Solution {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
-            } 
+            }
 
             if (nums[mid] >= nums[left]) {
-                if (target < nums[mid] && target >= nums[left]) {
+                if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
-                } else {
+                }
+                else {
                     left = mid + 1;
                 }
-            } else if (nums[mid] <= nums[right]) {
-                if (target > nums[mid] && target <= nums[right]) {
+            } 
+            else if (nums[mid] <= nums[right]) {
+                if (target <= nums[right] && target > nums[mid]) {
                     left = mid + 1;
-                } else {
+                }
+                else {
                     right = mid - 1;
                 }
             }
