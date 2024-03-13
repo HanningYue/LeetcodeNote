8e414c8e07c75caebbc17856d75ffff6255dda36
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-12 22:13:37 -0700

    Time: 5 ms (69.72%) | Memory: 44.7 MB (87.00%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,24 +1,24 @@
 class Solution {
+    int[][] dpTable;
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
-        int[][] dp = new int[m + 1][n + 1];
+        dpTable = new int[m + 1][n + 1];
         for (int i = 1; i <= m; i++) {
-            dp[i][0] = i;
+            dpTable[i][0] = i;
         }
         for (int j = 1; j <= n; j++) {
-            dp[0][j] = j;
+            dpTable[0][j] = j;
         }
-
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dpTable[i][j] = dpTable[i - 1][j - 1];
                 } else {
-                    dp[i][j] = findMin(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1);
+                    dpTable[i][j] = findMin(dpTable[i - 1][j] + 1, dpTable[i][j - 1] + 1, dpTable[i - 1][j - 1] + 1);
                 }
             }
         }
-        return dp[m][n];
+        return dpTable[m][n];
     }
     private int findMin(int a, int b, int c) {
         return Math.min(a, Math.min(b, c));
