d168b22e270f2ccfdd14f54bb297e8492060950b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-12 21:20:03 -0700

    Time: 3 ms (67.75%) | Memory: 43.6 MB (84.66%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,20 +2,20 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int[][] dpTable = new int[n][n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < dpTable.length; i++) {
             Arrays.fill(dpTable[i], Integer.MAX_VALUE);
         }
 
         dpTable[0][0] = triangle.get(0).get(0);
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < triangle.size(); i++) {
             List<Integer> currentRow = triangle.get(i);
             for (int j = 0; j < currentRow.size(); j++) {
-                if (j - 1 >= 0) {
+                if (j - 1 < 0) {
+                    dpTable[i][j] = dpTable[i - 1][j] + currentRow.get(j);
+                }
+                else if (j - 1 >= 0) {
                     dpTable[i][j] = 
                     Math.min(dpTable[i - 1][j], dpTable[i - 1][j - 1]) + currentRow.get(j);
-                } 
-                else {
-                    dpTable[i][j] = dpTable[i - 1][j] + currentRow.get(j);
                 }
             }
         }
