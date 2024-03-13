5f962d1ae56420e128e20bf677cc863b72925342
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-12 22:04:29 -0700

    Time: 5 ms (98.68%) | Memory: 57.7 MB (22.18%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -4,18 +4,18 @@ class Solution {
         int m = matrix.length, n = matrix[0].length;
         dpTable = new int[m][n];
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             dpTable[i][0] = matrix[i][0] - '0';
         }
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < matrix[0].length; j++) {
             dpTable[0][j] = matrix[0][j] - '0';
         }
 
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] != '0') {
-                    dpTable[i][j] 
-                    = findMin(dpTable[i - 1][j - 1], dpTable[i - 1][j], dpTable[i][j - 1]) + 1;
+                    dpTable[i][j] =
+                    findMin(dpTable[i - 1][j], dpTable[i][j - 1], dpTable[i - 1][j - 1]) + 1;
                 }
             }
         }
