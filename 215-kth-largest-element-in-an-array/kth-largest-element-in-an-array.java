61d513ed50b38bda452471d91a41e8be1ade8b62
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-13 16:45:03 -0700

    Time: 661 ms (7.62%) | Memory: 57.9 MB (45.12%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,12 +1,47 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for (int num : nums) {
-            minHeap.offer(num);
-            if (minHeap.size() > k) {
-                minHeap.poll();
+        k = nums.length - k;
+        shuffle(nums);
+
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int pivot = partition(nums, left, right);
+            if (pivot == k) {
+                return nums[pivot];
             }
-        }        
-        return minHeap.peek();
+            else if (pivot < k) {
+                left = pivot + 1;
+            } else {
+                right = pivot - 1;
+            }
+        }
+        return -1;
+    }
+    private void shuffle(int[] nums) {
+        Random rand = new Random();
+        for (int i = 0; i < nums.length; i++) {
+            int next = i + rand.nextInt(nums.length - i);
+            swap(nums, i, next);
+        }
+    }
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+    private int partition(int[] nums, int left, int right) {
+        int pivot = nums[left];
+        int leftP = left, rightP = right;
+        while (leftP < rightP) {
+            while (leftP < rightP && nums[rightP] >= pivot) {
+                rightP--;
+            }
+            while (leftP < rightP && nums[leftP] <= pivot) {
+                leftP++;
+            }
+            swap(nums, leftP, rightP);
+        }
+        swap(nums, left, rightP);
+        return rightP;
     }
 }
