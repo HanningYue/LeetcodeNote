d59a973f251c706bbae56d048367f8b869c67fdd
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-12 18:11:20 -0700

    Time: 0 ms (100.00%) | Memory: 46 MB (19.03%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,55 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int leftBoundary = findLeftBound(nums, target);
-        if (leftBoundary == -1) {
-            return new int[]{-1, -1};
-        }
-
-        int rightBoundary = findRightBound(nums, target);
-        if (rightBoundary == -1) {
-            return new int[]{-1, -1};
-        }
-        
-        return new int[]{leftBoundary, rightBoundary};
+        int leftBound = binarySearchLeft(nums, target);
+        int rightBound = binarySearchRight(nums, target);
+        return new int[]{leftBound, rightBound};
     }
-
-    private int findLeftBound(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                right = mid - 1;
-            }
-            else if (nums[mid] > target) {
-                right = mid - 1;
-            }
-            else if (nums[mid] < target){
-                left = mid + 1;
-            }
-        }
-        if (left == nums.length || nums[left] != target) {
-            return -1;
-        }
-        return left;
-    }
-
-    private int findRightBound(int[] nums, int target) {
+    private int binarySearchRight(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 left = mid + 1;
-            } 
-            else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-            else if (nums[mid] > target) {
+            } else if (nums[mid] > target) {
                 right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             }
         }
         if (right < 0 || nums[right] != target) {
             return -1;
         }
         return right;
+    }
+    private int binarySearchLeft(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        if (left >= nums.length || nums[left] != target) {
+            return -1;
+        }
+        return left;
     }
 }
