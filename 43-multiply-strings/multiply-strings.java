f1d110e1dc341acffda909bed4a8e18522ac322b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-13 16:05:08 -0700

    Time: 3 ms (63.06%) | Memory: 42.4 MB (46.07%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,6 +1,7 @@
 /**
 index       0   1   2
             1   2   3   i
+index           0   1
                 4   5   j
     _________________
                 1   5
@@ -11,15 +12,15 @@ index       0   1   2
         4
             5   3   5
         5
-0   1   2   3   4   5         index
+result[] 0  1   2   3   4, length of 3 + 2 = 5
+    0   1   2   3   4
                     i + j + 1
-               i + j + 1
          
 */
 class Solution {
     public String multiply(String num1, String num2) {
         int[] result = new int[num1.length() + num2.length()];
-        
+
         for (int i = num1.length() - 1; i >= 0; i--) {
             for (int j = num2.length() - 1; j >= 0; j--) {
                 int one = num1.charAt(i) - '0';
@@ -43,5 +44,6 @@ class Solution {
             i++;
         }
         return sb.length() == 0 ? "0" : sb.toString();
+
     }
 }
