2f88541b7f0f2371167d24ff4b0425baa5526a77
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-12 21:38:36 -0700

    Time: 5 ms (98.68%) | Memory: 57.7 MB (14.35%) - LeetSync

---
+++ 221-maximal-square/maximal-square.java
@@ -0,0 +1,30 @@
+class Solution {
+    int[][] dpTable;
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        dpTable = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dpTable[i][0] = matrix[i][0] - '0';
+        }
+        for (int j = 0; j < n; j++) {
+            dpTable[0][j] = matrix[0][j] - '0';
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != '0') {
+                    dpTable[i][j] 
+                    = Math.min(Math.min(dpTable[i - 1][j], dpTable[i][j - 1]), dpTable[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result = Math.max(result, dpTable[i][j]);
+            }
+        }
+        return result * result;
+    }
+}
