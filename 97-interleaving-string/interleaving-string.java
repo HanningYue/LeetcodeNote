f6b3be4454d592f1919df27908bcee5dbdbf8d12
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-13 15:34:41 -0700

    Time: 0 ms (100.00%) | Memory: 42 MB (25.87%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -5,32 +5,29 @@ class Solution {
         if (m + n != s3.length()) {
             return false;
         }
-
-        dpTable = new Boolean[m + 1][n + 1];
+        dpTable = new Boolean[m][n];
         return dp(s1, 0, s2, 0, s3, 0);
     }
+
     private boolean dp(String s1, int i, String s2, int j, String s3, int k) {
         if (k == s3.length()) {
             return true;
         }
-        if (dpTable[i][j] != null) {
+        if (i < s1.length() && j < s2.length() && dpTable[i][j] != null) {
             return dpTable[i][j];
         }
 
+        boolean valid = false;
         if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
-            dpTable[i][j] = dp(s1, i + 1, s2, j, s3, k + 1);
-            if (dpTable[i][j]) {
-                return true;
-            }
+            valid = dp(s1, i + 1, s2, j, s3, k + 1);
+        }
+        if (!valid && j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            valid = dp(s1, i, s2, j + 1, s3, k + 1);
         }
         
-        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
-            dpTable[i][j] = dp(s1, i, s2, j + 1, s3, k + 1);
-            return dpTable[i][j];
+        if (i < s1.length() && j < s2.length()) {
+            dpTable[i][j] = valid;
         }
-        
-        // If neither character matches, or both paths return false, set and return false
-        dpTable[i][j] = false;
-        return false;
+        return valid;
     }
 }
