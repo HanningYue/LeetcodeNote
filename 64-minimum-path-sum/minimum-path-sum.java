3235238dded73997b64084ab04288328b4db0640
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-13 11:37:38 -0700

    Time: 1 ms (95.90%) | Memory: 45.4 MB (64.43%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -10,10 +10,11 @@ class Solution {
         return dp(grid, m - 1, n - 1);
     }
     private int dp(int[][] grid, int row, int col) {
+        int m = grid.length, n = grid[0].length;
         if (row == 0 && col == 0) {
             return grid[0][0];
         }
-        if (row < 0 || col < 0) {
+        if (row < 0 || row >= m || col < 0 || col >= n) {
             return Integer.MAX_VALUE;
         }
         if (dpTable[row][col] != -1) {
