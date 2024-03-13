2868f356c6ffea944977a237f9281f9577ffadbe
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-13 15:45:54 -0700

    Time: 2 ms (98.83%) | Memory: 42.1 MB (81.55%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,19 +1,21 @@
 class Solution {
     public String multiply(String num1, String num2) {
         int[] result = new int[num1.length() + num2.length()];
+
         for (int i = num2.length() - 1; i >= 0; i--) {
             for (int j = num1.length() - 1; j >= 0; j--) {
                 int product = ((num2.charAt(i) - '0') * (num1.charAt(j) - '0'));
-                
                 int sum = product + result[i + j + 1];
                 result[i + j + 1] = sum % 10;
                 result[i + j] += sum / 10;
             }
-        }            
+        }
+        
         int i = 0;
         while (i < result.length && result[i] == 0) {
             i++;
         }
+
         StringBuilder sb = new StringBuilder();
         while (i < result.length) {
             sb.append(result[i]);
@@ -22,7 +24,7 @@ class Solution {
         return sb.length() == 0 ? "0" : sb.toString();
     }
 }
-//                 0   1   2
+// index           0   1   2
 //                 1   2   3
 //                 4   5   6
 //             ________________
