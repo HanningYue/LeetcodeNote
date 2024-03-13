8726efe11a4c14805057e3fbffdfc09223f91645
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-13 15:18:13 -0700

    Time: 0 ms (100.00%) | Memory: 41.9 MB (37.31%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,32 +1,36 @@
 class Solution {
-    int[][] dpTable;
+    Boolean[][] dpTable;
     public boolean isInterleave(String s1, String s2, String s3) {
         int m = s1.length(), n = s2.length();
         if (m + n != s3.length()) {
             return false;
         }
-        dpTable = new int[m + 1][n + 1];
-        for (int i = 0; i < dpTable.length; i++) {
-            Arrays.fill(dpTable[i], -1);
-        }
-        return dp(s1, 0, s2, 0, s3);
+
+        dpTable = new Boolean[m + 1][n + 1];
+        return dp(s1, 0, s2, 0, s3, 0);
     }
-    private boolean dp(String s1, int i, String s2, int j, String s3) {
-        int k = i + j;
+    private boolean dp(String s1, int i, String s2, int j, String s3, int k) {
         if (k == s3.length()) {
             return true;
         }
-        if (dpTable[i][j] != -1) {
-            return dpTable[i][j] == 1 ? true : false;
+        if (dpTable[i][j] != null) {
+            return dpTable[i][j];
         }
-        boolean result = false;
+
         if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
-            result = dp(s1, i + 1, s2, j, s3);
+            dpTable[i][j] = dp(s1, i + 1, s2, j, s3, k + 1);
+            if (dpTable[i][j]) {
+                return true;
+            }
         }
+        
         if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
-            result = result || dp(s1, i, s2, j + 1, s3);
+            dpTable[i][j] = dp(s1, i, s2, j + 1, s3, k + 1);
+            return dpTable[i][j];
         }
-        dpTable[i][j] = result == true ? 1 : 0;
-        return result;
+        
+        // If neither character matches, or both paths return false, set and return false
+        dpTable[i][j] = false;
+        return false;
     }
 }
