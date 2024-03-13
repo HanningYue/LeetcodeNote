4851648acf6d76ff253f871891df13572109fb14
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-12 18:58:23 -0700

    Time: 0 ms (100.00%) | Memory: 42.9 MB (53.14%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -5,11 +5,9 @@ class Solution {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
-            }
-            else if (nums[mid] > target) {
+            } else if (nums[mid] > target) {
                 right = mid - 1;
-            } 
-            else if (nums[mid] < target) {
+            } else if (nums[mid] < target) {
                 left = mid + 1;
             }
         }
