adb92d312eba2814731b8845d0c3295a9fed31b5
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-13 15:55:51 -0700

    Time: 3 ms (63.06%) | Memory: 41.8 MB (97.18%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,16 +1,37 @@
+/**
+index       0   1   2
+            1   2   3   i
+                4   5   j
+    _________________
+                1   5
+            1   0    
+            5
+            1   2
+            8
+        4
+            5   3   5
+        5
+0   1   2   3   4   5         index
+                    i + j + 1
+               i + j + 1
+         
+*/
 class Solution {
     public String multiply(String num1, String num2) {
         int[] result = new int[num1.length() + num2.length()];
-
-        for (int i = num2.length() - 1; i >= 0; i--) {
-            for (int j = num1.length() - 1; j >= 0; j--) {
-                int product = ((num2.charAt(i) - '0') * (num1.charAt(j) - '0'));
+        
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int one = num1.charAt(i) - '0';
+                int two = num2.charAt(j) - '0';
+                
+                int product = one * two;
                 int sum = product + result[i + j + 1];
                 result[i + j + 1] = sum % 10;
                 result[i + j] += sum / 10;
             }
         }
-        
+
         int i = 0;
         while (i < result.length && result[i] == 0) {
             i++;
@@ -24,19 +45,3 @@ class Solution {
         return sb.length() == 0 ? "0" : sb.toString();
     }
 }
-// index           0   1   2
-//                 1   2   3
-//                 4   5   6
-//             ________________
-//                     1   8
-//                 1   2
-//                 6
-//                 1   5
-//             1   0 
-//             5
-//             1   2   
-//             8
-//         4
-//     0   1   2   3   4   5   
-//                         i + j + 1
-//                     i + j
