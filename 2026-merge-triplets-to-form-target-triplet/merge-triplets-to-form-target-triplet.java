21b8010276f55a003939eab8e8cb34eac554b484
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-14 13:33:19 -0700

    Time: 4 ms (80.36%) | Memory: 122.6 MB (15.69%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,32 +1,30 @@
-/**
-To be true, 合适的triplets里面 3个element必须小于等于 target triplets 里3个element
-如果有任意一个element大于，不合适
-1. 先查看boolean greater
-2. 如果greater，跳过当前triplet
-3. 查看每一个element是否相等
-    如果相等 result当前index mark true
-返回result三个index都为true的情况
-*/
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         boolean[] result = new boolean[3];
         for (int[] triplet : triplets) {
-            boolean greater = false;
+            boolean greaterElement = false;
             for (int i = 0; i < 3; i++) {
                 if (triplet[i] > target[i]) {
-                    greater = true;
+                    greaterElement = true;
                     break;
                 }
             }
-            if (greater) {
+            
+            if (greaterElement) {
                 continue;
             }
+
             for (int i = 0; i < 3; i++) {
                 if (triplet[i] == target[i]) {
                     result[i] = true;
                 }
             }
         }
-        return result[0] && result[1] && result[2];
+        for (boolean val : result) {
+            if (!val) {
+                return false;
+            }
+        }
+        return true;
     }
 }
