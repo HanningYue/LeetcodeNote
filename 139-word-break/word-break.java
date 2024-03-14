2cd27dc8b5e65103eb203b97f279b76010056e98
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-14 13:03:28 -0700

    Time: 6 ms (61.07%) | Memory: 44.6 MB (29.85%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,29 +1,28 @@
 class Solution {
-    int[] dpTable;
+    Integer[] dpTable;
     public boolean wordBreak(String s, List<String> wordDict) {
-        dpTable = new int[s.length()];
-        Arrays.fill(dpTable, -1);
+        dpTable = new Integer[s.length()];
         Set<String> set = new HashSet<>(wordDict);
         return dp(s, 0, set);
     }
-    private boolean dp(String s, int index, Set<String> set) {
-        if (index == s.length()) {
+    private boolean dp(String s, int startIdx, Set<String> set) {
+        if (startIdx == s.length()) {
             return true;
         }
-        if (dpTable[index] != -1) {
-            return dpTable[index] == 1 ? true : false;
+        if (dpTable[startIdx] != null) {
+            return dpTable[startIdx] == 1 ? true : false;
         }
-        for (int length = 1; index + length <= s.length(); length++) {
-            String prefix = s.substring(index, index + length);
+        for (int length = 1; startIdx + length <= s.length(); length++) {
+            String prefix = s.substring(startIdx, startIdx + length);
             if (set.contains(prefix)) {
-                boolean sub = dp(s, index + length, set);
+                boolean sub = dp(s, startIdx + length, set);
                 if (sub) {
-                    dpTable[index] = 1;
+                    dpTable[startIdx] = 1;
                     return true;
                 }
             }
         }
-        dpTable[index] = 0;
+        dpTable[startIdx] = 0;
         return false;
     }
 }
