4be39e2ca776bf5171c19916d4de1bdf38d9136c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-14 16:21:15 -0700

    Time: 2 ms (78.41%) | Memory: 45.4 MB (53.65%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,12 +1,17 @@
+/**
+& : 1 & 1 = 1       0 & 1 = 0       0 & 0 = 0
+| : 1 | 1 = 1       0 | 1 = 1       0 | 0 = 0
+*/
 class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
         for (int i = 0; i < 32; i++) {
             int sum = 0;
             for (int num : nums) {
-                sum += ((num >> i) & 1);
-                sum = sum % 3;
+                int lastDigit = (num >> i) & 1;
+                sum += lastDigit;
             }
+            sum = sum % 3;
             result = result | (sum << i);
         }
         return result;
