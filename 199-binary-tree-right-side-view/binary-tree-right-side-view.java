a7e0fefb76c04f735c27d7d36d456dac51549972
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-14 13:20:08 -0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (93.17%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -16,17 +16,17 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        dfs(result, root, 0);
+        dfs(root, result, 0);
         return result;
     }
-    private void dfs(List<Integer> result, TreeNode root, int level) {
+    private void dfs(TreeNode root, List<Integer> result, int level) {
         if (root == null) {
             return;
         }
         if (result.size() == level) {
             result.add(root.val);
         }
-        dfs(result, root.right, level + 1);
-        dfs(result, root.left, level + 1);
+        dfs(root.right, result, level + 1);
+        dfs(root.left, result, level + 1);
     }
 }
