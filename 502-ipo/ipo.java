cac27e50ce02a2ac19135948736ce0e224c99856
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-13 17:23:46 -0700

    Time: 86 ms (49.49%) | Memory: 57.9 MB (79.62%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,20 +1,21 @@
 class Solution {
-    public int findMaximizedCapital(int number, int initial, int[] profits, int[] capital) {
-        //int[] arr {capital increasing, index} {profit decreasing, index}
+    public int findMaximizedCapital(int tasks, int initial, int[] profits, int[] capital) {
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[1] - a[1]);
         for (int i = 0; i < capital.length; i++) {
             minHeap.offer(new int[]{capital[i], profits[i]});
         }
 
-        for (int i = 0; i < number; i++) {
+        for (int i = 0; i < tasks; i++) {
             while (!minHeap.isEmpty() && minHeap.peek()[0] <= initial) {
-                maxHeap.offer(minHeap.poll());
+                int[] minCapital = minHeap.poll();
+                maxHeap.offer(minCapital);
             }
             if (maxHeap.isEmpty()) {
                 return initial;
             }
-            initial += maxHeap.poll()[1];
+            int[] maxProfit = maxHeap.poll();
+            initial += maxProfit[1];
         }
         return initial;
     }
