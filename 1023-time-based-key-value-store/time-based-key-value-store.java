a899213f7061a26657a06f065886f5c576c4938a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-20 02:03:23 -0700

    Time: 141 ms (65.09%) | Memory: 119.9 MB (42.56%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,13 +1,3 @@
-/*
-We need to use a map for relation of key and timestamp 
-    and a Pair for value of timestamp and timestamp
-Map<String, List<Pair<String, Integer>>>...     For each key, there might be ArrayList<Pair<String, Integer>>
-    key                value  timestamp
-When Binary search, 求target timestamp value, 正常+1 和 —1. 
-    high is the index of last Pair value in list
-但是在末尾加入检查条件，如果没有找到equal或者有好几个
-    如果high >= 0并且 getvalue <= timestamp，return 最大(右边界)的value
-*/
 class TimeMap {
     Map<String, List<Pair<String, Integer>>> map;
     public TimeMap() {
@@ -15,7 +5,7 @@ class TimeMap {
     }
     
     public void set(String key, String value, int timestamp) {
-        map.putIfAbsent(key,  new ArrayList<Pair<String, Integer>>());
+        map.putIfAbsent(key, new ArrayList<Pair<String, Integer>>());
         map.get(key).add(new Pair(value, timestamp));
     }
     
@@ -27,19 +17,19 @@ class TimeMap {
         return binarySearch(list, timestamp);
     }
     private String binarySearch(List<Pair<String, Integer>> list, int timestamp) {
-        int low = 0, high = list.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
+        int left = 0, right = list.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
             if (list.get(mid).getValue() == timestamp) {
                 return list.get(mid).getKey();
             } else if (list.get(mid).getValue() < timestamp) {
-                low = mid + 1;
+                left = mid + 1;
             } else {
-                high = mid - 1;
+                right = mid - 1;
             }
         }
-        if (high >= 0 && list.get(high).getValue() < timestamp) {
-            return list.get(high).getKey();
+        if (right >= 0) {
+            return list.get(right).getKey();
         }
         return "";
     }
