123b7064b67d39ca82c0d4e331d2988f0758d886
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-20 01:30:32 -0700

    Time: 1 ms (74.52%) | Memory: 45.8 MB (15.19%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,20 +1,17 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean firstRow = false, firstCol = false;
-
-        for (int i = 0; i < matrix[0].length; i++) {
-            if (matrix[0][i] == 0) {
-                firstRow = true;
-                break;
-            }
-        }
+        boolean firstRow = false;
+        boolean firstCol = false;
         for (int i = 0; i < matrix.length; i++) {
             if (matrix[i][0] == 0) {
                 firstCol = true;
-                break;
             }
         }
-
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (matrix[0][i] == 0) {
+                firstRow = true;
+            }
+        }
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
@@ -23,7 +20,6 @@ class Solution {
                 }
             }
         }
-
         for (int i = 1; i < matrix.length; i++) {
             for (int j = 1; j < matrix[0].length; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
