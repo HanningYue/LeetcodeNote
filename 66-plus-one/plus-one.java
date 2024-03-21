6555f612bfbea32a2d9a4de7b2b40f19ca448fd2
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-20 17:47:09 -0700

    Time: 0 ms (100.00%) | Memory: 41.5 MB (78.92%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -3,13 +3,14 @@ class Solution {
         int lastIdx = digits.length - 1;
         while (lastIdx >= 0) {
             if (digits[lastIdx] < 9) {
-                digits[lastIdx] += 1;
+                digits[lastIdx]++;
                 return digits;
             } else if (digits[lastIdx] == 9) {
-                digits[lastIdx] = 0;
+                digits[lastIdx] = 0;                
                 lastIdx--;
             }
         }
+
         int[] result = new int[digits.length + 1];
         result[0] = 1;
         return result;
