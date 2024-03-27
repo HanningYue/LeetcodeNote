ba3d9c81bd314064c804425ab18b7ce75625aca8
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-27 15:01:45 -0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (6.00%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,28 +1,24 @@
 class Solution {
-    int[] dpTable1;
-    int[] dpTable2;
+    Integer[] dpTable1;
+    Integer[] dpTable2;
     public int rob(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
+        if (nums.length == 1) {
             return nums[0];
         }
         
-        dpTable1 = new int[n];
-        dpTable2 = new int[n];
-        Arrays.fill(dpTable1, -1);
-        Arrays.fill(dpTable2, -1);
-        return Math.max(dp(nums, 0, n - 2, dpTable1), dp(nums, 1, n - 1, dpTable2));
+        dpTable1 = new Integer[nums.length];
+        dpTable2 = new Integer[nums.length];
+        return Math.max(dp(nums, 0, nums.length - 2, dpTable1), dp(nums, 1, nums.length - 1, dpTable2));
     }
-    private int dp(int[] nums, int start, int end, int[] dpTable) {
+    private int dp(int[] nums, int start, int end, Integer[] dpTable) {
         if (start > end) {
             return 0;
         }
-        if (dpTable[start] != -1) {
+        if (dpTable[start] != null) {
             return dpTable[start];
         }
-        int result = Math.max(dp(nums, start + 2, end, dpTable) + nums[start], 
-                              dp(nums, start + 1, end, dpTable));
-        dpTable[start] = result;
-        return result;
+        dpTable[start] = Math.max(dp(nums, start + 2, end, dpTable) + nums[start],
+                                dp(nums, start + 1, end, dpTable));
+        return dpTable[start];
     }
 }
