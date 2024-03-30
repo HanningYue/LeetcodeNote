65c7a1f5e834d7db11b16726faeffbb9b92c79a2
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-30 14:24:04 -0700

    Time: 54 ms (21.93%) | Memory: 124.3 MB (15.59%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,37 +1,66 @@
-class LRUCache {
-    int capacity;
-    LinkedHashMap<Integer, Integer> map;
-    public LRUCache(int capacity) {
-        map = new LinkedHashMap<>();
-        this.capacity = capacity;
+class Node {
+    Node prev, next;
+    int key, value;
+    public Node (int key, int value) {
+        this.key = key;
+        this.value = value;
     }
-    public void makeRecent(int key) {
-        int recent = map.get(key);
-        map.remove(key);
-        map.put(key, recent);
+}
+class LRUCache {
+    Map<Integer, Node> map;
+    Node head, tail;
+    int capacity, count;
+    public LRUCache(int capacity) {
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.next = null;
+        head.prev = null;
+        tail.prev = head;
+        this.capacity = capacity;
+        count = 0;
+    }
+    public void add(Node node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+    public void delete(Node node) {
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
     }
     public int get(int key) {
         if (map.containsKey(key)) {
-            makeRecent(key);
-            return map.get(key);
-        }
+            Node node = map.get(key);
+            int value = node.value;
+            delete(node);
+            add(node);
+            return value;
+        }   
         return -1;
     }
     public void put(int key, int value) {
         if (map.containsKey(key)) {
-            makeRecent(key);
-            map.put(key, value);
+            Node oldNode = map.get(key);
+            delete(oldNode);
+
+            Node newNode = new Node(key, value);
+            map.put(key, newNode);
+            add(newNode);
             return;
-        }   
+        } 
         else if (!map.containsKey(key)) {
-            if (map.size() >= this.capacity) {
-                int leastRecent = map.keySet().iterator().next();
-                map.remove(leastRecent);
-                map.put(key, value);
-            } else {
-                map.put(key, value);
-                makeRecent(key);
+            if (count == this.capacity) {
+                map.remove(tail.prev.key);
+                delete(tail.prev);
+                count--;
             }
+            Node newNode = new Node(key, value);
+            map.put(key, newNode);
+            add(newNode);
+            count++;
         }
     }
 }
