fc3f6c2ae9710d0877c2eab1ee16842c4c76107f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-30 15:01:13 -0700

    Time: 46 ms (63.00%) | Memory: 113.1 MB (65.76%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,29 +1,27 @@
 class Node {
-    Node prev, next;
     int key, value;
-    public Node (int key, int value) {
+    Node prev, next;
+    public Node(int key, int value) {
         this.key = key;
         this.value = value;
     }
 }
 class LRUCache {
-    Map<Integer, Node> map;
     Node head, tail;
+    Map<Integer, Node> map;
     int capacity, count;
     public LRUCache(int capacity) {
         map = new HashMap<>();
+        this.capacity = capacity;
+        count = 0;
         head = new Node(0, 0);
         tail = new Node(0, 0);
         head.next = tail;
-        tail.next = null;
-        head.prev = null;
         tail.prev = head;
-        this.capacity = capacity;
-        count = 0;
     }
     public void add(Node node) {
-        node.prev = head;
         node.next = head.next;
+        node.prev = head;
         head.next.prev = node;
         head.next = node;
     }
@@ -34,33 +32,29 @@ class LRUCache {
     public int get(int key) {
         if (map.containsKey(key)) {
             Node node = map.get(key);
-            int value = node.value;
             delete(node);
             add(node);
-            return value;
-        }   
+            return node.value;
+        }
         return -1;
     }
     public void put(int key, int value) {
         if (map.containsKey(key)) {
-            Node oldNode = map.get(key);
-            delete(oldNode);
-
-            Node newNode = new Node(key, value);
-            map.put(key, newNode);
-            add(newNode);
-            return;
+            Node node = map.get(key);
+            node.value = value;
+            delete(node);
+            add(node);
         } 
         else if (!map.containsKey(key)) {
-            if (count == this.capacity) {
+            Node newNode = new Node(key, value);
+            if (count < capacity) {
+                count++;
+            } else {
                 map.remove(tail.prev.key);
                 delete(tail.prev);
-                count--;
             }
-            Node newNode = new Node(key, value);
             map.put(key, newNode);
             add(newNode);
-            count++;
         }
     }
 }
