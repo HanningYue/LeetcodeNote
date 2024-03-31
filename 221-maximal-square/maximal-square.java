09340b863359e332832e86fcf7c3efa1a3c5a497
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-31 00:35:11 -0700

    Time: 10 ms (25.24%) | Memory: 56.9 MB (77.67%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,35 +1,28 @@
-/**
-dp 数组：以 matrix[i][j] 为右下角元素的最大的全为 1 正方形矩阵的边长为 dp[i][j]
-水桶效应 所以找min
-*/
 class Solution {
     Integer[][] dpTable;
     public int maximalSquare(char[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         dpTable = new Integer[m][n];
         
-        int maxSide = 0;
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                maxSide = Math.max(maxSide, findMaxSquare(matrix, row, col));
+        int side = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                side = Math.max(side, dp(matrix, i, j));
             }
         }
-        return maxSide * maxSide; // Return area
+        return side * side;
     }
-
-    private int findMaxSquare(char[][] matrix, int row, int col) {
-        if (row < 0 || col < 0 || matrix[row][col] == '0') {
+    private int dp(char[][] matrix, int row, int col) {
+        if (row < 0 || col < 0 || matrix[row][col] != '1') {
             return 0;
         }
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
-        
-        int up = findMaxSquare(matrix, row - 1, col);
-        int left = findMaxSquare(matrix, row, col - 1);
-        int leftUp = findMaxSquare(matrix, row - 1, col - 1);
-        
-        dpTable[row][col] = Math.min(Math.min(up, left), leftUp) + 1;
+        int left = dp(matrix, row, col - 1);
+        int up = dp(matrix, row - 1, col);
+        int leftUp = dp(matrix, row - 1, col - 1);
+        dpTable[row][col] = Math.min(leftUp, Math.min(left, up)) + 1;
         return dpTable[row][col];
     }
 }
