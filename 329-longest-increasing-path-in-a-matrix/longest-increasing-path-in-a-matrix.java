629382de0dd11237955086da029585fb0caea092
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-30 17:00:34 -0700

    Time: 8 ms (80.80%) | Memory: 44.9 MB (60.50%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -5,32 +5,30 @@ class Solution {
         
         dpTable = new Integer[matrix.length][matrix[0].length];
         int result = 1;
-        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
 
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
                 result = Math.max(result, 
-                dfs(matrix, i, j, visited, Integer.MIN_VALUE));
+                dfs(matrix, i, j, Integer.MIN_VALUE));
             }
         }
         
         return result;
     }
     
-    private int dfs(int[][] matrix, int row, int col, boolean[][] visited, int prev){
+    private int dfs(int[][] matrix, int row, int col, int prev){
         if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length 
-        || visited[row][col] || prev >= matrix[row][col]) {
+        || prev >= matrix[row][col]) {
             return 0;
         }
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
         
-        int up = dfs(matrix, row - 1, col, visited, matrix[row][col]) + 1;
-        int down = dfs(matrix, row + 1, col, visited, matrix[row][col]) + 1;
-        int left = dfs(matrix, row, col - 1, visited, matrix[row][col]) + 1;
-        int right = dfs(matrix, row, col + 1, visited, matrix[row][col]) + 1;
-        visited[row][col] = false;
+        int up = dfs(matrix, row - 1, col, matrix[row][col]) + 1;
+        int down = dfs(matrix, row + 1, col, matrix[row][col]) + 1;
+        int left = dfs(matrix, row, col - 1, matrix[row][col]) + 1;
+        int right = dfs(matrix, row, col + 1, matrix[row][col]) + 1;
         
         dpTable[row][col] = Math.max(up, Math.max(down, Math.max(left, right)));
         return dpTable[row][col];
