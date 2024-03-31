6a6049fad5aff13667899e4f7648e8ed273cb778
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-30 17:05:26 -0700

    Time: 10 ms (33.34%) | Memory: 45.1 MB (29.04%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -2,36 +2,34 @@ class Solution {
     Integer[][] dpTable;
     public int longestIncreasingPath(int[][] matrix) {
         if(matrix == null | matrix.length == 0) return 0;
-        
         dpTable = new Integer[matrix.length][matrix[0].length];
+        
         int result = 1;
-
-        for(int row = 0; row < matrix.length; row++){
-            for(int col = 0; col < matrix[0].length; col++){
-                result = Math.max(result, 
-                dfs(matrix, row, col, Integer.MIN_VALUE));
+        for (int row = 0; row < matrix.length; row++){
+            for (int col = 0; col < matrix[0].length; col++){
+                result = Math.max(result, dfs(matrix, row, col, Integer.MIN_VALUE));
             }
         }
-        
         return result;
     }
     
-    private int dfs(int[][] matrix, int row, int col, int prev){
+    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private int dfs(int[][] matrix, int row, int col, int prev) {
         if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length 
-        || prev >= matrix[row][col]) {
+            || prev >= matrix[row][col]) {
             return 0;
         }
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
-        
-        int result = 0;
-        result = Math.max(result, dfs(matrix, row - 1, col, matrix[row][col]) + 1);
-        result = Math.max(result, dfs(matrix, row + 1, col, matrix[row][col]) + 1);
-        result = Math.max(result, dfs(matrix, row, col - 1, matrix[row][col]) + 1);
-        result = Math.max(result, dfs(matrix, row, col + 1, matrix[row][col]) + 1);
-        
-        dpTable[row][col] = result;
-        return result;
+
+        int tempResult = 0;
+        for (int[] dir : directions) {
+            tempResult = Math.max(tempResult, 
+            dfs(matrix, row + dir[0], col + dir[1], matrix[row][col]) + 1);
+        }
+
+        dpTable[row][col] = tempResult;
+        return tempResult;
     }
 }
