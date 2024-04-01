6155f1f59dbc9c3b2139927ac3fe72d7972adc19
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-31 18:39:55 -0700

    Time: 9 ms (5.14%) | Memory: 45.4 MB (5.73%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -4,26 +4,27 @@ class Solution {
         dpTable = new Integer[word1.length()][word2.length()];
         return dp(word1, word1.length() - 1, word2, word2.length() - 1);
     }
-    private int dp(String word1, int i, String word2, int j) {
-        if (i == -1) {
-            return j + 1;
+    private int dp(String word1, int index1, String word2, int index2) {
+        if (index1 == -1) {
+            return index2 + 1;
+        } else if (index2 == -1) {
+            return index1 + 1;
         }
-        if (j == -1) {
-            return i + 1;
-        }
-        if (dpTable[i][j] != null) {
-            return dpTable[i][j];
+        if (dpTable[index1][index2] != null) {
+            return dpTable[index1][index2];
         }
 
-        int skip = dp(word1, i - 1, word2, j - 1);
-        int replace = dp(word1, i - 1, word2, j - 1) + 1;
-        int insert = dp(word1, i, word2, j - 1) + 1;
-        int delete = dp(word1, i - 1, word2, j) + 1;
-        if (word1.charAt(i) == word2.charAt(j)) {
-            dpTable[i][j] = skip;
-        } else if (word1.charAt(i) != word2.charAt(j)) {
-            dpTable[i][j] = Math.min(replace, Math.min(insert, delete));
+        int skip = dp(word1, index1 - 1, word2, index2 - 1);
+        int insert = dp(word1, index1, word2, index2 - 1) + 1;
+        int delete = dp(word1, index1 - 1, word2, index2) + 1;
+        int replace = dp(word1, index1 - 1, word2, index2 - 1) + 1;
+        
+        if (word1.charAt(index1) == word2.charAt(index2)) {
+            dpTable[index1][index2] = skip;
+        } else if (word1.charAt(index1) != word2.charAt(index2)) {
+            dpTable[index1][index2] = Math.min(insert, Math.min(delete, replace));
         }
-        return dpTable[i][j];
+        
+        return dpTable[index1][index2];
     }
 }
