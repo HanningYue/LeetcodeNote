90b19fbc462b7ba2b21cc66bc84a4cef7053e999
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-03-31 19:19:08 -0700

    Time: 5 ms (43.00%) | Memory: 52.9 MB (21.96%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -2,19 +2,25 @@ class Solution {
     public int majorityElement(int[] nums) {
         return divide(nums, 0, nums.length - 1);
     }
-    private int divide (int[] nums, int left, int right) {
+    private int divide(int[] nums, int left, int right) {
+        if (left > right) {
+            return 0;
+        }
         if (left == right) {
             return nums[left];
         }
         int mid = left + (right - left) / 2;
-        int leftPart = divide(nums, left, mid);
-        int rightPart = divide(nums, mid + 1, right);
-
-        int leftMaxCount = conquer(nums, left, right, leftPart);
-        int rightMaxCount = conquer(nums, left, right, rightPart);
-        return leftMaxCount > rightMaxCount ? leftPart : rightPart;
+        int leftHalf = divide(nums, left, mid);
+        int rightHalf = divide(nums, mid + 1, right);
+        int leftMajority = conquer(nums, leftHalf, left, right);
+        int rightMajority = conquer(nums, rightHalf, left, right);
+        if (leftMajority > rightMajority) {
+            return leftHalf;
+        } else {
+            return rightHalf;
+        }
     }
-    private int conquer(int[] nums, int left, int right, int target) {
+    private int conquer(int[] nums, int target, int left, int right) {
         int count = 0;
         for (int i = left; i <= right; i++) {
             if (nums[i] == target) {
