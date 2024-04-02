f8a97e895febdfb6bdbbb4bb74094d5a19fdf7cd
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-02 15:44:03 -0700

    Time: 204 ms (27.27%) | Memory: 41.6 MB (39.67%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -3,27 +3,25 @@ class Solution {
         boolean[][] visited = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, i, j, word, 0, visited)) {
+                if (dfs(board, i, j, 0, word, visited)) {
                     return true;
                 }
             }
         }
         return false;
     }
-    private int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private boolean dfs(char[][] board, int row, int col, String word, int stringIdx, boolean[][] visited) {
-        if (stringIdx == word.length()) {
+    public int[][] directions = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private boolean dfs(char[][] board, int row, int col, int index, String word, boolean[][] visited) {
+        if (index == word.length()) {
             return true;
         }
-
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col]
-        || board[row][col] != word.charAt(stringIdx)) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length
+        || board[row][col] != word.charAt(index) || visited[row][col]) {
             return false;
         }
-
         visited[row][col] = true;
-        for (int[] dir : directions) {
-            if (dfs(board, row + dir[0], col + dir[1], word, stringIdx + 1, visited)) {
+        for (int[] direction : directions) {
+            if (dfs(board, row + direction[0], col + direction[1], index + 1, word, visited)) {
                 return true;
             }
         }
