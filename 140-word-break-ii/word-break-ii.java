c1e6ef1999ead53430345be80e3185e2201cbff4
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-01 18:43:48 -0700

    Time: 4 ms (51.87%) | Memory: 41.4 MB (89.44%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -2,10 +2,10 @@ class Solution {
     List<String>[] dpTable;
     public List<String> wordBreak(String s, List<String> wordDict) {
         dpTable = new ArrayList[s.length()];
-        Set<String> dict = new HashSet<>(wordDict);
-        return dp(s, 0, dict);
+        Set<String> set = new HashSet<>(wordDict);
+        return dp(s, 0, set);
     }
-    private List<String> dp(String s, int index, Set<String> dict) {
+    private List<String> dp(String s, int index, Set<String> set) {
         List<String> result = new ArrayList<>();
         if (index == s.length()) {
             result.add("");
@@ -16,17 +16,18 @@ class Solution {
         }
         for (int length = 1; index + length <= s.length(); length++) {
             String prefix = s.substring(index, index + length);
-            if (dict.contains(prefix)) {
-                List<String> subList = dp(s, index + length, dict);
-                for (String subStr : subList) {
-                    if (subStr.isEmpty()) {
+            if (set.contains(prefix)) {
+                List<String> subList = dp(s, index + length, set);
+                for (String subString : subList) {
+                    if (subString.isEmpty()) {
                         result.add(prefix);
                     } else {
-                        result.add(prefix + " " + subStr);
-                   }
+                        result.add(prefix + " " + subString);
+                    }
                 }
             }
         }
+
         dpTable[index] = result;
         return result;
     }
