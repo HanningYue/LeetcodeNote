5126764a5995b7f8076ad9469057ed446df2ee0a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-03 18:09:48 -0700

    Time: 0 ms (100.00%) | Memory: 40.6 MB (17.01%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -5,18 +5,18 @@ class Solution {
         return dp(m - 1, n - 1);
     }
     private int dp(int row, int col) {
-        if (row < 0 || col < 0) {
-            return 0;
-        }
         if (row == 0 && col == 0) {
             return 1;
+        }
+        if (row < 0 || col < 0) {
+            return 0;
         }
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
-        int up = dp(row - 1, col);
         int left = dp(row, col - 1);
-        dpTable[row][col] = up + left;
+        int up = dp(row - 1, col);
+        dpTable[row][col] = left + up;
         return dpTable[row][col];
     }
 }
