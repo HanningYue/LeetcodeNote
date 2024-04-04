1ea996ebf767fa22b57e1d72400c1156f4bf8878
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-04 15:29:39 -0700

    Time: 3 ms (56.57%) | Memory: 45.7 MB (41.25%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,10 +1,25 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int furthest = nums.length - 1;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] + i >= furthest)
-                furthest = i;
+        int furthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            furthest = Math.max(furthest, i + nums[i]);
+            if (i == furthest) {
+                return false;
+            }
         }
-        return furthest == 0;
+        return furthest >= nums.length - 1;
     }
 }
+/*
+[1, 0, 0]
+0 1 2
+1 1
+  1
+[1, 2, 3, 4, 5, 6]
+ 0  1  2  3  4  5
+furthestIdx = 0 + 1 = 1
+furthestIdx = 1 + 2 = 3
+furthestIdx = 2 + 3 = 5
+furthestIdx = 3 + 4 = 7
+furthestIdx = 4 + 5 = 9
+*/
