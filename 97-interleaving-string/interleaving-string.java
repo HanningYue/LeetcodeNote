92b8e1db502a21178af9215524ae2eada45312e1
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-04 16:04:02 -0700

    Time: 1 ms (89.82%) | Memory: 41.5 MB (68.31%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -5,29 +5,28 @@ class Solution {
         if (m + n != s3.length()) {
             return false;
         }
-        dpTable = new Boolean[m][n];
+        dpTable = new Boolean[m + 1][n + 1];
         return dp(s1, 0, s2, 0, s3, 0);
     }
-
     private boolean dp(String s1, int i, String s2, int j, String s3, int k) {
         if (k == s3.length()) {
             return true;
         }
-        if (i < s1.length() && j < s2.length() && dpTable[i][j] != null) {
+        if (dpTable[i][j] != null) {
+            return dpTable[i][j];
+        }
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+            dpTable[i][j] = dp(s1, i + 1, s2, j, s3, k + 1);
+            if (dpTable[i][j]) {
+                return true;
+            }
+        }
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            dpTable[i][j] = dp(s1, i, s2, j + 1, s3, k + 1);
             return dpTable[i][j];
         }
 
-        boolean valid = false;
-        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
-            valid = dp(s1, i + 1, s2, j, s3, k + 1);
-        }
-        if (!valid && j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
-            valid = dp(s1, i, s2, j + 1, s3, k + 1);
-        }
-        
-        if (i < s1.length() && j < s2.length()) {
-            dpTable[i][j] = valid;
-        }
-        return valid;
+        dpTable[i][j] = false;
+        return false;
     }
 }
