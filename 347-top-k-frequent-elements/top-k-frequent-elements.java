3417a290e9483c6510fa3c8448e737f611a6df96
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-03 17:13:46 -0700

    Time: 12 ms (85.02%) | Memory: 49.6 MB (5.85%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,21 +1,32 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>(); //{integer, frequency}
+        Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        List<Integer> list = new ArrayList<>(map.keySet());
-        Collections.sort(list, new Comparator<Integer>() {
-            public int compare(Integer a, Integer b) {
-                return map.get(b) - map.get(a);
-            }
-        });
-        
-        int[] result = new int[k];
-        for (int i = 0; i < k; i++) {
-            result[i] = list.get(i);
+        List<List<Integer>> bucket = new ArrayList<>();
+        for (int i = 0; i <= nums.length; i++) {
+            bucket.add(new ArrayList<>());
         }
+
+        for (int num : map.keySet()) {
+            int freq = map.get(num);
+            bucket.get(freq).add(num);
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+        for (int i = nums.length; i > 0 && index < k; i--) {
+            for (int num : bucket.get(i)) {
+                result[index] = num;
+                index++;
+                if (index == k) {
+                    break;
+                }
+            }
+        }
+
         return result;
     }
 }
