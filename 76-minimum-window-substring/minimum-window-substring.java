6d7ab1d11977ac6d428a768b491ae1b1741afd12
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-04 17:37:04 -0700

    Time: 118 ms (8.06%) | Memory: 45.4 MB (25.07%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -5,33 +5,33 @@ class Solution {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        int slow = 0, fast = 0, match = 0;
+        int slow = 0, fast = 0;
+        int match = 0, length = Integer.MAX_VALUE;
         String result = "";
-        int length = Integer.MAX_VALUE;
         while (fast < s.length()) {
-            char c = s.charAt(fast);
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) - 1);
-                if (map.get(c) >= 0) {
+            char fastChar = s.charAt(fast);
+            if (map.containsKey(fastChar)) {
+                map.put(fastChar, map.get(fastChar) - 1);
+                if (map.get(fastChar) >= 0) {
                     match++;
                 }
             }
 
             while (match == t.length()) {
-                if (length > fast - slow + 1) {
+                if (fast - slow + 1 < length) {
                     length = fast - slow + 1;
                     result = s.substring(slow, fast + 1);
                 }
-                c = s.charAt(slow);
-                if (map.containsKey(c)) {
-                    map.put(c, map.get(c) + 1);
-                    if (map.get(c) > 0) {
+
+                char slowChar = s.charAt(slow);
+                if (map.containsKey(slowChar)) {
+                    map.put(slowChar, map.get(slowChar) + 1);
+                    if (map.get(slowChar) > 0) {
                         match--;
                     }
                 }
                 slow++;
             }
-            
             fast++;
         }
         return result;
