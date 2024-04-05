f95b1c146873ceaab73eedd787ba963e94dbd2d2
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-04 18:24:10 -0700

    Time: 9 ms (50.92%) | Memory: 45.2 MB (28.87%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,35 +1,33 @@
 class Solution {
     Integer[][] dpTable;
     public int longestIncreasingPath(int[][] matrix) {
-        if(matrix == null | matrix.length == 0) return 0;
-        dpTable = new Integer[matrix.length][matrix[0].length];
-        
+        int m = matrix.length, n = matrix[0].length;
+        dpTable = new Integer[m][n];
+
         int result = 1;
-        for (int row = 0; row < matrix.length; row++){
-            for (int col = 0; col < matrix[0].length; col++){
-                result = Math.max(result, dfs(matrix, row, col, Integer.MIN_VALUE));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result = Math.max(result, dfs(matrix, i, j, Integer.MIN_VALUE));
             }
         }
         return result;
     }
-    
-    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     private int dfs(int[][] matrix, int row, int col, int prev) {
         if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length 
-            || prev >= matrix[row][col]) {
+        || prev >= matrix[row][col]) 
+        {
             return 0;
         }
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
-
-        int tempResult = 0;
+        int currentLongest = 0;
         for (int[] dir : directions) {
-            tempResult = Math.max(tempResult, 
+            currentLongest = Math.max(currentLongest,
             dfs(matrix, row + dir[0], col + dir[1], matrix[row][col]) + 1);
         }
-
-        dpTable[row][col] = tempResult;
-        return tempResult;
+        dpTable[row][col] = currentLongest;
+        return dpTable[row][col];
     }
 }
