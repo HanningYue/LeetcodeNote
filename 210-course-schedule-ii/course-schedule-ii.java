d67a76ec62d6c6009a53bc3815d21064fa60c98e
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-04 18:48:31 -0700

    Time: 6 ms (57.79%) | Memory: 45.6 MB (71.37%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,31 +1,31 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         Map<Integer, Set<Integer>> graph = new HashMap<>();
-
+        for (int i = 0; i < numCourses; i++) {
+            graph.put(i, new HashSet<Integer>());
+        }
         for (int[] pre : prerequisites) {
-            int currentCourse = pre[0];
-            int preCourse = pre[1];
-            graph.putIfAbsent(currentCourse, new HashSet<>());
-            graph.get(currentCourse).add(preCourse);
+            int from = pre[1], to = pre[0];
+            graph.get(to).add(from);
         }
 
-        boolean[] visiting = new boolean[numCourses];
-        boolean[] visited = new boolean[numCourses];
-
         List<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[numCourses];
+        boolean[] visiting = new boolean[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(graph, visiting, visited, i, list)) {
+            if (!dfs(graph, i, visited, visiting, list)) {
                 return new int[]{};
             }
         }
+
         int[] result = new int[numCourses];
         for (int i = 0; i < list.size(); i++) {
             result[i] = list.get(i);
         }
         return result;
     }
-    private boolean dfs(Map<Integer, Set<Integer>> graph, boolean[] visiting, 
-    boolean[] visited, int vertex, List<Integer> list) {
+    private boolean dfs(Map<Integer, Set<Integer>> graph, int vertex, boolean[] visited, 
+                    boolean[] visiting, List<Integer> list) {
         if (visiting[vertex]) {
             return false;
         }
@@ -40,7 +40,7 @@ class Solution {
 
         visiting[vertex] = true;
         for (int neighbor : graph.get(vertex)) {
-            if (!dfs(graph, visiting, visited, neighbor, list)) {
+            if (!dfs(graph, neighbor, visited, visiting, list)) {
                 return false;
             }
         }
