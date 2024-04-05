69f517cc881cca44b85616bbe976bf81913e77bd
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-04 17:48:16 -0700

    Time: 123 ms (7.61%) | Memory: 45.9 MB (6.61%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -4,15 +4,15 @@ class Solution {
         for (char c : t.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-
+        
         int slow = 0, fast = 0;
         int match = 0, length = Integer.MAX_VALUE;
         String result = "";
         while (fast < s.length()) {
-            char c = s.charAt(fast);
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) - 1);
-                if (map.get(c) >= 0) {
+            char fastChar = s.charAt(fast);
+            if (map.containsKey(fastChar)) {
+                map.put(fastChar, map.get(fastChar) - 1);
+                if (map.get(fastChar) >= 0) {
                     match++;
                 }
             }
@@ -22,11 +22,10 @@ class Solution {
                     length = fast - slow + 1;
                     result = s.substring(slow, fast + 1);
                 }
-
-                c = s.charAt(slow);
-                if (map.containsKey(c)) {
-                    map.put(c, map.get(c) + 1);
-                    if (map.get(c) > 0) {
+                char slowChar = s.charAt(slow);
+                if (map.containsKey(slowChar)) {
+                    map.put(slowChar, map.get(slowChar) + 1);
+                    if (map.get(slowChar) > 0) {
                         match--;
                     }
                 }
