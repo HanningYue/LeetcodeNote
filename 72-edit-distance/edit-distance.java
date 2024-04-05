8a7c512b5fd4c61b5d758cae89e4842d361fd74b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-04 22:52:13 -0700

    Time: 3 ms (97.53%) | Memory: 44.7 MB (83.65%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,30 +1,28 @@
 class Solution {
     Integer[][] dpTable;
     public int minDistance(String word1, String word2) {
-        dpTable = new Integer[word1.length()][word2.length()];
-        return dp(word1, word1.length() - 1, word2, word2.length() - 1);
+        int m = word1.length(), n = word2.length();
+        dpTable = new Integer[m][n];
+        return dp(word1, m - 1, word2, n - 1);
     }
-    private int dp(String word1, int index1, String word2, int index2) {
-        if (index1 == -1) {
-            return index2 + 1;
-        } else if (index2 == -1) {
-            return index1 + 1;
-        }
-        if (dpTable[index1][index2] != null) {
-            return dpTable[index1][index2];
+    private int dp(String word1, int i, String word2, int j) {
+        if (i == -1) {
+            return j + 1;
+        } else if (j == -1) {
+            return i + 1;
         }
 
-        int skip = dp(word1, index1 - 1, word2, index2 - 1);
-        int insert = dp(word1, index1, word2, index2 - 1) + 1;
-        int delete = dp(word1, index1 - 1, word2, index2) + 1;
-        int replace = dp(word1, index1 - 1, word2, index2 - 1) + 1;
-        
-        if (word1.charAt(index1) == word2.charAt(index2)) {
-            dpTable[index1][index2] = skip;
-        } else if (word1.charAt(index1) != word2.charAt(index2)) {
-            dpTable[index1][index2] = Math.min(insert, Math.min(delete, replace));
+        if (dpTable[i][j] != null) {
+            return dpTable[i][j];
         }
-        
-        return dpTable[index1][index2];
+        if (word1.charAt(i) == word2.charAt(j)) {
+            dpTable[i][j] = dp(word1, i - 1, word2, j - 1);
+        } else {
+            int skipI = dp(word1, i - 1, word2, j) + 1;
+            int skipJ = dp(word1, i, word2, j - 1) + 1;
+            int skipBoth = dp(word1, i - 1, word2, j - 1) + 1;
+            dpTable[i][j] = Math.min(skipI, Math.min(skipJ, skipBoth));
+        }
+        return dpTable[i][j];
     }
 }
