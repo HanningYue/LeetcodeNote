ed1ac45908a602d6c446f2f722cf72bb44e378c8
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-05 23:59:14 -0700

    Time: 10 ms (46.31%) | Memory: 47.6 MB (68.04%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.java
+++ 286-walls-and-gates/walls-and-gates.java
@@ -1,6 +1,6 @@
 class Solution {
     public void wallsAndGates(int[][] rooms) {
-        Queue<int[]> queue = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>(); //save each gate
         for (int i = 0; i < rooms.length; i++) {
             for (int j = 0; j < rooms[0].length; j++) {
                 if (rooms[i][j] == 0) {
@@ -10,17 +10,14 @@ class Solution {
         }
 
         boolean[][] visited = new boolean[rooms.length][rooms[0].length];
-        int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 int[] gate = queue.poll();
-                for (int[] dir : dirs) {
+                for (int[] dir : directions) {
                     int row = gate[0], col = gate[1];
-                    int newRow = row + dir[0];
-                    int newCol = col + dir[1];
-
+                    int newRow = gate[0] + dir[0], newCol = gate[1] + dir[1];
                     if (newRow < 0 || newRow >= rooms.length || newCol < 0 || newCol >= rooms[0].length 
                     || rooms[newRow][newCol] != Integer.MAX_VALUE || visited[newRow][newCol]) {
                         continue;
