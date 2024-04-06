8239cee09ed16795dbfc2c725e3fec19d3f54951
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-06 00:41:56 -0700

    Time: 2 ms (72.58%) | Memory: 41.9 MB (88.83%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,43 +1,41 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int fresh = 0;
         Queue<int[]> queue = new LinkedList<>();
+        int fresh = 0;
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 2) {
                     queue.offer(new int[]{i, j});
                 } else if (grid[i][j] == 1) {
                     fresh++;
                 }
-            } 
+            }
         }
 
         int step = 0;
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         boolean[][] visited = new boolean[grid.length][grid[0].length];
-        int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-        while (!queue.isEmpty() && fresh != 0) {
+        while (!queue.isEmpty() && fresh > 0) {
             int size = queue.size();
             step++;
             for (int i = 0; i < size; i++) {
                 int[] rotten = queue.poll();
-                for (int[] dir : dirs) {
+                for (int[] dir : directions) {
                     int row = rotten[0], col = rotten[1];
-                    int newRow = row + dir[0];
-                    int newCol = col + dir[1];
-                    
-                    if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length 
+                    int newRow = row + dir[0], newCol = col + dir[1];
+                    if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length
                     || grid[newRow][newCol] != 1 || visited[newRow][newCol]) {
                         continue;
                     }
-                    
-                    grid[newRow][newCol] = 2;
                     visited[newRow][newCol] = true;
                     fresh--;
                     queue.offer(new int[]{newRow, newCol});
                 }
             }
         }
-        return fresh == 0 ? step : -1;
+        if (fresh == 0) {
+            return step;
+        }
+        return -1;
     }
 }
