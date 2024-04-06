2ef3feddf7b282bab33562bed41c08a132115251
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-05 17:36:32 -0700

    Time: 20 ms (23.51%) | Memory: 55.2 MB (87.01%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (!set.add(num)) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] == nums[i]) {
                 return true;
             }
         }
