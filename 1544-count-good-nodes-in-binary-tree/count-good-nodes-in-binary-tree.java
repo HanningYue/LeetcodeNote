8f95a30a247625d65c33e4688f4f1bc8e6f17047
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-06 00:49:43 -0700

    Time: 2 ms (83.54%) | Memory: 51.8 MB (94.28%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -1,22 +1,33 @@
 /**
-1. We want to return the total number of good nodes, create a reference variable
-2. we need to compare the current level node value with the previous path max 
-3. if the current node value is greater, we need to update the previous path max*/
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     int count = 0;
     public int goodNodes(TreeNode root) {
-        dfs(root, Integer.MIN_VALUE);
+        dfs(root, root.val);
         return count;
     }
-    private void dfs(TreeNode root, int pathMax) {
+    private void dfs(TreeNode root, int prevMax) {
         if (root == null) {
             return;
         }
-        if (root.val >= pathMax) {
-            pathMax = root.val;
+        if (root.val >= prevMax) {
+            prevMax = root.val;
             count++;
         }
-        dfs(root.left, pathMax);
-        dfs(root.right, pathMax);
+        dfs(root.left, prevMax);
+        dfs(root.right, prevMax);
     }
 }
