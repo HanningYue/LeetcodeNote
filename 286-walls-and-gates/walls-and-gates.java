5ecc0da590e525ddc0f6f2cefd883327adb15a06
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-05 21:19:58 -0700

    Time: 9 ms (62.73%) | Memory: 47.8 MB (51.37%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.java
+++ 286-walls-and-gates/walls-and-gates.java
@@ -1,11 +1,8 @@
-/**
-BFS, same as rotten orange, but we add gate to queue
-*/
 class Solution {
     public void wallsAndGates(int[][] rooms) {
         Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < rooms.length; i++) {
-            for (int j = 0; j < rooms[i].length; j++) {
+            for (int j = 0; j < rooms[0].length; j++) {
                 if (rooms[i][j] == 0) {
                     queue.offer(new int[]{i, j});
                 }
@@ -14,22 +11,24 @@ class Solution {
 
         boolean[][] visited = new boolean[rooms.length][rooms[0].length];
         int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 int[] gate = queue.poll();
                 for (int[] dir : dirs) {
                     int row = gate[0], col = gate[1];
-                    int x = row + dir[0];
-                    int y = col + dir[1];
+                    int newRow = row + dir[0];
+                    int newCol = col + dir[1];
 
-                    if (x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length 
-                    || rooms[x][y] != Integer.MAX_VALUE || visited[x][y]) {
+                    if (newRow < 0 || newRow >= rooms.length || newCol < 0 || newCol >= rooms[0].length 
+                    || rooms[newRow][newCol] != Integer.MAX_VALUE || visited[newRow][newCol]) {
                         continue;
                     }
-                    visited[x][y] = true;
-                    rooms[x][y] = rooms[row][col] + 1;
-                    queue.offer(new int[]{x, y});
+
+                    visited[newRow][newCol] = true;
+                    rooms[newRow][newCol] = rooms[row][col] + 1;
+                    queue.offer(new int[]{newRow, newCol});
                 }
             }
         }
