b1c9ade054de4ccf1032c2afe2cf0b7cea6cdf4a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-06 23:32:53 -0700

    Time: 237 ms (39.32%) | Memory: 44.6 MB (75.78%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.java
@@ -3,12 +3,11 @@ class Solution {
         int[][] reach = new int[grid.length][grid[0].length];
         int[][] distance = new int[grid.length][grid[0].length];
         int totalBuildings = 0;
-
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
                     totalBuildings++;
-                    if (!bfs(grid, i, j,distance, reach)) {
+                    if (!bfs(grid, i, j, reach, distance)) {
                         return -1;
                     }
                 }
@@ -20,14 +19,14 @@ class Solution {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 0 && reach[i][j] == totalBuildings) {
                     minDistance = Math.min(minDistance, distance[i][j]);
-                }                
+                }
             }
         }
         return minDistance == Integer.MAX_VALUE ? -1 : minDistance;
     }
 
     int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private boolean bfs(int[][] grid, int row, int col, int[][] distance, int[][] reach) {
+    private boolean bfs(int[][] grid, int row, int col, int[][] reach, int[][] distance) {
         boolean[][] visited = new boolean[grid.length][grid[0].length];
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{row, col});
@@ -41,15 +40,15 @@ class Solution {
                 for (int[] dir : directions) {
                     int oldRow = currentBuilding[0], oldCol = currentBuilding[1];
                     int newRow = oldRow + dir[0], newCol = oldCol + dir[1];
-                    if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length 
+                    if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length
                     || visited[newRow][newCol] || grid[newRow][newCol] != 0) {
                         continue;
                     }
-                    
-                    distance[newRow][newCol] += step;
-                    reach[newRow][newCol]++;
-                    visited[newRow][newCol] = true;
+
                     queue.offer(new int[]{newRow, newCol});
+                    visited[newRow][newCol] = true;
+                    reach[newRow][newCol]++;
+                    distance[newRow][newCol] += step;
                 }
             }
             step++;
