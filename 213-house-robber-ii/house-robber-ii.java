c6b7109e39ed82718be7826ca9eb78ebf9614cd3
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-07 17:14:12 -0700

    Time: 0 ms (100.00%) | Memory: 40.9 MB (48.35%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -5,14 +5,11 @@ class Solution {
         if (nums.length == 1) {
             return nums[0];
         }
-        
         dpTableOne = new Integer[nums.length];
         dpTableTwo = new Integer[nums.length];
-
-        int doNotRobLast = dp(nums, 0, nums.length - 2, dpTableOne);
-        int robLast = dp(nums, 1, nums.length - 1, dpTableTwo);
-        int result = Math.max(doNotRobLast, robLast);
-        return result;
+        int robLast = dp(nums, 1, nums.length - 1, dpTableOne);
+        int doNotRobLast = dp(nums, 0, nums.length - 2, dpTableTwo);
+        return Math.max(robLast, doNotRobLast);
     }
     private int dp(int[] nums, int start, int end, Integer[] dpTable) {
         if (start > end) {
@@ -23,8 +20,7 @@ class Solution {
         }
         int rob = dp(nums, start + 2, end, dpTable) + nums[start];
         int doNotRob = dp(nums, start + 1, end, dpTable);
-        int result = Math.max(rob, doNotRob);
-        dpTable[start] = result;
-        return result;
+        dpTable[start] = Math.max(doNotRob, rob);
+        return dpTable[start];
     }
 }
