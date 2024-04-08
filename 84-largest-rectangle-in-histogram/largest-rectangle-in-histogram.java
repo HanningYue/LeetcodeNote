b155365476bdd490a05ed7a36f1a3564313b75c8
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-07 19:29:50 -0700

    Time: 49 ms (79.03%) | Memory: 57.4 MB (52.21%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,9 +1,8 @@
 class Pair {
-    int height;
-    int index;
-    public Pair(int height, int index) {
-        this.height = height;
+    int index, height;
+    public Pair(int index, int height) {
         this.index = index;
+        this.height = height;
     }
 }
 class Solution {
@@ -13,18 +12,19 @@ class Solution {
 
         for (int i = 0; i < heights.length; i++) {
             int currentIndex = i, currentHeight = heights[i];
-        
             while (!stack.isEmpty() && stack.peek().height >= currentHeight) {
                 Pair lastRec = stack.pop();
-                result = Math.max(result, lastRec.height * (i - lastRec.index));
-                currentIndex = lastRec.index;        
+                int lastArea = (i - lastRec.index) * lastRec.height;
+                result = Math.max(result, lastArea);
+                currentIndex = lastRec.index;
             }
-        
-            stack.push(new Pair(currentHeight, currentIndex));
+            stack.push(new Pair(currentIndex, currentHeight));
         }
+
         while (!stack.isEmpty()) {
             Pair lastRec = stack.pop();
-            result = Math.max(result, (heights.length - lastRec.index) * lastRec.height);
+            int lastArea = (heights.length - lastRec.index) * lastRec.height;
+            result = Math.max(result, lastArea);
         }
         return result;
     }
