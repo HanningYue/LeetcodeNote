74c407a3879d01bedfd644e35e446aa08bd95588
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-08 16:36:37 -0700

    Time: 1 ms (98.58%) | Memory: 41.4 MB (61.48%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,50 +2,16 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '(' || c == '[' || c == '{') {
-                stack.push(c);
-            } else {
-                if (!stack.isEmpty() && stack.peek() == leftOf(c)) {
-                    stack.pop();
-                } else {
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
-    }
-    private char leftOf(char rightBracket) {
-        if (rightBracket == ')') {
-            return '(';
-        } else if (rightBracket == ']') {
-            return '[';
-        }
-        return '{';
-    }
-}
-
-/*
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        Map<Character,Character> map = new HashMap<>(); //{left, right};
-        map.put(']', '[');
-        map.put('}', '{');
-        map.put(')', '(');
-
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (map.containsKey(c)) {
-                if (!stack.isEmpty() && stack.peek().equals(map.get(c))) {
-                    stack.pop();
-                } else {
-                    return false;
-                }
-            } else {
-                stack.push(c);
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (stack.isEmpty() || c != stack.pop()) {
+                return false;
             }
         }
         return stack.isEmpty();
     }
 }
-*/
