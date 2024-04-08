c743a79fc5ad3e3edcc8265b978bf1d7881eda1d
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-07 18:45:09 -0700

    Time: 50 ms (78.09%) | Memory: 57.4 MB (44.36%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,41 +1,30 @@
 class Pair {
-    int index, height;
-    public Pair(int index, int height) {
-        this.index = index;
+    int height;
+    int index;
+    public Pair(int height, int index) {
         this.height = height;
-    }
-    public int getHeight() {
-        return height;
-    }
-    public int getIdx() {
-        return index;
+        this.index = index;
     }
 }
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int result = 0;
         Stack<Pair> stack = new Stack<>();
+        int result = 0;
+
         for (int i = 0; i < heights.length; i++) {
-            int currentIdx = i;
-            int currentHeight = heights[i];
-
-            while (!stack.isEmpty() && stack.peek().getHeight() > currentHeight) {
-                Pair lastRectangle = stack.pop();
-                int lastIdx = lastRectangle.getIdx();
-                int lastHeight = lastRectangle.getHeight();
-
-                result = Math.max(result, (i - lastIdx) * lastHeight);
-                currentIdx = lastIdx;
+            int currentIndex = i, currentHeight = heights[i];
+        
+            while (!stack.isEmpty() && stack.peek().height >= currentHeight) {
+                Pair lastRec = stack.pop();
+                result = Math.max(result, lastRec.height * (i - lastRec.index));
+                currentIndex = lastRec.index;        
             }
-            stack.push(new Pair(currentIdx, currentHeight));
+        
+            stack.push(new Pair(currentHeight, currentIndex));
         }
-
         while (!stack.isEmpty()) {
-            Pair lastRectangle = stack.pop();
-            int lastIdx = lastRectangle.getIdx();
-            int lastHeight = lastRectangle.getHeight();
-            
-            result = Math.max(result, (heights.length - lastIdx) * lastHeight);
+            Pair lastRec = stack.pop();
+            result = Math.max(result, (heights.length - lastRec.index) * lastRec.height);
         }
         return result;
     }
