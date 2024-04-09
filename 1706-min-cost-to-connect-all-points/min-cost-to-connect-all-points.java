42ed7c26d9d5a2df8568f2fe859d49662f30984c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-08 17:51:00 -0700

    Time: 133 ms (54.34%) | Memory: 62.5 MB (41.63%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,29 +1,29 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int cost = 0;
+        Set<Integer> visitedNode = new HashSet<>();
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
         minHeap.offer(new int[]{0, 0});
-        Set<Integer> visited = new HashSet<>();
-        
-        while (visited.size() < points.length) {
+        int cost = 0;
+
+        while (visitedNode.size() < points.length) {
             int[] point = minHeap.poll();
             int currentWeight = point[0];
             int currentNode = point[1];
-
-            if (visited.contains(currentNode)) {
+            if (visitedNode.contains(currentNode)) {
                 continue;
             }
-            visited.add(currentNode);
+
+            visitedNode.add(currentNode);
             cost += currentWeight;
 
             for (int nextNode = 0; nextNode < points.length; nextNode++) {
-                if (visited.contains(nextNode)) {
+                if (visitedNode.contains(nextNode)) {
                     continue;
                 }
-            
+
                 int nextWeight = Math.abs(points[nextNode][0] - points[currentNode][0])
                                 +Math.abs(points[nextNode][1] - points[currentNode][1]);
-                minHeap.offer(new int[]{nextWeight, nextNode});                
+                minHeap.offer(new int[]{nextWeight, nextNode});
             }
         }
         return cost;
