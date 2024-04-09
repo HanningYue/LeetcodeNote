2fef64c233739653cbdcacd06e1f5b18a826f768
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-08 17:26:55 -0700

    Time: 83 ms (24.43%) | Memory: 56.8 MB (33.42%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,16 +1,15 @@
 class Solution {
     Boolean[][] dpTable;
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
-        int sum = 0;
+        int n = nums.length, sum = 0;
         for (int num : nums) {
             sum += num;
         }
         if (sum % 2 != 0) {
             return false;
         }
-        dpTable = new Boolean[n][sum + 1];
         sum = sum / 2;
+        dpTable = new Boolean[n][sum + 1];
         return dp(nums, n - 1, sum);
     }
     private boolean dp(int[] nums, int index, int target) {
@@ -23,7 +22,6 @@ class Solution {
         if (dpTable[index][target] != null) {
             return dpTable[index][target];
         }
-
         boolean includeCurrent = dp(nums, index - 1, target - nums[index]);
         boolean excludeCurrent = dp(nums, index - 1, target);
         dpTable[index][target] = includeCurrent || excludeCurrent;
