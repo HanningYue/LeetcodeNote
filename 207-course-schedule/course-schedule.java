2a0dcc137e319bb2cd66de5ee427e273df491dfc
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-08 23:21:05 -0700

    Time: 7 ms (35.81%) | Memory: 45.8 MB (20.27%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -2,35 +2,35 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         Map<Integer, Set<Integer>> graph = new HashMap<>();
         for (int[] pre : prerequisites) {
-            int currentCourse = pre[0];
-            int preCourse = pre[1];
-            graph.putIfAbsent(currentCourse, new HashSet<>());
-            graph.get(currentCourse).add(preCourse);
+            int before = pre[1];
+            int after = pre[0];
+            graph.putIfAbsent(after, new HashSet<>());
+            graph.get(after).add(before);
         }
-
         boolean[] visited = new boolean[numCourses];
         boolean[] visiting = new boolean[numCourses];
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(graph, i, visited, visiting)) {
+            if (!dfs(graph, visited, visiting, i)) {
                 return false;
             }
         }
         return true;
     }
-    private boolean dfs(Map<Integer, Set<Integer>> graph, int vertex, boolean[] visited, boolean[] visiting) {
+    private boolean dfs(Map<Integer, Set<Integer>> graph, boolean[] visited, boolean[] visiting,
+    int vertex) {
         if (visiting[vertex]) {
             return false;
+        }
+        if (!graph.containsKey(vertex)) {
+            visited[vertex] = true;
+            return true;
         }
         if (visited[vertex]) {
             return true;
         }
-        if (!graph.containsKey(vertex)) {
-            return true;
-        }
-    
         visiting[vertex] = true;
         for (int neighbor : graph.get(vertex)) {
-            if (!dfs(graph, neighbor, visited, visiting)) {
+            if (!dfs(graph, visited, visiting, neighbor)) {
                 return false;
             }
         }
