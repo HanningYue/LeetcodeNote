7e0b88a918c70b04241ca4b6fbb84cd0d3d82673
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-08 23:27:55 -0700

    Time: 34 ms (8.39%) | Memory: 62.5 MB (5.10%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,9 +1,8 @@
 class Solution {
     Integer[][] dpTable;
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length(), n = text2.length();
-        dpTable = new Integer[m][n];
-        return dp(text1, m - 1, text2, n - 1);
+        dpTable = new Integer[text1.length()][text2.length()];
+        return dp(text1, text1.length() - 1, text2, text2.length() - 1);
     }
     private int dp(String text1, int i, String text2, int j) {
         if (i == -1 || j == -1) {
@@ -14,12 +13,12 @@ class Solution {
         }
 
         if (text1.charAt(i) == text2.charAt(j)) {
-            int foundSubsequence = dp(text1, i - 1, text2, j - 1) + 1;
-            dpTable[i][j] = foundSubsequence;
+            int found = dp(text1, i - 1, text2, j - 1) + 1;
+            dpTable[i][j] = found;
         } else {
-            int diffInText1 = dp(text1, i - 1, text2, j);
-            int diffInText2 = dp(text1, i, text2, j - 1);
-            dpTable[i][j] = Math.max(diffInText1, diffInText2);
+            int skipI = dp(text1, i - 1, text2, j);
+            int skipJ = dp(text1, i, text2, j - 1);
+            dpTable[i][j] = Math.max(skipI, skipJ);
         }
         return dpTable[i][j];
     }
