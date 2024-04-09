bee118d265af841d98a5774ab936e6408e08958a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-08 18:26:02 -0700

    Time: 3 ms (44.17%) | Memory: 46.7 MB (27.82%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.java
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.java
@@ -1,20 +1,19 @@
 class Solution {
     public int maxDistToClosest(int[] seats) {
-        int maxDistance = 0;
-        int lastOccupied = -1;
-        
+        int maxDistance = 0, lastPerson = -1;
         for (int i = 0; i < seats.length; i++) {
             if (seats[i] == 1) {
-                if (lastOccupied == -1) {
+                if (lastPerson == -1) {
                     maxDistance = i;
-                } else {
-                    maxDistance = Math.max(maxDistance, (i - lastOccupied) / 2);
+                } else if (lastPerson != -1) {
+                    maxDistance = Math.max(maxDistance, (i - lastPerson) / 2);
                 }
-                lastOccupied = i;
+                lastPerson = i;
             }
         }
+
         if (seats[seats.length - 1] == 0) {
-            maxDistance = Math.max(maxDistance, seats.length - 1 - lastOccupied);
+            maxDistance = Math.max(maxDistance, seats.length - 1 - lastPerson);
         }
         return maxDistance;
     }
