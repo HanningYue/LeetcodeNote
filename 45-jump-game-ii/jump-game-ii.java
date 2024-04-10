a4def2434ce762ed90f154546bc6411b280ac321
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-10 16:29:43 -0700

    Time: 2 ms (76.34%) | Memory: 44.9 MB (73.50%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,16 +1,14 @@
 class Solution {
     public int jump(int[] nums) {
-        int furthest = 0;
-        int subFurthest = 0, step = 0;
-
+        int subFurthest = 0, furthest = 0;
+        int step = 0;
         for (int i = 0; i < nums.length - 1; i++) {
-            furthest = Math.max(furthest, i + nums[i]);
-            if (i == subFurthest) {
-                subFurthest = furthest;
+            furthest = Math.max(furthest, nums[i] + i);
+            if (subFurthest == i) {
                 step++;
+                subFurthest = furthest;
             }
         }
-
         return step;
     }
 }
