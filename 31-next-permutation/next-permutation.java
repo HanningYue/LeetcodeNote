3637954a7708adde4006a256ec54b352b8f1af39
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-10 15:33:44 -0700

    Time: 0 ms (100.00%) | Memory: 42.6 MB (89.34%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int firstDecrease = nums.length - 2;
+        while (firstDecrease >= 0 && nums[firstDecrease + 1] <= nums[firstDecrease]) {
+            firstDecrease--;
+        }
+
+        if (firstDecrease >= 0) {
+            int firstGreater = nums.length - 1;
+            while (firstGreater >= 0 && nums[firstGreater] <= nums[firstDecrease]) {
+                firstGreater--;
+            }
+            swap(nums, firstDecrease, firstGreater);
+        }
+
+        reverse(nums, firstDecrease + 1);
+    }
+    private void reverse(int[] nums, int index) {
+        int left = index, right = nums.length - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
