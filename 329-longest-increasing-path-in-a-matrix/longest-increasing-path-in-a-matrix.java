451159c3a9737a85a82d2ea90c81a13ebb1b9453
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-09 23:55:39 -0700

    Time: 9 ms (51.27%) | Memory: 44.4 MB (95.58%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -3,32 +3,30 @@ class Solution {
     public int longestIncreasingPath(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         dpTable = new Integer[m][n];
-        
-        int prev = Integer.MIN_VALUE;
-        int result = 1;
+
+        int result = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                result = Math.max(result, dfs(matrix, i, j, prev));
+                result = Math.max(result, dfs(matrix, i, j, Integer.MIN_VALUE));
             }
         }
         return result;
     }
-
-    int[][] directions = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    private int dfs(int[][] matrix, int row, int col, int prev) {
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private int dfs(int[][] matrix, int row, int col, int previousNumber) {
         if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length 
-        || matrix[row][col] <= prev) {
+        || matrix[row][col] <= previousNumber) {
             return 0;
         }
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
 
-        int current = 1;
+        int currentPath = 1;
         for (int[] dir : directions) {
-            current = Math.max(current, dfs(matrix, row + dir[0], col + dir[1], matrix[row][col]) + 1);
+            currentPath = Math.max(currentPath, dfs(matrix, row + dir[0], col + dir[1], matrix[row][col]) + 1);
         }
-        dpTable[row][col] = current;
-        return current;
+        dpTable[row][col] = currentPath;
+        return currentPath;
     }
 }
