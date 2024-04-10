bec59efb20ef4740a85b8f3792be2f161bc62f48
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-09 21:22:42 -0700

    Time: 215 ms (16.27%) | Memory: 41.4 MB (55.82%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,27 +1,29 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        boolean[][] visited = new boolean[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, i, j, 0, word, visited)) {
+        int m = board.length, n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, i, j, word, 0, visited)) {
                     return true;
                 }
             }
         }
         return false;
     }
-    public int[][] directions = new int[][] {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private boolean dfs(char[][] board, int row, int col, int index, String word, boolean[][] visited) {
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private boolean dfs(char[][] board, int row, int col, String word, int index, boolean[][] visited) {
         if (index == word.length()) {
             return true;
         }
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length
-        || board[row][col] != word.charAt(index) || visited[row][col]) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col]
+        || board[row][col] != word.charAt(index)) {
             return false;
         }
+        
         visited[row][col] = true;
-        for (int[] direction : directions) {
-            if (dfs(board, row + direction[0], col + direction[1], index + 1, word, visited)) {
+        for (int[] dir : directions) {
+            if (dfs(board, row + dir[0], col + dir[1], word, index + 1, visited)) {
                 return true;
             }
         }
