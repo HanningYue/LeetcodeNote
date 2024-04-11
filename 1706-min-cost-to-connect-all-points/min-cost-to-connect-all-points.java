b804817786e1a13d6774251406add98e9478228e
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-10 19:51:26 -0700

    Time: 142 ms (52.41%) | Memory: 62.6 MB (40.14%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,31 +1,36 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
         Set<Integer> visitedNode = new HashSet<>();
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
-        minHeap.offer(new int[]{0, 0});
-        int cost = 0;
-
+        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[1] - b[1];
+            }
+        });
+        heap.offer(new int[]{0, 0});
+        
+        int result = 0;        
         while (visitedNode.size() < points.length) {
-            int[] point = minHeap.poll();
-            int currentWeight = point[0];
-            int currentNode = point[1];
-            if (visitedNode.contains(currentNode)) {
+            int[] currentNode = heap.poll();
+            int currentIndex = currentNode[0];
+            int currentWeight = currentNode[1];
+
+            if (visitedNode.contains(currentIndex)) {
                 continue;
             }
+            visitedNode.add(currentIndex);
+            result += currentWeight;
 
-            visitedNode.add(currentNode);
-            cost += currentWeight;
-
-            for (int nextNode = 0; nextNode < points.length; nextNode++) {
-                if (visitedNode.contains(nextNode)) {
+            for (int i = 0; i < points.length; i++) {
+                if (visitedNode.contains(i)) {
                     continue;
                 }
 
-                int nextWeight = Math.abs(points[nextNode][0] - points[currentNode][0])
-                                +Math.abs(points[nextNode][1] - points[currentNode][1]);
-                minHeap.offer(new int[]{nextWeight, nextNode});
+                int nextWeight = Math.abs(points[i][0] - points[currentIndex][0])
+                                +Math.abs(points[i][1] - points[currentIndex][1]);
+                int[] nextNode = new int[]{i, nextWeight};
+                heap.offer(nextNode);
             }
         }
-        return cost;
+        return result;
     }
 }
