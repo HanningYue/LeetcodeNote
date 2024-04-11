11f5713a0749f058ec4a8e7afa21ca17d1c441fd
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-10 19:29:42 -0700

    Time: 34 ms (71.04%) | Memory: 62.1 MB (24.46%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,23 +1,26 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>() {
+        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
             public int compare(int[] a, int[] b) {
-                return (a[0] + a[1]) - (b[0] + b[1]);
+                int sumA = a[0] + a[1];
+                int sumB = b[0] + b[1];
+                return sumA - sumB;
             }
         });
-
         for (int i = 0; i < nums1.length; i++) {
-            minHeap.offer(new int[]{nums1[i], nums2[0], 0});
+            heap.offer(new int[]{nums1[i], nums2[0], 0});
         }
-        while (!minHeap.isEmpty() && k > 0) {
-            int[] currentSmallest = minHeap.poll();
+
+        while (!heap.isEmpty() && k > 0) {
+            int[] currentSmallest = heap.poll();
             int numOne = currentSmallest[0];
-            int numTwo = currentSmallest[1], numTwoIdx = currentSmallest[2];
+            int numTwo = currentSmallest[1];
+            int numTwoIndex = currentSmallest[2];
             result.add(Arrays.asList(numOne, numTwo));
 
-            if (numTwoIdx + 1 < nums2.length) {
-                minHeap.offer(new int[]{numOne, nums2[numTwoIdx + 1], numTwoIdx + 1});
+            if (numTwoIndex + 1 < nums2.length) {
+                heap.offer(new int[]{numOne, nums2[numTwoIndex + 1], numTwoIndex + 1});
             }
             k--;
         }
