95f37fccff36f33fe24a6e6fe5c05a6cacc39730
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-11 02:16:14 -0700

    Time: 10 ms (13.73%) | Memory: 45.3 MB (35.80%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,35 +1,23 @@
-/**
-Graph, run DFS
-1. Create adjacency list, which needs to be SORTED! 
-    -> Map<String, PriorityQueue<String>> //minHeap by default, natural ordering
-    The priorityQueue here stores the LEXICOGRAPHICALLY smallest character
-2. We want to print out the result as a string List, use a Stack (LIFO) to help us construct travel path
-3. We are running DFS on the graph, so the order should be reversed, We are given List<List<String>> tickets
-    use LinkedList<String> to store travel path
-4. If we reach a DEAD END, we need to BACKTRACK, 
-    the condition to backtrack is when graph has no element -> graph.isEmpty() OR if (!graph.isEmpty) else
-    When backtracking, add stack.pop to the Head of result
-5. When backtracking decision, declear next City as PEEK()!, when dead-end, add to head of itinerary use POP()!
-Return itinerary
-*/
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
-        Map<String, PriorityQueue<String>> graph = new HashMap<>();
-        Stack<String> stack = new Stack<>();
-        LinkedList<String> result = new LinkedList<>();
-        stack.push("JFK");
+        List<String> result = new ArrayList<>();
 
+        Map<String, PriorityQueue<String>> graph = new HashMap<>();
         for (List<String> ticket : tickets) {
             String depart = ticket.get(0);
             String arrive = ticket.get(1);
-            graph.putIfAbsent(depart, new PriorityQueue<>());
-            graph.get(depart).offer(arrive);
+            graph.putIfAbsent(depart, new PriorityQueue<String>());
+            graph.get(depart).add(arrive);
         }
 
+        Stack<String> stack = new Stack<>();
+        stack.push("JFK");
+
         while (!stack.isEmpty()) {
-            String nextCity = stack.peek();
-            if (!graph.getOrDefault(nextCity, new PriorityQueue<>()).isEmpty()) {
-                stack.push(graph.get(nextCity).poll());
+            String lastCity = stack.peek();
+            if (!graph.getOrDefault(lastCity, new PriorityQueue<String>()).isEmpty()) {
+                String nextCity = graph.get(lastCity).poll();
+                stack.push(nextCity);
             } else {
                 result.add(0, stack.pop());
             }
