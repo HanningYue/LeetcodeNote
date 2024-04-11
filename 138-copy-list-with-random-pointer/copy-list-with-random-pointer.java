d6ba2327947357adae53e2c5a3ed2b0c315d9d5e
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-10 17:33:24 -0700

    Time: 0 ms (100.00%) | Memory: 44.2 MB (79.29%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,20 +15,18 @@ class Node {
 
 class Solution {
     Map<Node, Node> map = new HashMap<>();
-
     public Node copyRandomList(Node head) {
         if (head == null) {
             return null;
         }
-
         if (map.containsKey(head)) {
             return map.get(head);
         }
         Node copy = new Node(head.val);
         map.put(head, copy);
-        
-        copy.next = copyRandomList(head.next);
+
         copy.random = copyRandomList(head.random);
+        copy.next = copyRandomList(head.next);
         return copy;
     }
 }
