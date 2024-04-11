158f81f2838d1c565fffc05682447662dd84d7bf
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-11 02:20:08 -0700

    Time: 10 ms (13.73%) | Memory: 44.8 MB (95.59%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,26 +1,20 @@
 class Solution {
     public List<String> findItinerary(List<List<String>> tickets) {
         List<String> result = new ArrayList<>();
-
         Map<String, PriorityQueue<String>> graph = new HashMap<>();
+        
         for (List<String> ticket : tickets) {
-            String depart = ticket.get(0);
-            String arrive = ticket.get(1);
-            graph.putIfAbsent(depart, new PriorityQueue<String>());
-            graph.get(depart).add(arrive);
+            graph.computeIfAbsent(ticket.get(0), k -> new PriorityQueue<>()).add(ticket.get(1));
         }
 
         Stack<String> stack = new Stack<>();
         stack.push("JFK");
 
         while (!stack.isEmpty()) {
-            String lastCity = stack.peek();
-            if (!graph.getOrDefault(lastCity, new PriorityQueue<String>()).isEmpty()) {
-                String nextCity = graph.get(lastCity).poll();
-                stack.push(nextCity);
-            } else {
-                result.add(0, stack.pop());
+            while (graph.containsKey(stack.peek()) && !graph.get(stack.peek()).isEmpty()) {
+                stack.push(graph.get(stack.peek()).poll());
             }
+            result.add(0, stack.pop()); // add to result in reverse order
         }
         return result;
     }
