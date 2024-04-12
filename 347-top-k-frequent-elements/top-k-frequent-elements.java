df88a198f730cecf4712337728d7e5eb85ecaf2a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-12 14:29:32 -0700

    Time: 15 ms (47.99%) | Memory: 48.3 MB (60.16%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,30 +1,16 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] result = new int[k];
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        List<List<Integer>> freqBucket = new ArrayList<>();
-        for (int i = 0; i <= nums.length; i++) {
-            freqBucket.add(new ArrayList<>());
-        }
-
-        for (int num : map.keySet()) {
-            int freq = map.get(num);
-            freqBucket.get(freq).add(num);
-        }
-
-        int index = 0;
-        for (int maxFreq = nums.length; maxFreq > 0 && index < k; maxFreq--) {
-            for (int num : freqBucket.get(maxFreq)) {
-                result[index] = num;
-                index++;
-                if (index == k) {
-                    break;
-                }
-            }
+        
+        List<Integer> numList = new ArrayList<>(map.keySet());
+        Collections.sort(numList, (a, b) -> map.get(b) - map.get(a));
+        
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = numList.get(i);
         }
         return result;
     }
