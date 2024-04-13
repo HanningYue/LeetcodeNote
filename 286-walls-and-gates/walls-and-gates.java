788a6bb8fa6883837eb83a9d868a5c7adf78b518
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-12 22:38:37 -0700

    Time: 9 ms (62.05%) | Memory: 48 MB (42.58%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.java
+++ 286-walls-and-gates/walls-and-gates.java
@@ -1,31 +1,35 @@
 class Solution {
     public void wallsAndGates(int[][] rooms) {
-        Queue<int[]> queue = new LinkedList<>(); //save each gate
-        for (int i = 0; i < rooms.length; i++) {
-            for (int j = 0; j < rooms[0].length; j++) {
+        int m = rooms.length, n = rooms[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (rooms[i][j] == 0) {
                     queue.offer(new int[]{i, j});
                 }
             }
         }
+        boolean[][] visited = new boolean[m][n];
 
-        boolean[][] visited = new boolean[rooms.length][rooms[0].length];
         int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 int[] gate = queue.poll();
+                int row = gate[0];
+                int col = gate[1];
+                visited[row][col] = true;
+
                 for (int[] dir : directions) {
-                    int row = gate[0], col = gate[1];
-                    int newRow = gate[0] + dir[0], newCol = gate[1] + dir[1];
-                    if (newRow < 0 || newRow >= rooms.length || newCol < 0 || newCol >= rooms[0].length 
-                    || rooms[newRow][newCol] != Integer.MAX_VALUE || visited[newRow][newCol]) {
+                    int newRow = row + dir[0];
+                    int newCol = col + dir[1];
+                    if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || visited[newRow][newCol]
+                    || rooms[newRow][newCol] != Integer.MAX_VALUE) {
                         continue;
                     }
 
-                    visited[newRow][newCol] = true;
-                    rooms[newRow][newCol] = rooms[row][col] + 1;
                     queue.offer(new int[]{newRow, newCol});
+                    rooms[newRow][newCol] = rooms[row][col] + 1;
                 }
             }
         }
