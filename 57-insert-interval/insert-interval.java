604b4d84eeb4304d80356a50a1c5e7e14c72ceb0
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-13 22:10:43 -0700

    Time: 2 ms (36.39%) | Memory: 44.5 MB (92.18%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> result = new ArrayList<>();
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
         for (int i = 0; i < intervals.length; i++) {
-            if (intervals[i][1] < newInterval[0]) {
-                result.add(intervals[i]);
-            } else if (intervals[i][0] > newInterval[1]) {
+            int[] currentInterval = intervals[i];
+            if (newInterval[0] > currentInterval[1]) {
+                result.add(currentInterval);
+            } else if (currentInterval[0] > newInterval[1]) {
                 result.add(newInterval);
-                newInterval = intervals[i];
-            } else if (newInterval[1] >= intervals[i][0] && newInterval[0] <= intervals[i][1]) {
-                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+                newInterval = currentInterval;
+            } else {
+                newInterval[0] = Math.min(newInterval[0], currentInterval[0]);
+                newInterval[1] = Math.max(newInterval[1], currentInterval[1]);
             }
         }
         result.add(newInterval);
