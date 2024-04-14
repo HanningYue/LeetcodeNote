303c81e1492597a88d1c65ec6693eb52f8233a93
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-13 22:35:26 -0700

    Time: 1 ms (81.02%) | Memory: 42.2 MB (12.18%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -13,7 +13,6 @@ class Solution {
                 dp[i][1] = Math.max(dp[i - 1][1], 0 - prices[i]);
                 continue;
             }
-
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
             dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
         }
