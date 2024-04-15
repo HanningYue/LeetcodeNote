9aa0a22bfdb87b1a71db9103b058b1c620ed3739
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-14 20:14:26 -0700

    Time: 1 ms (98.59%) | Memory: 41.4 MB (43.71%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,16 +2,24 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(')');
-            } else if (c == '[') {
-                stack.push(']');
-            } else if (c == '{') {
-                stack.push('}');
-            } else if (stack.isEmpty() || c != stack.pop()) {
-                return false;
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (!stack.isEmpty() && leftOf(c) == stack.peek()) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
             }
         }
         return stack.isEmpty();
+    }
+    private char leftOf(char rightBracket) {
+        if (rightBracket == ')') {
+            return '(';
+        } else if (rightBracket == ']') {
+            return '[';
+        }
+        return '{';
     }
 }
