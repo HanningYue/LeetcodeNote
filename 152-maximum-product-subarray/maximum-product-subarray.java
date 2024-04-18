c03a3b9763ef95a5b8b70ba69f30196b5b2a9184
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-17 23:57:15 -0700

    Time: 5 ms (14.55%) | Memory: 44.1 MB (98.05%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,26 +1,24 @@
 class Solution {
-    Integer[] maxDpTable;
-    Integer[] minDpTable;
+    Integer[] minTable;
+    Integer[] maxTable;
     public int maxProduct(int[] nums) {
         int n = nums.length;
-        maxDpTable = new Integer[n];
-        minDpTable = new Integer[n];
-        maxDpTable[0] = minDpTable[0] = nums[0];
-        
+        minTable = new Integer[n];
+        maxTable = new Integer[n];
+        minTable[0] = maxTable[0] = nums[0];
+
         int result = nums[0];
         for (int i = 1; i < n; i++) {
-            maxDpTable[i] = max(nums[i], nums[i] * maxDpTable[i - 1], nums[i] * minDpTable[i - 1]);
-            minDpTable[i] = min(nums[i], nums[i] * maxDpTable[i - 1], nums[i] * minDpTable[i - 1]);
-            result = Math.max(result, maxDpTable[i]);
+            minTable[i] = min(nums[i], minTable[i - 1] * nums[i], maxTable[i - 1] * nums[i]);
+            maxTable[i] = max(nums[i], minTable[i - 1] * nums[i], maxTable[i - 1] * nums[i]);
+            result = Math.max(result, maxTable[i]);
         }
         return result;
     }
-
-    private int max(int a, int b, int c) {
-        return Math.max(a, Math.max(b, c));
-    }
-
     private int min(int a, int b, int c) {
         return Math.min(a, Math.min(b, c));
+    }
+    private int max(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
     }
 }
