f5e8fb15452598393e65a6bc8d0b0da84dd9dfdc
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-18 13:28:27 -0700

    Time: 8 ms (80.81%) | Memory: 44 MB (59.07%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,22 +1,26 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        List<Integer> tail = new ArrayList<>();
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        List<Integer> tails = new ArrayList<>();
         for (int num : nums) {
-            if (tail.isEmpty() || num > tail.get(tail.size() - 1)) {
-                tail.add(num);
+            if (tails.isEmpty() || num > tails.get(tails.size() -  1)) {
+                tails.add(num);
             } else {
-                int left = 0, right = tail.size() - 1;
+                int left = 0, right = tails.size() - 1;
                 while (left < right) {
                     int mid = left + (right - left) / 2;
-                    if (tail.get(mid) >= num) {
-                        right = mid;
-                    } else if (tail.get(mid) < num) {
+                    if (tails.get(mid) < num) {
                         left = mid + 1;
+                    } else if (tails.get(mid) >= num) {
+                        right = mid;
                     }
                 }
-                tail.set(left, num);
+                tails.set(right, num);
             }
         }
-        return tail.size();
+        return tails.size();
     }
 }
