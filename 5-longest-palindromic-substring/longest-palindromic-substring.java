d7f9b84b35b7b0b1485ec219a2c8f4c27ea00536
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-17 19:26:21 -0700

    Time: 19 ms (67.18%) | Memory: 45.2 MB (34.52%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -9,12 +9,11 @@ class Solution {
         }
         return result;
     }
-    private String palindrome(String s, int left, int right) {
-        while (left >= 0 && right < s.length() 
-        && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    private String palindrome(String s, int leftPointer, int rightPointer) {
+        while (leftPointer >= 0 && rightPointer < s.length() && s.charAt(leftPointer) == s.charAt(rightPointer)) {
+            leftPointer--;
+            rightPointer++;
         }
-        return s.substring(left + 1, right);
+        return s.substring(leftPointer + 1, rightPointer);
     }
 }
