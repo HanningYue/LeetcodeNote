052ba85a06e3b9ed0b8ebbc215337361f81e9e90
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-17 23:46:02 -0700

    Time: 268 ms (11.71%) | Memory: 45.2 MB (6.18%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,20 +1,15 @@
-/**
-1. We want to find the largest product subarray, we can traverse from left to right, right to left
-2. If there is 0 in the array, we return 1 for both side
-3. Update max at each iteration, remember to use *= */
-
 class Solution {
     public int maxProduct(int[] nums) {
-        int maxProduct = Integer.MIN_VALUE;
-        int leftProduct = 1;
-        int rightProduct = 1;
-        for (int i = 0; i < nums.length; i++) {
-            leftProduct = (leftProduct == 0 ? 1 : leftProduct);
-            rightProduct = (rightProduct == 0 ? 1 : rightProduct);
-            leftProduct *= nums[i];
-            rightProduct *= nums[nums.length - 1 - i];
-            maxProduct = Math.max(maxProduct, Math.max(leftProduct, rightProduct));
+        int n = nums.length;
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int runningProduct = 1;
+
+            for (int j = i; j < n; j++) {
+                runningProduct *= nums[j];
+                result = Math.max(result, runningProduct);
+            }
         }
-        return maxProduct;
+        return result;
     }
 }
