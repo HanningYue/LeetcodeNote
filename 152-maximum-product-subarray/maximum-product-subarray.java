b064616f2d8e17e33858203310454fa7d0bce715
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-17 23:48:13 -0700

    Time: 5 ms (14.55%) | Memory: 44.3 MB (93.25%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,15 +1,26 @@
 class Solution {
+    Integer[] maxDpTable;
+    Integer[] minDpTable;
     public int maxProduct(int[] nums) {
         int n = nums.length;
-        int result = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            int runningProduct = 1;
-
-            for (int j = i; j < n; j++) {
-                runningProduct *= nums[j];
-                result = Math.max(result, runningProduct);
-            }
+        maxDpTable = new Integer[n];
+        minDpTable = new Integer[n];
+        maxDpTable[0] = minDpTable[0] = nums[0];
+        
+        int result = nums[0];
+        for (int i = 1; i < n; i++) {
+            maxDpTable[i] = max(nums[i], nums[i] * maxDpTable[i - 1], nums[i] * minDpTable[i - 1]);
+            minDpTable[i] = min(nums[i], nums[i] * maxDpTable[i - 1], nums[i] * minDpTable[i - 1]);
+            result = Math.max(result, maxDpTable[i]);
         }
         return result;
+    }
+
+    private int max(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+
+    private int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
     }
 }
