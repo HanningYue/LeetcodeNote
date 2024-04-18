15d0d6be315efcad663786d4b99dc8b468545361
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-18 00:19:07 -0700

    Time: 1 ms (87.46%) | Memory: 45.4 MB (6.18%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,24 +1,16 @@
 class Solution {
-    Integer[] minTable;
-    Integer[] maxTable;
     public int maxProduct(int[] nums) {
+        int left = 1, right = 1;
         int n = nums.length;
-        minTable = new Integer[n];
-        maxTable = new Integer[n];
-        minTable[0] = maxTable[0] = nums[0];
-
         int result = nums[0];
-        for (int i = 1; i < n; i++) {
-            minTable[i] = min(nums[i], minTable[i - 1] * nums[i], maxTable[i - 1] * nums[i]);
-            maxTable[i] = max(nums[i], minTable[i - 1] * nums[i], maxTable[i - 1] * nums[i]);
-            result = Math.max(result, maxTable[i]);
+
+        for (int i = 0; i < nums.length; i++) {
+            left = left == 0 ? 1 : left;
+            right = right == 0 ? 1 : right;
+            left = left * nums[i];
+            right = right * nums[n - 1 - i];
+            result = Math.max(result, Math.max(left, right));
         }
         return result;
-    }
-    private int min(int a, int b, int c) {
-        return Math.min(a, Math.min(b, c));
-    }
-    private int max(int a, int b, int c) {
-        return Math.max(a, Math.max(b, c));
     }
 }
