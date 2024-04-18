d0d120fd115f489caeaaf6f69d712acaf9a600f7
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-18 00:14:19 -0700

    Time: 1 ms (60.37%) | Memory: 42.1 MB (98.13%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,22 +1,16 @@
-/**
-                []
-        1        2      3
-    2      3     3   
-3   
-*/
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTrack(nums, result, list, 0);
+        backTrack(result, list, nums, 0);
         return result;
     }
-    private void backTrack(int[] nums, List<List<Integer>> result, List<Integer> list, int index) {
+    private void backTrack(List<List<Integer>> result, List<Integer> list, int[] nums, int index) {    
         result.add(new ArrayList<>(list));
-
+        
         for (int i = index; i < nums.length; i++) {
             list.add(nums[i]);
-            backTrack(nums, result, list, i + 1);
+            backTrack(result, list, nums, i + 1);
             list.remove(list.size() - 1);
         }
     }
