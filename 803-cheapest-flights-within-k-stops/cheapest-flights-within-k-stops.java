edc82b1b470c1e40012481bfbc8746a0838e1e43
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-17 19:38:42 -0700

    Time: 8 ms (40.93%) | Memory: 44.8 MB (10.43%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,42 +1,34 @@
 class Solution {
-    Map<Integer, List<int[]>> map;
-    Integer[][] dpTable;
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        k++;
-        map = new HashMap<>();
-        dpTable = new Integer[n][k + 1];
-        for (int[] flight : flights) {
-            int from = flight[0];
-            int to = flight[1];
-            int price = flight[2];
-            map.putIfAbsent(to, new LinkedList<>());
-            map.get(to).add(new int[]{from, price});
+        Map<Integer, List<int[]>> adjacent = new HashMap<>();
+        for (int[] i : flights) {
+            adjacent.computeIfAbsent(i[0], value -> new ArrayList<>()).add(new int[] { i[1], i[2] });
         }
-        return dp(dst, k, src, dst);
-    }
-    private int dp(int sourceCity, int k, int src, int dst) {
-        if (src == sourceCity) {
-            return 0;
-        }
-        if (k == 0) {
-            return -1;
-        }
-        if (dpTable[sourceCity][k] != null) {
-            return dpTable[sourceCity][k];
-        }
+        
+        int[] stops = new int[n];
+        Arrays.fill(stops, Integer.MAX_VALUE);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        // {dist_from_src_node, node, number_of_stops_from_src_node}
+        pq.offer(new int[] { 0, src, 0 });
 
-        int result = Integer.MAX_VALUE;
-        if (map.containsKey(sourceCity)) {
-            for (int[] path : map.get(sourceCity)) {
-                int from = path[0];
-                int price = path[1];
-                int subProblem = dp(from, k - 1, src, dst);
-                if (subProblem != -1) {
-                    result = Math.min(result, subProblem + price);
-                }
+        while (!pq.isEmpty()) {
+            int[] temp = pq.poll();
+            int dist = temp[0];
+            int node = temp[1];
+            int steps = temp[2];
+            // We have already encountered a path with a lower cost and fewer stops,
+            // or the number of stops exceeds the limit.
+            if (steps > stops[node] || steps > k + 1)
+                continue;
+            stops[node] = steps;
+            if (node == dst)
+                return dist;
+            if (!adjacent.containsKey(node))
+                continue;
+            for (int[] a : adjacent.get(node)) {
+                pq.offer(new int[] { dist + a[1], a[0], steps + 1 });
             }
         }
-        dpTable[sourceCity][k] = result == Integer.MAX_VALUE ? -1 : result;
-        return dpTable[sourceCity][k];
+        return -1;
     }
 }
