31ce79a393c446ca09e696030afd3af81bc43c93
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-18 13:31:29 -0700

    Time: 12 ms (84.88%) | Memory: 45.2 MB (32.33%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -6,20 +6,20 @@ class Solution {
         }
         while (left < right) {
             int speed = left + (right - left) / 2;
-            int hours = findHours(piles, speed);
+            int hours = hoursToEatAllBanana(piles, speed);
             if (hours > h) {
                 left = speed + 1;
-            } else {
+            } else if (hours <= h) {
                 right = speed;
             }
         }
         return right;
     }
-    private int findHours(int[] piles, int speed) {
+    private int hoursToEatAllBanana(int[] piles, int speed) {
         int hours = 0;
-        for (int pile : piles) {
-            hours += pile / speed;
-            if (pile % speed != 0) {
+        for (int banana : piles) {
+            hours += banana / speed;
+            if (banana % speed != 0) {
                 hours++;
             }
         }
