193cc0b0eacf8658752878b3856fd0e61b102a42
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 14:59:49 -0700

    Time: 8 ms (68.69%) | Memory: 45.7 MB (84.47%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,27 +1,24 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        int room = 1;
         Arrays.sort(intervals, new Comparator<int[]>() {
             public int compare(int[] a, int[] b) {
                 return a[0] - b[0];
             }
         });
-
         PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
             public int compare(int[] a, int[] b) {
                 return a[1] - b[1];
             }
         });
         heap.offer(intervals[0]);
+        int room = 1;
 
         for (int i = 1; i < intervals.length; i++) {
-            int[] currentMeeting = intervals[i];
-
-            if (!heap.isEmpty() && currentMeeting[0] >= heap.peek()[1]) {
+            int[] currentInterval = intervals[i];
+            if (!heap.isEmpty() && currentInterval[0] >= heap.peek()[1]) {
                 heap.poll();
             }
-
-            heap.offer(currentMeeting);
+            heap.offer(currentInterval);
             room = Math.max(room, heap.size());
         }
         return room;
