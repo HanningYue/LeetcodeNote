34e9a3fc4ecc6a4d3e303be1a466602db372bc38
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 14:27:45 -0700

    Time: 6 ms (80.63%) | Memory: 45.9 MB (60.43%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -1,25 +1,14 @@
-/**
-We need to check if there are any overlaps between intervals
-    Sort the intervals by their start time, non-decreasing
-    Iterate the intervals, check each interval with the next one
-    If the current interval end is greater than the next interval start, return false
-    otherwise, return true
-*/
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        int n = intervals.length;
-
-        int[] starts = new int[n];
-        int[] ends = new int[n];
-        for (int i = 0; i < n; i++) {
-            starts[i] = intervals[i][0];
-            ends[i] = intervals[i][1];
-        }
-        Arrays.sort(starts);
-        Arrays.sort(ends);
-
-        for (int current = 0; current < n - 1; current++) {
-            if (starts[current + 1] < ends[current]) {
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            public int compare (int[] a, int[] b) {
+                return a[1] - b[1];
+            }
+        });
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= intervals[i - 1][1]) {
+                continue;
+            } else {
                 return false;
             }
         }
