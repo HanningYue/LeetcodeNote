ea3861d86e98ebc3a934ad2d6a3965b31a559285
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-18 23:29:00 -0700

    Time: 1 ms (78.08%) | Memory: 44.6 MB (44.01%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -1,3 +1,32 @@
+class UF {
+    int[] parent;
+    int count;
+    public UF(int numberOfNode) {
+        this.count = numberOfNode;
+        parent = new int[numberOfNode];
+        for (int i = 0; i < numberOfNode; i++) {
+            parent[i] = i;
+        }
+    }
+    public int find(int vertex) {
+        if (parent[vertex] != vertex) {
+            parent[vertex] = find(parent[vertex]);
+        }
+        return parent[vertex];
+    }
+    public void union(int p, int q) {
+        int parentP = find(p);
+        int parentQ = find(q);
+        if (parentQ == parentP) {
+            return;
+        }
+        parent[parentP] = parentQ;
+        count--;
+    }
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+}
 class Solution {
     public boolean validTree(int n, int[][] edges) {
         UF uf = new UF(n);
@@ -10,36 +39,5 @@ class Solution {
             uf.union(vertexOne, vertexTwo);
         }
         return uf.count == 1;
-    }
-}
-class UF {
-    int count;
-    int[] parent;
-    public UF(int n) {
-        this.count = n;
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-    }
-    public int find(int vertex) {
-        if (parent[vertex] != vertex) {
-            parent[vertex] = find(parent[vertex]);
-        }
-        return parent[vertex];
-    } 
-    public void union(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        if (rootP == rootQ) {
-            return;
-        }
-        parent[rootP] = rootQ;
-        count--;
-    }
-    public boolean connected(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        return rootP == rootQ;
     }
 }
