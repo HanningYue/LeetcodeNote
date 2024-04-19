77dbc46e5b83474448d3cfa3d47e4592f56b9391
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 14:13:30 -0700

    Time: 24 ms (96.41%) | Memory: 45.2 MB (81.64%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
+        Arrays.sort(hand);
         Map<Integer, Integer> map = new HashMap<>();
         for (int card : hand) {
             map.put(card, map.getOrDefault(card, 0) + 1);
         }
 
-        Arrays.sort(hand);
         for (int i = 0; i < hand.length; i++) {
-            if (map.get(hand[i]) == 0) {
+            int currentCard = hand[i];
+            if (map.get(currentCard) == 0) {
                 continue;
             }
+
             for (int j = 0; j < groupSize; j++) {
-                int consecutive = hand[i] + j;
-                if (map.getOrDefault(consecutive, 0) == 0) {
+                int nextCard = currentCard + j;
+                if (map.getOrDefault(nextCard, 0) == 0) {
                     return false;
                 }
-
-                map.put(consecutive, map.get(consecutive) - 1);
+                map.put(nextCard, map.get(nextCard) - 1);
             }
         }
         return true;
