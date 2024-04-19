ff3fd13200c79f55d9728257b3ed1c9b05ce150e
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 00:52:04 -0700

    Time: 1 ms (83.74%) | Memory: 40.8 MB (54.22%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -3,19 +3,13 @@ class Solution {
         int result = 0;
         while (x != 0) {
             int lastDigit = x % 10;
-            if (result > 0) {
-                if (result > Integer.MAX_VALUE / 10 
-                || (result == Integer.MAX_VALUE / 10 && lastDigit > Integer.MAX_VALUE % 10)) {
-                    return 0;
-                }
-            } else if (result < 0) {
-                if (result < Integer.MIN_VALUE / 10
-                || (result == Integer.MIN_VALUE / 10 && lastDigit < Integer.MIN_VALUE % 10)) {
-                    return 0;
-                }
+            int tempResult = result * 10 + lastDigit;
+            if (tempResult / 10 != result) {
+                return 0;
             }
-            result = result * 10 + lastDigit;
+
             x = x / 10;
+            result = result * 10 + lastDigit;
         }
         return result;
     }
