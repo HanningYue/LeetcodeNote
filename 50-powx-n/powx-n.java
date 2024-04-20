9cb48363617ed9dc2e800071c1f6245c904069cd
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 20:31:42 -0700

    Time: 0 ms (100.00%) | Memory: 42.7 MB (25.42%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,20 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
         long N = n;
-        return recursion(x, N);
+        return dfs(x, N);
     }
-    public double recursion(double x, long n) {
+
+    public double dfs(double x, long n) {
         if (n == 0) {
             return 1.0;
         } else if (n == 1) {
             return x;
         } else if (n < 0) {
-            x = 1 / x;
-            n = -n;
-            return recursion(x, n);
-        } else {
-            double result = recursion(x * x, n / 2);
-            if (n % 2 == 1) {
+            return dfs(1 / x, -n);
+        } else {            
+            double result = dfs(x * x, n / 2);
+            if (n % 2 != 0) {
                 result = result * x;
             }
             return result;
