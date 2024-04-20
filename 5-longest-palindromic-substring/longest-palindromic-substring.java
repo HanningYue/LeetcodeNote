0378d20d4e949d95d3137e3fcde738acd12f40bf
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 19:41:08 -0700

    Time: 199 ms (27.05%) | Memory: 57.9 MB (5.12%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,19 +1,38 @@
 class Solution {
+    Boolean[][] dpTable;
     public String longestPalindrome(String s) {
-        String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            String even = palindrome(s, i, i + 1);
-            String odd = palindrome(s, i, i);
-            result = even.length() > result.length() ? even : result;
-            result = odd.length() > result.length() ? odd : result;
+        dp(s);
+        int n = s.length();
+        int start = 0, maxLength = 1;
+        
+        for (int left = 0; left < n; left++) {
+            for (int right = left; right < n; right++) {
+                if (dpTable[left][right] && (right - left + 1) > maxLength) {
+                    maxLength = right - left + 1;
+                    start = left;
+                }
+            }
         }
-        return result;
+        return s.substring(start, start + maxLength);
     }
-    private String palindrome(String s, int leftPointer, int rightPointer) {
-        while (leftPointer >= 0 && rightPointer < s.length() && s.charAt(leftPointer) == s.charAt(rightPointer)) {
-            leftPointer--;
-            rightPointer++;
+    private void dp(String s) {
+        int n = s.length();
+        dpTable = new Boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            dpTable[i][i] = true;
         }
-        return s.substring(leftPointer + 1, rightPointer);
+        
+        for (int i = 0; i < n - 1; i++) {
+            boolean palindrome = s.charAt(i) == s.charAt(i + 1);
+            dpTable[i][i + 1] = palindrome;
+        }
+
+        for (int length = 3; length <= n; length++) {
+            for (int left = 0; left <= n - length; left++) {
+                int right = left + length - 1;
+                boolean palindrome = s.charAt(left) == s.charAt(right);
+                dpTable[left][right] = palindrome && dpTable[left + 1][right - 1];
+            }
+        }
     }
 }
