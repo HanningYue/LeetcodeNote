14cb0811d06ab407d379ba5f291e3e715ceed4af
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 21:04:34 -0700

    Time: 202 ms (8.94%) | Memory: 62.2 MB (51.16%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
@@ -10,9 +10,8 @@ class Solution {
         int n = word.length();
         int result = 0;
         for (int i = n - 1, right = n; right > result && i >= 0; i--) {
-            int now = 0;
             StringBuilder sb = new StringBuilder();
-            for (int j = i; j < right && j - i < length; ++j) {
+            for (int j = i; j < right && j - i < length; j++) {
                 sb.append(word.charAt(j));
                 if (set.contains(sb.toString())) {
                     right = j;
