31d364a4c2148c45ec7bc000839dcc4731e7704e
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 19:44:59 -0700

    Time: 28 ms (24.19%) | Memory: 57.6 MB (5.18%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,17 +1,34 @@
 class Solution {
-    int count = 0;
+    Boolean[][] dpTable;
     public int countSubstrings(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            palindrome(s, i, i);
-            palindrome(s, i, i + 1);
-        }
+        dp(s);
+        int count = 0;
+        for (int left = 0; left < s.length(); left++) {
+            for (int right = left; right < s.length(); right++) {
+                if (dpTable[left][right]) {
+                    count++;
+                }
+            }
+        }        
         return count;
     }
-    private void palindrome(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-            count++;
+    private void dp(String s) {
+        int n = s.length();
+        dpTable = new Boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            dpTable[i][i] = true;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean adjacent = s.charAt(i) == s.charAt(i + 1);
+            dpTable[i][i + 1] = adjacent;
+        }
+
+        for (int length = 3; length <= n; length++) {
+            for (int left = 0; left <= n - length; left++) {
+                int right = left + length - 1;
+                dpTable[left][right] = s.charAt(left) == s.charAt(right) && dpTable[left + 1][right - 1];
+            }
         }
     }
 }
