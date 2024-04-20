03e0d4fe7a95b37ae0f3c9026bdd81cf06cadc2a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 20:59:13 -0700

    Time: 155 ms (31.93%) | Memory: 61.7 MB (85.35%) - LeetSync

--- 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
+++ 2884-length-of-the-longest-valid-substring/length-of-the-longest-valid-substring.java
@@ -1,25 +1,26 @@
 class Solution {
     public int longestValidSubstring(String word, List<String> forbidden) {
-           int len = 0;
-        Set<String> all = new HashSet<>();
+        int length = 0;
+        Set<String> set = new HashSet<>();
         for (String s : forbidden) {
-            all.add(s);
-            len = Math.max(len, s.length());
+            set.add(s);
+            length = Math.max(length, s.length());
         }
+
         int n = word.length();
-        int r = 0;
-        for (int i = n - 1, right = n; right > r && i >= 0; --i) {
+        int result = 0;
+        for (int i = n - 1, right = n; right > result && i >= 0; i--) {
             int now = 0;
-            StringBuilder temp = new StringBuilder();
-            for (int j = i; j < right && j - i < len; ++j) {
-                temp.append(word.charAt(j));
-                if (all.contains(temp.toString())) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = i; j < right && j - i < length; ++j) {
+                sb.append(word.charAt(j));
+                if (set.contains(sb.toString())) {
                     right = j;
                     break;
                 }
             }
-            r = Math.max(r, right - i);
+            result = Math.max(result, right - i);
         }
-        return r;
+        return result;
     }
 }
