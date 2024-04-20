b35ee14f2479ac7426d739f7c76baa7778d72b41
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-19 20:18:49 -0700

    Time: 196 ms (27.27%) | Memory: 57.9 MB (5.12%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,11 +1,12 @@
 class Solution {
     Boolean[][] dpTable;
     public String longestPalindrome(String s) {
+        int n = s.length();
         dp(s);
         
-        int start = 0, maxLength = 1;
-        for (int left = 0; left < s.length(); left++) {
-            for (int right = left; right < s.length(); right++) {
+        int maxLength = 0, start = 0;
+        for (int left = 0; left < n; left++) {
+            for (int right = left; right < n; right++) {
                 if (dpTable[left][right] && maxLength < (right - left + 1)) {
                     maxLength = right - left + 1;
                     start = left;
@@ -14,24 +15,20 @@ class Solution {
         }
         return s.substring(start, start + maxLength);
     }
-
     private void dp(String s) {
         int n = s.length();
         dpTable = new Boolean[n][n];
         for (int i = 0; i < n; i++) {
             dpTable[i][i] = true;
         }
-        
         for (int i = 0; i < n - 1; i++) {
-            boolean palindrome = s.charAt(i) == s.charAt(i + 1);
-            dpTable[i][i + 1] = palindrome;
+            dpTable[i][i + 1] = s.charAt(i) == s.charAt(i + 1);
         }
 
         for (int length = 3; length <= n; length++) {
             for (int left = 0; left <= n - length; left++) {
                 int right = left + length - 1;
-                boolean palindrome = s.charAt(left) == s.charAt(right);
-                dpTable[left][right] = palindrome && dpTable[left + 1][right - 1];
+                dpTable[left][right] = s.charAt(left) == s.charAt(right) && dpTable[left + 1][right - 1];
             }
         }
     }
