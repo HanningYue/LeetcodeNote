d4acd3eedfbb991b814840daf445f3fa95968b32
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-21 00:03:22 -0700

    Time: 2 ms (68.87%) | Memory: 44.2 MB (61.61%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -14,31 +14,36 @@
  * }
  */
 class Solution {
+    Map<Integer, Integer> inMap;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        Map<Integer, Integer> map = new HashMap<>();
+        inMap = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
-            map.put(inorder[i], i);
+            inMap.put(inorder[i], i);
         }
-        return dfs(map, preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
+        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
-    private TreeNode dfs(Map<Integer, Integer> map, int[] preorder, int preleft, int preright, 
-    int[] inorder, int inleft, int inright) {
+    private TreeNode build(int[] preorder, int preleft, int preright, int[] inorder, int inleft, int inright) {
         if (preleft > preright) {
             return null;
         }
-        
-        int headVal = preorder[preleft];
-        int midIndex = map.get(headVal);
-        int leftSize = midIndex - inleft;
 
-        TreeNode newHead = new TreeNode(headVal);
-        newHead.left = dfs(map, preorder, preleft + 1, preleft + leftSize, inorder, inleft, midIndex - 1);
-        newHead.right = dfs(map, preorder, preleft + leftSize + 1, preright, inorder, midIndex + 1, inright);
-        return newHead;
+        int root = preorder[preleft];
+        int rootIndex = inMap.get(root);
+        int leftSubTreeSize = rootIndex - inleft;
+
+        TreeNode newRoot = new TreeNode(root);
+        newRoot.left = build(preorder, preleft + 1, preleft + leftSubTreeSize, inorder, inleft, rootIndex - 1);
+        newRoot.right = build(preorder, preleft + leftSubTreeSize + 1, preright, inorder, rootIndex + 1, inright);
+        return newRoot;
     }
 }
 /**
-3 9 20 15 7
-9 3 15 20 7
-0 1  2  3 4
+        1
+    2       3
+4    5    6    7
+4 2 5 1 6 3 7
+1 2 4 5 3 6 7
+0 1 2 3 4 5 6
+3
+
 */
