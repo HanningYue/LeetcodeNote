8bc93ce413039284fb724b3877a74efdf1ac3f75
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-21 01:31:57 -0700

    Time: 0 ms (100.00%) | Memory: 41.6 MB (18.52%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,41 +1,25 @@
-/**
-O(n) Linear Time
-Minimum and Maximum is the potential left open bracket, not necessarily the real number
-This part if (minimum > 0) { minimum--; } is essentially saying if we have any open left brackets or "*", which can potentially act as "(", we match them with the current right bracket or "*". If we didn't decrement minimum when we encounter a "*" or ")", we could end up with an incorrect surplus of open brackets.
-'('
-    minimum++
-    maximum++
-')'
-    minimum-- if minimum is greater than 0
-    maximum--
-'*'
-    minimum-- if minimum is greater than 0, * == ) to cancel a (
-    maximum++ * == (
-At any time, ( needs to be greater than )
-*/
 class Solution {
     public boolean checkValidString(String s) {
-        int maximumLeftBracket = 0;
-        int minimumLeftBracket = 0;
+        int maxLeft = 0, minLeft = 0;
         for (char c : s.toCharArray()) {
             if (c == '(') {
-                maximumLeftBracket++;
-                minimumLeftBracket++;
+                minLeft++;
+                maxLeft++;
             } else if (c == ')') {
-                if (minimumLeftBracket > 0) {
-                    minimumLeftBracket--;
+                if (minLeft > 0) {
+                    minLeft--;
                 }
-                maximumLeftBracket--;
+                maxLeft--;
             } else if (c == '*') {
-                if (minimumLeftBracket > 0) {
-                    minimumLeftBracket--;
+                if (minLeft > 0) {
+                    minLeft--;
                 }
-                maximumLeftBracket++;
+                maxLeft++;
             }
-            if (maximumLeftBracket < 0) {
+            if (maxLeft < 0) {
                 return false;
             }
         }
-        return minimumLeftBracket == 0;
+        return minLeft == 0;
     }
 }
