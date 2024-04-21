c892383b44e58d5fa341b228a8c99f56edd1941f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-21 01:05:51 -0700

    Time: 16 ms (30.23%) | Memory: 44.1 MB (67.38%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,44 +1,43 @@
 class Pair {
-    int row, col, maxWater;
-    public Pair(int row, int col, int maxWater) {
+    int maxElevation;
+    int row, col;
+    public Pair(int row, int col, int maxElevation) {
         this.row = row;
         this.col = col;
-        this.maxWater = maxWater;
+        this.maxElevation = maxElevation;
     }
 }
 class Solution {
-    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public int swimInWater(int[][] grid) {
-        PriorityQueue<Pair> heap = new PriorityQueue<Pair>(new Comparator<Pair>() {
+        PriorityQueue<Pair> heap = new PriorityQueue<>(new Comparator<Pair>() {
             public int compare(Pair a, Pair b) {
-                return a.maxWater - b.maxWater;
+                return a.maxElevation - b.maxElevation;
             }
         });
+        heap.offer(new Pair(0, 0, grid[0][0]));
+        
         int m = grid.length, n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
-        heap.offer(new Pair(0, 0, grid[0][0]));
-        visited[0][0] = true;
+        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
         while (!heap.isEmpty()) {
             Pair current = heap.poll();
             int currentRow = current.row;
             int currentCol = current.col;
-            int currentMaxWater = current.maxWater;
-            visited[currentRow][currentCol] = true;
+            int currentmaxElevation = current.maxElevation;
             if (currentRow == m - 1 && currentCol == n - 1) {
-                return currentMaxWater;
+                return currentmaxElevation;
             }
 
+            visited[currentRow][currentCol] = true;
             for (int[] dir : directions) {
                 int newRow = currentRow + dir[0];
                 int newCol = currentCol + dir[1];
-                if (newRow < 0 || newRow >= grid.length || newCol < 0 || newCol >= grid[0].length
-                || visited[newRow][newCol]) {
+                if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || visited[newRow][newCol]) {
                     continue;
                 }
-
-                int newMaxWater = Math.max(currentMaxWater, grid[newRow][newCol]);
-                heap.offer(new Pair(newRow, newCol, newMaxWater));
+                int newMaxElevation = Math.max(grid[newRow][newCol], currentmaxElevation);
+                heap.offer(new Pair(newRow, newCol, newMaxElevation));
             }
         }
         return -1;
