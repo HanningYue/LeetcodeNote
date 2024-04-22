2f73a08de56c901fabe53388e021491e6764f311
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-22 00:50:21 -0700

    Time: 32 ms (53.06%) | Memory: 64.8 MB (5.89%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,19 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> deque = new LinkedList<>();//index
+        Deque<Integer> deque = new LinkedList<>(); //Save index only
         int[] result = new int[nums.length - k + 1];
 
-        for (int i = 0; i < nums.length; i++) {
-            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+        int windowRight = 0;
+        while (windowRight < nums.length) {
+            while (!deque.isEmpty() && nums[windowRight] >= nums[deque.peekLast()]) {
                 deque.pollLast();
             }
-            deque.offer(i);
-            if (i - k + 1 > deque.peekFirst()) {
+            deque.offer(windowRight);
+            if (windowRight - k + 1 > deque.peekFirst()) {
                 deque.pollFirst();
             }
-            if (i - k + 1 >= 0) {
-                result[i - k + 1] = nums[deque.peekFirst()];
+            if (windowRight - k + 1 >= 0) {
+                result[windowRight - k + 1] = nums[deque.peekFirst()];
             }
+            windowRight++;
         }
         return result;
     }
