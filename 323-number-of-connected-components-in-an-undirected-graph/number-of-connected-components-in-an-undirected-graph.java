0ddf1381592f9866fb7327775588eb305ecbf149
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-22 01:34:26 -0700

    Time: 1 ms (100.00%) | Memory: 44.1 MB (89.53%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,32 +1,42 @@
 class Solution {
     public int countComponents(int n, int[][] edges) {
-        Map<Integer, Set<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            graph.put(i, new HashSet<>());
-        }
+        UnionFind uf = new UnionFind(n);
         for (int[] edge : edges) {
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
-        }
-
-        Set<Integer> visited = new HashSet<>();
-        int count = 0;
-        for (int vertex : graph.keySet()) {
-            if (visited.contains(vertex)) {
+            int vertexOne = edge[0], vertexTwo = edge[1];
+            if (uf.connected(vertexOne, vertexTwo)) {
                 continue;
             }
-            count++;
-            dfs(vertex, graph, visited);
+            uf.union(vertexOne, vertexTwo);
         }
-        return count;
+        return uf.count;
     }
-    private void dfs(int vertex, Map<Integer, Set<Integer>> graph, Set<Integer> visited) {
-        if (visited.contains(vertex)) {
+}
+class UnionFind {
+    int[] parent;
+    int count;
+    public UnionFind(int numberOfVertex) {
+        this.count = numberOfVertex;
+        parent = new int[numberOfVertex];
+        for (int i = 0; i < numberOfVertex; i++) {
+            parent[i] = i;
+        }
+    }
+    public int find(int vertex) {
+        if (parent[vertex] != vertex) {
+            parent[vertex] = find(parent[vertex]);
+        }
+        return parent[vertex];
+    }
+    public void union(int vertexOne, int vertexTwo) {
+        int parentOne = find(vertexOne);
+        int parentTwo = find(vertexTwo);
+        if (parentOne == parentTwo) {
             return;
         }
-        visited.add(vertex);
-        for (int neighbor : graph.get(vertex)) {
-            dfs(neighbor, graph, visited);
-        }
+        parent[parentOne] = parentTwo;
+        count--;
+    }
+    public boolean connected(int vertexOne, int vertexTwo) {
+        return find(vertexOne) == find(vertexTwo);
     }
 }
