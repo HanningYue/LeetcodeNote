6b67d70736a05f2d3a34344fe36986854f094c55
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-22 00:59:33 -0700

    Time: 4 ms (95.73%) | Memory: 45.6 MB (29.78%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.java
+++ 271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -1,29 +1,24 @@
-/*
-.substring is exclusive // Remember to reset index
-1. Encode, use stringbuilder, construct str in strs in the form 
-    (5/hello) -> (str.length(), '/' and str)
-2. Decode, find index of slash from current index, take length 
-    (index)int length, (index)int slash, String str
-    user index to find int length, String str// Hint: use Integer.valueOf and substring
-*/
 public class Codec {
+
+    // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         StringBuilder sb = new StringBuilder();
         for (String str : strs) {
-            sb.append(str.length()).append('/').append(str);
+            sb.append(str.length()).append("/").append(str);
         }
         return sb.toString();
     }
 
+    // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List<String> result = new ArrayList<>();
         int index = 0;
         while (index < s.length()) {
-            int slashIndex = s.indexOf('/', index);
-            int length = Integer.valueOf(s.substring(index, slashIndex));
-            String string = s.substring(slashIndex + 1, slashIndex + length + 1);
-            index = slashIndex + 1 + length;
-            result.add(string);
+            int slashIndex = s.indexOf("/", index);
+            int length = Integer.parseInt(s.substring(index, slashIndex));
+            String str = s.substring(slashIndex + 1, slashIndex + length + 1);
+            index = slashIndex + length + 1;
+            result.add(str);
         }
         return result;
     }
