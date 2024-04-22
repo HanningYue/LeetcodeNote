b13319c5d0ec5cb39c02ee46165e71d4c32073db
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-22 01:21:47 -0700

    Time: 2 ms (68.77%) | Memory: 44.3 MB (76.77%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,30 +1,30 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         int m = grid.length, n = grid[0].length;
+
         boolean[][] visited = new boolean[m][n];
         int result = 0;
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) {
-                    result = Math.max(result, dfs(grid, i, j, visited));
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    result = Math.max(result, dfs(grid, visited, i, j));
                 }
             }
         }
         return result;
     }
     int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private int dfs(int[][] grid, int row, int col, boolean[][] visited) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col]
-        || grid[row][col] != 1) {
+    private int dfs(int[][] grid, boolean[][] visited, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length 
+        || visited[row][col] || grid[row][col] != 1) {
             return 0;
         }
-
-        int count = 1;
         visited[row][col] = true;
+        
+        int currentIslandArea = 1;
         for (int[] dir : directions) {
-            count += dfs(grid, row + dir[0], col + dir[1], visited);
+            currentIslandArea += dfs(grid, visited, row + dir[0], col + dir[1]);
         }
-        return count;
+        return currentIslandArea;
     }
 }
