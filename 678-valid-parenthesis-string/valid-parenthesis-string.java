ecd1406afe4c6a9897f5a0ebba0d01619b3c1b22
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-22 00:55:16 -0700

    Time: 0 ms (100.00%) | Memory: 41.1 MB (68.31%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,21 +1,23 @@
 class Solution {
     public boolean checkValidString(String s) {
-        int maxLeft = 0, minLeft = 0;
+        int minLeft = 0, maxLeft = 0;
+
         for (char c : s.toCharArray()) {
             if (c == '(') {
+                maxLeft++;
                 minLeft++;
-                maxLeft++;
             } else if (c == ')') {
-                if (minLeft > 0) {
-                    minLeft--;
-                }
                 maxLeft--;
-            } else if (c == '*') {
                 if (minLeft > 0) {
                     minLeft--;
                 }
+            } else if (c == '*') {
                 maxLeft++;
+                if (minLeft > 0) {
+                    minLeft--;
+                }
             }
+            //If anytime maxLeft is negative, not possible immediately
             if (maxLeft < 0) {
                 return false;
             }
