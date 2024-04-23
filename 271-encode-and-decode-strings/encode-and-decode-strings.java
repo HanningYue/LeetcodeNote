f6b6d7777411bfab5e02f65d643c21b9f141843b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-22 17:08:46 -0700

    Time: 4 ms (95.62%) | Memory: 45.4 MB (61.57%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.java
+++ 271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -1,5 +1,5 @@
 public class Codec {
-    //length, original string. How to distinguish? Add a slash
+
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         StringBuilder sb = new StringBuilder();
@@ -12,15 +12,13 @@ public class Codec {
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List<String> result = new ArrayList<>();
-        
         int index = 0;
         while (index < s.length()) {
             int slashIndex = s.indexOf("/", index);
             int length = Integer.parseInt(s.substring(index, slashIndex));
-            String str = s.substring(slashIndex + 1, slashIndex + length + 1);
+            String str = s.substring(slashIndex + 1, slashIndex + 1 + length);
             result.add(str);
-
-            index = slashIndex + length + 1;
+            index = slashIndex + 1 + length;
         }
         return result;
     }
