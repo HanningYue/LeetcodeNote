1037120c9a2f370cde1ec62293772401d71641af
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-23 00:10:34 -0700

    Time: 2227 ms (5.11%) | Memory: 57.5 MB (55.06%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,27 +1,19 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         k = nums.length - k;
-        shuffleArr(nums);
-        
+
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int pivotIdx = partition(nums, left, right);
-            if (pivotIdx == k) {
-                return nums[pivotIdx];
-            } else if (pivotIdx < k) {
-                left = pivotIdx + 1;
-            } else if (pivotIdx > k) {
-                right = pivotIdx - 1;
+            int pivotIndex = partition(nums, left, right);
+            if (pivotIndex == k) {
+                return nums[pivotIndex];
+            } else if (pivotIndex < k) {
+                left = pivotIndex + 1;
+            } else if (pivotIndex > k) {
+                right = pivotIndex - 1;
             }
         }
         return -1;
-    }
-    private void shuffleArr(int[] nums) {
-        Random rand = new Random();
-        for (int i = 0; i < nums.length; i++) {
-            int nextIdx = i + rand.nextInt(nums.length - i);
-            swap(nums, i, nextIdx);
-        }
     }
     private void swap(int[] nums, int left, int right) {
         int temp = nums[left];
@@ -30,7 +22,7 @@ class Solution {
     }
     private int partition(int[] nums, int left, int right) {
         int pivot = nums[left];
-
+        
         int leftP = left, rightP = right;
         while (leftP < rightP) {
             while (leftP < rightP && nums[rightP] >= pivot) {
