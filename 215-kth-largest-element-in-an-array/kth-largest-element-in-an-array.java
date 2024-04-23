758f841b810c27078a72d75c051658b66ce21523
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-23 00:10:47 -0700

    Time: 27 ms (75.46%) | Memory: 57.7 MB (46.06%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,39 +1,50 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        k = nums.length - k;
+        int left = 0;
+        int right = nums.length - 1;
 
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int pivotIndex = partition(nums, left, right);
-            if (pivotIndex == k) {
-                return nums[pivotIndex];
-            } else if (pivotIndex < k) {
-                left = pivotIndex + 1;
-            } else if (pivotIndex > k) {
-                right = pivotIndex - 1;
+        while(true){
+            // pos + 1 就是第几大数
+            int pos = findPosition(nums, left, right);
+            if(pos + 1 == k){
+                return nums[pos];
+            } else if(pos + 1 > k){ // 选定的数字小了, 结果在 pos 左边
+                right = pos - 1;
+            } else {
+                left = pos + 1; // 选定的数字大了, 结果在 pos 右边
             }
         }
-        return -1;
+
     }
-    private void swap(int[] nums, int left, int right) {
-        int temp = nums[left];
+
+    private int findPosition(int[] nums, int left, int right){
+        // 把nums[left]这个数当作pivot
+        int curr = nums[left];
+
+        int l = left + 1;
+        int r = right;
+        // 使得 curr 左边的值都大于它, curr右边的值都小于它
+        // 要使用 <=, 否则[2,1] 1 这种情况会报错
+        while(l <= r){
+            if(nums[l] < curr && nums[r] > curr){
+                swap(nums, l, r);
+                l++;
+                r--;
+            }
+            // 跳过符合条件的值
+            if(nums[l] >= curr) l++;
+            if(nums[r] <= curr) r--;
+        }
+
+        // 把curr放到对的位置
+        // r指向第一个比curr大的数, 所以要让curr和r交换
+        swap(nums, left, r);
+        return r;
+    }
+
+    private void swap(int[] nums, int left, int right){
+        int tmp = nums[left];
         nums[left] = nums[right];
-        nums[right] = temp;
-    }
-    private int partition(int[] nums, int left, int right) {
-        int pivot = nums[left];
-        
-        int leftP = left, rightP = right;
-        while (leftP < rightP) {
-            while (leftP < rightP && nums[rightP] >= pivot) {
-                rightP--;
-            }
-            while (leftP < rightP && nums[leftP] <= pivot) {
-                leftP++;
-            }
-            swap(nums, leftP, rightP);
-        }
-        swap(nums, left, rightP);
-        return rightP;
+        nums[right] = tmp;
     }
 }
