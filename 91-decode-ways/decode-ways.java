e93ca4e0d2de201d19931cc4e360565c9dad95c2
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-22 18:20:19 -0700

    Time: 0 ms (100.00%) | Memory: 41.5 MB (77.46%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -5,28 +5,24 @@ class Solution {
         dpTable = new Integer[n];
         return dp(s, 0);
     }
-
     private int dp(String s, int index) {
         if (index == s.length()) {
             return 1;
         }
-
         if (s.charAt(index) == '0') {
             return 0;
         }
-        
         if (dpTable[index] != null) {
             return dpTable[index];
         }
 
         int count = dp(s, index + 1);
-        if (index < s.length() - 1) {
+        if (index + 1 < s.length()) {
             int twoDigits = (s.charAt(index) - '0') * 10 + (s.charAt(index + 1) - '0');
             if (twoDigits >= 10 && twoDigits <= 26) {
                 count += dp(s, index + 2);
             }
         }
-
         dpTable[index] = count;
         return count;
     }
