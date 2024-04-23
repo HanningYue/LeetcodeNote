0993d6cc93a6b00be39b877434cfa7fd72d075b2
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-23 00:39:35 -0700

    Time: 32 ms (73.44%) | Memory: 57 MB (84.14%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,49 +1,40 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int left = 0;
-        int right = nums.length - 1;
-
-        while(true){
-            // pos + 1 就是第几大数
-            int pos = findPosition(nums, left, right);
-            if(pos + 1 == k){
-                return nums[pos];
-            } else if(pos + 1 > k){ // 选定的数字小了, 结果在 pos 左边
-                right = pos - 1;
-            } else if (pos + 1 < k){
-                left = pos + 1; // 选定的数字大了, 结果在 pos 右边
+        int left = 0, right = nums.length - 1;
+        while (true) {
+            int pivot = findPivot(nums, left, right);
+            if (pivot + 1 == k) {
+                return nums[pivot];
+            } else if (pivot + 1 > k) {
+                right = pivot - 1;
+            } else if (pivot + 1 < k) {
+                left = pivot + 1;
             }
         }
     }
+    private int findPivot(int[] nums, int left, int right) {
+        int pivot = nums[left];
+        int leftP = left + 1, rightP = right;
 
-    private int findPosition(int[] nums, int left, int right){
-        // 把nums[left]这个数当作pivot
-        int curr = nums[left];
-
-        int l = left + 1;
-        int r = right;
-        // 使得 curr 左边的值都大于它, curr右边的值都小于它
-        // 要使用 <=, 否则[2,1] 1 这种情况会报错
-        while(l <= r){
-            if(nums[l] < curr && nums[r] > curr){
-                swap(nums, l, r);
-                l++;
-                r--;
+        while (leftP <= rightP) {
+            while (leftP <= rightP && nums[leftP] < pivot && nums[rightP] > pivot) {
+                swap(nums, leftP, rightP);
+                leftP++;
+                rightP--;
             }
-            // 跳过符合条件的值
-            if(nums[l] >= curr) l++;
-            if(nums[r] <= curr) r--;
+            if (nums[leftP] >= pivot) {
+                leftP++;
+            }
+            if (nums[rightP] <= pivot) {
+                rightP--;
+            }
         }
-
-        // 把curr放到对的位置
-        // r指向第一个比curr大的数, 所以要让curr和r交换
-        swap(nums, left, r);
-        return r;
+        swap(nums, left, rightP);
+        return rightP;
     }
-
-    private void swap(int[] nums, int left, int right){
-        int tmp = nums[left];
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
         nums[left] = nums[right];
-        nums[right] = tmp;
+        nums[right] = temp;
     }
 }
