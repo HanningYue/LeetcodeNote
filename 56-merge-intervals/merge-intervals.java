7b2b2b5b81110b84c4135b8d32a403a0752f6869
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-23 23:41:04 -0700

    Time: 7 ms (96.28%) | Memory: 46.3 MB (75.61%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,26 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        List<int[]> result = new ArrayList<>();
-        Arrays.sort(intervals, new Comparator<int[]> () {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
+        List<int[]> result = new ArrayList<>();
         int[] previousInterval = intervals[0];
+
         for (int i = 1; i < intervals.length; i++) {
-            int[] currentInterval = intervals[i];
-            if (currentInterval[0] > previousInterval[1]) {
+            if (intervals[i][0] > previousInterval[1]) {
                 result.add(previousInterval);
-                previousInterval = currentInterval;
-            } else if (previousInterval[0] > currentInterval[1]) {
-                result.add(currentInterval);
+                previousInterval = intervals[i];
             } else {
-                previousInterval[1] = Math.max(previousInterval[1], currentInterval[1]);
-                previousInterval[0] = Math.min(previousInterval[0], currentInterval[0]);
+                previousInterval[0] = Math.min(previousInterval[0], intervals[i][1]);
+                previousInterval[1] = Math.max(previousInterval[1], intervals[i][1]);
             }
         }
         result.add(previousInterval);
+
         return result.toArray(new int[result.size()][]);
     }
 }
