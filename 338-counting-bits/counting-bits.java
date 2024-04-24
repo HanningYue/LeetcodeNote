82e562ab0d7630faffc9217246b3c6ed95280124
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-23 20:19:52 -0700

    Time: 2 ms (95.09%) | Memory: 46.8 MB (11.67%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -2,11 +2,11 @@ class Solution {
     public int[] countBits(int n) {
         int[] result = new int[n + 1];
         for (int i = 0; i <= n; i++) {
-            result[i] = countOne(i);
+            result[i] = countOfOne(i);
         }
         return result;
     }
-    private int countOne(int n) {
+    private int countOfOne(int n) {
         int count = 0;
         while (n != 0) {
             n = n & (n - 1);
