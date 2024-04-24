c694cebcff710d39649dc12673d5e9e66f302445
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-23 20:59:32 -0700

    Time: 236 ms (24.88%) | Memory: 57.5 MB (5.19%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,20 +1,34 @@
 class Solution {
+    Boolean[][] dpTable;
     public String longestPalindrome(String s) {
-        String result = "";
-        for (int i = 0; i < s.length(); i++) {
-            String even = foundPalindrome(s, i, i + 1);
-            String odd = foundPalindrome(s, i, i);
-            result = even.length() > result.length() ? even : result;
-            result = odd.length() > result.length() ? odd : result;
+        int n = s.length();
+        dpTable = new Boolean[n][n];
+        dp(s, dpTable);
+
+        int maxLength = 0, start = 0;
+        for (int left = 0; left < n; left++) {
+            for (int right = left; right < n; right++) {
+                if (dpTable[left][right] && maxLength < (right - left + 1)) {
+                    maxLength = right - left + 1;
+                    start = left;
+                }
+            }
         }
-        return result;
+        return s.substring(start, start + maxLength);
     }
-    
-    private String foundPalindrome(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    private void dp(String s, Boolean[][] dpTable) {
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            dpTable[i][i] = true;
         }
-        return s.substring(left + 1, right);
+        for (int i = 0; i < n - 1; i++) {
+            dpTable[i][i + 1] = s.charAt(i) == s.charAt(i + 1);
+        }
+        for (int length = 3; length <= n; length++) {
+            for (int left = 0; left <= n - length; left++) {
+                int right = left + length - 1;
+                dpTable[left][right] = s.charAt(left) == s.charAt(right) && dpTable[left + 1][right - 1];
+            }
+        }
     }
 }
