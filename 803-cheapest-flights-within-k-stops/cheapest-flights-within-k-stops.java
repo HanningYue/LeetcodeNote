a0cc4972433143024e49c5c88fc9096f4c6e5fc8
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-23 21:25:19 -0700

    Time: 9 ms (20.91%) | Memory: 45.5 MB (5.33%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,48 +1,42 @@
 class State {
-    int cost, city, stop;
-    public State(int cost, int city, int stop) {
-        this.cost = cost;
+    int city, cost, stop;
+    public State(int city, int cost, int stop) {
         this.city = city;
+        this.cost = cost;
         this.stop = stop;
-    }
-}
-class Pair {
-    int city, cost;
-    public Pair(int city, int cost) {
-        this.city = city;
-        this.cost = cost;
     }
 }
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         k = k + 1;
-        Map<Integer, Set<Pair>> graph = new HashMap<>();
+        Map<Integer, Set<State>> graph = new HashMap<>();
         for (int[] flight : flights) {
             int from = flight[0], to = flight[1], cost = flight[2];
             graph.putIfAbsent(from, new HashSet<>());
-            graph.get(from).add(new Pair(to, cost));
+            graph.get(from).add(new State(to, cost, 0));
         }
-        int[] minStop = new int[n];
-        Arrays.fill(minStop, Integer.MAX_VALUE);
+
+        int[] minStopToCurrentCity = new int[n];
+        Arrays.fill(minStopToCurrentCity, Integer.MAX_VALUE);
 
         PriorityQueue<State> heap = new PriorityQueue<>((a, b) -> a.cost - b.cost);
-        heap.offer(new State(0, src, 0));
-
-        while(!heap.isEmpty()) {
+        heap.offer(new State(src, 0, 0));
+        while (!heap.isEmpty()) {
             State current = heap.poll();
-            if (current.stop > minStop[current.city] || current.stop > k) {
+            if (current.stop > minStopToCurrentCity[current.city] || current.stop > k) {
                 continue;
             }
-            minStop[current.city] = current.stop;
+
+            minStopToCurrentCity[current.city] = current.stop;
             if (current.city == dst) {
                 return current.cost;
             }
-
+            
             if (!graph.containsKey(current.city)) {
                 continue;
             }
-            for (Pair pair : graph.get(current.city)) {
-                heap.offer(new State(current.cost + pair.cost, pair.city, current.stop + 1));
+            for (State neighbor : graph.get(current.city)) {
+                heap.offer(new State(neighbor.city, neighbor.cost + current.cost, current.stop + 1));
             }
         }
         return -1;
