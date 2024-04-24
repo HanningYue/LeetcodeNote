9e89e1819a929485820257a6cb4d2bf12cb1893c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-23 20:33:39 -0700

    Time: 1 ms (39.03%) | Memory: 43.3 MB (18.78%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -2,24 +2,19 @@ class Solution {
     Integer[] dpTable;
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        dpTable = new Integer[n];
-        int startFromLast = dp(cost, n - 1);
-        int startFromSecondLast = dp(cost, n - 2);
-        return Math.min(startFromLast, startFromSecondLast);
+        dpTable = new Integer[n + 1];
+        return dp(cost, n);
     }
     private int dp(int[] cost, int index) {
-        if (index < 0) {
+        if (index <= 1) {
             return 0;
-        }
-        if (index == 1 || index == 0) {
-            return cost[index];
         }
         if (dpTable[index] != null) {
             return dpTable[index];
         }
-        int climbOne = dp(cost, index - 1);
-        int climbTwo = dp(cost, index - 2);
-        dpTable[index] = Math.min(climbOne, climbTwo) + cost[index];
+        int climbOne = dp(cost, index - 1) + cost[index - 1];
+        int climbTwo = dp(cost, index - 2) + cost[index - 2];
+        dpTable[index] = Math.min(climbOne, climbTwo);
         return dpTable[index];
     }
 }
