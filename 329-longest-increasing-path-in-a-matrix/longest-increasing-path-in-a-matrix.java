7a1764443fe6700326521acd6fdf098a49d67811
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-25 23:03:02 -0700

    Time: 9 ms (52.57%) | Memory: 44.7 MB (83.85%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -3,30 +3,30 @@ class Solution {
     public int longestIncreasingPath(int[][] matrix) {
         int m = matrix.length, n = matrix[0].length;
         dpTable = new Integer[m][n];
-
         int result = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                result = Math.max(result, dfs(matrix, i, j, Integer.MIN_VALUE));
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                result = Math.max(result, dfs(matrix, row, col, -1));
             }
         }
         return result;
     }
+
     int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private int dfs(int[][] matrix, int row, int col, int previousNumber) {
-        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length 
-        || matrix[row][col] <= previousNumber) {
+    private int dfs(int[][] matrix, int row, int col, int previousValue) {
+        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length || previousValue >= matrix[row][col]) {
             return 0;
         }
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
 
-        int currentPath = 1;
+        int count = 1;
         for (int[] dir : directions) {
-            currentPath = Math.max(currentPath, dfs(matrix, row + dir[0], col + dir[1], matrix[row][col]) + 1);
+            int goFourDirectional = dfs(matrix, row + dir[0], col + dir[1], matrix[row][col]);
+            count = Math.max(count, goFourDirectional + 1);
         }
-        dpTable[row][col] = currentPath;
-        return currentPath;
+        dpTable[row][col] = count;
+        return dpTable[row][col];
     }
 }
