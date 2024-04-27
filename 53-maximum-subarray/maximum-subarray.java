f5c8942a07cd6d95fe82ba46b1f0cb3fb75b2877
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-26 23:02:05 -0700

    Time: 12 ms (5.01%) | Memory: 56.8 MB (76.08%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,12 +1,32 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int sum = nums[0], max = nums[0], min = Math.min(0, nums[0]);
-
-        for (int i = 1; i < nums.length; i++) {
-            sum += nums[i];
-            max = Math.max(max, sum - min);
-            min = Math.min(min, sum);
+        return divide(nums, 0, nums.length - 1);
+    }
+    private int divide(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
         }
-        return max;
+        int mid = left + (right - left) / 2;
+        int leftSubarray = divide(nums, left, mid);
+        int rightSubarray = divide(nums, mid + 1, right);
+        int combine = merge(nums, left, mid, right);
+
+        return Math.max(combine, Math.max(leftSubarray, rightSubarray));
+    }
+    private int merge(int[] nums, int left, int mid, int right) {
+        int leftSum = Integer.MIN_VALUE, rightSum = Integer.MIN_VALUE;
+
+        int sum = 0;
+        for (int leftP = mid; leftP >= left; leftP--) {
+            sum += nums[leftP];
+            leftSum = Math.max(leftSum, sum);
+        }
+
+        sum = 0;
+        for (int rightP = mid + 1; rightP <= right; rightP++) {
+            sum += nums[rightP];
+            rightSum = Math.max(rightSum, sum);
+        }
+        return leftSum + rightSum;
     }
 }
