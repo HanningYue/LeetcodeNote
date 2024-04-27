0836c51e7c7211b2ed62e59cad33f1e21fbc1f45
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-27 00:28:14 -0700

    Time: 7 ms (20.42%) | Memory: 45.2 MB (17.94%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -1,42 +1,37 @@
-public class Solution {
+class Solution {
     public boolean validTree(int n, int[][] edges) {
-        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            graph.put(i, new HashSet<>());
+            graph.put(i, new ArrayList<>());
         }
         for (int[] edge : edges) {
             graph.get(edge[0]).add(edge[1]);
             graph.get(edge[1]).add(edge[0]);
         }
 
-        Set<Integer> visited = new HashSet<>();
         Set<Integer> visiting = new HashSet<>();
-        
-        if (!dfs(0, visited, visiting, graph, -1)) {
+        Set<Integer> visited = new HashSet<>();
+        if (!dfs(0, graph, visiting, visited, -1)) {
             return false;
         }
-        
-        
         return visited.size() == n;
     }
-
-    private boolean dfs(int vertex, Set<Integer> visited, Set<Integer> visiting, 
-                        Map<Integer, Set<Integer>> graph, int parent) {
+    private boolean dfs(int vertex, Map<Integer, List<Integer>> graph, Set<Integer> visiting, 
+    Set<Integer> visited, int parent) {
         if (visiting.contains(vertex)) {
             return false;
         }
         if (visited.contains(vertex)) {
             return true;
         }
-
         visiting.add(vertex);
         for (int neighbor : graph.get(vertex)) {
-            if (neighbor != parent && !dfs(neighbor, visited, visiting, graph, vertex)) {
+            if (neighbor != parent && !dfs(neighbor, graph, visiting, visited, vertex)) {
                 return false;
             }
         }
-        visited.add(vertex);
         visiting.remove(vertex);
+        visited.add(vertex);
         return true;
     }
 }
