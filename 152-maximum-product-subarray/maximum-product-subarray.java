9768e5d8b376d0eb795c6bc7ec911450a3da71bd
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-26 23:46:28 -0700

    Time: 1 ms (86.90%) | Memory: 44.3 MB (92.88%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,25 +1,47 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int n = nums.length;
-        int[] leftProducts = new int[n];
-        int[] rightProducts = new int[n];
-
-        leftProducts[0] = nums[0];
-        rightProducts[n - 1] = nums[n - 1];
-
-        for (int i = 1; i < n; i++) {
-            leftProducts[i] = (leftProducts[i - 1] == 0) ? nums[i] : leftProducts[i - 1] * nums[i];
-        }
-
-        for (int i = n - 2; i >= 0; i--) {
-            rightProducts[i] = (rightProducts[i + 1] == 0) ? nums[i] : rightProducts[i + 1] * nums[i];
-        }
-        
         int result = nums[0];
-        for (int i = 0; i < n; i++) {
-            result = Math.max(result, Math.max(leftProducts[i], rightProducts[i]));
+
+        int[] leftArray = new int[nums.length];
+        leftArray[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            leftArray[i] = leftArray[i - 1] == 0 ? nums[i] : leftArray[i - 1] * nums[i];
         }
 
+        int[] rightArray = new int[nums.length];
+        rightArray[nums.length - 1] = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            rightArray[i] = rightArray[i + 1] == 0 ? nums[i] : rightArray[i + 1] * nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            result = Math.max(result, Math.max(leftArray[i], rightArray[i]));
+        }
         return result;
     }
 }
+
+/**
+class Solution {
+    public int maxProduct(int[] nums) {
+        int result = nums[0];
+
+        int leftRunningProduct = 1, rightRunningProduct = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftRunningProduct == 0) {
+                leftRunningProduct = 1;
+            }
+            leftRunningProduct *= nums[i];
+            
+            if (rightRunningProduct == 0) {
+                rightRunningProduct = 1;
+            }
+            rightRunningProduct *= nums[nums.length - 1 - i];
+
+            result = Math.max(result, Math.max(leftRunningProduct, rightRunningProduct));
+        }
+        
+        return result;
+    }
+}
+*/
