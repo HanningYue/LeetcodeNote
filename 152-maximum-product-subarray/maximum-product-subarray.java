988992b8371ed9202c732d7c2fd2b30e7ffd3613
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-26 23:06:35 -0700

    Time: 1 ms (86.90%) | Memory: 44.6 MB (65.43%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,16 +1,22 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int left = 1, right = 1;
-        int n = nums.length;
         int result = nums[0];
 
+        int leftRunningProduct = 1, rightRunningProduct = 1;
         for (int i = 0; i < nums.length; i++) {
-            left = left == 0 ? 1 : left;
-            right = right == 0 ? 1 : right;
-            left = left * nums[i];
-            right = right * nums[n - 1 - i];
-            result = Math.max(result, Math.max(left, right));
+            if (leftRunningProduct == 0) {
+                leftRunningProduct = 1;
+            }
+            leftRunningProduct *= nums[i];
+            
+            if (rightRunningProduct == 0) {
+                rightRunningProduct = 1;
+            }
+            rightRunningProduct *= nums[nums.length - 1 - i];
+
+            result = Math.max(result, Math.max(leftRunningProduct, rightRunningProduct));
         }
+        
         return result;
     }
 }
