7c60a49751d568c12f0185baff9146e485f956ab
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-27 23:16:04 -0700

    Time: 5 ms (99.54%) | Memory: 47.4 MB (83.96%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -2,18 +2,17 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
-            String code = encode(str);
-            map.putIfAbsent(code, new ArrayList<>());
-            map.get(code).add(str);
+            String encoded = encode(str);
+            map.putIfAbsent(encoded, new ArrayList<>());
+            map.get(encoded).add(str);
         }
 
         List<List<String>> result = new ArrayList<>(map.values());
         return result;
     }
-
-    public String encode(String str) {
+    private String encode(String s) {
         char[] count = new char[26];
-        for (char c : str.toCharArray()) {
+        for (char c : s.toCharArray()) {
             count[c - 'a']++;
         }
         return new String(count);
