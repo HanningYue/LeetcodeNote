fe635edce0adca2ff04a4307f183cff5457e719a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-28 15:19:34 -0700

    Time: 1 ms (100.00%) | Memory: 42.2 MB (28.14%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,45 @@
+class Solution {
+    Boolean[][] dpTable;
+    public boolean isMatch(String s, String p) {
+        int n = s.length(), m = p.length();
+        dpTable = new Boolean[n][m];
+        return dp(s, 0, p, 0);
+    }
+    private boolean dp(String s, int i, String p, int j) {
+        if (j == p.length()) {
+            return i == s.length();
+        }
+        if (i == s.length()) {
+            if ((p.length() - j) % 2 != 0) {
+                return false;
+            }
+
+            for (; j + 1 < p.length(); j += 2) {
+                if (p.charAt(j + 1) != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if (dpTable[i][j] != null) {
+            return dpTable[i][j];
+        }
+        
+        boolean result = false;
+        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.') {
+            if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+                result = dp(s, i + 1, p, j) || dp(s, i, p, j + 2);
+            } else {
+                result = dp(s, i + 1, p, j + 1);
+            }
+        } else {
+            if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+                result = dp(s, i, p, j + 2);
+            } else {
+                return false;
+            }
+        }
+        dpTable[i][j] = result;
+        return result;
+    }
+}
