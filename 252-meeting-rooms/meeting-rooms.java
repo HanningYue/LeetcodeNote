f494217f47ca96778af1d7384f4dcea6f306219a
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-27 20:32:30 -0700

    Time: 6 ms (81.05%) | Memory: 46.1 MB (23.81%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare (int[] a, int[] b) {
-                return a[1] - b[1];
-            }
-        });
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] >= intervals[i - 1][1]) {
-                continue;
-            } else {
+            if (intervals[i][0] < intervals[i - 1][1]) {
                 return false;
             }
         }
