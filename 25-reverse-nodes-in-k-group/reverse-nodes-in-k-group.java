fd3b75e9c25c4d66a902e61472e90696ecc2e9b5
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-28 14:45:26 -0700

    Time: 0 ms (100.00%) | Memory: 44.2 MB (55.67%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -13,20 +13,20 @@ class Solution {
         if (head == null || head.next == null) {
             return head;
         }
-        ListNode kth = head;
+
+        ListNode kth = head;   
         for (int i = 0; i < k; i++) {
             if (kth == null) {
                 return head;
             }
             kth = kth.next;
         }
-
         ListNode reverseHead = reverseRange(head, kth);
         head.next = reverseKGroup(kth, k);
         return reverseHead;
     }
     private ListNode reverseRange(ListNode left, ListNode right) {
-        ListNode previous = null, current = left;
+        ListNode current = left, previous = null;
         while (current != right) {
             ListNode next = current.next;
             current.next = previous;
