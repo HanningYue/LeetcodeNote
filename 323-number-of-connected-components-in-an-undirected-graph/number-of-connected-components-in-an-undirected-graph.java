13c3d89034b80fe2e8ff0b9d74a0b6abed9897af
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-30 00:27:06 -0700

    Time: 9 ms (20.07%) | Memory: 45.9 MB (7.23%) - LeetSync

--- 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
+++ 323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -1,37 +1,31 @@
 class Solution {
     public int countComponents(int n, int[][] edges) {
         Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new HashSet<>());
+        }
         for (int[] edge : edges) {
-            int vertexOne = edge[0];
-            int vertexTwo = edge[1];
-            graph.putIfAbsent(vertexOne, new HashSet<>());
-            graph.putIfAbsent(vertexTwo, new HashSet<>());
-            graph.get(vertexTwo).add(vertexOne);
-            graph.get(vertexOne).add(vertexTwo);
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
 
+        Set<Integer> visited = new HashSet<>();
         int count = 0;
-        boolean[] visited = new boolean[n];
         for (int vertex = 0; vertex < n; vertex++) {
-            if (!visited[vertex]) {
+            if (!visited.contains(vertex)) {
                 count++;
-                dfs(graph, visited, vertex);
+                dfs(vertex, graph, visited);
             }
         }
         return count;
     }
-
-    private void dfs(Map<Integer, Set<Integer>> graph, boolean[] visited, int vertex) {
-        if (visited[vertex]) {
+    private void dfs(int vertex, Map<Integer, Set<Integer>> graph, Set<Integer> visited) {
+        if (visited.contains(vertex)) {
             return;
         }
-        visited[vertex] = true;
-        if (!graph.containsKey(vertex)) {
-            return;
-        }
-        
+        visited.add(vertex);
         for (int neighbor : graph.get(vertex)) {
-            dfs(graph, visited, neighbor);
+            dfs(neighbor, graph, visited);
         }
     }
 }
