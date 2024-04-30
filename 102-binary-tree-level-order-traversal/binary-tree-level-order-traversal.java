82d81af6850299b70274eb55ab9b85998b19a794
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-30 00:10:12 -0700

    Time: 0 ms (100.00%) | Memory: 45.1 MB (10.45%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -16,27 +16,19 @@
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
+        dfs(root, result, 0);
+        return result;
+    }
+    private void dfs(TreeNode root, List<List<Integer>> result, int level) {
         if (root == null) {
-            return result;
+            return;
         }
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            List<Integer> currentLevel = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode current = queue.poll();
-                if (current.left != null) {
-                    queue.offer(current.left);
-                }
-                if (current.right != null) {
-                    queue.offer(current.right);
-                }
-                currentLevel.add(current.val);
-            }
-            result.add(currentLevel);
+        if (level == result.size()) {
+            result.add(new ArrayList<>());
         }
-        return result;
+        result.get(level).add(root.val);
+        dfs(root.left, result, level + 1);
+        dfs(root.right, result, level + 1);
     }
 }
