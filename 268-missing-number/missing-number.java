c2c23f8ffce14e7e434c44e9417f6d84b98929fc
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-30 20:05:27 -0700

    Time: 1 ms (40.71%) | Memory: 44.8 MB (86.58%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,9 +1,22 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int result = nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            result = result ^ i ^ nums[i];
+        int i = 0;
+        while (i < nums.length) {
+            int suppose = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[suppose]) {
+                int temp = nums[i];
+                nums[i] = nums[suppose];
+                nums[suppose] = temp;
+            } else {
+                i++;
+            }
         }
-        return result;
+        
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j) {
+                return j;
+            }
+        }
+        return nums.length;
     }
 }
