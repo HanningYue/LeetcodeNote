80b10a0541c98c8557a0d3bc651c310b9abb4282
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-04-30 19:55:14 -0700

    Time: 1 ms (74.48%) | Memory: 42.5 MB (96.91%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,10 +1,10 @@
 class UnionFind {
-    int[] parent;
     int count;
-    public UnionFind(int numberOfVertex) {
-        this.count = numberOfVertex;
-        parent = new int[numberOfVertex];
-        for (int i = 0; i < numberOfVertex; i++) {
+    int[] parent;
+    public UnionFind(int n) {
+        this.count = n;
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
     }
@@ -14,30 +14,30 @@ class UnionFind {
         }
         return parent[vertex];
     }
-    public void union(int vertexOne, int vertexTwo) {
-        int parentOne = find(vertexOne);
-        int parentTwo = find(vertexTwo);
+    public void union(int one, int two) {
+        int parentOne = find(one);
+        int parentTwo = find(two);
         if (parentOne == parentTwo) {
             return;
         }
         parent[parentOne] = parentTwo;
         count--;
     }
-    public boolean connected(int vertexOne, int vertexTwo) {
-        return find(vertexOne) == find(vertexTwo);
+    public boolean connected(int one, int two) {
+        return find(one) == find(two);
     }
 }
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        int n = edges.length;
+        int n = edges.length + 1;
         UnionFind uf = new UnionFind(n);
         for (int[] edge : edges) {
-            int vertexOne = edge[0] - 1;
-            int vertexTwo = edge[1] - 1;
-            if (uf.connected(vertexOne, vertexTwo)) {
+            int one = edge[0];
+            int two = edge[1];
+            if (uf.connected(one, two)) {
                 return edge;
             }
-            uf.union(vertexOne, vertexTwo);
+            uf.union(one, two);
         }
         return new int[]{};
     }
