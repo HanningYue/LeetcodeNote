26841397e48ecacb96f5d433d31d33a6447bc795
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-02 15:22:09 -0700

    Time: 26 ms (32.36%) | Memory: 45.2 MB (17.17%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -5,24 +5,24 @@ class Solution {
         return dp(coins, amount);
     }
     private int dp(int[] coins, int amount) {
-        if (amount < 0) {
-            return -1;
-        }
         if (amount == 0) {
             return 0;
+        }
+        if (amount < 0) {
+            return -1;
         }
         if (dpTable[amount] != null) {
             return dpTable[amount];
         }
-
-        int result = Integer.MAX_VALUE;
+        
+        int currentFewest = Integer.MAX_VALUE;
         for (int coin : coins) {
             int subProblem = dp(coins, amount - coin);
             if (subProblem != -1) {
-                result = Math.min(result, subProblem + 1);
+                currentFewest = Math.min(currentFewest, subProblem + 1);
             }
         }
-        dpTable[amount] = result == Integer.MAX_VALUE ? -1 : result;
+        dpTable[amount] = currentFewest == Integer.MAX_VALUE ? -1 : currentFewest;
         return dpTable[amount];
     }
 }
