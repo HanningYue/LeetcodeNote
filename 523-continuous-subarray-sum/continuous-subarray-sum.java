b4fa768fb1b62b978dc94073fbc1cfc79eb16553
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-01 22:05:31 -0700

    Time: 21 ms (92.63%) | Memory: 63 MB (7.56%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
-
         int remainder = 0;
+        map.put(0, -1);
+        
         for (int i = 0; i < nums.length; i++) {
             remainder = (remainder + nums[i]) % k;
+
             if (map.containsKey(remainder)) {
-                if (i - map.get(remainder) > 1) {
+                if (i - map.get(remainder) >= 2) {
                     return true;
                 }
             } else {
