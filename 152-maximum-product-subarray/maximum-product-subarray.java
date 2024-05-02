7fe177c5f3865471808342a542e9323f9efdafbc
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-01 23:38:41 -0700

    Time: 2 ms (58.48%) | Memory: 44.7 MB (52.04%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,22 +1,17 @@
 class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
-        int[] leftArray = new int[n];
-        leftArray[0] = nums[0];
+        int max = nums[0];
+        int min = nums[0];
+        int result = nums[0];
+
         for (int i = 1; i < n; i++) {
-            leftArray[i] = leftArray[i - 1] == 0 ? nums[i] : leftArray[i - 1] * nums[i];
-        }
+            int tempMax = Math.max(nums[i], Math.max(max * nums[i], min * nums[i]));
+            min = Math.min(nums[i], Math.min(max * nums[i], min * nums[i]));
 
-        int[] rightArray = new int[n];
-        rightArray[n - 1] = nums[n - 1];
-        for (int i = n -  2; i >= 0; i--) {
-            rightArray[i] = rightArray[i + 1] == 0 ? nums[i] : rightArray[i + 1] * nums[i];
-        }
-
-        int result = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            result = Math.max(result, Math.max(leftArray[i], rightArray[i]));
-        }
+            max = tempMax;
+            result = Math.max(result, max);
+        }        
         return result;
     }
 }
