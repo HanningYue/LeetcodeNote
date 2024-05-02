cb6822dd69f2fd94006c42558b44a5ddc1a743ab
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-01 23:34:59 -0700

    Time: 1 ms (87.20%) | Memory: 44.3 MB (96.17%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,47 +1,22 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int result = nums[0];
-
-        int[] leftArray = new int[nums.length];
+        int n = nums.length;
+        int[] leftArray = new int[n];
         leftArray[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
+        for (int i = 1; i < n; i++) {
             leftArray[i] = leftArray[i - 1] == 0 ? nums[i] : leftArray[i - 1] * nums[i];
         }
 
-        int[] rightArray = new int[nums.length];
-        rightArray[nums.length - 1] = nums[nums.length - 1];
-        for (int i = nums.length - 2; i >= 0; i--) {
+        int[] rightArray = new int[n];
+        rightArray[n - 1] = nums[n - 1];
+        for (int i = n -  2; i >= 0; i--) {
             rightArray[i] = rightArray[i + 1] == 0 ? nums[i] : rightArray[i + 1] * nums[i];
         }
 
-        for (int i = 0; i < nums.length; i++) {
+        int result = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
             result = Math.max(result, Math.max(leftArray[i], rightArray[i]));
         }
         return result;
     }
 }
-
-/**
-class Solution {
-    public int maxProduct(int[] nums) {
-        int result = nums[0];
-
-        int leftRunningProduct = 1, rightRunningProduct = 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (leftRunningProduct == 0) {
-                leftRunningProduct = 1;
-            }
-            leftRunningProduct *= nums[i];
-            
-            if (rightRunningProduct == 0) {
-                rightRunningProduct = 1;
-            }
-            rightRunningProduct *= nums[nums.length - 1 - i];
-
-            result = Math.max(result, Math.max(leftRunningProduct, rightRunningProduct));
-        }
-        
-        return result;
-    }
-}
-*/
