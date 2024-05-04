31671eb824c9f8070866c7be7d12769290bdb83f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-04 01:14:18 -0700

    Time: 2 ms (98.24%) | Memory: 43 MB (77.79%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
+        int[] resultArray = new int[26];
+
+        for (char c : s.toCharArray()) {
+            resultArray[c - 'a']++;
         }
 
-        int[] countArr = new int[26];
-        for (char c : s.toCharArray()) {
-            countArr[c - 'a']++;
-        }
         for (char c : t.toCharArray()) {
-            countArr[c - 'a']--;
+            resultArray[c - 'a']--;
         }
-        for (int freq : countArr) {
-            if (freq != 0) {
+
+        for (int value : resultArray) {
+            if (value != 0) {
                 return false;
             }
         }
