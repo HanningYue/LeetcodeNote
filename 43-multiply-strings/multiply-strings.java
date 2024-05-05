a15918d2e55e7bebf6447771d64453e0b7a2553f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-05 00:52:09 -0700

    Time: 3 ms (58.44%) | Memory: 41.8 MB (91.38%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -3,24 +3,23 @@ class Solution {
         int[] result = new int[num1.length() + num2.length()];
         for (int i = num1.length() - 1; i >= 0; i--) {
             for (int j = num2.length() - 1; j >= 0; j--) {
-                int num1Last = num1.charAt(i) - '0';
-                int num2Last = num2.charAt(j) - '0';
+                int numOne = num1.charAt(i) - '0';
+                int numTwo = num2.charAt(j) - '0';
 
-                int product = num1Last * num2Last;
+                int product = numOne * numTwo;
                 int sum = product + result[i + j + 1];
                 result[i + j + 1] = sum % 10;
                 result[i + j] = result[i + j] + sum / 10;
             }
         }
-        
+
+        StringBuilder sb = new StringBuilder();
         int k = 0;
         while (k < result.length && result[k] == 0) {
             k++;
         }
-        StringBuilder sb = new StringBuilder();
         while (k < result.length) {
-            sb.append(result[k]);
-            k++;
+            sb.append(result[k++]);
         }
         return sb.length() == 0 ? "0" : sb.toString();
     }
