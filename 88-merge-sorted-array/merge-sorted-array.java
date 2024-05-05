ceb26d5f817bd5c2bb4639bb8d68497cefed1a76
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-04 20:51:50 -0700

    Time: 0 ms (100.00%) | Memory: 41.8 MB (82.16%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,23 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int lastP = m + n - 1;
-        int pointer1 = m - 1;
-        int pointer2 = n - 1;
+        int lastIndexOfOne = m - 1;
+        int lastIndexOfTwo = n - 1;
+        int totalLength = m + n - 1;
 
-        while (pointer2 >= 0 && pointer1 >= 0) {
-            if (nums2[pointer2] >= nums1[pointer1]) {
-                nums1[lastP--] = nums2[pointer2--];
-            }
-            else if (nums1[pointer1] > nums2[pointer2]){
-                nums1[lastP--] = nums1[pointer1--];                
+        while (lastIndexOfOne >= 0 && lastIndexOfTwo >= 0) {
+            if (nums1[lastIndexOfOne] < nums2[lastIndexOfTwo]) {
+                nums1[totalLength--] = nums2[lastIndexOfTwo--];
+            } else {
+                nums1[totalLength--] = nums1[lastIndexOfOne--];
             }
         }
-
-        while (pointer2 >= 0) {
-            nums1[lastP--] = nums2[pointer2--];
-        }
-        while (pointer1 >= 0) {
-            nums1[lastP--] = nums1[pointer1--];
+        while (lastIndexOfTwo >= 0) {
+            nums1[totalLength--] = nums2[lastIndexOfTwo--];
         }
     }
 }
