95e99e24019b9972ce35ca64427b4b2aade7e46c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-05 21:12:21 -0700

    Time: 3 ms (58.12%) | Memory: 41.8 MB (95.22%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,39 +1,26 @@
-/**
-    1   2   3
-        5   6
-    _________
-    [0 1 2 3 4]
-        1   8
-    1   2
-    6
-*/
 class Solution {
     public String multiply(String num1, String num2) {
-        int m = num1.length(), n = num2.length();
-        int[] resultArray = new int[m + n];
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
+        int[] result = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
                 int numOne = num1.charAt(i) - '0';
                 int numTwo = num2.charAt(j) - '0';
 
                 int product = numOne * numTwo;
-                int sum = product + resultArray[i + j + 1];
-                resultArray[i + j + 1] = sum % 10;
-                resultArray[i + j] = resultArray[i + j] + sum / 10;
+                int sum = product + result[i + j + 1];
+                result[i + j + 1] = sum % 10;
+                result[i + j] = result[i + j] + sum / 10;
             }
         }
-
         int k = 0;
-        while (k < resultArray.length && resultArray[k] == 0) {
+        while (k < result.length && result[k] == 0) {
             k++;
         }
-
+        
         StringBuilder sb = new StringBuilder();
-        while (k < resultArray.length) {
-            sb.append(resultArray[k]);
-            k++;
+        while (k < result.length) {
+            sb.append(result[k++]);
         }
-
         return sb.length() == 0 ? "0" : sb.toString();
     }
 }
