12bcf7047d461c86b45b4ef1b81bc73b35a4d78f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-05 22:45:54 -0700

    Time: 4 ms (55.30%) | Memory: 53.1 MB (35.27%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,18 +1,30 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majorityElement = 0;
-        int count = 0;
+        return divide(nums, 0, nums.length - 1);
+    }
+    private int divide(int[] nums, int left, int right) {
+        if (left > right) {
+            return nums[0];
+        }
 
-        for (int num : nums) {
-            if (count == 0) {
-                majorityElement = num;
+        if (left == right) {
+            return nums[left];
+        }
+        int mid = left + (right - left) / 2;
+        int leftNum = divide(nums, left, mid);
+        int rightNum = divide(nums, mid + 1, right);
+        
+        int leftMajority = find(nums, leftNum, left, right);
+        int rightMajority = find(nums, rightNum, left, right);
+        return leftMajority > rightMajority ? leftNum : rightNum;
+    }
+    private int find(int[] nums, int target, int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            if (nums[i] == target) {
                 count++;
-            } else if (num == majorityElement) {
-                count++;
-            } else if (num != majorityElement) {
-                count--;
             }
         }
-        return majorityElement;
+        return count;
     }
 }
