fcb21d08099302b00c0744096c67b5b750b9525f
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-06 02:33:33 -0700

    Time: 5 ms (29.83%) | Memory: 41.5 MB (78.52%) - LeetSync

--- 140-word-break-ii/word-break-ii.java
+++ 140-word-break-ii/word-break-ii.java
@@ -3,10 +3,11 @@ class Solution {
     public List<String> wordBreak(String s, List<String> wordDict) {
         dpTable = new ArrayList[s.length()];
         Set<String> set = new HashSet<>(wordDict);
-        return dp(s, 0, set);
+        return dp(s, set, 0);
     }
-    private List<String> dp(String s, int index, Set<String> set) {
+    private List<String> dp(String s, Set<String> set, int index) {
         List<String> result = new ArrayList<>();
+
         if (index == s.length()) {
             result.add("");
             return result;
@@ -14,15 +15,15 @@ class Solution {
         if (dpTable[index] != null) {
             return dpTable[index];
         }
-        for (int length = 1; index + length <= s.length(); length++) {
-            String prefix = s.substring(index, index + length);
-            if (set.contains(prefix)) {
-                List<String> subList = dp(s, index + length, set);
-                for (String subString : subList) {
-                    if (subString.isEmpty()) {
-                        result.add(prefix);
+        for (int length = 1; length + index <= s.length(); length++) {
+            String current = s.substring(index, index + length);
+            if (set.contains(current)) {
+                List<String> subList = dp(s, set, index + length);
+                for (String str : subList) {
+                    if (str.length() == 0) {
+                        result.add(current);
                     } else {
-                        result.add(prefix + " " + subString);
+                        result.add(current + " " + str);
                     }
                 }
             }
