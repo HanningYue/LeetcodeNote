2d2655b88f4574666645610a558a0336869afc90
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-06 02:45:23 -0700

    Time: 4 ms (76.36%) | Memory: 123.1 MB (7.14%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         boolean[] result = new boolean[3];
+        
         for (int[] triplet : triplets) {
-            
             boolean greater = false;
             for (int i = 0; i < 3; i++) {
                 if (triplet[i] > target[i]) {
@@ -10,11 +10,10 @@ class Solution {
                     break;
                 }
             }
-            
             if (greater) {
                 continue;
             }
-            
+
             for (int i = 0; i < 3; i++) {
                 if (triplet[i] == target[i]) {
                     result[i] = true;
