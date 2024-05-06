e2312281d48b7635fde550c49b9ea3ba2ee4c623
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-06 01:43:24 -0700

    Time: 10 ms (17.73%) | Memory: 44.2 MB (52.17%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,28 +1,26 @@
 class Solution {
-    Boolean[] dpTable;
     public boolean wordBreak(String s, List<String> wordDict) {
-        dpTable = new Boolean[s.length()];
         Set<String> set = new HashSet<>(wordDict);
-        return dp(s, 0, set);
-    }
-    private boolean dp(String s, int index, Set<String> set) {
-        if (index == s.length()) {
-            return true;
-        }
-        if (dpTable[index] != null) {
-            return dpTable[index];
-        }
-        for (int length = 1; index + length <= s.length(); length++) {
-            String prefix = s.substring(index, index + length);
-            if (set.contains(prefix)) {
-                boolean subProblem = dp(s, index + length, set);
-                if (subProblem) {
-                    dpTable[index] = true;
-                    return true;
+        boolean[] visited = new boolean[s.length() + 1];
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+        while (!queue.isEmpty()) {
+            int currentIndex = queue.poll();
+            if (currentIndex == s.length()) {
+                return true;
+            }
+
+            for (int length = 1; length + currentIndex <= s.length(); length++) {
+                if (visited[currentIndex + length]) {
+                    continue;
+                }
+                if (set.contains(s.substring(currentIndex, currentIndex + length))) {
+                    visited[currentIndex + length] = true;
+                    queue.offer(currentIndex + length);
                 }
             }
         }
-        dpTable[index] = false;
         return false;
     }
 }
