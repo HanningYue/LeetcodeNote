dee01f2494efa988b1dea9645b9fe23338ff3784
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-08 12:48:34 -0700

    Time: 2 ms (66.36%) | Memory: 41.2 MB (84.12%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,21 +1,20 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int atMost = citations.length;
-
-        for (int cit : citations) {
-            if (cit >= atMost) {
-                map.put(atMost, map.getOrDefault(atMost, 0) + 1);
-            } else {
-                map.put(cit, map.getOrDefault(cit, 0) + 1);
+        int maxH = citations.length;
+        Map<Integer, Integer> map = new HashMap<>(); //Hold number of citation and frequency
+        for (int citation : citations) {
+            if (citation >= maxH) {
+                map.put(maxH, map.getOrDefault(maxH, 0) + 1);
+            } else if (citation < maxH) {
+                map.put(citation, map.getOrDefault(citation, 0) + 1);
             }
         }
 
-        int freq = 0;
-        for (int i = atMost; i >= 0; i--) {
-            freq += map.getOrDefault(i, 0);
-            if (freq >= i) {
-                return i;
+        int frequency = 0;
+        for (int atMostCitation = maxH; atMostCitation >= 0; atMostCitation--) {
+            frequency += map.getOrDefault(atMostCitation, 0);
+            if (frequency >= atMostCitation) {
+                return atMostCitation;
             }
         }
         return -1;
