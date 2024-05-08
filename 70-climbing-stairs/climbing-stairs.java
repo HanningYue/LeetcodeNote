b29c04cbc1c0f8da38a74d3fa656fa5122a4f801
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-08 03:58:19 -0700

    Time: 0 ms (100.00%) | Memory: 40 MB (84.99%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,22 +1,14 @@
 class Solution {
-    Integer[] dpTable;
     public int climbStairs(int n) {
-        dpTable = new Integer[n + 1];
-        return dp(n);
-    }
-    private int dp(int n) {
-        if (n == 0) {
-            return 0;
+        if (n == 1) {
+            return 1;
         }
-        if (n <= 2) {
-            return n;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        if (dpTable[n] != null) {
-            return dpTable[n];
-        }
-        int climbOne = dp(n - 1);
-        int climbTwo = dp(n - 2);
-        dpTable[n] = climbOne + climbTwo;
-        return dpTable[n];
+        return dp[n];
     }
 }
