b3137eb6207b11b45a29750b6098fd2f5cd25653
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-08 14:14:02 -0700

    Time: 63 ms (32.10%) | Memory: 61.5 MB (19.25%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,44 +1,12 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int left = 0, right = nums.length - 1;
-        while (true) {
-            int pivot = findPivotIndex(nums, left, right);
-            if (pivot + 1 == k) {
-                return nums[pivot];
-            } else if (pivot + 1 > k) {
-                right = pivot - 1;
-            } else if (pivot + 1 < k) {
-                left = pivot + 1;
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int num : nums) {
+            heap.offer(num);
+            if (heap.size() > k) {
+                heap.poll();
             }
         }
-    }
-    private int findPivotIndex(int[] nums, int left, int right) {
-        int pivotValue = nums[left];
-        int leftP = left + 1, rightP = right;
-
-        while (leftP <= rightP) {
-            // Move leftP right until finding an element smaller than pivotValue
-            while (leftP <= rightP && nums[leftP] >= pivotValue) {
-                leftP++;
-            }
-            // Move rightP left until finding an element greater than pivotValue
-            while (leftP <= rightP && nums[rightP] < pivotValue) {
-                rightP--;
-            }
-            // Swap elements to move larger elements to the left and smaller to the right
-            if (leftP < rightP) {
-                swap(nums, leftP, rightP);
-                leftP++;
-                rightP--;
-            }
-        }
-        // Swap pivot into correct place
-        swap(nums, left, rightP);
-        return rightP;
-    }
-    private void swap(int[] nums, int left, int right) {
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
+        return heap.peek();
     }
 }
