f3b08d01790cd4dfec44d6bc80d7af8d51871cf6
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-08 04:56:41 -0700

    Time: 1 ms (99.88%) | Memory: 46.4 MB (14.68%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,13 +1,9 @@
-/**
-XOR ^ :  1 ^ 1 = 0       0 ^ 0 = 0         1 ^ 0 = 1
-If two numbers are identical, a ^ a = 0
-*/
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
+        int a = 0;
         for (int num : nums) {
-            result = result ^ num;
+            a = a ^ num;
         }
-        return result;
+        return a;
     }
 }
