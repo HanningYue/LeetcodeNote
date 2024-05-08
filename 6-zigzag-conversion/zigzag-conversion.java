bac02bf37470c1183786e7fabb9986bd3b9e6e0c
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-07 21:00:59 -0700

    Time: 5 ms (68.89%) | Memory: 45.2 MB (45.63%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,29 +1,29 @@
 class Solution {
     public String convert(String s, int numRows) {
-        StringBuilder[] sbArr = new StringBuilder[numRows];
-        for (int i = 0; i < sbArr.length; i++) {
-            sbArr[i] = new StringBuilder();
+        StringBuilder[] sbArray = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            sbArray[i] = new StringBuilder();
         }
 
-        int sIdx = 0;
-        while (sIdx < s.length()) {
+        int sIndex = 0;
+        while (sIndex < s.length()) {
             int currentRow = 0;
-            while (currentRow < numRows && sIdx < s.length()) {
-                sbArr[currentRow].append(s.charAt(sIdx));
-                sIdx++;
+            while (currentRow < numRows && sIndex < s.length()) {
+                sbArray[currentRow].append(s.charAt(sIndex));
                 currentRow++;
+                sIndex++;
             }
-            currentRow -= 2;
-            while (currentRow > 0 && sIdx < s.length()) {
-                sbArr[currentRow].append(s.charAt(sIdx));
-                sIdx++;
+            currentRow = currentRow - 2;
+            while (currentRow > 0 && sIndex < s.length()) {
+                sbArray[currentRow].append(s.charAt(sIndex));
                 currentRow--;
+                sIndex++;
             }
         }
-
+        
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < sbArr.length; i++) {
-            result.append(sbArr[i].toString());
+        for (StringBuilder row : sbArray) {
+            result.append(row.toString());
         }
         return result.toString();
     }
