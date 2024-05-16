919e427de17c90edd3d3a6507c3b21eaa03c69eb
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-16 16:59:43 -0700

    Time: 2171 ms (6.82%) | Memory: 61 MB (29.10%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -3,27 +3,30 @@ class Solution {
     public int findKthLargest(int[] nums, int k) {
         return quickSelect(nums, 0, nums.length - 1, nums.length - k);
     }
-    private int quickSelect(int[] nums, int left, int right, int k) {
+
+    private int quickSelect(int[] nums, int left, int right, int targetK) {
         if (left == right) {
             return nums[left];
         }
-        int pivotIndex = left + rand.nextInt(right - left);
-        pivotIndex = partition(nums, left, right, pivotIndex);
-        if (pivotIndex == k) {
+
+        int pivotIndex = partition(nums, left, right);
+        if (pivotIndex == targetK) {
             return nums[pivotIndex];
-        } else if (k < pivotIndex) {
-            return quickSelect(nums, left, pivotIndex - 1, k);
+        } else if (pivotIndex < targetK) {
+            return quickSelect(nums, pivotIndex + 1, right, targetK);
         } else {
-            return quickSelect(nums, pivotIndex + 1, right, k);
+            return quickSelect(nums, left, pivotIndex - 1, targetK);
         }
     }
-    private int partition(int[] nums, int left, int right, int pivotIndex) {
-        int pivot = nums[pivotIndex];
-        swap(nums, pivotIndex, right);
+
+    private int partition(int[] nums, int left, int right) {
+        int pivotIndex = left + rand.nextInt(right - left + 1);
+        int pivotValue = nums[pivotIndex];
+        swap(nums, right, pivotIndex);
 
         int leftP = left;
         for (int i = left; i <= right; i++) {
-            if (nums[i] < pivot) {
+            if (nums[i] < pivotValue) {
                 swap(nums, leftP, i);
                 leftP++;
             }
@@ -31,6 +34,7 @@ class Solution {
         swap(nums, leftP, right);
         return leftP;
     }
+    
     private void swap(int[] nums, int left, int right) {
         int temp = nums[left];
         nums[left] = nums[right];
