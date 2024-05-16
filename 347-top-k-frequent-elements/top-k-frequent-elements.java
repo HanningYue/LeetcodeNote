1ad29b49f99b14396828e0d655044256a7b5da79
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-16 16:58:58 -0700

    Time: 13 ms (75.65%) | Memory: 49 MB (11.92%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,5 +1,7 @@
 class Solution {
     Map<Integer, Integer> map;
+    Random rand = new Random();
+
     public int[] topKFrequent(int[] nums, int k) {
         map = new HashMap<>();
         for (int num : nums) {
@@ -15,29 +17,37 @@ class Solution {
         quickSelect(temp, 0, temp.length - 1, temp.length - k);
         return Arrays.copyOfRange(temp, temp.length - k, temp.length);
     }
-    private void quickSelect(int[] temp, int left, int right, int targetK) {   
+
+    private void quickSelect(int[] temp, int left, int right, int targetK) {
+        if (left >= right) {
+            return;
+        }
         int pivotIndex = partition(temp, left, right);
         if (pivotIndex == targetK) {
             return;
-        } else if (pivotIndex > targetK) {
-            quickSelect(temp, left, pivotIndex - 1, targetK);
-        } else {
+        } else if (pivotIndex < targetK) {
             quickSelect(temp, pivotIndex + 1, right, targetK);
+        } else {
+            quickSelect(temp, left, pivotIndex - 1, targetK);
         }
     }
+
     private int partition(int[] temp, int left, int right) {
-        int pivotFrequency = map.get(temp[right]);
+        int pivotIndex = left + rand.nextInt(right - left + 1);
+        int pivotFrequency = map.get(temp[pivotIndex]);
+        swap(temp, pivotIndex, right);
 
         int leftP = left;
         for (int i = left; i <= right; i++) {
             if (map.get(temp[i]) < pivotFrequency) {
-                swap(temp, i, leftP);
+                swap(temp, leftP, i);
                 leftP++;
             }
         }
         swap(temp, leftP, right);
         return leftP;
     }
+    
     private void swap(int[] nums, int left, int right) {
         int temp = nums[left];
         nums[left] = nums[right];
