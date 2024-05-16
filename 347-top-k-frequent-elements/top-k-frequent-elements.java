a60907fe2bb7b3333fa94b7cda65a3f7b5f31d91
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-16 16:37:16 -0700

    Time: 12 ms (82.95%) | Memory: 49.3 MB (5.05%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -6,48 +6,46 @@ class Solution {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        int[] tempArray = new int[map.size()];
+        int[] temp = new int[map.size()];
         int index = 0;
         for (int num : map.keySet()) {
-            tempArray[index++] = num;
+            temp[index++] = num;
         }
 
-        int topKIndex = tempArray.length - k;
-        quickSelect(tempArray, 0, tempArray.length - 1, topKIndex);
-
-        return Arrays.copyOfRange(tempArray, topKIndex, tempArray.length);
+        quickSelect(temp, 0, temp.length - 1, temp.length - k);
+        return Arrays.copyOfRange(temp, temp.length - k, temp.length);
     }
-
-    private void quickSelect(int[] tempArray, int left, int right, int k) {
+    private void quickSelect(int[] temp, int left, int right, int targetK) {
+        if (left >= right) {
+            return;
+        }
         if (left < right) {
-            int pivotIndex = partition(tempArray, left, right);
-            if (pivotIndex == k) {
+            int pivotIndex = partition(temp, left, right);
+            if (pivotIndex == targetK) {
                 return;
-            } else if (pivotIndex < k) {
-                quickSelect(tempArray, pivotIndex + 1, right, k);
+            } else if (pivotIndex > targetK) {
+                quickSelect(temp, left, pivotIndex - 1, targetK);
             } else {
-                quickSelect(tempArray, left, pivotIndex - 1, k);
+                quickSelect(temp, pivotIndex + 1, right, targetK);
             }
         }
     }
+    private int partition(int[] temp, int left, int right) {
+        int pivotFrequency = map.get(temp[right]);
 
-    private int partition(int[] tempArray, int left, int right) {
-        int pivotFreq = map.get(tempArray[right]);
         int leftP = left;
-        
         for (int i = left; i <= right; i++) {
-            if (map.get(tempArray[i]) < pivotFreq) {
-                swap(tempArray, leftP, i);
+            if (map.get(temp[i]) < pivotFrequency) {
+                swap(temp, i, leftP);
                 leftP++;
             }
         }
-        swap(tempArray, leftP, right);
+        swap(temp, leftP, right);
         return leftP;
     }
-
-    private void swap(int[] tempArray, int left, int right) {
-        int temp = tempArray[left];
-        tempArray[left] = tempArray[right];
-        tempArray[right] = temp;
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
