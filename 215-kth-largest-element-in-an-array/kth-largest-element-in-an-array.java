fc2851f7070f76fbe0936e1d4a5063a87f69dba8
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-16 16:17:02 -0700

    Time: 2159 ms (7.57%) | Memory: 60.6 MB (30.16%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,51 +1,39 @@
 class Solution {
     Random rand = new Random();
-
     public int findKthLargest(int[] nums, int k) {
         return quickSelect(nums, 0, nums.length - 1, nums.length - k);
     }
-
-    private int quickSelect(int[] nums, int left, int right, int k_smallest) {
-        if (left == right) return nums[left];
-
-        // Randomly choose pivot. This is crucial for performance improvement.
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        if (left == right) {
+            return nums[left];
+        }
         int pivotIndex = left + rand.nextInt(right - left);
-
         pivotIndex = partition(nums, left, right, pivotIndex);
-
-        // The pivot is in its final sorted position
-        if (k_smallest == pivotIndex) {
-            return nums[k_smallest];
-        } else if (k_smallest < pivotIndex) {
-            return quickSelect(nums, left, pivotIndex - 1, k_smallest);
+        if (pivotIndex == k) {
+            return nums[pivotIndex];
+        } else if (k < pivotIndex) {
+            return quickSelect(nums, left, pivotIndex - 1, k);
         } else {
-            return quickSelect(nums, pivotIndex + 1, right, k_smallest);
+            return quickSelect(nums, pivotIndex + 1, right, k);
         }
     }
-
     private int partition(int[] nums, int left, int right, int pivotIndex) {
         int pivot = nums[pivotIndex];
-        // move pivot to end
         swap(nums, pivotIndex, right);
-        int storeIndex = left;
 
-        // Move all smaller elements to the left
+        int leftP = left;
         for (int i = left; i <= right; i++) {
             if (nums[i] < pivot) {
-                swap(nums, storeIndex, i);
-                storeIndex++;
+                swap(nums, leftP, i);
+                leftP++;
             }
         }
-
-        // Move pivot to its final place
-        swap(nums, storeIndex, right);
-
-        return storeIndex;
+        swap(nums, leftP, right);
+        return leftP;
     }
-
-    private void swap(int[] nums, int a, int b) {
-        int tmp = nums[a];
-        nums[a] = nums[b];
-        nums[b] = tmp;
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
