468e5753138f1955de058127eb1e41b1cba084e7
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-16 17:46:34 -0700

    Time: 30 ms (75.26%) | Memory: 51.5 MB (60.64%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
 
-        Arrays.sort(nums);
         for (int k = 0; k < nums.length - 2; k++) {
-            if (k > 0 && nums[k - 1] == nums[k]) {
+            if (k > 0 && nums[k] == nums[k - 1]) {
                 continue;
             }
             int i = k + 1, j = nums.length - 1;
@@ -12,17 +12,17 @@ class Solution {
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    while (i < j && nums[i + 1] == nums[i]) {
+                    while (i + 1 < j && nums[i + 1] == nums[i]) {
                         i++;
                     }
                     i++;
-                    while (i < j && nums[j - 1] == nums[j]) {
+                    while (i + 1 < j && nums[j - 1] == nums[j]) {
                         j--;
                     }
                     j--;
                 } else if (sum < 0) {
                     i++;
-                } else if (sum > 0) {
+                } else {
                     j--;
                 }
             }
