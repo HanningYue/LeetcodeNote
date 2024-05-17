81c2b81e8d3530579c69ba546d6896967900e3b0
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-16 18:46:04 -0700

    Time: 1 ms (86.16%) | Memory: 41.1 MB (98.36%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,10 +1,10 @@
 class Solution {
     Boolean[][] dpTable;
     public boolean isInterleave(String s1, String s2, String s3) {
-        int m = s1.length(), n = s2.length();
-        if (m + n != s3.length()) {
+        if (s1.length() + s2.length() != s3.length()) {
             return false;
         }
+        int m = s1.length(), n = s2.length();
         dpTable = new Boolean[m + 1][n + 1];
         return dp(s1, 0, s2, 0, s3, 0);
     }
@@ -15,13 +15,13 @@ class Solution {
         if (dpTable[i][j] != null) {
             return dpTable[i][j];
         }
-        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+        if (i + 1 <= s1.length() && s1.charAt(i) == s3.charAt(k)) {
             dpTable[i][j] = dp(s1, i + 1, s2, j, s3, k + 1);
             if (dpTable[i][j]) {
                 return true;
             }
         }
-        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+        if (j + 1 <= s2.length() && s2.charAt(j) == s3.charAt(k)) {
             dpTable[i][j] = dp(s1, i, s2, j + 1, s3, k + 1);
             if (dpTable[i][j]) {
                 return true;
