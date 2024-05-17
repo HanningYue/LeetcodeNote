792671cbe869463280b05abd7af064df75f0e892
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-16 21:20:49 -0700

    Time: 12 ms (82.95%) | Memory: 45.5 MB (88.46%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,7 +1,6 @@
 class Solution {
-    Map<Integer, Integer> map;
     Random rand = new Random();
-
+    Map<Integer, Integer> map;
     public int[] topKFrequent(int[] nums, int k) {
         map = new HashMap<>();
         for (int num : nums) {
@@ -13,11 +12,9 @@ class Solution {
         for (int num : map.keySet()) {
             temp[index++] = num;
         }
-
         quickSelect(temp, 0, temp.length - 1, temp.length - k);
         return Arrays.copyOfRange(temp, temp.length - k, temp.length);
     }
-
     private void quickSelect(int[] temp, int left, int right, int targetK) {
         if (left >= right) {
             return;
@@ -27,18 +24,17 @@ class Solution {
             return;
         } else if (pivotIndex < targetK) {
             quickSelect(temp, pivotIndex + 1, right, targetK);
-        } else {
+        } else if (pivotIndex > targetK) {
             quickSelect(temp, left, pivotIndex - 1, targetK);
         }
     }
-
     private int partition(int[] temp, int left, int right) {
         int pivotIndex = left + rand.nextInt(right - left + 1);
         int pivotFrequency = map.get(temp[pivotIndex]);
         swap(temp, pivotIndex, right);
 
         int leftP = left;
-        for (int i = left; i <= right; i++) {
+        for (int i = left; i < right; i++) {
             if (map.get(temp[i]) < pivotFrequency) {
                 swap(temp, leftP, i);
                 leftP++;
@@ -47,7 +43,6 @@ class Solution {
         swap(temp, leftP, right);
         return leftP;
     }
-    
     private void swap(int[] nums, int left, int right) {
         int temp = nums[left];
         nums[left] = nums[right];
