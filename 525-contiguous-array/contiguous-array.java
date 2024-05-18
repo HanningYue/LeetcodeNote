2f29ac88e2b02bde7523efc3839e769be9289610
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-18 00:19:04 -0700

    Time: 23 ms (84.83%) | Memory: 54.9 MB (76.73%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -2,8 +2,8 @@ class Solution {
     public int findMaxLength(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
-        
-        int maxLength = 0, pairs = 0;
+
+        int pairs = 0, maxLength = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 0) {
                 pairs--;
@@ -13,8 +13,9 @@ class Solution {
 
             if (map.containsKey(pairs)) {
                 maxLength = Math.max(maxLength, i - map.get(pairs));
+            } else {
+                map.put(pairs, i);
             }
-            map.putIfAbsent(pairs, i);
         }
         return maxLength;
     }
