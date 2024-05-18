d94ce92ff9aced9c383bdec2608ff21ac7ebe52b
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-17 19:24:30 -0700

    Time: 4 ms (41.87%) | Memory: 49.6 MB (31.28%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,21 +2,20 @@ class Solution {
     public int numIslands(char[][] grid) {
         int count = 0;
         boolean[][] visited = new boolean[grid.length][grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (!visited[row][col] && grid[row][col] == '1') {
                     count++;
-                    dfs(grid, i, j, visited);
+                    dfs(grid, row, col, visited);
                 }
             }
         }
         return count;
     }
-
     int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     private void dfs(char[][] grid, int row, int col, boolean[][] visited) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col]
-        || grid[row][col] != '1') {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length 
+        || visited[row][col] || grid[row][col] != '1') {
             return;
         }
         visited[row][col] = true;
