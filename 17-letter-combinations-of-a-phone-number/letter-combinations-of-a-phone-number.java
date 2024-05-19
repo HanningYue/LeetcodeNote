fbe51acb9a2ba8f43dfee3cf0ac01d1b54a31538
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-18 23:32:57 -0700

    Time: 0 ms (100.00%) | Memory: 41.8 MB (84.91%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,9 +1,10 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
-        if (digits.length() == 0 || digits == null) {
+        if (digits.length() == 0) {
             return result;
         }
+
         Map<Character, String> map = new HashMap<>();
         map.put('2', "abc");
         map.put('3', "def");
@@ -13,21 +14,24 @@ class Solution {
         map.put('7', "pqrs");
         map.put('8', "tuv");
         map.put('9', "wxyz");
-
+        
         StringBuilder sb = new StringBuilder();
-        backTrack(digits, map, result, 0, sb);
+        backTrack(digits, map, 0, sb, result);
         return result;
     }
-    private void backTrack(String digits, Map<Character, String> map, List<String> result, int index, StringBuilder sb) {
+
+    private void backTrack(String digits, Map<Character, String> map, int index, 
+                           StringBuilder sb, List<String> result) 
+    {
         if (index == digits.length()) {
             result.add(sb.toString());
             return;
         }
-
+        
         String cor = map.get(digits.charAt(index));
         for (int i = 0; i < cor.length(); i++) {
             sb.append(cor.charAt(i));
-            backTrack(digits, map, result, index + 1, sb);
+            backTrack(digits, map, index + 1, sb, result);
             sb.deleteCharAt(sb.length() - 1);
         }
     }
