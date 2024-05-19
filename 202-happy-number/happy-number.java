d2cda42823292943ac354c59449640200ac87bfb
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-19 13:09:10 -0700

    Time: 0 ms (100.00%) | Memory: 40.6 MB (57.16%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n, fast = process(n);
+        int slow = process(n), fast = process(process(n));
         while (slow != fast) {
             slow = process(slow);
             fast = process(process(fast));
         }
         return slow == 1;
     }
-    private int process(int num) {
-        int result = 0;
-        while (num != 0) {
-            int lastDigit = num % 10;
-            result += lastDigit * lastDigit;
-            num = num / 10;
+    private int process(int n) {
+        int sum = 0;
+        while (n != 0) {
+            int lastDigit = n % 10;
+            sum += lastDigit * lastDigit;
+            n = n / 10;
         }
-        return result;
+        return sum;
     }
 }
