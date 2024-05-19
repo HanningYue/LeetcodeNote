aabfb972d706cf282b203ea62d8c8e767c543ac9
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-19 11:59:41 -0700

    Time: 4 ms (99.99%) | Memory: 47.5 MB (58.96%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -1,16 +1,23 @@
+/**
+1. Want to find the number of arrays, but CONTIGUOUS, so we can apply range(j - i) technique to locate all the subarrays
+
+2. So in the slidingwindow while loop, when the condition product is NOT (greater than or equal to k) we update the product, remove slow, outside while loop, we have product which is strictly less than k, we use range(j - i) since it is CONTIGUOUS*/
+
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int slow = 0;
-        int result = 0;
-        int runningSum = 1;
-        for (int fast = 0; fast < nums.length; fast++) {
-            runningSum *= nums[fast];
-            while (runningSum >= k && slow <= fast) {
-                runningSum /= nums[slow];
+        if (k <= 1) return 0;
+        
+        int product = 1;
+        int slow = 0, fast = 0, count = 0;
+        while (fast < nums.length) {
+            product *= nums[fast];
+            while (product >= k) {
+                product /= nums[slow];
                 slow++;
             }
-            result += fast - slow + 1;
+            count += fast - slow + 1;
+            fast++;
         }
-        return result;
+        return count;
     }
 }
