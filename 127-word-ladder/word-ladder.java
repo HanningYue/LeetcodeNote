4c43db6772f2cb5e706d741875e7092189e3843d
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-19 15:00:06 -0700

    Time: 71 ms (78.34%) | Memory: 46.5 MB (59.88%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,21 +1,21 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>(wordList);
         Queue<String> queue = new LinkedList<>();
         queue.offer(beginWord);
-        Set<String> set = new HashSet<>(wordList);
 
         int step = 1;
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
-                String currentWord = queue.poll();
-                if (currentWord.equals(endWord)) {
+                String current = queue.poll();
+                if (current.equals(endWord)) {
                     return step;
                 }
 
-                StringBuilder sb = new StringBuilder(currentWord);
-                for (int j = 0; j < currentWord.length(); j++) {
-                    char original = currentWord.charAt(j);
+                StringBuilder sb = new StringBuilder(current);
+                for (int j = 0; j < sb.length(); j++) {
+                    char original = sb.charAt(j);
                     for (char c = 'a'; c <= 'z'; c++) {
                         sb.setCharAt(j, c);
                         if (set.contains(sb.toString())) {
