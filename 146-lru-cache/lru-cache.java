302d3b3333ef8ac6f8e1b9478c65b92019e04b03
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-20 20:09:01 -0700

    Time: 54 ms (22.51%) | Memory: 118.2 MB (33.32%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,11 +1,17 @@
 class LRUCache {
-    int capacity;
     LinkedHashMap<Integer, Integer> map;
+    int capacity;
     public LRUCache(int capacity) {
-        map = new LinkedHashMap<>();
         this.capacity = capacity;
+        map = new LinkedHashMap<>();
     }
     
+    public void makeRecent(int key) {
+        int value = map.get(key);
+        map.remove(key);
+        map.put(key, value);
+    }
+
     public int get(int key) {
         if (map.containsKey(key)) {
             makeRecent(key);
@@ -14,20 +20,14 @@ class LRUCache {
         return -1;
     }
 
-    public void makeRecent(int key) {
-        int value = map.get(key);
-        map.remove(key);
-        map.put(key, value);
-    }
-    
     public void put(int key, int value) {
         if (map.containsKey(key)) {
-            makeRecent(key);
             map.put(key, value);
+            makeRecent(key);
         } else {
             if (map.size() >= capacity) {
-                int lru = map.keySet().iterator().next();
-                map.remove(lru);
+                int lruKey = map.keySet().iterator().next();
+                map.remove(lruKey);
             }
             map.put(key, value);
         }
