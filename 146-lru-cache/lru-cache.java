7f517b8215bb835b7098a9ab130030e2be2b46de
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-20 20:25:28 -0700

    Time: 45 ms (69.94%) | Memory: 124.2 MB (16.38%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,35 +1,67 @@
+class Node {
+    Node prev, next;
+    int key, value;
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
 class LRUCache {
-    LinkedHashMap<Integer, Integer> map;
+    Node head, tail;
+    Map<Integer, Node> map;
     int capacity;
     public LRUCache(int capacity) {
+        map = new HashMap<>();    
         this.capacity = capacity;
-        map = new LinkedHashMap<>();
-    }
-    
-    public void makeRecent(int key) {
-        int value = map.get(key);
-        map.remove(key);
-        map.put(key, value);
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
     }
 
+    public void add(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public void delete(Node node) {
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+    }
+    
     public int get(int key) {
         if (map.containsKey(key)) {
-            makeRecent(key);
-            return map.get(key);
+            Node node = map.get(key);
+            int value = node.value;
+            delete(node);
+            add(node);
+            return value;
         }
         return -1;
     }
-
+    
     public void put(int key, int value) {
         if (map.containsKey(key)) {
-            map.put(key, value);
-            makeRecent(key);
+            Node oldNode = map.get(key);
+            delete(oldNode);
+   
+            Node newNode = new Node(key, value);
+            add(newNode);
+            map.put(key, newNode);
         } else {
             if (map.size() >= capacity) {
-                int lruKey = map.keySet().iterator().next();
+                int lruKey = tail.prev.key;
+                Node lruNode = tail.prev;
+
                 map.remove(lruKey);
+                delete(lruNode);
             }
-            map.put(key, value);
+            Node newNode = new Node(key, value);
+            add(newNode);
+            map.put(key, newNode);
         }
     }
 }
