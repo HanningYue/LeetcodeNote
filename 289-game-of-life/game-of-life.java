07190cb9e65e53acbfb3a65602d507c8d5ea7fa0
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-20 19:35:11 -0700

    Time: 0 ms (100.00%) | Memory: 41.4 MB (77.37%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,20 +1,19 @@
 class Solution {
     public void gameOfLife(int[][] board) {
         int m = board.length, n = board[0].length;
-
         int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
                 int countLive = 0;
                 for (int[] dir : directions) {
-                    int newRow = row + dir[0], newCol = col + dir[1];
+                    int newRow = dir[0] + row, newCol = dir[1] + col;
                     if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n 
                     || board[newRow][newCol] == 0 || board[newRow][newCol] == 3) {
                         continue;
                     }
                     countLive++;
                 }
-                
+
                 if (board[row][col] == 1) {
                     if (countLive < 2 || countLive > 3) {
                         board[row][col] = 2;
@@ -26,7 +25,6 @@ class Solution {
                 }
             }
         }
-        
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
                 board[row][col] = board[row][col] % 2;
