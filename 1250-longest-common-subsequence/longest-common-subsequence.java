466b3e480bfa06cdc87dcb56fc2d948af3bc5290
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-21 21:51:25 -0700

    Time: 33 ms (10.93%) | Memory: 62.2 MB (5.04%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -6,17 +6,17 @@ class Solution {
         return dp(text1, m - 1, text2, n - 1);
     }
     private int dp(String text1, int i, String text2, int j) {
-        if (i == -1 || j == -1) {
+        if (i == -1) {
+            return 0;
+        } else if (j == -1) {
             return 0;
         }
-
         if (dpTable[i][j] != null) {
             return dpTable[i][j];
         }
-
         if (text1.charAt(i) == text2.charAt(j)) {
-            int foundSame = dp(text1, i - 1, text2, j - 1) + 1;
-            dpTable[i][j] = foundSame;
+            int skipBoth = dp(text1, i - 1, text2, j - 1);
+            dpTable[i][j] = skipBoth + 1;
         } else {
             int skipI = dp(text1, i - 1, text2, j);
             int skipJ = dp(text1, i, text2, j - 1);
