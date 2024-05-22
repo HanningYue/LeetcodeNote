da23569f260cbf61587112f138594be5b5d06e27
Taylor Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-21 20:11:56 -0700

    Time: 1 ms (100.00%) | Memory: 44.7 MB (20.39%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,25 +10,36 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        return divide(lists, 0, lists.length - 1);
+    }
+    private ListNode divide(ListNode[] lists, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        if (left == right) {
+            return lists[left];
+        }
+
+        int mid = left + (right - left) / 2;
+        ListNode leftPart = divide(lists, left, mid);
+        ListNode rightPart = divide(lists, mid + 1, right);
+        ListNode merge = mergeTwoLists(lists, leftPart, rightPart);
+        return merge;
+    }
+    private ListNode mergeTwoLists(ListNode[] lists, ListNode p, ListNode q) {
         ListNode dummy = new ListNode(-1);
-        ListNode resultHead = dummy;
-
-        PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
-        for (ListNode head : lists) {
-            if (head != null) {
-                heap.offer(head);
+        ListNode current = dummy;
+        while (p != null && q != null) {
+            if (p.val < q.val) {
+                current.next = p;
+                p = p.next;
+            } else {
+                current.next = q;
+                q = q.next;
             }
+            current = current.next;
         }
-
-        while (!heap.isEmpty()) {
-            ListNode current = heap.poll();
-            resultHead.next = current;
-            resultHead = resultHead.next;
-
-            if (current.next != null) {
-                heap.offer(current.next);
-            }
-        }
+        current.next = (p == null) ? q : p;
         return dummy.next;
     }
 }
