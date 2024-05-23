1509fd9d14afc7604eb2427006e0c170842b0228
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-22 22:36:46 -0700

    Time: 202 ms (20.65%) | Memory: 41.2 MB (81.74%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        if (word.length() == 0) {
-            return true;
-        }
-
         int m = board.length, n = board[0].length;
-        boolean[][] visited = new boolean[m][n];
         
+        boolean[][] visited = new boolean[m][n];
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
-                char startChar = board[row][col];
-                if (startChar == word.charAt(0)) {
-                    if (dfs(board, visited, row, col, 0, word)) {
+                if (board[row][col] == word.charAt(0)) {
+                    if (dfs(board, row, col, word, 0, visited)) {
                         return true;
                     }
                 }
@@ -19,19 +14,19 @@ class Solution {
         }
         return false;
     }
+    
     int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private boolean dfs(char[][] board, boolean[][] visited, int row, int col, int index, String word) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col]
-        || board[row][col] != word.charAt(index)) {
+    private boolean dfs(char[][] board, int row, int col, String word, int index, boolean[][] visited) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length 
+        || visited[row][col] || word.charAt(index) != board[row][col]) {
             return false;
         }
         if (index == word.length() - 1) {
             return true;
         }
-
         visited[row][col] = true;
         for (int[] dir : directions) {
-            if (dfs(board, visited, row + dir[0], col + dir[1], index + 1, word)) {
+            if (dfs(board, row + dir[0], col + dir[1], word, index + 1, visited)) {
                 return true;
             }
         }
