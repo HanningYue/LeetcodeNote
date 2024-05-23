08cf4b6616cb1f701b33a3b89cdfc221ff1c7c4c
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-22 21:31:20 -0700

    Time: 0 ms (100.00%) | Memory: 41.1 MB (97.19%) - LeetSync

--- 228-summary-ranges/summary-ranges.java
+++ 228-summary-ranges/summary-ranges.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            String currentInterval = "";
-            int a = nums[i];
-            currentInterval += String.valueOf(nums[i]);
-            while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {
-                i++;
+        int n = nums.length;
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int tempIndex = i;
+            sb.append(nums[tempIndex]);
+            while (tempIndex < n - 1 && nums[tempIndex + 1] == nums[tempIndex] + 1) {
+                tempIndex++;
             }
-            if (nums[i] == a) {
-                result.add(currentInterval);
+            if (tempIndex == i) {
+                result.add(sb.toString());
                 continue;
-            } 
-            else {
-                currentInterval = currentInterval + "->" + String.valueOf(nums[i]);
-                result.add(currentInterval);
+            } else {
+                sb.append("->");
+                sb.append(nums[tempIndex]);
+                result.add(sb.toString());
+                i = tempIndex;
             }
         }
         return result;
