9c1294caaa4fdde0a703c2b39b446a5c9496ac79
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-22 22:51:40 -0700

    Time: 227 ms (58.06%) | Memory: 44.7 MB (79.92%) - LeetSync

--- 212-word-search-ii/word-search-ii.java
+++ 212-word-search-ii/word-search-ii.java
@@ -3,16 +3,18 @@ class TrieNode {
     boolean isEnd;
     String word;
     public TrieNode() {
-        children = new TrieNode[26];
         word = "";
+        children = new TrieNode[26];
         isEnd = false;
     }
 }
 class Solution {
     TrieNode root;
+    
     public List<String> findWords(char[][] board, String[] words) {
-        List<String> result = new ArrayList<>();
         root = new TrieNode();
+        List<String> result = new ArrayList<>();
+
         for (String word : words) {
             insert(word);
         }
@@ -21,44 +23,44 @@ class Solution {
         boolean[][] visited = new boolean[m][n];
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
-                char c = board[row][col];
+                int c = board[row][col];
                 if (root.children[c - 'a'] != null) {
                     TrieNode node = root;
-                    dfs(board, row, col, result, node, visited);
+                    dfs(board, row, col, node, result, visited);
                 }
             }
         }
         return result;
     }
-
+    
     int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private void dfs(char[][] board, int row, int col, List<String> result, TrieNode node, boolean[][] visited) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col] 
-        || node.children[board[row][col] - 'a'] == null) {
+    private void dfs(char[][] board, int row, int col, TrieNode node, List<String> result, boolean[][] visited) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length
+        || node.children[board[row][col] - 'a'] == null || visited[row][col]) {
             return;
         }
-        
+    
         char c = board[row][col];
         if (node.children[c - 'a'].isEnd) {
-            node.children[c - 'a'].isEnd = false;
             result.add(node.children[c - 'a'].word);
+            node.children[c - 'a'].isEnd = false;
         }
         visited[row][col] = true;
         for (int[] dir : directions) {
-            dfs(board, row + dir[0], col + dir[1], result, node.children[c - 'a'], visited);
+            dfs(board, row + dir[0], col + dir[1], node.children[c - 'a'], result, visited);
         }
         visited[row][col] = false;
     }
 
-    private void insert(String word) {
+    private void insert(String s) {
         TrieNode node = root;
-        for (char c : word.toCharArray()) {
+        for (char c : s.toCharArray()) {
             if (node.children[c - 'a'] == null) {
                 node.children[c - 'a'] = new TrieNode();
             }
             node = node.children[c - 'a'];
         }
-        node.word = word;
         node.isEnd = true;
+        node.word = s;
     }
 }
