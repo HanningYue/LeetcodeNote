ef4888ef2a4a5973e58958f1f36c17af0a659668
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-22 20:45:36 -0700

    Time: 1 ms (98.56%) | Memory: 45.1 MB (13.05%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,20 +1,25 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n = intervals.length, i = 0;
         List<int[]> result = new ArrayList<>();
+
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
         
-        for (int i = 0; i < intervals.length; i++) {
-            int[] currentInterval = intervals[i];
-            if (currentInterval[0] > newInterval[1]) {
-                result.add(newInterval);
-                newInterval = currentInterval;
-            } else if (newInterval[0] > currentInterval[1]) {
-                result.add(currentInterval);
-            } else if (newInterval[0] <= currentInterval[1] && newInterval[1] >= currentInterval[0]) {
-                newInterval[0] = Math.min(newInterval[0], currentInterval[0]);
-                newInterval[1] = Math.max(newInterval[1], currentInterval[1]);
-            }
+        while (i < n && newInterval[1] >= intervals[i][0]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
         result.add(newInterval);
+
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+
         return result.toArray(new int[result.size()][]);
     }
 }
