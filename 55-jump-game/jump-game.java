8fe3512ebd4a8e641f50de38125cdec11ba53780
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-29 22:11:00 -0700

    Time: 3 ms (53.56%) | Memory: 45.2 MB (86.70%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean canJump(int[] nums) {
         int maxJumpAtEachStep = 0;
         for (int i = 0; i < nums.length - 1; i++) {
-            maxJumpAtEachStep = Math.max(maxJumpAtEachStep, nums[i] + i);
-            if (i == maxJumpAtEachStep) {
+            maxJumpAtEachStep = Math.max(maxJumpAtEachStep, i + nums[i]);
+            if (maxJumpAtEachStep == i) {
                 return false;
             }
         }
