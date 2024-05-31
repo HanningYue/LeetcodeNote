11a3b2b26a1688f489b2b8ceb5158790e736e093
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-05-30 23:13:41 -0700

    Time: 5 ms (52.84%) | Memory: 42.9 MB (24.54%) - LeetSync

---
+++ 785-basic-calculator-iii/basic-calculator-iii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int calculate(String s) {
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : s.toCharArray()) {
+            if (c != ' ') {
+                queue.offer(c);
+            }
+        }
+        return recursion(queue);
+    }
+    private int recursion(Queue<Character> queue) {
+        Stack<Integer> stack = new Stack<>();
+        
+        char sign = '+';
+        int sum = 0;
+        while (!queue.isEmpty()) {
+            char current = queue.poll();
+            if (Character.isDigit(current)) {
+                sum = sum * 10 + current - '0';
+            }
+            if (current == '(') {
+                sum = recursion(queue);
+            }
+            if (!Character.isDigit(current) || queue.isEmpty()) {
+                if (sign == '+') {
+                    stack.push(sum);
+                } else if (sign == '-') {
+                    stack.push(-sum);
+                } else if (sign == '*') {
+                    stack.push(stack.pop() * sum);
+                } else if (sign == '/') {
+                    stack.push(stack.pop() / sum);
+                }
+                sign = current;
+                sum = 0;
+            }
+            if (current == ')') {
+                break;
+            }
+        }
+
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
+    }
+}
