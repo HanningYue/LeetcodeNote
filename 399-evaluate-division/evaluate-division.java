81c16431c8f1e6bcac22029d91e057e07e91c2b3
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-01 19:27:19 -0700

    Time: 1 ms (98.78%) | Memory: 42.3 MB (23.35%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -1,18 +1,18 @@
 class Node {
-    public String key;
-    public double value;
+    String key;
+    double value;
     public Node(String key, double value) {
         this.key = key;
         this.value = value;
     }
 }
 class Solution {
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+    public double[] calcEquation(List<List<String>> equations, double[] values, 
+                                 List<List<String>> queries) {
         Map<String, List<Node>> graph = new HashMap<>();
         for (int i = 0; i < equations.size(); i++) {
             List<String> equation = equations.get(i);
-            String dividend = equation.get(0);
-            String divisor = equation.get(1);
+            String dividend = equation.get(0), divisor = equation.get(1);
             graph.putIfAbsent(dividend, new ArrayList<>());
             graph.get(dividend).add(new Node(divisor, values[i]));
 
@@ -23,30 +23,27 @@ class Solution {
         double[] result = new double[queries.size()];
         for (int i = 0; i < queries.size(); i++) {
             List<String> query = queries.get(i);
-            String dividend = query.get(0);
-            String divisor = query.get(1);
+            String dividend = query.get(0), divisor = query.get(1);
             Set<String> visited = new HashSet<>();
-            result[i] = dfs(graph, dividend, divisor, 1, visited);
+            result[i] = dfs(dividend, divisor, graph, 1, visited);
         }
         return result;
     }
-    private double dfs(Map<String, List<Node>> graph, String dividend, String divisor, 
-    double value, Set<String> visited) 
-    {
-        if (!graph.containsKey(dividend)) {
-            return -1.0;
-        }
-        if (visited.contains(dividend)) {
+
+    private double dfs(String dividend, String divisor, Map<String, List<Node>> graph, 
+    double value, Set<String> visited) {
+        if (visited.contains(dividend) || !graph.containsKey(dividend)) {
             return -1.0;
         }
         if (dividend.equals(divisor)) {
             return value;
         }
+
         visited.add(dividend);
         for (Node neighbor : graph.get(dividend)) {
-            double sub = dfs(graph, neighbor.key, divisor, value * neighbor.value, visited);
-            if (sub != -1.0) {
-                return sub;
+            double subProblem = dfs(neighbor.key, divisor, graph, neighbor.value * value, visited);
+            if (subProblem != -1.0) {
+                return subProblem;
             }
         }
         visited.remove(dividend);
