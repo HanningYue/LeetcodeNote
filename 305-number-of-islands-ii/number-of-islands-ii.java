4030a2b68ee46dd3724861aa46d17332fb0753ce
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-01 18:34:03 -0700

    Time: 11 ms (55.90%) | Memory: 48 MB (20.65%) - LeetSync

--- 305-number-of-islands-ii/number-of-islands-ii.java
+++ 305-number-of-islands-ii/number-of-islands-ii.java
@@ -1,11 +1,42 @@
+class Solution {
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    public List<Integer> numIslands2(int m, int n, int[][] positions) {
+        List<Integer> result = new ArrayList<>();
+        
+        Uf uf = new Uf(m * n);
+        int count = 0;
+        for (int[] position : positions) {
+            int currentRow = position[0], currentCol = position[1];
+            int currentOneD = currentRow * n + currentCol;
+
+            if (uf.parent[currentOneD] == -1) {
+                uf.parent[currentOneD] = currentOneD;
+                count++;
+
+                for (int[] dir : directions) {
+                    int newRow = dir[0] + currentRow, newCol = dir[1] + currentCol;
+                    int newOneD = newRow * n + newCol;
+
+                    if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || uf.parent[newOneD] == -1) {
+                        continue;
+                    }
+                    if (!uf.isConnected(newOneD, currentOneD)) {
+                        uf.union(newOneD, currentOneD);
+                        count--;
+                    }
+                }
+            }
+            result.add(count);
+        }
+        return result;
+    }
+}
 class Uf {
-    int countOfComponent;
     int[] parent;
     public Uf(int n) {
-        this.countOfComponent = n;
         parent = new int[n];
         for (int i = 0; i < n; i++) {
-            parent[i] = i;
+            parent[i] = -1;
         }
     }
     public int find(int a) {
@@ -15,51 +46,13 @@ class Uf {
         return parent[a];
     }
     public void union(int a, int b) {
-        int parentA = find(a), parentB = find(b);
-        if (parentA == parentB) {
+        if (find(a) == find(b)) {
             return;
         }
-
+        int parentA = find(a), parentB = find(b);
         parent[parentA] = parentB;
-        countOfComponent--;
     }
     public boolean isConnected(int a, int b) {
         return find(a) == find(b);
-    }
-}
-
-class Solution {
-    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    public List<Integer> numIslands2(int m, int n, int[][] positions) {
-        List<Integer> result = new ArrayList<>();
-    
-        Set<Integer> visited = new HashSet<>();
-        int count = 0;
-        Uf uf = new Uf(m * n);
-
-        for (int[] position : positions) {
-            int currentRow = position[0], currentCol = position[1];
-            int currentOneD = currentRow * n + currentCol;
-            if (visited.add(currentOneD)) {
-                count++;
-                uf.parent[currentOneD] = currentOneD;
-
-                for (int[] dir : directions) {
-                    int newRow = dir[0] + currentRow;
-                    int newCol = dir[1] + currentCol;
-                    int newOneD = newRow * n + newCol;
-
-                    if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || !visited.contains(newOneD)) {
-                        continue;
-                    }
-                    if (!uf.isConnected(currentOneD, newOneD)) {
-                        uf.union(currentOneD, newOneD);
-                        count--;
-                    }
-                }
-            }
-            result.add(count);
-        }    
-        return result;
     }
 }
