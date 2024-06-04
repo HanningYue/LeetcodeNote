e3428afcc91d4dc740cdfdbb313bc6ec8b443beb
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-03 23:46:44 -0700

    Time: 6 ms (55.77%) | Memory: 45.7 MB (60.04%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,31 +1,30 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int n = numCourses;
+        int[] result = new int[n];
+        boolean[] visited = new boolean[n];
+        boolean[] visiting = new boolean[n];
+        
         Map<Integer, Set<Integer>> graph = new HashMap<>();
-        for (int i = 0; i < numCourses; i++) {
-            graph.put(i, new HashSet<Integer>());
-        }
         for (int[] pre : prerequisites) {
-            int from = pre[1], to = pre[0];
+            int to = pre[0], from = pre[1];
+            graph.putIfAbsent(to, new HashSet<>());
             graph.get(to).add(from);
         }
-
-        List<Integer> list = new ArrayList<>();
-        boolean[] visited = new boolean[numCourses];
-        boolean[] visiting = new boolean[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            if (!dfs(graph, i, visited, visiting, list)) {
+        
+        List<Integer> order = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!dfs(graph, visited, visiting, i, order)) {
                 return new int[]{};
             }
         }
-
-        int[] result = new int[numCourses];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
+        for (int i = 0; i < order.size(); i++) {
+            result[i] = order.get(i);
         }
         return result;
     }
-    private boolean dfs(Map<Integer, Set<Integer>> graph, int vertex, boolean[] visited, 
-                    boolean[] visiting, List<Integer> list) {
+    private boolean dfs(Map<Integer, Set<Integer>> graph, boolean[] visited, boolean[] visiting,
+    int vertex, List<Integer> order) {
         if (visiting[vertex]) {
             return false;
         }
@@ -33,20 +32,19 @@ class Solution {
             return true;
         }
         if (!graph.containsKey(vertex)) {
-            list.add(vertex);
+            order.add(vertex);
             visited[vertex] = true;
             return true;
         }
-
         visiting[vertex] = true;
         for (int neighbor : graph.get(vertex)) {
-            if (!dfs(graph, neighbor, visited, visiting, list)) {
+            if (!dfs(graph, visited, visiting, neighbor, order)) {
                 return false;
             }
         }
-        visiting[vertex] = false;
         visited[vertex] = true;
-        list.add(vertex);
+        visiting[vertex] = false;
+        order.add(vertex);
         return true;
     }
 }
