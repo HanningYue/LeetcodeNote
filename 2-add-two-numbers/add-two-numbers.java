6736205710843818cef34a89323a119f1a45b36b
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-03 22:32:13 -0700

    Time: 1 ms (100.00%) | Memory: 44.3 MB (60.69%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,37 +1,22 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        
-        int remainder = 0;
-        while (l1 != null || l2 != null || remainder != 0) {
-            int l1Val = l1 == null ? 0 : l1.val;
-            int l2Val = l2 == null ? 0 : l2.val;
-            int sum = l1Val + l2Val + remainder;
-            remainder = sum / 10;
-            
-            int currentNodeValue = sum % 10;
-            ListNode currentNode = new ListNode(currentNodeValue);
-            current.next = currentNode;
-            current = current.next;
+        return addTwoNumbersHelper(l1, l2, 0);
+    }
 
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
+    private ListNode addTwoNumbersHelper(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null && l2 == null && carry == 0) {
+            return null;
         }
-        return dummy.next;
+
+        int l1Val = (l1 != null) ? l1.val : 0;
+        int l2Val = (l2 != null) ? l2.val : 0;
+        int sum = l1Val + l2Val + carry;
+        carry = sum / 10;
+
+        ListNode currentNode = new ListNode(sum % 10);
+        currentNode.next = addTwoNumbersHelper((l1 != null) ? l1.next : null, 
+                                                (l2 != null) ? l2.next : null, carry);
+
+        return currentNode;
     }
 }
