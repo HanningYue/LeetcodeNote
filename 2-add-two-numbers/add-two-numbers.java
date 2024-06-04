9b2d9c34b64edbf360a648747499d0e70deec6dd
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-03 22:20:25 -0700

    Time: 1 ms (100.00%) | Memory: 44.6 MB (25.37%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -9,36 +9,29 @@
  * }
  */
 class Solution {
-    ListNode dummy = new ListNode(-1);
-    ListNode current = dummy;
-    int carry = 0;
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        recursion(l1, l2, 0);
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        
+        int remainder = 0;
+        while (l1 != null || l2 != null || remainder != 0) {
+            int l1Val = l1 == null ? 0 : l1.val;
+            int l2Val = l2 == null ? 0 : l2.val;
+            int sum = l1Val + l2Val + remainder;
+            remainder = sum / 10;
+            
+            int currentNodeValue = sum % 10;
+            ListNode currentNode = new ListNode(currentNodeValue);
+            current.next = currentNode;
+            current = current.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
         return dummy.next;
-    }
-    private ListNode recursion(ListNode l1, ListNode l2, int carry) {
-        if (l1 == null && l2 == null && carry == 0) {
-            return null;
-        }
-
-        int l1Value = l1 == null ? 0 : l1.val;
-        int l2Value = l2 == null ? 0 : l2.val;
-
-        int sum = l1Value + l2Value + carry;
-        int remainder = sum % 10;
-        carry = sum / 10;
-        ListNode newNode = new ListNode(remainder);
-        current.next = newNode;
-        current = current.next;
-
-        if (l1 != null && l2 != null) {
-            return recursion(l1.next, l2.next, carry);
-        } else if (l1 != null) {
-            return recursion(l1.next, null, carry);
-        } else if (l2 != null) {
-            return recursion(null, l2.next, carry);
-        } else {
-            return recursion(null, null, carry);
-        }
     }
 }
