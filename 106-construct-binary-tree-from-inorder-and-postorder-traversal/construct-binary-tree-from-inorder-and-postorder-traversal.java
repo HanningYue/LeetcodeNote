9e5b144f3902bb529e05b13d24703b1c096c19f9
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-06 14:39:41 -0700

    Time: 1 ms (97.22%) | Memory: 44.5 MB (17.78%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -14,26 +14,32 @@
  * }
  */
 class Solution {
+    Map<Integer, Integer> map;
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        Map<Integer, Integer> map = new HashMap<>();
+        map = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
             map.put(inorder[i], i);
         }
-        return dfs(map, postorder, 0, postorder.length - 1, inorder, 0, inorder.length - 1);
+        return recursion(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
     }
-    private TreeNode dfs(Map<Integer, Integer> map, int[] postorder, int postleft, int postright,
-    int[] inorder, int inleft, int inright) {
-        if (postleft > postright) {
+    private TreeNode recursion(int[] inorder, int inleft, int inright, 
+    int[] postorder, int postleft, int postright) {
+        if (inleft > inright) {
             return null;
         }
+
         int headValue = postorder[postright];
         int headIndex = map.get(headValue);
-        int leftSize = headIndex - inleft;
+        int leftSubtreeSize = headIndex - inleft;
 
-        TreeNode newHead = new TreeNode(headValue);
-        newHead.left = dfs(map, postorder, postleft, postleft + leftSize - 1, inorder, inleft, headIndex - 1);
-        newHead.right = dfs(map, postorder, postleft + leftSize, postright - 1, inorder, headIndex + 1, inright);
-        return newHead;
+        TreeNode root = new TreeNode(headValue);
+        root.left = recursion(inorder, inleft, headIndex - 1, 
+        postorder, postleft, postleft + leftSubtreeSize - 1);
+
+        root.right = recursion(inorder, headIndex + 1, inright, 
+        postorder, postleft + leftSubtreeSize, postright - 1);
+        return root;
+
     }
 }
 /**
