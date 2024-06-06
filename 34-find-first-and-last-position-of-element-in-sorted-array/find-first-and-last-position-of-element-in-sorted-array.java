2868ead3fd176e5cc05188e7cba7da8c11e73cc7
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-05 20:28:06 -0700

    Time: 0 ms (100.00%) | Memory: 45.9 MB (22.71%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,27 +1,11 @@
 class Solution {
+    int leftBound, rightBound;
     public int[] searchRange(int[] nums, int target) {
-        int leftBound = binarySearchLeft(nums, target);
-        int rightBound = binarySearchRight(nums, target);
-        return new int[]{leftBound, rightBound};
+        findLeft(nums, target);
+        findRight(nums, target);
+        return new int[]{leftBound, rightBound};        
     }
-    private int binarySearchRight(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                left = mid + 1;
-            } else if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            }
-        }
-        if (right < 0 || nums[right] != target) {
-            return -1;
-        }
-        return right;
-    }
-    private int binarySearchLeft(int[] nums, int target) {
+    private void findLeft(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
@@ -33,9 +17,28 @@ class Solution {
                 right = mid - 1;
             }
         }
-        if (left >= nums.length || nums[left] != target) {
-            return -1;
+        if (left <= nums.length - 1 && nums[left] == target) {
+            leftBound = left;
+        } else {
+            leftBound = -1;
         }
-        return left;
+    }
+    private void findRight(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        if (right >= 0 && nums[right] == target) {
+            rightBound = right;
+        } else {
+            rightBound = -1;
+        }
     }
 }
