a7567938402082b59b710340c41781a7b68bad3c
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-06 16:28:40 -0700

    Time: 0 ms (100.00%) | Memory: 40.9 MB (40.33%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,20 +1,19 @@
 class Solution {
-    Integer[] robLast;
-    Integer[] notRobLast;
+    Integer[] dpTableOne;
+    Integer[] dpTableTwo;
     public int rob(int[] nums) {
         if (nums.length == 1) {
             return nums[0];
         }
-
+        
         int n = nums.length;
-        robLast = new Integer[n];
-        notRobLast = new Integer[n];
+        dpTableOne = new Integer[n];
+        dpTableTwo = new Integer[n];
 
-        int resultRobLast = dp(nums, 1, n - 1, robLast);
-        int resultNotRobLast = dp(nums, 0, n - 2, notRobLast);
-        return Math.max(resultRobLast, resultNotRobLast);
+        int robLast = dp(nums, 1, n - 1, dpTableOne);
+        int notRobLast = dp(nums, 0, n - 2, dpTableTwo);
+        return Math.max(robLast, notRobLast);
     }
-
     private int dp(int[] nums, int start, int end, Integer[] dpTable) {
         if (start > end) {
             return 0;
@@ -22,7 +21,6 @@ class Solution {
         if (dpTable[start] != null) {
             return dpTable[start];
         }
-
         int robCurrent = dp(nums, start + 2, end, dpTable) + nums[start];
         int notRobCurrent = dp(nums, start + 1, end, dpTable);
         dpTable[start] = Math.max(robCurrent, notRobCurrent);
