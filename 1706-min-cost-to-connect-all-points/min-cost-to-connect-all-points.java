bc8c2a15679afd8ea6128d37492f445d81e837cc
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-09 00:58:54 -0700

    Time: 182 ms (53.48%) | Memory: 79.3 MB (23.88%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,30 +1,30 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        PriorityQueue<int[]> heap = new PriorityQueue<int[]>(new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[1] - b[1];
-            }
-        });
-        heap.offer(new int[]{0, 0});
+        PriorityQueue<Pair> heap = new PriorityQueue<>((a, b) -> a.distance - b.distance);
+        heap.offer(new Pair(0, 0));
         Set<Integer> visited = new HashSet<>();
         int cost = 0;
-
+        
         while (visited.size() < points.length) {
-            int[] point = heap.poll();
-            int index = point[0];
-            int distance = point[1];
-            if (visited.contains(index)) {
+            Pair currentPoint = heap.poll();
+            if (visited.contains(currentPoint.index)) {
                 continue;
-            }
-            cost += distance;
-            visited.add(index);
-            
-            for (int nextIndex = 0; nextIndex < points.length; nextIndex++) {
-                int distanceToCurrent = Math.abs(points[nextIndex][0] - points[index][0])
-                                       +Math.abs(points[nextIndex][1] - points[index][1]);
-                heap.offer(new int[]{nextIndex, distanceToCurrent});
+            } 
+
+            visited.add(currentPoint.index);
+            cost += currentPoint.distance;
+            for (int nextPoint = 0; nextPoint < points.length; nextPoint++) {
+                int nextDistance = Math.abs(points[nextPoint][0] - points[currentPoint.index][0]) + Math.abs(points[nextPoint][1] - points[currentPoint.index][1]);
+                heap.offer(new Pair(nextPoint, nextDistance));
             }
         }
         return cost;
+    }
+}
+class Pair {
+    int index, distance;
+    public Pair(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
     }
 }
