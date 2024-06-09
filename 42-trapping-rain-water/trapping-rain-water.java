81a91bf7b46c75bf1622be3a7ac84980c0666f7d
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-09 02:55:11 -0700

    Time: 6 ms (9.82%) | Memory: 44.1 MB (99.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,21 +1,33 @@
-class Solution {
-    public int trap(int[] height) {
-        int left = 0, right = height.length - 1;
-        int result = 0;
-        int leftMax = 0, rightMax = 0;
+class Pair {
+    int index, height;
+    public Pair(int index, int height) {
+        this.index = index;
+        this.height = height;
+    }
+}
 
-        while (left < right) {
-            leftMax = Math.max(leftMax, height[left]);
-            rightMax = Math.max(rightMax, height[right]);
-            
-            if (leftMax < rightMax) {
-                result += (leftMax - height[left]);
-                left++;
-            } else {
-                result += (rightMax - height[right]);
-                right--;
+public class Solution {
+    public int trap(int[] height) {
+        Stack<Pair> stack = new Stack<>();
+        int waterTrapped = 0;
+
+        for (int i = 0; i < height.length; i++) {
+            int currentIndex = i, currentHeight = height[i];
+
+            while (!stack.isEmpty() && stack.peek().height < currentHeight) {
+                Pair top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                Pair left = stack.peek();
+                int distance = currentIndex - left.index - 1;
+                int boundedHeight = Math.min(left.height, currentHeight) - top.height;
+                waterTrapped += distance * boundedHeight;
             }
+
+            stack.push(new Pair(currentIndex, currentHeight));
         }
-        return result;
+
+        return waterTrapped;
     }
 }
