960764557bf21a1a48e503ceb3bb34f7ac840c77
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-09 01:55:16 -0700

    Time: 4 ms (43.76%) | Memory: 42 MB (90.87%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        // TreeMap to store nodes by column index
+        Map<Integer, List<Integer>> result = new TreeMap<>();
+        // Queue for BFS
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0));
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Map<Integer, List<Integer>> currentLevelMap = new HashMap<>();
+            
+            for (int i = 0; i < size; i++) {
+                Pair currentPair = queue.poll();
+                TreeNode currentNode = currentPair.node;
+                int currentCol = currentPair.col;
+                
+                currentLevelMap.putIfAbsent(currentCol, new ArrayList<>());
+                currentLevelMap.get(currentCol).add(currentNode.val);
+                
+                if (currentNode.left != null) {
+                    queue.offer(new Pair(currentNode.left, currentCol - 1));
+                }
+                
+                if (currentNode.right != null) {
+                    queue.offer(new Pair(currentNode.right, currentCol + 1));
+                }
+            }
+            
+            for (int col : currentLevelMap.keySet()) {
+                Collections.sort(currentLevelMap.get(col));
+                result.putIfAbsent(col, new ArrayList<>());
+                result.get(col).addAll(currentLevelMap.get(col));
+            }
+        }
+        
+        return new ArrayList<>(result.values());
+    }
+}
+class Pair {
+    TreeNode node;
+    int col;
+    public Pair(TreeNode node, int col) {
+        this.node = node;
+        this.col = col;
+    }
+}
