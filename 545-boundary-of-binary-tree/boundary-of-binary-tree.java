4950d625e4a977e8a16584b2b5a2a24989f92abc
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-06-09 02:43:34 -0700

    Time: 0 ms (100.00%) | Memory: 44.4 MB (67.66%) - LeetSync

--- 545-boundary-of-binary-tree/boundary-of-binary-tree.java
+++ 545-boundary-of-binary-tree/boundary-of-binary-tree.java
@@ -1,61 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     List<Integer> result = new ArrayList<>();
     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
-        if (root == null) {
-            return result;
-        }
-        if (!isLeaf(root)) {
-            result.add(root.val);
-        }
-        addLeftBoundary(root.left);
-        addLeaves(root);
-        addRightBoundary(root.right);
+        result.add(root.val);
+        dfs(root.left, true, false);
+        dfs(root.right, false, true);
         return result;
     }
-    
-    private void addLeftBoundary(TreeNode node) {
-        while (node != null) {
-            if (!isLeaf(node)) {
-                result.add(node.val);
-            }
-            if (node.left != null) {
-                node = node.left;
-            } else {
-                node = node.right;
-            }
-        }
-    }
-    
-    private void addLeaves(TreeNode node) {
-        if (node == null) {
+    private void dfs(TreeNode root, boolean leftBoundary, boolean rightBoundary) {
+        if (root == null) {
             return;
         }
-        if (isLeaf(node)) {
-            result.add(node.val);
-        } else {
-            addLeaves(node.left);
-            addLeaves(node.right);
+        if (leftBoundary) {
+            result.add(root.val);
+        }
+        if (isLeaf(root) && !leftBoundary) {
+            result.add(root.val);
+        }
+        
+        dfs(root.left, leftBoundary && root.left != null, rightBoundary && root.right == null);
+        dfs(root.right, leftBoundary && root.left == null, rightBoundary && root.right != null);
+
+        if (rightBoundary && !leftBoundary && !isLeaf(root)) {
+            result.add(root.val);
         }
     }
-    
-    private void addRightBoundary(TreeNode node) {
-        Stack<Integer> stack = new Stack<>();
-        while (node != null) {
-            if (!isLeaf(node)) {
-                stack.push(node.val);
-            }
-            if (node.right != null) {
-                node = node.right;
-            } else {
-                node = node.left;
-            }
-        }
-        while (!stack.isEmpty()) {
-            result.add(stack.pop());
-        }
-    }
-    
-    private boolean isLeaf(TreeNode node) {
-        return node.left == null && node.right == null;
+    private boolean isLeaf(TreeNode root) {
+        return root.left == null && root.right == null;
     }
 }
