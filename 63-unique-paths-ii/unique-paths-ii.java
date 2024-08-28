b5780b9da51f3cfac74986d9fb2fe819a6576e73
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-27 22:22:34 -0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (36.54%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,21 +3,23 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         dpTable = new Integer[m][n];
-        return dp(m - 1, n - 1, obstacleGrid);
+        return dp(obstacleGrid, m - 1, n - 1);
     }
-    private int dp(int row, int col, int[][] board) {
-        if (row < 0 || col < 0 || board[row][col] == 1) {
+    private int dp(int[][] obstacleGrid, int row, int col) {
+        if (row < 0 || col < 0 || obstacleGrid[row][col] == 1) {
             return 0;
         }
         if (row == 0 && col == 0) {
             return 1;
         }
+
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
-        int moveUp = dp(row - 1, col, board);
-        int moveLeft = dp(row, col - 1, board);
-        dpTable[row][col] = moveUp + moveLeft;
+
+        int goLeft = dp(obstacleGrid, row, col - 1);
+        int goUp = dp(obstacleGrid, row - 1, col);
+        dpTable[row][col] = goLeft + goUp;
         return dpTable[row][col];
     }
 }
