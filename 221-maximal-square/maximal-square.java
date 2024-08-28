422c27e161147e57e4672ddf4daf1b8b2fd82568
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-27 23:43:32 -0700

    Time: 9 ms (28.24%) | Memory: 56.2 MB (85.54%) - LeetSync

--- 221-maximal-square/maximal-square.java
+++ 221-maximal-square/maximal-square.java
@@ -1,17 +1,18 @@
 class Solution {
     Integer[][] dpTable;
     public int maximalSquare(char[][] matrix) {
-        int m = matrix.length, n = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
         dpTable = new Integer[m][n];
-
-        int maxSquareSide = 0;
+        
+        int maxSide = 0;
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
-                maxSquareSide = Math.max(maxSquareSide, dp(matrix, row, col));
+                maxSide = Math.max(maxSide, dp(matrix, row, col));
             }
         }
-        
-        return maxSquareSide * maxSquareSide;
+ 
+        return maxSide * maxSide;
     }
     private int dp(char[][] matrix, int row, int col) {
         if (row < 0 || col < 0 || matrix[row][col] != '1') {
@@ -20,10 +21,11 @@ class Solution {
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
-        int left = dp(matrix, row, col - 1);
-        int up = dp(matrix, row - 1, col);
-        int leftUp = dp(matrix, row - 1, col - 1);
-        dpTable[row][col] = Math.min(left, Math.min(up, leftUp)) + 1;
+        
+        int moveLeft = dp(matrix, row, col - 1);
+        int moveUp = dp(matrix, row - 1, col);
+        int moveUpLeft = dp(matrix, row - 1, col - 1);
+        dpTable[row][col] = Math.min(moveLeft, Math.min(moveUp, moveUpLeft)) + 1;
         return dpTable[row][col];
     }
 }
