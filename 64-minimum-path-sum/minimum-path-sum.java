7cc9e731b8db79178f8e0dd55f986878124e8768
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-27 22:46:37 -0700

    Time: 2 ms (67.33%) | Memory: 46.9 MB (6.78%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,21 +3,23 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         dpTable = new Integer[m][n];
-        return dp(grid, m - 1, n - 1);
+        return minimalSum(grid, m - 1, n - 1);
     }
-    private int dp(int[][] grid, int row, int col) {
+    private int minimalSum(int[][] grid, int row, int col) {
         if (row < 0 || col < 0) {
             return Integer.MAX_VALUE;
         }
         if (row == 0 && col == 0) {
             return grid[0][0];
         }
+
         if (dpTable[row][col] != null) {
             return dpTable[row][col];
         }
-        int left = dp(grid, row, col - 1);
-        int up = dp(grid, row - 1, col);
-        dpTable[row][col] = Math.min(left, up) + grid[row][col];
+
+        int moveLeft = minimalSum(grid, row, col - 1);
+        int moveUp = minimalSum(grid, row - 1, col);
+        dpTable[row][col] = Math.min(moveLeft, moveUp) + grid[row][col];
         return dpTable[row][col];
     }
 }
