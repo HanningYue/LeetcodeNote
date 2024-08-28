7111d40164942bcd0b9f7a26f5d291dc3b99abc7
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-27 23:00:13 -0700

    Time: 1 ms (89.87%) | Memory: 41.7 MB (51.69%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,10 +1,11 @@
 class Solution {
     Boolean[][] dpTable;
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) {
+        int m = s1.length(), n = s2.length();
+        if (m + n != s3.length()) {
             return false;
         }
-        int m = s1.length(), n = s2.length();
+
         dpTable = new Boolean[m + 1][n + 1];
         return dp(s1, 0, s2, 0, s3, 0);
     }
@@ -15,18 +16,23 @@ class Solution {
         if (dpTable[i][j] != null) {
             return dpTable[i][j];
         }
-        if (i + 1 <= s1.length() && s1.charAt(i) == s3.charAt(k)) {
-            dpTable[i][j] = dp(s1, i + 1, s2, j, s3, k + 1);
+
+        if (j < s2.length() && s3.charAt(k) == s2.charAt(j)) {
+            boolean skipI = dp(s1, i, s2, j + 1, s3, k + 1);
+            dpTable[i][j] = skipI;
+            if (dpTable[i][j]) {
+                return true;
+            }
+        } 
+ 
+        if (i < s1.length() && s3.charAt(k) == s1.charAt(i)) {
+            boolean skipJ = dp(s1, i + 1, s2, j, s3, k + 1);
+            dpTable[i][j] = skipJ;
             if (dpTable[i][j]) {
                 return true;
             }
         }
-        if (j + 1 <= s2.length() && s2.charAt(j) == s3.charAt(k)) {
-            dpTable[i][j] = dp(s1, i, s2, j + 1, s3, k + 1);
-            if (dpTable[i][j]) {
-                return true;
-            }
-        }
+
         dpTable[i][j] = false;
         return false;
     }
