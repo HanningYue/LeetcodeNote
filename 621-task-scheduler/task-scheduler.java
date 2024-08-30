bfbd65f844d54c6eee03d7a4390da7be0f9c9f9f
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-30 14:07:43 -0700

    Time: 21 ms (55.24%) | Memory: 45.9 MB (34.85%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,46 +1,48 @@
 class Pair {
     int frequency;
-    int time;
-    public Pair(int frequency, int time) {
+    int timeStamp;
+    public Pair(int frequency, int timeStamp) {
         this.frequency = frequency;
-        this.time = time;
+        this.timeStamp = timeStamp;
     }
 }
+
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<Integer>() {
+        PriorityQueue<Integer> frequencyHeap = new PriorityQueue<>(new Comparator<Integer>() {
             public int compare(Integer a, Integer b) {
                 return b - a;
             }
-        });        
-
+        });
+        
         int[] frequencyArray = new int[26];
         for (char task : tasks) {
             frequencyArray[task - 'A']++;
         }
+
         for (int freq : frequencyArray) {
             if (freq > 0) {
-                heap.offer(freq);
+                frequencyHeap.offer(freq);
             }
         }
 
-        int result = 0;
         Queue<Pair> queue = new LinkedList<>();
-        while (!heap.isEmpty() || !queue.isEmpty()) {
+        int result = 0;
+        while (!queue.isEmpty() || !frequencyHeap.isEmpty()) {
             result++;
-
-            if (!heap.isEmpty()) {
-                int currentMaxFrequency = heap.poll();
-                currentMaxFrequency--;
-                if (currentMaxFrequency > 0) {
-                    queue.offer(new Pair(currentMaxFrequency, result + n));                   
+            if (!frequencyHeap.isEmpty()) {
+                int maxFreq = frequencyHeap.poll();
+                maxFreq--;
+                if (maxFreq > 0) {
+                    queue.offer(new Pair(maxFreq, result + n));
                 }
             }
 
-            if (!queue.isEmpty() && result == queue.peek().time) {
-                heap.offer(queue.poll().frequency);
+            if (!queue.isEmpty() && result == queue.peek().timeStamp) {
+                frequencyHeap.offer(queue.poll().frequency);
             }
         }
+        
         return result;
     }
 }
