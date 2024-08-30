ece3b0ad810b5f695e5fb373319d00f27976ca80
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-30 15:44:16 -0700

    Time: 1 ms (98.85%) | Memory: 40.8 MB (86.89%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,22 +1,24 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> b - a);
+        PriorityQueue<Integer> heap = new PriorityQueue<>(new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return b - a;
+            }
+        });
+
         for (int stone : stones) {
             heap.offer(stone);
         }
-
         while (heap.size() >= 2) {
-            int stoneOne = heap.poll();
-            int stoneTwo = heap.poll();
-            if (stoneOne == stoneTwo) {
+            int heaviestOne = heap.poll();
+            int heaviestTwo = heap.poll();
+            
+            if (heaviestOne == heaviestTwo) {
                 continue;
             } else {
-                heap.offer(Math.abs(stoneOne - stoneTwo));
+                heap.offer(heaviestOne - heaviestTwo);
             }
         }
-        if (heap.size() == 0) {
-            return 0;
-        }
-        return heap.poll();
+        return heap.size() == 0 ? 0 : heap.poll();
     }
 }
