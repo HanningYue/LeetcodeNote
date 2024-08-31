6f7e55e6770e8e69825bee7e5b2310bec2381b4e
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-30 19:08:04 -0700

    Time: 16 ms (9.61%) | Memory: 46.7 MB (63.59%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,20 +2,21 @@ class Solution {
     Integer[][] dpTable;
     public int change(int amount, int[] coins) {
         dpTable = new Integer[coins.length][amount + 1];
-        return dp(amount, coins, 0);
+        return dp(amount, 0, coins);
     }
-    private int dp(int amount, int[] coins, int index) {
-        if (amount < 0 || index == coins.length) {
+    private int dp(int amount, int index, int[] coins) {
+        if (index == coins.length || amount < 0) {
             return 0;
         }
         if (amount == 0) {
             return 1;
         }
+        
         if (dpTable[index][amount] != null) {
             return dpTable[index][amount];
         }
-        int useCurrent = dp(amount - coins[index], coins, index);
-        int skipCurrent = dp(amount, coins, index + 1);
+        int useCurrent = dp(amount - coins[index], index, coins);
+        int skipCurrent = dp(amount, index + 1, coins);
         dpTable[index][amount] = useCurrent + skipCurrent;
         return dpTable[index][amount];
     }
