b28bc7dfbd97fcdeed4a7801486c2e693f02df06
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-31 11:26:21 -0700

    Time: 6 ms (83.14%) | Memory: 44.3 MB (59.39%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -6,40 +6,46 @@ class State {
         this.stop = stop;
     }
 }
+
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         Map<Integer, List<State>> graph = new HashMap<>();
         for (int i = 0; i < n; i++) {
             graph.put(i, new ArrayList<>());
-        }
+        }        
         for (int[] flight : flights) {
             int from = flight[0], to = flight[1], cost = flight[2];
             graph.get(from).add(new State(to, cost, 0));
         }
 
-        int[] minStopToCurrent = new int[n];
-        Arrays.fill(minStopToCurrent, Integer.MAX_VALUE);
-        minStopToCurrent[src] = 0;
-        
+        int[] minStop = new int[n];
+        Arrays.fill(minStop, Integer.MAX_VALUE);
+        minStop[src] = 0;
+
         k = k + 1;
-        PriorityQueue<State> heap = new PriorityQueue<>((a, b) -> a.cost - b.cost);
+        PriorityQueue<State> heap = new PriorityQueue<>(new Comparator<State>() {
+            public int compare (State a, State b) {
+                return a.cost - b.cost;
+            }
+        });
         heap.offer(new State(src, 0, 0));
-        
+
         while (!heap.isEmpty()) {
             State current = heap.poll();
-            if (minStopToCurrent[current.vertex] < current.stop || current.stop > k) {
+            if (current.stop > minStop[current.vertex] || current.stop > k) {
                 continue;
             }
 
             if (current.vertex == dst) {
                 return current.cost;
             }
-            minStopToCurrent[current.vertex] = current.stop;
+            minStop[current.vertex] = current.stop;
 
             for (State neighbor : graph.get(current.vertex)) {
                 heap.offer(new State(neighbor.vertex, neighbor.cost + current.cost, current.stop + 1));
             }
         }
+
         return -1;
     }
 }
