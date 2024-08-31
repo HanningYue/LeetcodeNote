532b58aabb2d9b489eb5a48c8eee54b8cc1807c2
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-31 14:43:36 -0700

    Time: 6 ms (60.49%) | Memory: 44.5 MB (31.15%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -2,27 +2,29 @@ class Solution {
     Boolean[] dpTable;
     public boolean wordBreak(String s, List<String> wordDict) {
         dpTable = new Boolean[s.length()];
-        Set<String> set = new HashSet<>(wordDict);
-        return dp(s, set, 0);
+        HashSet<String> set = new HashSet<>(wordDict);
+        return dp(s, 0, set);
     }
-    private boolean dp(String s, Set<String> set, int index) {
+    private boolean dp(String s, int index, HashSet<String> set) {
         if (index == s.length()) {
             return true;
         }
         if (dpTable[index] != null) {
             return dpTable[index];
         }
+
         for (int length = 1; length + index <= s.length(); length++) {
-            String current = s.substring(index, index + length);
+            String current = s.substring(index, length + index);
             if (set.contains(current)) {
-                boolean subProblem = dp(s, set, index + length);
+                boolean subProblem = dp(s, index + length, set);
                 if (subProblem) {
                     dpTable[index] = true;
-                    return true;
+                    return dpTable[index];
                 }
             }
         }
         dpTable[index] = false;
-        return false;
+
+        return dpTable[index];
     }
 }
