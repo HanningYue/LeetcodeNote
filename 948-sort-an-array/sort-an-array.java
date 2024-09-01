07209054a13fb95fd556ab5808bcfcc25b12ef39
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-01 11:36:28 -0700

    Time: 25 ms (62.30%) | Memory: 56.1 MB (48.46%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,37 +1,37 @@
 class Solution {
-    int[] tempArr;
+    int[] tempArray;
     public int[] sortArray(int[] nums) {
-        tempArr = new int[nums.length];
-        sort(nums, 0, nums.length - 1);
+        tempArray = new int[nums.length];
+        divideAndConquer(nums, 0, nums.length - 1);
         return nums;
     }
-    private void sort(int[] nums, int left, int right) {
+    private void divideAndConquer(int[] nums, int left, int right) {
         if (left >= right) {
             return;
         }
         int mid = left + (right - left) / 2;
-        sort(nums, left, mid);
-        sort(nums, mid + 1, right);
+        divideAndConquer(nums, left, mid);
+        divideAndConquer(nums, mid + 1, right);
         merge(nums, left, right, mid);
     }
     private void merge(int[] nums, int left, int right, int mid) {
-        int tempP = left, leftP = left, rightP = mid + 1;
-
-        while (leftP <= mid && rightP <= right) {
-            if (nums[leftP] < nums[rightP]) {
-                tempArr[tempP++] = nums[leftP++];
+        int pointerOne = left, pointerTwo = mid + 1;
+        int pointerThree = left;
+        while (pointerOne <= mid && pointerTwo <= right) {
+            if (nums[pointerOne] < nums[pointerTwo]) {
+                tempArray[pointerThree++] = nums[pointerOne++];
             } else {
-                tempArr[tempP++] = nums[rightP++];
+                tempArray[pointerThree++] = nums[pointerTwo++];
             }
         }
-        while (leftP <= mid) {
-            tempArr[tempP++] = nums[leftP++];
-        }   
-        while (rightP <= right) {
-            tempArr[tempP++] = nums[rightP++];
+        while (pointerOne <= mid) {
+            tempArray[pointerThree++] = nums[pointerOne++];
+        }
+        while (pointerTwo <= right) {
+            tempArray[pointerThree++] = nums[pointerTwo++];
         }
         for (int i = left; i <= right; i++) {
-            nums[i] = tempArr[i];           
+            nums[i] = tempArray[i];
         }
     }
 }
