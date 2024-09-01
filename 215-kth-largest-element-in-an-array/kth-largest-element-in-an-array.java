fee3f08f8c6c097abcf09e77aee5a6cc2a0b48b5
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-01 16:38:23 -0700

    Time: 2173 ms (5.11%) | Memory: 61.4 MB (20.42%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,12 +1,40 @@
 class Solution {
+    Random rand = new Random();
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        for (int num : nums) {
-            heap.offer(num);
-            if (heap.size() > k) {
-                heap.poll();
+        return quickSelect(nums, 0, nums.length - 1, nums.length - k);
+    }
+    private int quickSelect(int[] nums, int left, int right, int targetK) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        int pivotIndex = partition(nums, left, right);
+        if (pivotIndex == targetK) {
+            return nums[pivotIndex];
+        } else if (pivotIndex > targetK) {
+            return quickSelect(nums, left, pivotIndex - 1, targetK);
+        } else {
+            return quickSelect(nums, pivotIndex + 1, right, targetK);
+        }
+    }
+    private int partition(int[] nums, int left, int right) {
+        int pivotIndex = left + rand.nextInt(right - left);
+        int pivotValue = nums[pivotIndex];
+        swap(nums, pivotIndex, right);
+
+        int leftP = left;
+        for (int i = left; i < right; i++) {
+            if (nums[i] < pivotValue) {
+                swap(nums, leftP, i);
+                leftP++;
             }
         }
-        return heap.peek();
+        swap(nums, leftP, right);
+        return leftP;
+    }
+    private void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
