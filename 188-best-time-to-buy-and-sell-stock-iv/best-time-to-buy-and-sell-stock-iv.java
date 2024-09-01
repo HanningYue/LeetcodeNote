ec5e08752c0e46aa62d8e6db89035f38dd877d81
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-31 22:45:02 -0700

    Time: 9 ms (26.63%) | Memory: 48.3 MB (9.31%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,19 +1,20 @@
-//k == 0 ?
 class Solution {
-    public int maxProfit(int maxK, int[] prices) {
+    public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][maxK + 1][2];
+
+        int[][][] dp = new int[n][k + 1][2];
         for (int i = 0; i < n; i++) {
-            for (int k = maxK; k >= 1; k--) {
+            for (int maxK = k; maxK >= 1; maxK--) {
                 if (i - 1 == -1) {
-                    dp[i][k][0] = 0;
-                    dp[i][k][1] = 0 - prices[i];
+                    dp[i][maxK][0] = 0;
+                    dp[i][maxK][1] = 0 - prices[i];
                     continue;
                 }
-                dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
-                dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
+
+                dp[i][maxK][0] = Math.max(dp[i - 1][maxK][0], dp[i - 1][maxK][1] + prices[i]);
+                dp[i][maxK][1] = Math.max(dp[i - 1][maxK][1], dp[i - 1][maxK - 1][0] - prices[i]);
             }
         }
-        return dp[n - 1][maxK][0];
+        return dp[n - 1][k][0];
     }
 }
