0dfabe6f0c3047fa1910461d1c454565f0a94e2a
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-08-31 21:20:29 -0700

    Time: 87 ms (32.71%) | Memory: 65.9 MB (32.85%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -2,6 +2,7 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int maxK = 2;
+
         int[][][] dp = new int[n][maxK + 1][2];
         for (int i = 0; i < n; i++) {
             for (int k = maxK; k >= 1; k--) {
@@ -10,6 +11,7 @@ class Solution {
                     dp[i][k][1] = 0 - prices[i];
                     continue;
                 }
+
                 dp[i][k][0] = Math.max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i]);
                 dp[i][k][1] = Math.max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i]);
             }
