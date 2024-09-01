305b0b7fd0431532e7a3e78054b3d9ea41daf01e
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-01 11:22:59 -0700

    Time: 2 ms (63.07%) | Memory: 45.9 MB (55.19%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,37 +1,35 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int[] mergedArray = merge(nums1, nums2);
-        return findMedian(mergedArray);
+        int[] sorted = merge(nums1, nums2);
+        return findMedian(sorted);
     }
-    
     private int[] merge(int[] nums1, int[] nums2) {
-        int n = nums1.length, m = nums2.length;
-        int[] result = new int[n + m];
-
+        int[] result = new int[nums1.length + nums2.length];
+        
+        int m = nums1.length, n = nums2.length;
         int pointerOne = 0, pointerTwo = 0, pointerThree = 0;
-        while (pointerOne < n && pointerTwo < m) {
+        
+        while (pointerOne < m && pointerTwo < n) {
             if (nums1[pointerOne] < nums2[pointerTwo]) {
                 result[pointerThree++] = nums1[pointerOne++];
             } else {
                 result[pointerThree++] = nums2[pointerTwo++];
             }
         }
-        while (pointerOne < n) {
-            result[pointerThree++] = nums1[pointerOne++];
+        while (pointerOne < m) {
+                result[pointerThree++] = nums1[pointerOne++];
         }
-        while (pointerTwo < m) {
-            result[pointerThree++] = nums2[pointerTwo++];
+        while (pointerTwo < n) {
+                result[pointerThree++] = nums2[pointerTwo++];
         }
 
         return result;
     }
-
-    private double findMedian(int[] arr) {
-        int middleIndex = arr.length / 2;
-        if (arr.length % 2 == 0) {
-            return (arr[middleIndex - 1] + arr[middleIndex]) / 2.0;
-        } else {
-            return arr[middleIndex];
+    private double findMedian(int[] nums) {
+        int middleIndex = nums.length / 2;
+        if (nums.length % 2 == 0) {
+            return (nums[middleIndex - 1] + nums[middleIndex]) / 2.0;
         }
+        return nums[middleIndex];
     }
 }
