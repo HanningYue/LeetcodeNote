aad0f8e52ed5831f490cce689c5053b8494f4898
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-01 17:02:15 -0700

    Time: 1 ms (100.00%) | Memory: 57.3 MB (41.50%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -2,15 +2,16 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int i = 0;
         while (i < nums.length) {
-            int naturalIndex = nums[i] - 1;
-            if (nums[i] > 0 && nums[i] < nums.length && nums[i] != nums[naturalIndex]) {
+            int supposeIndex = nums[i] - 1;
+            if (nums[i] < nums.length && nums[i] > 0 && nums[supposeIndex] != nums[i]) {
                 int temp = nums[i];
-                nums[i] = nums[naturalIndex];
-                nums[naturalIndex] = temp;
+                nums[i] = nums[supposeIndex];
+                nums[supposeIndex] = temp;
             } else {
                 i++;
             }
         }
+
         for (int j = 0; j < nums.length; j++) {
             if (nums[j] != j + 1) {
                 return j + 1;
