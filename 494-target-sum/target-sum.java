4212d5b760d3603d19b4950db9f7f02e44faa929
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-02 15:46:27 -0700

    Time: 8 ms (66.59%) | Memory: 44.7 MB (25.62%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -5,27 +5,25 @@ class Solution {
         for (int num : nums) {
             sum += num;
         }
-        if (Math.abs(target) > Math.abs(sum)) {
+        if (Math.abs(sum) < Math.abs(target)) {
             return 0;
         }
 
         dpTable = new Integer[nums.length][sum * 2 + 1];
-        return dp(nums, target, 0, 0, sum);
+        return dp(nums, 0, 0, target, sum);
     }
-    private int dp(int[] nums, int target, int runningSum, int index, int sum) {
+    private int dp(int[] nums, int runningSum, int index, int target, int sum) {
         if (index == nums.length) {
             if (runningSum == target) {
                 return 1;
             }
             return 0;
         }
-
         if (dpTable[index][runningSum + sum] != null) {
             return dpTable[index][runningSum + sum];
         }
-
-        int add = dp(nums, target, runningSum + nums[index], index + 1, sum);
-        int minus = dp(nums, target, runningSum - nums[index], index + 1, sum);
+        int add = dp(nums, runningSum + nums[index], index + 1, target, sum);
+        int minus = dp(nums, runningSum - nums[index], index + 1, target, sum);
         dpTable[index][runningSum + sum] = add + minus;
         return dpTable[index][runningSum + sum];
     }
