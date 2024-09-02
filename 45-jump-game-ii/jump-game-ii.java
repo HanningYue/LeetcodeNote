f1818e45363639e65b3aaea930e78dee121ce846
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-01 17:57:47 -0700

    Time: 2 ms (59.54%) | Memory: 45.1 MB (51.01%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,13 +1,12 @@
 class Solution {
     public int jump(int[] nums) {
-        int maxJumpAtEachStep = 0;
-        int step = 0, furthest = 0;
-
+        int maxJumpAtEachStep = 0, furthest = 0;
+        int step = 0;
         for (int i = 0; i < nums.length - 1; i++) {
             maxJumpAtEachStep = Math.max(maxJumpAtEachStep, nums[i] + i);
-            if (i == furthest) {
-                step++;
+            if (furthest == i) {
                 furthest = maxJumpAtEachStep;
+                step++;
             }
         }
         return step;
