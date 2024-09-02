6fd1533f830ed5059c0b6c908b0155f07e3a85e8
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-02 16:15:11 -0700

    Time: 1 ms (85.70%) | Memory: 41 MB (30.61%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -3,17 +3,17 @@ class Solution {
         if (x == 1) {
             return 1;
         }
-        
+
         int left = 0, right = x / 2;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            long product = (long) mid * mid;
-            if (product > x) {
-                right = mid - 1;
+            long product = (long)mid * mid;
+            if (product == x) {
+                return mid;
             } else if (product < x) {
                 left = mid + 1;
-            } else if (product == x) {
-                return mid;
+            } else if (product > x) {
+                right = mid - 1;
             }
         }
         return right;
