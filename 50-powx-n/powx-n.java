9f7fdfcfa022ac3a617caba350d424b5a2d21acc
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-02 16:53:29 -0700

    Time: 0 ms (100.00%) | Memory: 41.6 MB (90.34%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,24 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        return recursion(x, n);
+        return recursion(x, n);        
     }
-    private double recursion(double x, int n) {
-        double result = 1.0;
-        if (n == 0) {
-            return 1.0;
+    private double recursion(double x, long n) {
+        if (n < 0) {
+            return recursion(1 / x, -n);
+        } else if (n == 0) {
+            return 1;
         } else if (n == 1) {
             return x;
-        } else if (n < 0) {
-            x = 1 / x;
-            n = -n;
         }
-        
-        while (n != 0) {
-            if (n % 2 != 0) {
-                result = result * x;
-            }
-            x = x * x;
-            n = n / 2;
+        double result = recursion(x * x, n / 2);
+        if (n % 2 == 1) {
+            result = result * x;
         }
         return result;
     }
