c993a2c2969ef45fcaa7a42d016787a42a3bbfa1
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-02 17:58:36 -0700

    Time: 19 ms (84.02%) | Memory: 44.5 MB (67.97%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.java
+++ 149-max-points-on-a-line/max-points-on-a-line.java
@@ -1,30 +1,28 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        int globalMax = 0;
-
+        int result = 0;
         for (int i = 0; i < points.length; i++) {
-            Map<Double, Integer> slopeAndFreq = new HashMap<>();
+            Map<Double, Integer> slopeFreq = new HashMap<>();
+            int localMaxPoint = 0;
             Double slope;
-            int currentMax = 0;
 
             for (int j = i + 1; j < points.length; j++) {
                 int diffX = points[j][0] - points[i][0];
                 int diffY = points[j][1] - points[i][1];
 
-                if (diffX == 0) {
-                    slope = Double.MAX_VALUE;
-                } else if (diffY == 0) {
+                if (diffY == 0) {
                     slope = 0.0;
+                } else if (diffX == 0) {
+                    slope = Double.MAX_VALUE;
                 } else {
                     slope = (double) diffY / diffX;
                 }
 
-                slopeAndFreq.put(slope, slopeAndFreq.getOrDefault(slope, 0) + 1);
-                currentMax = Math.max(currentMax, slopeAndFreq.get(slope));
+                slopeFreq.put(slope, slopeFreq.getOrDefault(slope, 0) + 1);
+                localMaxPoint = Math.max(localMaxPoint, slopeFreq.get(slope));
             }
-
-            globalMax = Math.max(globalMax, currentMax + 1);
+            result = Math.max(result, localMaxPoint + 1);
         }
-        return globalMax;
+        return result;
     }
 }
