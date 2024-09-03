256e3ef6d4f779d3bf88dd9b58f1528897a02f55
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-02 18:11:11 -0700

    Time: 1 ms (91.12%) | Memory: 44.5 MB (96.37%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -26,13 +26,9 @@ class Solution {
             List<Integer> currentLevel = new ArrayList<>();
             for (int i = 0; i < size; i++) {
                 TreeNode current = queue.poll();
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
                 currentLevel.add(current.val);
-                if (current.left != null) {
-                    queue.offer(current.left);
-                }
-                if (current.right != null) {
-                    queue.offer(current.right);
-                }
             }
             result.add(currentLevel);
         }
