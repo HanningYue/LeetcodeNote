d0f335ce84546de031df74f0d26ffdb3dad76567
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-03 10:13:05 -0700

    Time: 1 ms (99.83%) | Memory: 46.4 MB (11.29%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int a = 0;
-        for (int num : nums) {
-            a = a ^ num;
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
         }
-        return a;
+        return result;
     }
 }
