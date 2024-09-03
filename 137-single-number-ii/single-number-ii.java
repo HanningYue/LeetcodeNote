c5c76c6c158290f156cd8ac3112b123e5cdc34e3
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-03 10:42:58 -0700

    Time: 2 ms (78.17%) | Memory: 45.2 MB (69.34%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,15 +1,14 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int lastDigit = 0;
+        int result = 0;
         for (int i = 0; i < 32; i++) {
             int lastDigitSum = 0;
             for (int num : nums) {
                 lastDigitSum += (num >> i) & 1;
             }
-
-            lastDigitSum = lastDigitSum % 3;
-            lastDigit = lastDigit | (lastDigitSum << i);
+            lastDigitSum %= 3;
+            result = result | (lastDigitSum << i);
         }
-        return lastDigit;
+        return result;
     }
 }
