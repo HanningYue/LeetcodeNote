46f1f9dc5970e1a6d800fa03c6742a77e22c4575
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-03 11:41:14 -0700

    Time: 0 ms (100.00%) | Memory: 43 MB (91.83%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -15,20 +15,17 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return BST(root, null, null);
+        return recursion(root, null, null);
     }
-    private boolean BST(TreeNode root, TreeNode leftMax, TreeNode rightMin) {
+    private boolean recursion(TreeNode root, TreeNode leftMax, TreeNode rightMin) {
         if (root == null) {
             return true;
         }
-
-        boolean leftTrue = BST(root.left, leftMax, root);
-        boolean rightTrue = BST(root.right, root, rightMin);
-        if (leftMax != null && root.val <= leftMax.val
-        || rightMin != null && root.val >= rightMin.val) {
+        boolean leftTrue = recursion(root.left, leftMax, root);
+        boolean rightTrue = recursion(root.right, root, rightMin);
+        if (leftMax != null && root.val <= leftMax.val || rightMin != null && root.val >= rightMin.val) {
             return false;
         }
-
         return leftTrue && rightTrue;
     }
 }
