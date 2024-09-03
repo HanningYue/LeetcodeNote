5a9446763435e41fa947fe6af3bd71d24074c6fb
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-03 12:41:07 -0700

    Time: 5 ms (61.04%) | Memory: 45.2 MB (94.70%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.java
+++ 271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -12,15 +12,17 @@ public class Codec {
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List<String> result = new ArrayList<>();
+
         int index = 0;
         while (index < s.length()) {
             int slashIndex = s.indexOf("/", index);
-            int length = Integer.valueOf(s.substring(index, slashIndex));
-
-            String str = s.substring(slashIndex + 1, slashIndex + length + 1);
+            int length = Integer.parseInt(s.substring(index, slashIndex));
+            String originalString = s.substring(slashIndex + 1, slashIndex + 1 + length);
             index = slashIndex + length + 1;
-            result.add(str);
+
+            result.add(originalString);
         }
+
         return result;
     }
 }
