bffd9cc109ea820fe672f4871c534ed267139330
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-04 16:23:43 -0700

    Time: 30 ms (77.83%) | Memory: 58 MB (83.95%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,37 +1,34 @@
 class Pair {
-    int nums1, nums2, nums2Idx;
-    public Pair(int nums1, int nums2, int nums2Idx) {
-        this.nums1 = nums1;
-        this.nums2 = nums2;
-        this.nums2Idx = nums2Idx;
+    int indexOne, indexTwo, sum;
+    public Pair(int indexOne, int indexTwo, int sum) {
+        this.indexOne = indexOne;
+        this.indexTwo = indexTwo;
+        this.sum = sum;
     }
 }
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        PriorityQueue<Pair> heap = new PriorityQueue<>(new Comparator<Pair>() {
-            public int compare(Pair a, Pair b) {
-                int sumA = a.nums1 + a.nums2;
-                int sumB = b.nums1 + b.nums2;
-                return sumA - sumB;
-            }
-        });
-        for (int i = 0; i < nums1.length; i++) {
-            heap.offer(new Pair(nums1[i], nums2[0], 0));
-        }
-        
-        int count = 0;
-        while (!heap.isEmpty() && count < k) {
-            Pair currentSmallest = heap.poll();
-            int numOne = currentSmallest.nums1;
-            int numTwo = currentSmallest.nums2;
-            int numTwoIndex = currentSmallest.nums2Idx;
-            result.add(Arrays.asList(numOne, numTwo));
+        PriorityQueue<Pair> heap = new PriorityQueue<>((a, b) -> a.sum - b.sum);
 
-            if (numTwoIndex + 1 < nums2.length) {
-                heap.offer(new Pair(numOne, nums2[numTwoIndex + 1], numTwoIndex + 1));
+        for (int i = 0; i < nums1.length; i++) {
+            heap.offer(new Pair(i, 0, nums1[i] + nums2[0]));
+        }
+        while (!heap.isEmpty() && k > 0) {
+            List<Integer> temp = new ArrayList<>();
+            Pair currentPair = heap.poll();
+            int currentIndexOne = currentPair.indexOne;
+            int currentIndexTwo = currentPair.indexTwo;
+
+            if (currentIndexTwo + 1 < nums2.length) {
+                heap.offer(new Pair(currentIndexOne, currentIndexTwo + 1, 
+                nums1[currentIndexOne] + nums2[currentIndexTwo + 1]));
             }
-            count++;
+
+            temp.add(nums1[currentIndexOne]);
+            temp.add(nums2[currentIndexTwo]);
+            result.add(temp);
+            k--;
         }
         return result;
     }
