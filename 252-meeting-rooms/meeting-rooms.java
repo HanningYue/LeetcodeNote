ee76f96c8de19ea995a6874bb67ed01e69133f26
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-04 18:33:19 -0700

    Time: 6 ms (74.58%) | Memory: 46.2 MB (18.19%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.java
+++ 252-meeting-rooms/meeting-rooms.java
@@ -3,15 +3,16 @@ class Solution {
         if (intervals.length == 0) {
             return true;
         }
+        
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         int previousEnd = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
-            int currentStart = intervals[i][0];
-            if (currentStart < previousEnd) {
+            int[] currentMeeting = intervals[i];
+            if (currentMeeting[0] < previousEnd) {
                 return false;
+            } else if (currentMeeting[0] >= previousEnd) {
+                previousEnd = currentMeeting[1];
             }
-            int currentEnd = intervals[i][1];
-            previousEnd = currentEnd;
         }
         return true;
     }
