2267667279ca3743e3ea1b89fd8c8843c0174d64
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-04 18:40:26 -0700

    Time: 142 ms (15.40%) | Memory: 61.9 MB (50.84%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,30 +1,20 @@
-class Pair {
-    int cap;
-    int profit;
-    public Pair (int cap, int profit) {
-        this.cap = cap;
-        this.profit = profit;
-    }
-}
-
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        PriorityQueue<Pair> descendingProfit = new PriorityQueue<>((a, b) -> b.profit - a.profit);
-        PriorityQueue<Pair> aescendingCapital = new PriorityQueue<>((a, b) -> a.cap - b.cap);
-        
+        PriorityQueue<int[]> ascendingCapital = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        PriorityQueue<int[]> descendingProfit = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+
         for (int i = 0; i < capital.length; i++) {
-            aescendingCapital.offer(new Pair(capital[i], profits[i]));
+            ascendingCapital.offer(new int[]{capital[i], profits[i]});
         }
 
         for (int i = 0; i < k; i++) {
-            while (!aescendingCapital.isEmpty() && aescendingCapital.peek().cap <= w) {
-                descendingProfit.offer(aescendingCapital.poll());
+            while (!ascendingCapital.isEmpty() && ascendingCapital.peek()[0] <= w) {
+                descendingProfit.offer(ascendingCapital.poll());
             }
             if (descendingProfit.isEmpty()) {
                 return w;
             }
-            Pair currentMaxProfit = descendingProfit.poll();
-            w += currentMaxProfit.profit;
+            w += descendingProfit.poll()[1];
         }
         return w;
     }
