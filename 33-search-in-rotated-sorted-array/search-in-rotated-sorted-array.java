171a8b1104ce5d2174645bc3b2dd86e4746910cc
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-06 12:40:20 -0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (94.30%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -8,19 +8,21 @@ class Solution {
             }
 
             if (nums[mid] >= nums[left]) {
-                if (target >= nums[left] && target < nums[mid]) {
+                if (target < nums[mid] && target >= nums[left]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else if (nums[mid] <= nums[right]) {
+            } 
+            
+            if (nums[mid] <= nums[right]) {
                 if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
                 }
             }
-        }
+        }        
         return -1;
     }
 }
