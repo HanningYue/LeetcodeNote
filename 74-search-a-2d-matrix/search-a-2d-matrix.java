2a2bd22e992680f8b2049083fcc65c7a3248a833
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-06 17:32:43 -0700

    Time: 0 ms (100.00%) | Memory: 41.7 MB (97.43%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,24 +1,17 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
-
-        int left = 0, right = m * n - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int value = helper(matrix, mid);
-            if (value == target) {
+        int row = 0, col = n - 1;
+        while (row < m && col >= 0) {
+            int current = matrix[row][col];
+            if (current < target) {
+                row++;
+            } else if (current > target) {
+                col--;
+            } else if (current == target) {
                 return true;
-            } else if (value < target) {
-                left = mid + 1;
-            } else if (value > target) {
-                right = mid - 1;
             }
         }
         return false;
-    }
-    private int helper(int[][] matrix, int index) {
-        int row = index / matrix[0].length;
-        int col = index % matrix[0].length;
-        return matrix[row][col];
     }
 }
