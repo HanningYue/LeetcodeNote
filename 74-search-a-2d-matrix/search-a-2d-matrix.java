8024eb5a331708af44c4c265005d81000a250ac5
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-06 17:39:17 -0700

    Time: 0 ms (100.00%) | Memory: 41.9 MB (83.19%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,17 +1,25 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
-        int row = 0, col = n - 1;
-        while (row < m && col >= 0) {
-            int current = matrix[row][col];
-            if (current < target) {
-                row++;
-            } else if (current > target) {
-                col--;
-            } else if (current == target) {
+        int totalIndex = m * n - 1;
+        int left = 0, right = totalIndex;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int twoDIndex = oneDtoTwo(matrix, mid);
+            if (twoDIndex == target) {
                 return true;
+            } else if (twoDIndex < target) {
+                left = mid + 1;
+            } else if (twoDIndex > target) {
+                right = mid - 1;
             }
         }
         return false;
+    }
+    private int oneDtoTwo(int[][] matrix, int pivot) {
+        int col = matrix[0].length;
+        int rowIndex = pivot / col;
+        int colIndex = pivot % col;
+        return matrix[rowIndex][colIndex];
     }
 }
