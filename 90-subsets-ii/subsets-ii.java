c0c9e2161bace0008935fa9bf25166da8852e7a1
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-08 19:10:39 -0700

    Time: 1 ms (99.93%) | Memory: 43.2 MB (81.93%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,27 +1,20 @@
-/**
-              []
-    1         2       2
-2       2   1   2   1   2
-2       2   2   1   2   1
-*/
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTrack(nums, result, list, 0);
+        backTrack(result, list, nums, 0);
         return result;
     }
-    private void backTrack(int[] nums, List<List<Integer>> result, List<Integer> list, int index) {
+    private void backTrack(List<List<Integer>> result, List<Integer> list, int[] nums, int index) {
         result.add(new ArrayList<>(list));
-
         for (int i = index; i < nums.length; i++) {
-            if (i > index && nums[i] == nums[i - 1]) {
+            if (i > index && nums[i - 1] == nums[i]) {
                 continue;
             }
             list.add(nums[i]);
-            backTrack(nums, result, list, i + 1);
+            backTrack(result, list, nums, i + 1);
             list.remove(list.size() - 1);
-        }
+        }        
     }
 }
