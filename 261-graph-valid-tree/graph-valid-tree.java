bff9d85bb10d3bbef89d4314cef9f340aad2d2f5
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-08 17:16:06 -0700

    Time: 2 ms (61.45%) | Memory: 44.9 MB (28.66%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.java
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -3,7 +3,6 @@ class Solution {
         if (edges.length != n - 1) {
             return false;
         }
-        
         Map<Integer, Set<Integer>> graph = new HashMap<>();
         for (int i = 0; i < n; i++) {
             graph.put(i, new HashSet<>());
@@ -13,19 +12,17 @@ class Solution {
             graph.get(from).add(to);
             graph.get(to).add(from);
         }
-
         Set<Integer> visited = new HashSet<>();
-        dfs(0, graph, visited);
+        dfs(0, visited, graph);
         return visited.size() == n;
     }
-
-    private void dfs(int vertex, Map<Integer, Set<Integer>> graph, Set<Integer> visited) {
+    private void dfs(int vertex, Set<Integer> visited, Map<Integer, Set<Integer>> graph) {
         if (visited.contains(vertex)) {
             return;
         }
         visited.add(vertex);
         for (int neighbor : graph.get(vertex)) {
-            dfs(neighbor, graph, visited);
+            dfs(neighbor, visited, graph);
         }
     }
 }
