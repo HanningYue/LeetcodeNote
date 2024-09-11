a11a75e6f4c5c3d96da9552e4c01d2a9f91886a5
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-11 15:56:53 -0700

    Time: 20 ms (58.58%) | Memory: 45 MB (56.29%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -2,18 +2,18 @@ class Solution {
     public String longestPalindrome(String s) {
         String result = "";
         for (int i = 0; i < s.length(); i++) {
-            String even = substring(s, i - 1, i);
-            String odd = substring(s, i, i);
-            result = result.length() > even.length() ? result : even;
-            result = result.length() > odd.length() ? result : odd;
+            String evenCase = recursion(s, i - 1, i);
+            String oddCase = recursion(s, i, i);
+            result = evenCase.length() > result.length() ? evenCase : result;
+            result = oddCase.length() > result.length() ? oddCase : result;
         }
         return result;
     }
-    private String substring(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    private String recursion(String s, int leftP, int rightP) {
+        while (leftP >= 0 && rightP < s.length() && s.charAt(leftP) == s.charAt(rightP)) {
+            leftP--;
+            rightP++;
         }
-        return s.substring(left + 1, right);
+        return s.substring(leftP + 1, rightP);
     }
 }
