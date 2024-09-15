7a2083c6bfd70186a059952deefbd76a3c5b5615
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-14 22:23:02 -0700

    Time: 22 ms (84.16%) | Memory: 47.9 MB (95.72%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,24 +1,26 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> heap = new PriorityQueue<>(new Comparator<int[]>() {
+        PriorityQueue<int[]> heap = new PriorityQueue<>((new Comparator<int[]>() {
             public int compare(int[] a, int[] b) {
-                int distanceA = a[0] * a[0] + a[1] * a[1];
-                int distanceB = b[0] * b[0] + b[1] * b[1];
-                return distanceB - distanceA;
+                return distance(a) - distance(b);
             }
-        });
+        }));
+
+
         for (int[] point : points) {
             heap.offer(point);
-            if (heap.size() > k) {
-                heap.poll();
-            }
         }
-        
+
         int[][] result = new int[k][2];
-        int index = 0;
-        while (!heap.isEmpty()) {
-            result[index++] = heap.poll();
+        int i = 0;
+        while (!heap.isEmpty() && i < k) {
+            result[i++] = heap.poll();
         }
         return result;
+    }
+    private int distance(int[] point) {
+        int x = point[0] * point[0];
+        int y = point[1] * point[1];
+        return x + y;
     }
 }
