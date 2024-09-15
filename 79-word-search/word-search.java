cb66e01d66242f38f33a54aca6c35457bdfe0203
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-14 20:01:28 -0700

    Time: 204 ms (23.52%) | Memory: 41.1 MB (95.88%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,32 +1,31 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length, n = board[0].length;
-        
-        boolean[][] visited = new boolean[m][n];
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (board[row][col] == word.charAt(0)) {
-                    if (dfs(board, row, col, word, 0, visited)) {
-                        return true;
-                    }
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != word.charAt(0)) {
+                    continue;
                 }
+                if (backTrack(board, word, i, j, 0, visited)) {
+                    return true;
+                }                
             }
         }
         return false;
     }
-    
+
     int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private boolean dfs(char[][] board, int row, int col, String word, int index, boolean[][] visited) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length 
-        || visited[row][col] || word.charAt(index) != board[row][col]) {
-            return false;
-        }
-        if (index == word.length() - 1) {
+    private boolean backTrack(char[][] board, String word, int row, int col, int index, boolean[][] visited) {
+        if (index == word.length()) {
             return true;
+        }
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length 
+        || board[row][col] != word.charAt(index) || visited[row][col]) {
+            return false;
         }
         visited[row][col] = true;
         for (int[] dir : directions) {
-            if (dfs(board, row + dir[0], col + dir[1], word, index + 1, visited)) {
+            if (backTrack(board, word, row + dir[0], col + dir[1], index + 1, visited)) {
                 return true;
             }
         }
