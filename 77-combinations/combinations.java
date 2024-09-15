abc1d9272ea05b5bb15451f7424246672983573f
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-14 19:54:16 -0700

    Time: 18 ms (79.21%) | Memory: 94.3 MB (85.27%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -2,17 +2,17 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTrack(result, list, 1, k, n);
+        backTrack(n, k, list, result, 1);
         return result;
     }
-    private void backTrack(List<List<Integer>> result, List<Integer> list, int index, int k, int n) {
+    private void backTrack(int n, int k, List<Integer> list, List<List<Integer>> result, int index) {
         if (list.size() == k) {
             result.add(new ArrayList<>(list));
             return;
         }
         for (int i = index; i <= n; i++) {
             list.add(i);
-            backTrack(result, list, i + 1, k, n);
+            backTrack(n, k, list, result, i + 1);
             list.remove(list.size() - 1);
         }
     }
