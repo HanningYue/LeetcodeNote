2fd83ec8459720f83a9e4ffb7b4749f6b06363ae
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-15 23:48:46 -0700

    Time: 8 ms (50.09%) | Memory: 42.1 MB (99.63%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,11 +1,31 @@
 class Solution {
     public String reverseWords(String s) {
-        StringBuilder sb = build(s);
-        reverseSB(sb, 0, sb.length() - 1);
+        StringBuilder sb = buildString(s);
+        reverseStringBuilder(sb, 0, sb.length() - 1);
         reverseWord(sb);
         return sb.toString();
     }
-    private StringBuilder build(String s) {
+    private void reverseWord(StringBuilder sb) {
+        int wordStart = 0, wordEnd = 0;
+        while (wordEnd < sb.length()) {
+            while (wordEnd < sb.length() && sb.charAt(wordEnd) != ' ') {
+                wordEnd++;
+            }
+            reverseStringBuilder(sb, wordStart, wordEnd - 1);
+            wordStart = wordEnd + 1;
+            wordEnd++;
+        }
+    }
+    private void reverseStringBuilder(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+            left++;
+            right--;
+        }
+    }
+    private StringBuilder buildString(String s) {
         int left = 0, right = s.length() - 1;
         while (left <= right && s.charAt(left) == ' ') {
             left++;
@@ -25,25 +45,5 @@ class Solution {
             left++;
         }
         return sb;
-    }
-    private void reverseSB(StringBuilder sb, int left, int right) {
-        while (left < right) {
-            char temp = sb.charAt(left);
-            sb.setCharAt(left, sb.charAt(right));
-            sb.setCharAt(right, temp);
-            left++;
-            right--;
-        }
-    }
-    private void reverseWord(StringBuilder sb) {
-        int wordStart = 0, wordEnd = 0;
-        while (wordEnd < sb.length()) {
-            while (wordEnd < sb.length() && sb.charAt(wordEnd) != ' ') {
-                wordEnd++;
-            }
-            reverseSB(sb, wordStart, wordEnd - 1);
-            wordStart = wordEnd + 1;
-            wordEnd++;
-        }
     }
 }
