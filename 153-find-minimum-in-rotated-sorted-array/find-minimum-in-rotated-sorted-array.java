049c9fbb8b9fc69f2495340c646bf254f59066c4
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-16 00:31:44 -0700

    Time: 0 ms (100.00%) | Memory: 41.9 MB (44.68%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -10,11 +10,12 @@ class Solution {
         int left = 0, right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] > nums[mid + 1]) {
+            if (nums[0] < nums[right]);
+            if (nums[mid + 1] < nums[mid]) {
                 return nums[mid + 1];
             }
 
-            if (nums[mid] < nums[mid - 1]) {
+            if (nums[mid - 1] > nums[mid]) {
                 return nums[mid];
             }
 
