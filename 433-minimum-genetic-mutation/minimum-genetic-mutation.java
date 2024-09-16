a0c1fecfddce67bdb2dddc54ce27c4448d4a05d0
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-16 15:49:05 -0700

    Time: 1 ms (80.45%) | Memory: 41.1 MB (83.78%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.java
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.java
@@ -1,12 +1,15 @@
 class Solution {
     public int minMutation(String startGene, String endGene, String[] bank) {
-        Set<String> set = new HashSet<>(Arrays.asList(bank));
+        char[] mutation = new char[]{'A', 'C', 'G', 'T'};
+        Set<String> set = new HashSet<>();
+        for (String gene : bank) {
+            set.add(gene);
+        }
         if (!set.contains(endGene)) {
             return -1;
         }
 
-        char[] mutation = new char[]{'A', 'C', 'T', 'G'};
-        Queue<String> queue = new LinkedList<>();
+        Queue<String> queue = new ArrayDeque<>();
         queue.offer(startGene);
         int step = 0;
         while (!queue.isEmpty()) {
@@ -18,16 +21,16 @@ class Solution {
                 }
 
                 StringBuilder sb = new StringBuilder(current);
-                for (int j = 0; j < current.length(); j++) {
-                    char original = current.charAt(j);
+                for (int j = 0; j < sb.length(); j++) {
+                    char original = sb.charAt(j);
                     for (char c : mutation) {
                         sb.setCharAt(j, c);
                         if (set.contains(sb.toString())) {
-                            set.remove(sb.toString());
                             queue.offer(sb.toString());
+                            set.remove(sb.toString());
                         }
+                        sb.setCharAt(j, original);
                     }
-                    sb.setCharAt(j, original);
                 }
             }
             step++;
