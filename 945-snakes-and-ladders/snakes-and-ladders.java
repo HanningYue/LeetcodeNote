d27e5fbd70628457d39d77bc79a530410b3905ae
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-16 16:17:45 -0700

    Time: 6 ms (73.15%) | Memory: 45 MB (24.21%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.java
+++ 945-snakes-and-ladders/snakes-and-ladders.java
@@ -1,14 +1,13 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-
         boolean[][] visited = new boolean[n][n];
         visited[n - 1][0] = true;
 
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.offer(1);        
         int step = 0;
-        while(!queue.isEmpty()) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(1);
+        while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 int current = queue.poll();
@@ -17,33 +16,34 @@ class Solution {
                 }
 
                 for (int j = 1; j <= 6; j++) {
-                    int nextStep = current + j;
-                    if (nextStep > n * n) {
+                    int next = current + j;
+                    if (next > n * n) {
                         break;
                     }
 
-                    int[] nextCell = oneDimension(nextStep, n);
-                    if (visited[nextCell[0]][nextCell[1]]) {
+                    int[] nextCell = oneDimension(next, n);
+                    int nextRow = nextCell[0], nextCol = nextCell[1];
+                    if (visited[nextRow][nextCol]) {
                         continue;
                     }
 
-                    visited[nextCell[0]][nextCell[1]] = true;
-                    if (board[nextCell[0]][nextCell[1]] != -1) {
-                        nextStep = board[nextCell[0]][nextCell[1]];
+                    if (board[nextRow][nextCol] != -1) {
+                        next = board[nextRow][nextCol];
                     }
-                    queue.offer(nextStep);
+                    visited[nextRow][nextCol] = true;
+                    queue.offer(next);
                 }
             }
             step++;
         }
         return -1;
     }
-    private int[] oneDimension(int index, int colLength) {
-        int row = (index - 1) / colLength;
-        int col = (index - 1) % colLength;
+    private int[] oneDimension(int index, int n) {
+        int row = (index - 1) / n;
+        int col = (index - 1) % n;
 
-        int resultRow = colLength - row - 1;
-        int resultCol = row % 2 == 0 ? col : colLength - col - 1;
+        int resultRow = n - row - 1;
+        int resultCol = row % 2 == 0 ? col : n - col - 1;
         return new int[]{resultRow, resultCol};
     }
 }
