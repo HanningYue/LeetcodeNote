4a18d1c17a25b4923d04cda44ae32935fca250a4
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-16 23:47:34 -0700

    Time: 16 ms (25.39%) | Memory: 44.1 MB (85.51%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.java
+++ 794-swim-in-rising-water/swim-in-rising-water.java
@@ -1,7 +1,5 @@
 class State {
-    int row;
-    int col;
-    int maxHeight;
+    int row, col, maxHeight;
     public State(int row, int col, int maxHeight) {
         this.row = row;
         this.col = col;
@@ -11,13 +9,10 @@ class State {
 class Solution {
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        boolean[][] visited = new boolean[n][n];
 
-        PriorityQueue<State> heap = new PriorityQueue<>(new Comparator<State>() {
-            public int compare(State a, State b) {
-                return a.maxHeight - b.maxHeight;
-            }
-        });
+        boolean[][] visited = new boolean[n][n];
+        visited[0][0] = true;
+        PriorityQueue<State> heap = new PriorityQueue<>((a, b) -> a.maxHeight - b.maxHeight);
         heap.offer(new State(0, 0, grid[0][0]));
 
         int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
@@ -25,11 +20,11 @@ class Solution {
             int size = heap.size();
             for (int i = 0; i < size; i++) {
                 State current = heap.poll();
+                visited[current.row][current.col] = true;
 
                 if (current.row == n - 1 && current.col == n - 1) {
                     return current.maxHeight;
                 }
-                visited[current.row][current.col] = true;
 
                 for (int[] dir : directions) {
                     int newRow = dir[0] + current.row;
@@ -40,8 +35,8 @@ class Solution {
                     if (visited[newRow][newCol]) {
                         continue;
                     }
-                    int newHeight = grid[newRow][newCol];
-                    heap.offer(new State(newRow, newCol, Math.max(current.maxHeight, newHeight)));
+
+                    heap.offer(new State(newRow, newCol, Math.max(current.maxHeight, grid[newRow][newCol])));
                 }
             }
         }
