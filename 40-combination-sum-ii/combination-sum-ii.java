d90b11b40814c334fef8459bb72bd76b779fade7
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-18 19:15:58 -0700

    Time: 4 ms (41.93%) | Memory: 42.8 MB (95.98%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -3,26 +3,24 @@ class Solution {
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
-        backTrack(result, list, candidates, target, 0, 0);
+        backTrack(candidates, result, list, target, 0);
         return result;
     }
-    private void backTrack(List<List<Integer>> result, List<Integer> list, int[] candidates, int target, int index, 
-    int runningSum) {
-        if (runningSum == target) {
+    private void backTrack(int[] candidates, List<List<Integer>> result, List<Integer> list, int target,
+    int index) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
             result.add(new ArrayList<>(list));
             return;
         }
-        if (runningSum > target) {
-            return;
-        }
         for (int i = index; i < candidates.length; i++) {
-            if (i > index && candidates[i - 1] == candidates[i]) {
+            if (i > index && candidates[i] == candidates[i - 1]) {
                 continue;
             }
-            runningSum += candidates[i];
             list.add(candidates[i]);
-            backTrack(result, list, candidates, target, i + 1, runningSum);
-            runningSum -= candidates[i];
+            backTrack(candidates, result, list, target - candidates[i], i + 1);
             list.remove(list.size() - 1);
         }
     }
