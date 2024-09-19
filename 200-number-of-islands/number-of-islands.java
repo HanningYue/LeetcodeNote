65e4e3bc200909ca326b3589d859275f5ab1180f
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-18 17:43:53 -0700

    Time: 4 ms (48.59%) | Memory: 49.1 MB (85.22%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,35 +2,29 @@ class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length, n = grid[0].length;
         boolean[][] visited = new boolean[m][n];
-        int count = 0;
-        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
+        int result = 0;
         for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {        
+            for (int col = 0; col < n; col++) {
                 if (grid[row][col] == '1' && !visited[row][col]) {
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[]{row, col});
-                    visited[row][col] = true;
-                    
-                    while (!queue.isEmpty()) {
-                        int[] currentOne = queue.poll();
-                        int currentRow = currentOne[0], currentCol = currentOne[1];
-
-                        for (int[] dir : directions) {
-                            int nextRow = currentRow + dir[0], nextCol = currentCol + dir[1];
-                            if (nextRow < 0 || nextRow >= m || nextCol < 0 || nextCol >= n 
-                            || visited[nextRow][nextCol] || grid[nextRow][nextCol] != '1') {
-                                continue;
-                            }
-
-                            queue.offer(new int[]{nextRow, nextCol});
-                            visited[nextRow][nextCol] = true;
-                        }
-                    }
-                    count++;
+                    result++;
+                    dfs(row, col, grid, visited);
                 }
             }
         }
-        return count;
+        return result;
+    }
+
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    private void dfs(int row, int col, char[][] grid, boolean[][] visited) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+            return;
+        }
+        if (visited[row][col] || grid[row][col] != '1') {
+            return;
+        }
+        visited[row][col] = true;
+        for (int[] dir : directions) {
+            dfs(row + dir[0], col + dir[1], grid, visited);
+        }
     }
 }
