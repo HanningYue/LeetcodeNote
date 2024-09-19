3afbb8b1172ed69b2267fb0019c23677516e3c4c
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-18 18:10:43 -0700

    Time: 2 ms (88.32%) | Memory: 45 MB (93.30%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -7,27 +7,29 @@ class Solution {
             dfs(board, row, 0, visited);
             dfs(board, row, n - 1, visited);
         }
-
         for (int col = 0; col < n; col++) {
             dfs(board, 0, col, visited);
             dfs(board, m - 1, col, visited);
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'O') {
-                    board[i][j] = 'X';
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == 'O') {
+                    board[row][col] = 'X';
                 }
-                if (board[i][j] == '#') {
-                    board[i][j] = 'O';
+                if (board[row][col] == '#') {
+                    board[row][col] = 'O';
                 }
             }
         }
     }
-    
+
     int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     private void dfs(char[][] board, int row, int col, boolean[][] visited) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || visited[row][col]) {
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
+            return;
+        }
+        if (visited[row][col]) {
             return;
         }
 
@@ -39,7 +41,7 @@ class Solution {
 
         visited[row][col] = true;
         for (int[] dir : directions) {
-            dfs(board, dir[0] + row, dir[1] + col, visited);
+            dfs(board, row + dir[0], col + dir[1], visited);
         }
     }
 }
