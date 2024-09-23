13b338170801987a757b1b7c88b455104fdf1b47
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-22 19:15:50 -0700

    Time: 0 ms (100.00%) | Memory: 41.2 MB (81.26%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,33 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
-        int m = matrix.length, n = matrix[0].length;
-        int totalElements = m * n;
+        int m = matrix.length;
+        int n = matrix[0].length;
         
         int topRow = 0, bottomRow = m - 1;
         int leftCol = 0, rightCol = n - 1;
-        while (result.size() < totalElements) {
+        while (result.size() < m * n) {
             if (topRow <= bottomRow) {
-                for (int i = leftCol; i <= rightCol; i++) {
-                    result.add(matrix[topRow][i]);
+                for (int col = leftCol; col <= rightCol; col++) {
+                    result.add(matrix[topRow][col]);
                 }
                 topRow++;
             }
             if (leftCol <= rightCol) {
-                for (int i = topRow; i <= bottomRow; i++) {
-                    result.add(matrix[i][rightCol]);
+                for (int row = topRow; row <= bottomRow; row++) {
+                    result.add(matrix[row][rightCol]);
                 }
                 rightCol--;
             }
             if (topRow <= bottomRow) {
-                for (int i = rightCol; i >= leftCol; i--) {
-                    result.add(matrix[bottomRow][i]);
+                for (int col = rightCol; col >= leftCol; col--) {
+                    result.add(matrix[bottomRow][col]);
                 }
                 bottomRow--;
             }
             if (leftCol <= rightCol) {
-                for (int i = bottomRow; i >= topRow; i--) {
-                    result.add(matrix[i][leftCol]);
+                for (int row = bottomRow; row >= topRow; row--) {
+                    result.add(matrix[row][leftCol]);
                 }
                 leftCol++;
             }
