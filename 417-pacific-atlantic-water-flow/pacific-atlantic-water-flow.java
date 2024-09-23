af03fa07b4e4935ccfa89ac1ce548dc70563c0fc
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-23 01:12:07 -0700

    Time: 4 ms (92.66%) | Memory: 45.7 MB (49.21%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.java
@@ -2,10 +2,9 @@ class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> result = new ArrayList<>();
         int m = heights.length, n = heights[0].length;
-        
+
         boolean[][] pacific = new boolean[m][n];
         boolean[][] atlantic = new boolean[m][n];
-
         for (int row = 0; row < m; row++) {
             dfs(heights, pacific, row, 0, Integer.MIN_VALUE);
             dfs(heights, atlantic, row, n - 1, Integer.MIN_VALUE);
@@ -14,11 +13,11 @@ class Solution {
             dfs(heights, pacific, 0, col, Integer.MIN_VALUE);
             dfs(heights, atlantic, m - 1, col, Integer.MIN_VALUE);
         }
-
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (pacific[row][col] && atlantic[row][col]) {
-                    result.add(Arrays.asList(row, col));
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    result.add(Arrays.asList(i, j));
                 }
             }
         }
@@ -26,13 +25,14 @@ class Solution {
     }
 
     int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    private void dfs(int[][] heights, boolean[][] ocean, int row, int col, int previousValue) {
+    private void dfs(int[][] heights, boolean[][] ocean, int row, int col, int prev) {
         if (row < 0 || row >= heights.length || col < 0 || col >= heights[0].length || ocean[row][col]) {
             return;
         }
-        if (heights[row][col] < previousValue) {
+        if (heights[row][col] < prev) {
             return;
         }
+
         ocean[row][col] = true;
         for (int[] dir : directions) {
             dfs(heights, ocean, row + dir[0], col + dir[1], heights[row][col]);
