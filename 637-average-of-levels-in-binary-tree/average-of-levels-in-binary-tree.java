f9adecd2c52415e57c45ac589b3f0dfeb8a72bdd
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-22 20:46:46 -0700

    Time: 2 ms (95.01%) | Memory: 45.4 MB (88.73%) - LeetSync

--- 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
+++ 637-average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.java
@@ -16,27 +16,19 @@
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
         List<Double> result = new ArrayList<>();
-        
-        List<List<Integer>> allLevel = new ArrayList<>();
-        dfs(root, allLevel, 0);
-        for (List<Integer> eachLevel : allLevel) {
-            double currentLevelSum = 0;
-            for (int i = 0; i < eachLevel.size(); i++) {
-                currentLevelSum += eachLevel.get(i);
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double levelSum = 0.0;
+            for (int i = 0; i < size; i++) {
+                TreeNode current = queue.poll();
+                levelSum += current.val;
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
             }
-            result.add(currentLevelSum / eachLevel.size());
+            result.add(levelSum / size);
         }
         return result;
-    }
-    private void dfs(TreeNode root, List<List<Integer>> result, int level) {
-        if (root == null) {
-            return;
-        }
-        if (result.size() == level) {
-            result.add(new ArrayList<>());
-        }
-        result.get(level).add(root.val);
-        dfs(root.left, result, level + 1);
-        dfs(root.right, result, level + 1);
     }
 }
