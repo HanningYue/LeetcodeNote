7a5da302ab859fdccff065c66c7ed77fa900e9c2
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-23 20:45:38 -0700

    Time: 2 ms (63.29%) | Memory: 44.2 MB (54.30%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -22,28 +22,17 @@ class Solution {
         }
         return recursion(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
     }
-    private TreeNode recursion(int[] inorder, int inleft, int inright, 
-    int[] postorder, int postleft, int postright) {
-        if (inleft > inright) {
+    private TreeNode recursion(int[] inorder, int inleft, int inright, int[] postorder, int postleft, int postright) {
+        if (postleft > postright) {
             return null;
         }
-
         int headValue = postorder[postright];
         int headIndex = map.get(headValue);
-        int leftSubtreeSize = headIndex - inleft;
+        int leftSubSize = headIndex - inleft - 1;
 
-        TreeNode root = new TreeNode(headValue);
-        root.left = recursion(inorder, inleft, headIndex - 1, 
-        postorder, postleft, postleft + leftSubtreeSize - 1);
-
-        root.right = recursion(inorder, headIndex + 1, inright, 
-        postorder, postleft + leftSubtreeSize, postright - 1);
-        return root;
-
+        TreeNode head = new TreeNode(headValue);
+        head.left = recursion(inorder, inleft, headIndex - 1, postorder, postleft, postleft + leftSubSize);
+        head.right = recursion(inorder, headIndex + 1, inright, postorder, postleft + leftSubSize + 1, postright - 1);
+        return head;
     }
 }
-/**
-9 3 15 20 7
-9 15 7 20 3
-0 1  2  3 4
-*/
