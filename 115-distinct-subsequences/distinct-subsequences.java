a069c235529e2bf22540736a678eda9c954b1d50
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-24 15:50:42 -0700

    Time: 2 ms (99.65%) | Memory: 51.4 MB (6.74%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,27 +1,28 @@
-class Solution {
+class Solution {     
     Integer[][] dpTable;
     public int numDistinct(String s, String t) {
-        dpTable = new Integer[s.length()][t.length()];
+        int m = s.length(), n = t.length();
+        dpTable = new Integer[m][n];
         return dp(s, 0, t, 0);
     }
     private int dp(String s, int i, String t, int j) {
         if (j == t.length()) {
             return 1;
-        }
+        } 
         if (t.length() - j > s.length() - i) {
             return 0;
         }
+        
         if (dpTable[i][j] != null) {
             return dpTable[i][j];
         }
-        
         if (s.charAt(i) == t.charAt(j)) {
-            int useCurrent = dp(s, i + 1, t, j + 1);
-            int notUseCurrent = dp(s, i + 1, t, j);
-            dpTable[i][j] = useCurrent + notUseCurrent;
-        } else {
-            int skipCurrent = dp(s, i + 1, t, j);
-            dpTable[i][j] = skipCurrent;
+            int use = dp(s, i + 1, t, j + 1);
+            int notUse = dp(s, i + 1, t, j);
+            dpTable[i][j] = use + notUse;
+        } else if (s.charAt(i) != t.charAt(j)) {
+            int skip = dp(s, i + 1, t, j);
+            dpTable[i][j] = skip;
         }
         return dpTable[i][j];
     }
