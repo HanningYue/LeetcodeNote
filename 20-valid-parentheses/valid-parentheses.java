3562a168eb8b0f84d5088d69389a30952ab9d04e
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-24 13:17:44 -0700

    Time: 1 ms (98.58%) | Memory: 42.2 MB (7.26%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -2,20 +2,29 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '{') {
-                stack.push('}');
-            } else if (c == '[') {
-                stack.push(']');
-            } else if (c == '(') {
-                stack.push(')');
+            if (c == '[' || c == '{' || c == '(') {
+                stack.push(c);
             } else {
-                if (stack.isEmpty() || stack.peek() != c) {
+                if (stack.isEmpty()) {
                     return false;
-                } else {
+                }
+                
+                if (stack.peek() == match(c)) {
                     stack.pop();
+                } else if (stack.peek() != match(c)) {
+                    return false;
                 }
             }
         }
         return stack.isEmpty();
+    }
+
+    private char match(char c) {
+        if (c == ']') {
+            return '[';
+        } else if (c == '}') {
+            return '{';
+        }
+        return '(';
     }
 }
