abba615faaa846d51655eb9abc437f368fc32455
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-24 14:47:26 -0700

    Time: 167 ms (7.07%) | Memory: 63.4 MB (41.65%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        PriorityQueue<Integer> maxProfit = new PriorityQueue<>((a, b) -> profits[b] - profits[a]);
         PriorityQueue<Integer> minCapital = new PriorityQueue<>((a, b) -> capital[a] - capital[b]);
+        PriorityQueue<Integer> maxProfit = new PriorityQueue<>((a, b) -> profits[b] - profits[a]);
+
         for (int i = 0; i < capital.length; i++) {
-            minCapital.add(i);
+            minCapital.offer(i);
         }
 
-        int result = 0;
         for (int i = 0; i < k; i++) {
             while (!minCapital.isEmpty() && capital[minCapital.peek()] <= w) {
                 maxProfit.offer(minCapital.poll());
