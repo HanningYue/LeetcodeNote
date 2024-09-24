05cae6ee65cdaced98cb5ca102e51d4cee170f31
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-24 14:53:15 -0700

    Time: 7 ms (98.19%) | Memory: 46.6 MB (42.30%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -2,18 +2,24 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         List<int[]> result = new ArrayList<>();
-
-        int[] previousInterval = intervals[0];
+        
+        int[] previous = intervals[0];
         for (int i = 1; i < intervals.length; i++) {
-            int[] currentInterval = intervals[i];
-            if (currentInterval[0] > previousInterval[1]) {
-                result.add(previousInterval);
-                previousInterval = currentInterval;
-            } else {
-                previousInterval[1] = Math.max(previousInterval[1], currentInterval[1]);
+            if (intervals[i][0] <= previous[1]) {
+                previous[0] = Math.min(previous[0], intervals[i][0]);
+                previous[1] = Math.max(previous[1], intervals[i][1]);
+            } else if (intervals[i][0] > previous[1]) {
+                result.add(previous);
+                previous = intervals[i];
             }
         }
-        result.add(previousInterval);
-        return result.toArray(new int[result.size()][]);
+        result.add(previous);
+        
+        int[][] answer = new int[result.size()][2];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i][0] = result.get(i)[0];
+            answer[i][1] = result.get(i)[1];
+        }
+        return answer;
     }
 }
