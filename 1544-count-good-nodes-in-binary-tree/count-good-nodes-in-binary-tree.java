e1b2aede17073499c5e1efc5624cba02aef55315
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-26 13:26:58 -0700

    Time: 11 ms (7.76%) | Memory: 54.3 MB (8.50%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -13,21 +13,42 @@
  *     }
  * }
  */
-class Solution {
-    int count = 0;
-    public int goodNodes(TreeNode root) {
-        dfs(root, Integer.MIN_VALUE);
-        return count;
+class State {
+    TreeNode root;
+    int currentMax;
+    public State(TreeNode root, int currentMax) {
+        this.root = root;
+        this.currentMax = currentMax;
     }
-    private void dfs(TreeNode root, int prevMax) {
-        if (root == null) {
-            return;
+}
+class Solution {
+    public int goodNodes(TreeNode root) {
+        int count = 1;
+        Queue<State> queue = new ArrayDeque<>();
+        queue.offer(new State(root, root.val));
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                State current = queue.poll();
+                if (current.root.left != null) {
+                    if (current.root.left.val >= current.currentMax) {
+                        queue.offer(new State(current.root.left, current.root.left.val));
+                        count++;
+                    } else {
+                        queue.offer(new State(current.root.left, current.currentMax));
+                    }
+                }
+
+                if (current.root.right != null) {
+                    if (current.root.right.val >= current.currentMax) {
+                        queue.offer(new State(current.root.right, current.root.right.val));
+                        count++;
+                    } else {
+                        queue.offer(new State(current.root.right, current.currentMax));
+                    }
+                }
+            }
         }
-        if (prevMax <= root.val) {
-            prevMax = root.val;
-            count++;
-        }
-        dfs(root.left, prevMax);
-        dfs(root.right, prevMax);
+        return count;
     }
 }
