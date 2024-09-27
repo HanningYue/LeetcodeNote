04bbcb88c07a2ab303bab497168459dd1c353924
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-27 12:18:30 -0700

    Time: 4 ms (91.61%) | Memory: 46 MB (39.66%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,50 +1,43 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<Integer> list = new ArrayList<>();
+        int[] result = new int[numCourses];
+        List<Integer> order = new ArrayList<>();
         boolean[] visiting = new boolean[numCourses];
         boolean[] visited = new boolean[numCourses];
-        
         Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.put(i, new ArrayList<>());
+        }
         for (int[] pre : prerequisites) {
-            int from = pre[1];
-            int to = pre[0];
-            graph.putIfAbsent(to, new ArrayList<>());
+            int from = pre[1], to = pre[0];
             graph.get(to).add(from);
         }
         for (int i = 0; i < numCourses; i++) {
-            if (!dfs(i, graph, visiting, visited, list)) {
+            if (!dfs(graph, visiting, visited, i, order)) {
                 return new int[]{};
             }
         }
-
-        int[] result = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            result[i] = list.get(i);
+        for (int i = 0; i < order.size(); i++) {
+            result[i] = order.get(i);
         }
         return result;
     }
-    private boolean dfs(int vertex, Map<Integer, List<Integer>> graph, boolean[] visiting,
-    boolean[] visited, List<Integer> list) {
-        if (visited[vertex]) {
-            return true;
-        }
-        if (!graph.containsKey(vertex)) {
-            list.add(vertex);
-            visited[vertex] = true;
-            return true;
-        }
+    private boolean dfs(Map<Integer, List<Integer>> graph, boolean[] visiting, boolean[] visited, int vertex, List<Integer> order) {
         if (visiting[vertex]) {
             return false;
         }
+        if (visited[vertex]) {
+            return true;
+        }
         visiting[vertex] = true;
         for (int neighbor : graph.get(vertex)) {
-            if (!dfs(neighbor, graph, visiting, visited, list)) {
+            if (!dfs(graph, visiting, visited, neighbor, order)) {
                 return false;
             }
         }
-        list.add(vertex);
-        visiting[vertex] = false;
+        order.add(vertex);
         visited[vertex] = true;
+        visiting[vertex] = false;
         return true;
     }
 }
