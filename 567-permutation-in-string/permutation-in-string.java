7c52996ff057dcd38c26058ab076e4103db72dbe
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-27 12:32:58 -0700

    Time: 23 ms (41.49%) | Memory: 42.7 MB (61.72%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -5,8 +5,7 @@ class Solution {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        int match = 0;
-        int slow = 0, fast = 0;
+        int slow = 0, fast = 0, match = 0;
         while (fast < s2.length()) {
             char fastChar = s2.charAt(fast);
             if (map.containsKey(fastChar)) {
@@ -15,10 +14,11 @@ class Solution {
                     match++;
                 }
             }
+
             while (match == s1.length()) {
                 if (fast - slow + 1 == s1.length()) {
                     return true;
-                }
+                } 
                 char slowChar = s2.charAt(slow);
                 if (map.containsKey(slowChar)) {
                     map.put(slowChar, map.get(slowChar) + 1);
