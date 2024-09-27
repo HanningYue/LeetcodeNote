eaf850bf3f68217e92869231730c2c2f4ceaaae4
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-27 13:16:29 -0700

    Time: 100 ms (15.37%) | Memory: 59.3 MB (26.72%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,34 +1,32 @@
-class Pair {
-    int position;
-    int speed;
-    public Pair(int position, int speed) {
+class State {
+    int position, speed;
+    public State(int position, int speed) {
         this.position = position;
         this.speed = speed;
     }
 }
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        List<Pair> list = new ArrayList<Pair>();
-        int n = position.length;
-        for (int i = 0; i < n; i++) {
-            list.add(new Pair(position[i], speed[i]));
+        List<State> list = new ArrayList<>();
+        for (int i = 0; i < position.length; i++) {
+            int currentPos = position[i];
+            int currentSpeed = speed[i];
+            list.add(new State(currentPos, currentSpeed));
         }
+        Collections.sort(list, (a, b) -> a.position - b.position);
 
-        Collections.sort(list, new Comparator<Pair>() {
-            public int compare(Pair a, Pair b) {
-                return a.position - b.position;
-            }
-        });
-
-        double[] time = new double[n];
-        for (int i = 0; i < n; i++) {
-            Pair furthest = list.get(i);
-            time[i] = (double) (target - furthest.position) / furthest.speed;
+        List<Double> time = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            int pos = target - list.get(i).position;
+            int currentSpeed = list.get(i).speed;
+            
+            double currentTime = (double)pos / currentSpeed;
+            time.add(currentTime);
         }
 
         Stack<Double> stack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            double currentTime = time[i];
+        for (int i = 0; i < time.size(); i++) {
+            double currentTime = time.get(i);
             while (!stack.isEmpty() && currentTime >= stack.peek()) {
                 stack.pop();
             }
