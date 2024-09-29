d4523e98a8f9f734efaa46ec079ae80c282058c5
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-29 16:21:01 -0700

    Time: 101 ms (14.95%) | Memory: 59.3 MB (27.27%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,27 +1,25 @@
-class State {
-    int position, speed;
-    public State(int position, int speed) {
+class Car {
+    int position;
+    int speed;
+    public Car(int position, int speed) {
         this.position = position;
         this.speed = speed;
     }
 }
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        List<State> list = new ArrayList<>();
+        List<Car> list = new ArrayList<>();
         for (int i = 0; i < position.length; i++) {
-            int currentPos = position[i];
-            int currentSpeed = speed[i];
-            list.add(new State(currentPos, currentSpeed));
+            list.add(new Car(position[i], speed[i]));
         }
         Collections.sort(list, (a, b) -> a.position - b.position);
 
         List<Double> time = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            int pos = target - list.get(i).position;
+        for (int i = 0; i < position.length; i++) {
+            int distanceToTarget = target - list.get(i).position;
             int currentSpeed = list.get(i).speed;
-            
-            double currentTime = (double)pos / currentSpeed;
-            time.add(currentTime);
+            double timeToReach = (double)distanceToTarget / currentSpeed;
+            time.add(timeToReach);
         }
 
         Stack<Double> stack = new Stack<>();
