6e26e3cb36ed3336c46f91755e2303bac3cba1ab
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-09-29 16:24:01 -0700

    Time: 18 ms (92.07%) | Memory: 56.8 MB (71.99%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,35 +1,19 @@
-class Car {
-    int position;
-    int speed;
-    public Car(int position, int speed) {
-        this.position = position;
-        this.speed = speed;
-    }
-}
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        List<Car> list = new ArrayList<>();
-        for (int i = 0; i < position.length; i++) {
-            list.add(new Car(position[i], speed[i]));
-        }
-        Collections.sort(list, (a, b) -> a.position - b.position);
+        double[] times = new double[target];
 
-        List<Double> time = new ArrayList<>();
         for (int i = 0; i < position.length; i++) {
-            int distanceToTarget = target - list.get(i).position;
-            int currentSpeed = list.get(i).speed;
-            double timeToReach = (double)distanceToTarget / currentSpeed;
-            time.add(timeToReach);
+            times[position[i]] = (double)(target - position[i]) / speed[i];
         }
 
-        Stack<Double> stack = new Stack<>();
-        for (int i = 0; i < time.size(); i++) {
-            double currentTime = time.get(i);
-            while (!stack.isEmpty() && currentTime >= stack.peek()) {
-                stack.pop();
+        int result = 0;
+        double currentTime = 0;
+        for (int i = target - 1; i >= 0; i--) {
+            if (times[i] > currentTime) {
+                result++;
+                currentTime = times[i];
             }
-            stack.push(currentTime);
         }
-        return stack.size();
+        return result;
     }
 }
