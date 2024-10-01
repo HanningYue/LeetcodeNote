3cfc9b323b340941d938b82b9b70e23f92eb9524
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-01 15:31:17 -0700

    Time: 0 ms (100.00%) | Memory: 42 MB (68.35%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,8 +1,5 @@
 class Solution {
     public double myPow(double x, int n) {
-        return iterative(x, n);        
-    }
-    private double iterative(double x, long n) {
         if (n < 0) {
             x = 1 / x;
             n = -n;
@@ -11,12 +8,12 @@ class Solution {
         } else if (n == 1) {
             return x;
         }
-
+        
         double result = 1.0;
         while (n != 0) {
-            if (n % 2 == 1) {
+            if (n % 2 != 0) {
                 result = result * x;
-            }    
+            }
             x = x * x;
             n = n / 2;
         }
