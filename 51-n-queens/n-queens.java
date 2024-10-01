d94d9725a5384c86f8aeb358e844054a444fbfd6
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-01 14:13:27 -0700

    Time: 5 ms (38.21%) | Memory: 44.7 MB (78.12%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,36 +1,41 @@
 class Solution {
-    List<String> board;
+    int n;
     List<List<String>> result;
+    List<String> board;
     public List<List<String>> solveNQueens(int n) {
-        board = new ArrayList<>();
+        this.n = n;
         result = new ArrayList<>();
-        for (int row = 0; row < n; row++) {
-            StringBuilder sb = new StringBuilder();
-            for (int col = 0; col < n; col++) {
-                sb.append(".");
+        board = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            StringBuilder row = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                row.append(".");
             }
-            board.add(sb.toString());
+            board.add(row.toString());
         }
         backTrack(0);
         return result;
     }
+
     private void backTrack(int row) {
-        if (row == board.size()) {
+        if (row == n) {
             result.add(new ArrayList<>(board));
             return;
         }
-        StringBuilder sb = new StringBuilder(board.get(row));
-        for (int col = 0; col < board.size(); col++) {
-            if (valid(row, col)) {
-                sb.setCharAt(col, 'Q');
-                board.set(row, sb.toString());
+
+        StringBuilder currentRow = new StringBuilder(board.get(row));
+        for (int col = 0; col < n; col++) {
+            if (isValid(row, col)) {
+                currentRow.setCharAt(col, 'Q');
+                board.set(row, currentRow.toString());
                 backTrack(row + 1);
-                sb.setCharAt(col, '.');
-                board.set(row, sb.toString());
+                currentRow.setCharAt(col, '.');
+                board.set(row, currentRow.toString());
             }
         }
     }
-    private boolean valid(int row, int col) {
+
+    private boolean isValid(int row, int col) {
         for (int i = 0; i < row; i++) {
             if (board.get(i).charAt(col) == 'Q') {
                 return false;
@@ -41,7 +46,7 @@ class Solution {
                 return false;
             }
         }
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.size(); i--, j++) {
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
             if (board.get(i).charAt(j) == 'Q') {
                 return false;
             }
