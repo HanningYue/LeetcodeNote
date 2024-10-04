cd99559b96267d7b240c7251734152b2384ebc41
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-04 16:50:33 -0700

    Time: 8 ms (64.65%) | Memory: 49.5 MB (22.54%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,18 +1,24 @@
 class NumArray {
-    int[] prefix;
-    public NumArray(int[] nums) {
-        prefix = new int[nums.length + 1];
+    // 前缀和数组
+    private int[] preSum;
 
-        prefix[0] = 0;
-        for (int i = 1; i <= nums.length; i++) {
-            prefix[i] = prefix[i - 1] + nums[i - 1];
+    // 输入一个数组，构造前缀和
+    public NumArray(int[] nums) {
+        // preSum[0] = 0，便于计算累加和
+        preSum = new int[nums.length + 1];
+        // 计算 nums 的累加和
+        for (int i = 1; i < preSum.length; i++) {
+            preSum[i] = preSum[i - 1] + nums[i - 1];
         }
     }
-    
+
+    // 查询闭区间 [left, right] 的累加和
     public int sumRange(int left, int right) {
-        return prefix[right + 1] - prefix[left];
+        return preSum[right + 1] - preSum[left];
     }
 }
+// 详细解析参见：
+// https://labuladong.online/algo/data-structure/prefix-sum/
 
 /**
  * Your NumArray object will be instantiated and called as such:
