d7baa5ddd88768cf20b91e1f48f9f9773f9a8fbe
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-04 08:29:30 -0700

    Time: 28 ms (64.91%) | Memory: 68.4 MB (5.17%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -3,17 +3,17 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
-        }   
+        }
 
         int maxLength = 0;
         for (int num : set) {
             if (!set.contains(num - 1)) {
-                int length = 1;
+                int localLength = 1;
                 while (set.contains(num + 1)) {
-                    length++;
+                    localLength++;
                     num = num + 1;
                 }
-                maxLength = Math.max(maxLength, length);
+                maxLength = Math.max(maxLength, localLength);
             }
         }
         return maxLength;
