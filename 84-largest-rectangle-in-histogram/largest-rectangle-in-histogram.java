5b4b4301ad98851fb60f2d2a7aad0bbe19ae8636
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-04 09:21:03 -0700

    Time: 48 ms (82.94%) | Memory: 56 MB (97.48%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,33 +1,33 @@
+class State {
+    int index;
+    int height;
+    public State(int index, int height) {
+        this.index = index;
+        this.height = height;
+    }
+}
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Pair> stack = new Stack<>();
         int maxArea = 0;
-
+        Stack<State> stack = new Stack<>();
         for (int i = 0; i < heights.length; i++) {
             int previousIndex = i, currentHeight = heights[i];
 
             while (!stack.isEmpty() && stack.peek().height > currentHeight) {
-                Pair lastRec = stack.pop();
-                int area = (i - lastRec.index) * lastRec.height;
-                maxArea = Math.max(maxArea, area);
-
-                previousIndex = lastRec.index;
+                State previousRec = stack.pop();
+                int localArea = (i - previousRec.index) * previousRec.height;
+                maxArea = Math.max(maxArea, localArea);
+                previousIndex = previousRec.index;
             }
-            stack.push(new Pair(previousIndex, currentHeight));
+
+            stack.push(new State(previousIndex, currentHeight));
         }
 
         while (!stack.isEmpty()) {
-            Pair lastRec = stack.pop();
-            int area = (heights.length - lastRec.index) * lastRec.height;
-            maxArea = Math.max(maxArea, area);
+            State lastRec = stack.pop();
+            int localArea = (heights.length - lastRec.index) * lastRec.height;
+            maxArea = Math.max(maxArea, localArea);
         }
         return maxArea;
-    }
-}
-class Pair {
-    int index, height;
-    public Pair(int index, int height) {
-        this.index = index;
-        this.height = height;
     }
 }
