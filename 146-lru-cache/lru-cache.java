6bafcc6b9e91450d209558dcfb094e697b6911b8
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-04 09:53:15 -0700

    Time: 49 ms (45.98%) | Memory: 114.8 MB (47.01%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,65 +1,31 @@
-class Node {
-    Node prev, next;
-    int key, value;
-    public Node(int key, int value) {
-        this.key = key;
-        this.value = value;
-    }
-}
 class LRUCache {
-    Node head, tail;
+    LinkedHashMap<Integer, Integer> map;
     int capacity;
-    Map<Integer, Node> map;
     public LRUCache(int capacity) {
-        map = new HashMap<>();
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head.next = tail;
-        tail.prev = head;
         this.capacity = capacity;
+        map = new LinkedHashMap<>();
     }
     
-    private void add(Node node) {
-        node.prev = head;
-        node.next = head.next;
-
-        head.next.prev = node;
-        head.next = node;
-    }
-
-    private void delete(Node node) {
-        node.next.prev = node.prev;
-        node.prev.next = node.next;
-    }
-
     public int get(int key) {
-        if (map.containsKey(key)) {
-            Node node = map.get(key);
-            int value = node.value;
-            delete(node);
-            add(node);
-            return value;
+        if (!map.containsKey(key)) {
+            return -1;
         }
-        return -1;
+        int value = map.get(key);
+        map.remove(key);
+        map.put(key, value);
+        return value;
     }
     
     public void put(int key, int value) {
         if (map.containsKey(key)) {
-            Node oldNode = map.get(key);
-            delete(oldNode);
-
-            Node newNode = new Node(key, value);
-            map.put(key, newNode);
-            add(newNode);
+            map.remove(key);
+            map.put(key, value);
         } else {
-            if (map.size() >= capacity) {
-                int lru = tail.prev.key;
-                delete(tail.prev);
+            if (capacity == map.size()) {
+                int lru = map.keySet().iterator().next();
                 map.remove(lru);
             }
-            Node newNode = new Node(key, value);
-            map.put(key, newNode);
-            add(newNode);
+            map.put(key, value);
         }
     }
 }
