0e57dc4b99cabb717544380bae4ccd664cbe68f3
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-04 10:38:25 -0700

    Time: 4 ms (43.55%) | Memory: 44.6 MB (7.85%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -9,7 +9,7 @@ class Solution {
             map.put(nums2[i], stack.isEmpty() ? -1 : stack.peek());
             stack.push(nums2[i]);
         }
-
+        
         int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
             result[i] = map.get(nums1[i]);
