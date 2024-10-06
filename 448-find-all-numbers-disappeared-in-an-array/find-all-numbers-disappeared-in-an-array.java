94310b050c3aff59dce62c2831bf487e87679c70
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-06 14:46:28 -0700

    Time: 6 ms (57.83%) | Memory: 53.8 MB (64.32%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -4,14 +4,15 @@ class Solution {
         int i = 0;
         while (i < nums.length) {
             int suppose = nums[i] - 1;
-            if (nums[suppose] != suppose + 1) {
-                int temp = nums[suppose];
-                nums[suppose] = nums[i];
-                nums[i] = temp;
+            if (nums[i] >= 1 && nums[i] <= nums.length && nums[i] != nums[suppose]) {
+                int temp = nums[i];
+                nums[i] = nums[suppose];
+                nums[suppose] = temp;
             } else {
                 i++;
             }
         }
+
         for (int j = 0; j < nums.length; j++) {
             if (nums[j] != j + 1) {
                 result.add(j + 1);
