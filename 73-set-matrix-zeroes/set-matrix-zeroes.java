d6315e998a15b2b435eb716e3ba9aed7da903478
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-05 19:56:10 -0700

    Time: 0 ms (100.00%) | Memory: 45.8 MB (9.49%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,18 +1,19 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        boolean firstCol = false, firstRow = false;
+
         int m = matrix.length, n = matrix[0].length;
-        boolean firstRow = false, firstCol = false;
-        for (int col = 0; col < n; col++) {
-            if (matrix[0][col] == 0) {
-                firstRow = true;
-            }
-        }
         for (int row = 0; row < m; row++) {
             if (matrix[row][0] == 0) {
                 firstCol = true;
             }
         }
-        
+        for (int col = 0; col < n; col++) {
+            if (matrix[0][col] == 0) {
+                firstRow = true;
+            }
+        }
+
         for (int row = 1; row < m; row++) {
             for (int col = 1; col < n; col++) {
                 if (matrix[row][col] == 0) {
@@ -30,14 +31,14 @@ class Solution {
             }
         }
 
-        if (firstCol) {
-            for (int row = 0; row < m; row++) {
-                matrix[row][0] = 0;
-            }
-        }
         if (firstRow) {
             for (int col = 0; col < n; col++) {
                 matrix[0][col] = 0;
+            }
+        }
+        if (firstCol) {
+            for (int row = 0; row < m; row++) {
+                matrix[row][0] = 0;
             }
         }
     }
