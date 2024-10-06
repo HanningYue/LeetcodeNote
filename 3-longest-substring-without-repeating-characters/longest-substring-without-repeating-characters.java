7fd0a614b9e3b52df198f5d3a672d1b12c4f1f7a
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-06 12:43:20 -0700

    Time: 6 ms (69.79%) | Memory: 44.5 MB (57.86%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -5,8 +5,10 @@ class Solution {
         int slow = 0, fast = 0;
         while (fast < s.length()) {
             char fastChar = s.charAt(fast);
-            if (map.containsKey(fastChar) && map.get(fastChar) >= slow) {
-                slow = map.get(fastChar) + 1;
+            if (map.containsKey(fastChar)) {
+                if (map.get(fastChar) >= slow) {
+                    slow = map.get(fastChar) + 1;
+                }
             }
             maxLength = Math.max(maxLength, fast - slow + 1);
             map.put(fastChar, fast);
