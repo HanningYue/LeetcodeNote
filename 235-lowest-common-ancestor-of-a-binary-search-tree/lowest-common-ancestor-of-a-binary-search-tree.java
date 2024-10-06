5d851cd433cae1e6c5e1803dc2c0650e9b212c64
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-05 21:33:45 -0700

    Time: 6 ms (32.89%) | Memory: 45.1 MB (37.62%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,35 +10,21 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
-        parentMap.put(root, null);
-
-        Queue<TreeNode> queue = new ArrayDeque<>();
-        queue.offer(root);
-        while (!parentMap.containsKey(p) || !parentMap.containsKey(q)) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode current = queue.poll();
-                if (current.left != null) {
-                    parentMap.put(current.left, current);
-                    queue.offer(current.left);
-                }
-                if (current.right != null) {
-                    parentMap.put(current.right, current);
-                    queue.offer(current.right);
-                }
-            }
+        if (root == null) {
+            return null;
         }
 
-        Set<TreeNode> set = new HashSet<>();
-        while (p != null) {
-            set.add(p);
-            p = parentMap.get(p);
+        if (p.val > q.val) {
+            return lowestCommonAncestor(root, q, p);
         }
 
-        while (!set.contains(q)) {
-            q = parentMap.get(q);
+        if (root.val >= p.val && root.val <= q.val) {
+            return root;
         }
-        return q;
+
+        TreeNode leftSub = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightSub = lowestCommonAncestor(root.right, p, q);
+        
+        return leftSub == null ? rightSub : leftSub;
     }
 }
