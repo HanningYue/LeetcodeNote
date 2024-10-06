e78907055cd8d41b8667c14faa272de2eec1654f
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-06 14:13:29 -0700

    Time: 4 ms (49.97%) | Memory: 49.3 MB (72.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,45 +1,27 @@
-class State {
-    int row;
-    int col;
-    public State(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
-}
 class Solution {
+    int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
     public int numIslands(char[][] grid) {
+        int count = 0;
         int m = grid.length, n = grid[0].length;
-        int[][] directions = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
         boolean[][] visited = new boolean[m][n];
-        int result = 0;
-
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
                 if (!visited[row][col] && grid[row][col] == '1') {
-                    Queue<State> queue = new ArrayDeque<>();
-                    queue.offer(new State(row, col));
-
-                    while (!queue.isEmpty()) {
-                        State current = queue.poll();
-                        int currentRow = current.row, currentCol = current.col;
-
-                        for (int[] dir : directions) {
-                            int newRow = currentRow + dir[0];
-                            int newCol = currentCol + dir[1];
-                            if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n) {
-                                continue;
-                            }
-                            if (visited[newRow][newCol] || grid[newRow][newCol] != '1') {
-                                continue;
-                            }
-                            queue.offer(new State(newRow, newCol));
-                            visited[newRow][newCol] = true;
-                        }
-                    }
-                    result++;
+                    count++;
+                    dfs(grid, row, col, visited);
                 }
             }
         }
-        return result;
+        return count;
+    }
+    private void dfs(char[][] grid, int row, int col, boolean[][] visited) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || visited[row][col] 
+        || grid[row][col] == '0') {
+            return;
+        }
+        visited[row][col] = true;
+        for (int[] dir : directions) {
+            dfs(grid, row + dir[0], col + dir[1], visited);
+        }
     }
 }
