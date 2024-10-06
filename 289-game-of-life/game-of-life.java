8f2dd205d394c7c4f7c7da9eceae347e927a8205
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-05 20:50:58 -0700

    Time: 0 ms (100.00%) | Memory: 41.5 MB (74.66%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,30 +1,34 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-        int m = board.length, n = board[0].length;
         int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        int m = board.length, n = board[0].length;
+        
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
-                int countLive = 0;
+                int count = 0;
                 for (int[] dir : directions) {
-                    int newRow = dir[0] + row, newCol = dir[1] + col;
-                    if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n 
-                    || board[newRow][newCol] == 0 || board[newRow][newCol] == 3) {
+                    int newRow = dir[0] + row;
+                    int newCol = dir[1] + col;
+                    if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || board[newRow][newCol] == 0 || board[newRow][newCol] == 3) {
                         continue;
                     }
-                    countLive++;
+                    count++;
                 }
 
-                if (board[row][col] == 1) {
-                    if (countLive < 2 || countLive > 3) {
-                        board[row][col] = 2;
-                    }
-                } else if (board[row][col] == 0) {
-                    if (countLive == 3) {
+                if (board[row][col] == 0) {
+                    if (count == 3) {
                         board[row][col] = 3;
+                    }
+                }
+                
+                if (board[row][col] == 1) {
+                    if (count != 2 && count != 3) {
+                        board[row][col] = 2;
                     }
                 }
             }
         }
+
         for (int row = 0; row < m; row++) {
             for (int col = 0; col < n; col++) {
                 board[row][col] = board[row][col] % 2;
