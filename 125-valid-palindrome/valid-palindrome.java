8c0c97ead4b890663340d1902329bb72c5c8a563
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-07 12:27:27 -0700

    Time: 3 ms (72.86%) | Memory: 42.6 MB (95.28%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -3,14 +3,17 @@ class Solution {
         int left = 0, right = s.length() - 1;
         while (left < right) {
             char leftChar = s.charAt(left);
-            char rightChar = s.charAt(right);
-
+            char rightChar = s.charAt(right);        
             if (!Character.isLetterOrDigit(leftChar)) {
                 left++;
-            } else if (!Character.isLetterOrDigit(rightChar)) {
+            }
+            if (!Character.isLetterOrDigit(rightChar)) {
                 right--;
-            } else {
-                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+            }
+            if (Character.isLetterOrDigit(leftChar) && Character.isLetterOrDigit(rightChar)) {
+                char upperLeft = Character.toUpperCase(leftChar);
+                char upperRight = Character.toUpperCase(rightChar);
+                if (upperLeft != upperRight) {
                     return false;
                 }
                 left++;
