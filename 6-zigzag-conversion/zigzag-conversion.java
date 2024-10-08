d8a2ffd8d9317fe83716d587ae5b9dac2de97c4a
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-08 13:59:31 -0700

    Time: 8 ms (35.17%) | Memory: 45.4 MB (37.29%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,5 +1,3 @@
-//down: 0 to numRows - 1
-//up: numRows - 2 to 1
 class Solution {
     public String convert(String s, int numRows) {
         if (numRows == 1) {
@@ -10,22 +8,22 @@ class Solution {
             sb[i] = new StringBuilder();
         }
 
-        boolean movingDown = true;
         int currentRow = 0;
+        boolean movingDown = true;
         for (char c : s.toCharArray()) {
             sb[currentRow].append(c);
             if (currentRow == 0) {
                 movingDown = true;
-            }
-            if (currentRow == numRows - 1) {
+            } else if (currentRow == numRows - 1) {
                 movingDown = false;
             }
+            
             currentRow += movingDown ? 1 : -1;
         }
 
         String result = "";
-        for (StringBuilder S : sb) {
-            result += S;
+        for (StringBuilder str : sb) {
+            result += str.toString();
         }
         return result;
     }
