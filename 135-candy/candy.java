441fdf2a1dd04f897580016b216ecfb91732bec0
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-08 12:56:59 -0700

    Time: 2 ms (99.81%) | Memory: 45.6 MB (46.22%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -9,16 +9,20 @@ class Solution {
                 result[i] = result[i - 1] + 1;
             }
         }
+
         for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1] && result[i] <= result[i + 1]) {
+            if (ratings[i] > ratings[i + 1]) {
+                if (result[i] > result[i + 1]) {
+                    continue;
+                }
                 result[i] = result[i + 1] + 1;
             }
         }
 
-        int candy = 0;
-        for (int i = 0; i < n; i++) {
-            candy += result[i];
+        int sum = 0;
+        for (int num : result) {
+            sum += num;
         }
-        return candy;
+        return sum;
     }
 }
