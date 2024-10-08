00aacc3293bfad2f131a58d9be78d7dc2b4fb3de
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-08 16:25:10 -0700

    Time: 2752 ms (5.06%) | Memory: 58.1 MB (46.57%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,9 +1,9 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        k = nums.length - k;
+        k = nums.length - k;        
         int left = 0, right = nums.length - 1;
         while (left <= right) {
-            int pivotIndex = quickSelect(nums, left, right);
+            int pivotIndex = partition(nums, left, right);
             if (pivotIndex == k) {
                 return nums[pivotIndex];
             } else if (pivotIndex > k) {
@@ -14,22 +14,21 @@ class Solution {
         }
         return -1;
     }
-    private int quickSelect(int[] nums, int left, int right) {
+    private int partition(int[] nums, int left, int right) {
         if (left == right) {
             return left;
         }
 
         int pivotIndex = left;
-        int pivotValue = nums[left];
         int leftP = left, rightP = right;
         while (leftP <= rightP) {
-            while (leftP <= rightP && nums[leftP] <= pivotValue) {
+            while (leftP <= rightP && nums[leftP] <= nums[pivotIndex]) {
                 leftP++;
             }
-            while (leftP <= rightP && nums[rightP] > pivotValue) {
+            while (leftP <= rightP && nums[rightP] >= nums[pivotIndex]) {
                 rightP--;
             }
-            if (leftP < rightP) {
+            if (leftP <= rightP) {
                 swap(nums, leftP, rightP);
                 leftP++;
                 rightP--;
