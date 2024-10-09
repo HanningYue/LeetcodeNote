2bd416b4ebf4df500d050a7a485c2ae9958e1fc1
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-08 20:48:55 -0700

    Time: 2758 ms (5.06%) | Memory: 57.1 MB (84.00%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,20 +1,19 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        k = nums.length - k;        
+        k = nums.length - k;
         int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int pivotIndex = partition(nums, left, right);
-            if (pivotIndex == k) {
-                return nums[pivotIndex];
-            } else if (pivotIndex > k) {
-                right = pivotIndex - 1;
-            } else if (pivotIndex < k) {
-                left = pivotIndex + 1;
+        while (true) {
+            int pivot = quickSelect(nums, left, right);
+            if (pivot == k) {
+                return nums[pivot];
+            } else if (pivot > k) {
+                right = pivot - 1;
+            } else if (pivot < k) {
+                left = pivot + 1;
             }
-        }
-        return -1;
+        }       
     }
-    private int partition(int[] nums, int left, int right) {
+    private int quickSelect(int[] nums, int left, int right) {
         if (left == right) {
             return left;
         }
