a99ed8818718685d6209c2d73944abdc6195ff6a
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-16 11:37:54 -0700

    Time: 4 ms (54.31%) | Memory: 53.4 MB (9.49%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,23 +1,25 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
         return divide(nums, 0, nums.length - 1);
     }
     private int divide(int[] nums, int left, int right) {
-        // if (left > right) {
-        //     return nums[0];
-        // }
-        if (left == right) {
+        if (left >= right) {
             return nums[left];
         }
         int mid = left + (right - left) / 2;
-        int leftNumber = divide(nums, left, mid);
-        int rightNumber = divide(nums, mid + 1, right);
-        
-        int leftCount = count(nums, leftNumber, left, right);
-        int rightCount = count(nums, rightNumber, left, right);
-        return leftCount > rightCount ? leftNumber: rightNumber;
+        int leftSub = divide(nums, left, mid);
+        int rightSub = divide(nums, mid + 1, right);
+        int leftFrequency = conquer(nums, left, right, leftSub);
+        int rightFrequency = conquer(nums, left, right, rightSub);
+        if (leftFrequency > rightFrequency) {
+            return leftSub;
+        }
+        return rightSub;
     }
-    private int count(int[] nums, int target, int left, int right) {
+    private int conquer(int[] nums, int left, int right, int target) {
         int count = 0;
         for (int i = left; i <= right; i++) {
             if (nums[i] == target) {
