1d6725b253864014270c710e43a7fe62fae4fb23
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-16 12:05:41 -0700

    Time: 8 ms (18.17%) | Memory: 45.1 MB (7.11%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,31 +1,34 @@
 class Solution {
     public String intToRoman(int num) {
         Map<Integer, String> map = new HashMap<>();
-        map.put(1000, "M");
-        map.put(900, "CM");
-        map.put(500, "D");
-        map.put(400, "CD");
-        map.put(100, "C");
-        map.put(90, "XC");
-        map.put(50, "L");
-        map.put(40, "XL");
-        map.put(10, "X");
-        map.put(9, "IX");
-        map.put(5, "V");
-        map.put(4, "IV");
         map.put(1, "I");
-
-        int[] highToLow = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        map.put(4, "IV");
+        map.put(5, "V");
+        map.put(9, "IX");
+        map.put(10, "X");
+        map.put(40, "XL");
+        map.put(50, "L");
+        map.put(90, "XC");
+        map.put(100, "C");
+        map.put(400, "CD");
+        map.put(500, "D");
+        map.put(900, "CM");
+        map.put(1000, "M");
         
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < highToLow.length; i++) {
-            if (num / highToLow[i] != 0) {
-                int frequency = num / highToLow[i];
-                String cor = map.get(highToLow[i]);
-                sb.append(cor.repeat(frequency));
-                num = num % highToLow[i];
+        int[] array = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String result = "";
+        for (int i = 0; i < array.length; i++) {
+            if (num == 0) {
+                return result;
             }
+            int currentNum = array[i];
+            int currentFreq = num / currentNum;
+            String currentString = map.get(currentNum);
+            for (int j = 0; j < currentFreq; j++) {
+                result += currentString;
+            }
+            num = num % currentNum;
         }
-        return sb.toString();
+        return result;
     }
 }
