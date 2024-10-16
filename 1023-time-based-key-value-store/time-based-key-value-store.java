d93a3ad8802ca5ce8817460a787bada0087fe2f1
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-16 11:00:47 -0700

    Time: 133 ms (87.11%) | Memory: 118.3 MB (80.05%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -1,7 +1,7 @@
 class State {
     String value;
     int timestamp;
-    public State (String value, int timestamp) {
+    public State(String value, int timestamp) {
         this.value = value;
         this.timestamp = timestamp;
     }
@@ -9,12 +9,12 @@ class State {
 class TimeMap {
     Map<String, List<State>> map;
     public TimeMap() {
-        map = new HashMap<>();
+        map = new HashMap<>();        
     }
     
     public void set(String key, String value, int timestamp) {
-        State state = new State(value, timestamp);
         map.putIfAbsent(key, new ArrayList<>());
+        State state = new State(value, timestamp);
         map.get(key).add(state);
     }
     
@@ -25,22 +25,19 @@ class TimeMap {
         List<State> list = map.get(key);
         return binarySearch(list, timestamp);
     }
-
     private String binarySearch(List<State> list, int timestamp) {
         int left = 0, right = list.size() - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            State midState = list.get(mid);
-
-            if (midState.timestamp == timestamp) {
-                return midState.value;
-            } else if (midState.timestamp < timestamp) {
+            State state = list.get(mid);
+            if (state.timestamp == timestamp) {
+                return state.value;
+            } else if (state.timestamp < timestamp) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-
         if (right >= 0 && right < list.size()) {
             return list.get(right).value;
         }
