b8e77a4d7805fcc298ae2cd75eb5173f2d136747
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-19 21:56:01 -0700

    Time: 0 ms (100.00%) | Memory: 42.1 MB (44.82%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,23 +1,16 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int indexOne = m - 1, indexTwo = n - 1;
         int indexThree = m + n - 1;
-        
+        int indexOne = m - 1, indexTwo = n - 1;
         while (indexOne >= 0 && indexTwo >= 0) {
             if (nums2[indexTwo] >= nums1[indexOne]) {
-                nums1[indexThree] = nums2[indexTwo];
-                indexTwo--;
+                nums1[indexThree--] = nums2[indexTwo--];
             } else {
-                nums1[indexThree] = nums1[indexOne];
-                indexOne--;
+                nums1[indexThree--] = nums1[indexOne--];
             }
-            indexThree--;
         }
-
         while (indexTwo >= 0) {
-            nums1[indexThree] = nums2[indexTwo];
-            indexTwo--;
-            indexThree--;
+            nums1[indexThree--] = nums2[indexTwo--];
         }
     }
 }
