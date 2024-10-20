fb57479cf2d6b2a2aee56ea48f8e163a1d6c45a5
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-19 20:41:57 -0700

    Time: 4 ms (80.72%) | Memory: 121.5 MB (24.89%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
         boolean[] mark = new boolean[target.length];
-
         for (int[] triplet : triplets) {
             boolean greater = false;
             for (int i = 0; i < triplet.length; i++) {
@@ -14,13 +13,12 @@ class Solution {
                 continue;
             }
 
-            for (int i = 0; i < triplet.length; i++) {
-                if (triplet[i] == target[i]) {
-                    mark[i] = true;
+            for (int j = 0; j < triplet.length; j++) {
+                if (triplet[j] == target[j]) {
+                    mark[j] = true;
                 }
             }
         }
-
         for (boolean val : mark) {
             if (!val) {
                 return false;
