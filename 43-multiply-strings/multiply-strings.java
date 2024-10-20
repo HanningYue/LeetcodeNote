4062ccfcd4b057d15fabaa6bf14e7bdf6abd3d8a
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-20 14:33:11 -0700

    Time: 3 ms (64.43%) | Memory: 42.1 MB (70.56%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,25 +1,28 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        int[] result = new int[num1.length() + num2.length()];
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            for (int j = num2.length() - 1; j >= 0; j--) {
-                int numOne = num1.charAt(i) - '0';
-                int numTwo = num2.charAt(j) - '0';
+        int m = num1.length(), n = num2.length();
+        
+        int[] result = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int numberOne = num1.charAt(i) - '0';
+                int numberTwo = num2.charAt(j) - '0';
 
-                int product = numOne * numTwo;
+                int product = numberOne * numberTwo;
                 int sum = product + result[i + j + 1];
                 result[i + j + 1] = sum % 10;
                 result[i + j] = result[i + j] + sum / 10;
             }
         }
+
         int k = 0;
         while (k < result.length && result[k] == 0) {
             k++;
         }
-        
         StringBuilder sb = new StringBuilder();
         while (k < result.length) {
-            sb.append(result[k++]);
+            sb.append(result[k]);
+            k++;
         }
         return sb.length() == 0 ? "0" : sb.toString();
     }
