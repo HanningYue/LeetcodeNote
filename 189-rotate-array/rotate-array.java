6d4ee3a59ff77118f58b1f18e9f256fe6fd44dba
Jamie Yue <102259888+HanningYue@users.noreply.github.com>
2024-10-19 21:48:08 -0700

    Time: 0 ms (100.00%) | Memory: 57.2 MB (62.29%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,14 +1,12 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k = k % n;
-        
-        reverse(nums, 0, n - 1);
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
+        reverse(nums, k, nums.length - 1);
     }
     private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
+        while (left <= right) {
             int temp = nums[left];
             nums[left] = nums[right];
             nums[right] = temp;
@@ -17,3 +15,5 @@ class Solution {
         }
     }
 }
+// 7654321
+// 5671234
